a618b10cdf226c67a3d4eb22eb380fea8dfa756f
Vitaly Kuznetsov <vkuznets@redhat.com>
2021-01-26 14:48:09 +0100

    KVM: x86: hyper-v: Introduce to_kvm_hv() helper
    mainline inclusion
    from mainline-v5.12-rc1
    commit 05f04ae4ffcc77cfcda86adc4e2c13aa72143c03
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBTVRA
    
    ----------------------------------------
    
    commit 05f04ae4ffcc77cfcda86adc4e2c13aa72143c03 upstream
    
    Spelling '&kvm->arch.hyperv' correctly is hard. Also, this makes the code
    more consistent with vmx/svm where to_kvm_vmx()/to_kvm_svm() are already
    being used.
    
    Opportunistically change kvm_hv_msr_{get,set}_crash_{data,ctl}() and
    kvm_hv_msr_set_crash_data() to take 'kvm' instead of 'vcpu' as these
    MSRs are partition wide.
    
    No functional change intended.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Message-Id: <20210126134816.1880136-9-vkuznets@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -53,6 +53,7 @@
 #include "capabilities.h"
 #include "cpuid.h"
 #include "evmcs.h"
+#include "hyperv.h"
 #include "irq.h"
 #include "kvm_cache_regs.h"
 #include "lapic.h"
@@ -618,7 +619,7 @@ static int hv_enable_direct_tlbflush(struct kvm_vcpu *vcpu)
 {
 	struct hv_enlightened_vmcs *evmcs;
 	struct hv_partition_assist_pg **p_hv_pa_pg =
-			&vcpu->kvm->arch.hyperv.hv_pa_pg;
+			&to_kvm_hv(vcpu->kvm)->hv_pa_pg;
 	/*
 	 * Synthetic VM-Exit is not enabled in current code and so All
 	 * evmcs in singe VM shares same assist page.
