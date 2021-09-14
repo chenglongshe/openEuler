5ecc82f67ce94bf32207eb2f8a5938685fc3a95b
Maxim Levitsky <mlevitsk@redhat.com>
2021-09-14 18:48:19 +0300

    KVM: x86: SVM: add module param to control LBR virtualization
    mainline inclusion
    from mainline-v5.16-rc1
    commit 4c84926e229e0efdafa2756d7e6c4ae2fb0b7945
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICSPRX
    
    ----------------------------------------
    
    commit 4c84926e229e0efdafa2756d7e6c4ae2fb0b7945 upstream
    
    This is useful for debug and also makes it consistent with
    the rest of the SVM optional features.
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>
    Message-Id: <20210914154825.104886-9-mlevitsk@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Rahul Kumar <Kumar.Rahul2@amd.com>
    Signed-off-by: mohanasv2 <mohanasv@amd.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -190,6 +190,10 @@ module_param(vgif, int, 0444);
 /* enable/disable SEV support */
 static int sev = IS_ENABLED(CONFIG_AMD_MEM_ENCRYPT_ACTIVE_BY_DEFAULT);
 module_param(sev, int, 0444);
+/* enable/disable LBR virtualization */
+static int lbrv = true;
+module_param(lbrv, int, 0444);
+
 
 static bool __read_mostly dump_invalid_vmcb = 0;
 module_param(dump_invalid_vmcb, bool, 0644);
@@ -1039,6 +1043,13 @@ static __init int svm_hardware_setup(void)
 			vgif = false;
 		else
 			pr_info("Virtual GIF supported\n");
+	}
+
+	if (lbrv) {
+		if (!boot_cpu_has(X86_FEATURE_LBRV))
+			lbrv = false;
+		else
+			pr_info("LBR virtualization supported\n");
 	}
 
 	svm_set_cpu_caps();
@@ -2771,7 +2782,7 @@ static int svm_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr)
 		wrmsrl(MSR_TSC_AUX, svm->tsc_aux);
 		break;
 	case MSR_IA32_DEBUGCTLMSR:
-		if (!boot_cpu_has(X86_FEATURE_LBRV)) {
+		if (!lbrv) {
 			vcpu_unimpl(vcpu, "%s: MSR_IA32_DEBUGCTL 0x%llx, nop\n",
 				    __func__, data);
 			break;
