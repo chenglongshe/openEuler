314abb7d9db3c81431037adcf089fa277f50f972
Steven Price <steven.price@arm.com>
2021-11-17 11:23:26 +0000

    arm64: RME: ioctls to create and configure realms
    community inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBY08N
    
    Reference: https://lore.kernel.org/kvm/yq5amsgsrzva.fsf@kernel.org/T/#meb4cb12031992f1275fe7f5b40ac38c1e883ac87
    
    --------------------------------
    
    Add the KVM_CAP_ARM_RME_CREATE_RD ioctl to create a realm. This involves
    delegating pages to the RMM to hold the Realm Descriptor (RD) and for
    the base level of the Realm Translation Tables (RTT). A VMID also need
    to be picked, since the RMM has a separate VMID address space a
    dedicated allocator is added for this purpose.
    
    KVM_CAP_ARM_RME_CONFIG_REALM is provided to allow configuring the realm
    before it is created. Configuration options can be classified as:
    
     1. Parameters specific to the Realm stage2 (e.g. IPA Size, vmid, stage2
        entry level, entry level RTTs, number of RTTs in start level, LPA2)
        Most of these are not measured by RMM and comes from KVM book
        keeping.
    
     2. Parameters controlling "Arm Architecture features for the VM". (e.g.
        SVE VL, PMU counters, number of HW BRPs/WPs), configured by the VMM
        using the "user ID register write" mechanism. These will be
        supported in the later patches.
    
     3. Parameters are not part of the core Arm architecture but defined
        by the RMM spec (e.g. Hash algorithm for measurement,
        Personalisation value). These are programmed via
        KVM_CAP_ARM_RME_CONFIG_REALM.
    
    For the IPA size there is the possibility that the RMM supports a
    different size to the IPA size supported by KVM for normal guests. At
    the moment the 'normal limit' is exposed by KVM_CAP_ARM_VM_IPA_SIZE and
    the IPA size is configured by the bottom bits of vm_type in
    KVM_CREATE_VM. This means that it isn't easy for the VMM to discover
    what IPA sizes are supported for Realm guests. Since the IPA is part of
    the measurement of the realm guest the current expectation is that the
    VMM will be required to pick the IPA size demanded by attestation and
    therefore simply failing if this isn't available is fine. An option
    would be to expose a new capability ioctl to obtain the RMM's maximum
    IPA size if this is needed in the future.
    
    Co-developed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Steven Price <steven.price@arm.com>
    
    Conflicts:
    	arch/arm64/kvm/rme.c
    	arch/arm64/kvm/mmu.c
    [zhuangyiwei: use kvm->arch.vtcr to access vtcr instead of kvm->arch.mmu.vtcr which is not changed in OLK-6.6]
    [zhuangyiwei: do not extract part of kvm_init_stage2_mmu to a new function kvm_init_ipa_range and directly add codes in kvm_init_stage2_mmu]
    Signed-off-by: Yiwei Zhuang <zhuangyiwei@huawei.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -245,6 +245,13 @@ int kvm_vm_ioctl_enable_cap(struct kvm *kvm,
 		}
 		mutex_unlock(&kvm->slots_lock);
 		break;
+	case KVM_CAP_ARM_RME:
+		if (!kvm_is_realm(kvm))
+			return -EINVAL;
+		mutex_lock(&kvm->lock);
+		r = kvm_realm_enable_cap(kvm, cap);
+		mutex_unlock(&kvm->lock);
+		break;
 #ifdef CONFIG_ARM64_HDBSS
 	case KVM_CAP_ARM_HW_DIRTY_STATE_TRACK:
 		r = kvm_cap_arm_enable_hdbss(kvm, cap);
@@ -317,6 +324,13 @@ int kvm_arch_init_vm(struct kvm *kvm, unsigned long type)
 
 	bitmap_zero(kvm->arch.vcpu_features, KVM_VCPU_MAX_FEATURES);
 
+	/* Initialise the realm bits after the generic bits are enabled */
+	if (kvm_is_realm(kvm)) {
+		ret = kvm_init_realm_vm(kvm);
+		if (ret)
+			goto err_free_cpumask;
+	}
+
 	return 0;
 
 err_free_cpumask:
@@ -353,6 +367,7 @@ void kvm_arch_destroy_vm(struct kvm *kvm)
 	kvm_unshare_hyp(kvm, kvm + 1);
 
 	kvm_arm_teardown_hypercalls(kvm);
+	kvm_destroy_realm(kvm);
 }
 
 extern struct static_key_false ipiv_enable;
@@ -461,6 +476,9 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 		break;
 	case KVM_CAP_ARM_SUPPORTED_BLOCK_SIZES:
 		r = kvm_supported_block_sizes();
+		break;
+	case KVM_CAP_ARM_RME:
+		r = static_key_enabled(&kvm_rme_is_available);
 		break;
 #ifdef CONFIG_VIRT_PLAT_DEV
 	case KVM_CAP_ARM_VIRT_MSI_BYPASS:
