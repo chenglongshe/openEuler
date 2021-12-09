7527521807171fad6c23abe29c2f19815d78b689
Maxim Levitsky <mlevitsk@redhat.com>
2021-12-09 13:54:36 +0200

    KVM: x86: add a tracepoint for APICv/AVIC interrupt delivery
    mainline inclusion
    from mainline-v5.17-rc1
    commit 8e819d75cbcf541a833219521379114a76a645a6
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBTVRA
    
    ----------------------------------------
    
    commit 8e819d75cbcf541a833219521379114a76a645a6 upstream
    
    This allows to see how many interrupts were delivered via the
    APICv/AVIC from the host.
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>
    Message-Id: <20211209115440.394441-3-mlevitsk@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: chaithanyaLagisetty <LagisettyVenkata.NagaChaitanya@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -12318,6 +12318,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_avic_unaccelerated_access);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_avic_incomplete_ipi);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_avic_ga_log);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_apicv_update_request);
+EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_apicv_accept_irq);
 
 static int __init kvm_x86_init(void)
 {
