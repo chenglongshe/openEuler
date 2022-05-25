4bff3390eac97a94d510812a895523872e5e7f36
Steven Price <steven.price@arm.com>
2022-05-25 15:46:50 +0100

    arm64: RME: Allocate/free RECs to match vCPUs
    community inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBY08N
    
    Reference: https://lore.kernel.org/kvm/yq5amsgsrzva.fsf@kernel.org/T/#meb4cb12031992f1275fe7f5b40ac38c1e883ac87
    
    --------------------------------
    
    The RMM maintains a data structure known as the Realm Execution Context
    (or REC). It is similar to struct kvm_vcpu and tracks the state of the
    virtual CPUs. KVM must delegate memory and request the structures are
    created when vCPUs are created, and suitably tear down on destruction.
    
    RECs must also be supplied with addition pages - auxiliary (or AUX)
    granules - for storing the larger registers state (e.g. for SVE). The
    number of AUX granules for a REC depends on the parameters with which
    the Realm was created - the RMM makes this information available via the
    RMI_REC_AUX_COUNT call performed after creating the Realm Descriptor (RD).
    
    Note that only some of register state for the REC can be set by KVM, the
    rest is defined by the RMM (zeroed). The register state then cannot be
    changed by KVM after the REC is created (except when the guest
    explicitly requests this e.g. by performing a PSCI call). The RMM also
    requires that the VMM creates RECs in ascending order of the MPIDR.
    
    See Realm Management Monitor specification (DEN0137) for more information:
    https://developer.arm.com/documentation/den0137/
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Yiwei Zhuang <zhuangyiwei@huawei.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -583,6 +583,8 @@ int kvm_arch_vcpu_create(struct kvm_vcpu *vcpu)
 	vcpu_clear_flag(vcpu, VCPU_INITIALIZED);
 	bitmap_zero(vcpu->arch.features, KVM_VCPU_MAX_FEATURES);
 
+	vcpu->arch.rec.mpidr = INVALID_HWID;
+
 	vcpu->arch.mmu_page_cache.gfp_zero = __GFP_ZERO;
 
 	/* Set up the timer */
@@ -1463,6 +1465,9 @@ static unsigned long system_supported_vcpu_features(void)
 	if (!cpus_have_final_cap(ARM64_HAS_32BIT_EL1))
 		clear_bit(KVM_ARM_VCPU_EL1_32BIT, &features);
 
+	if (!static_branch_unlikely(&kvm_rme_is_available))
+		clear_bit(KVM_ARM_VCPU_REC, &features);
+
 	return features;
 }
 
@@ -1492,6 +1497,10 @@ static int kvm_vcpu_init_check_features(struct kvm_vcpu *vcpu,
 
 	/* NV is incompatible with AArch32 */
 	if (test_bit(KVM_ARM_VCPU_HAS_EL2, &features))
+		return -EINVAL;
+
+	/* RME is incompatible with AArch32 */
+	if (test_bit(KVM_ARM_VCPU_REC, &features))
 		return -EINVAL;
 
 	return 0;
