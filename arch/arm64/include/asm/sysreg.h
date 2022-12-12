c90d821eff77dfdde54f7329bcab3ae2308ef69f
Mark Brown <broonie@kernel.org>
2022-12-12 13:43:36 +0000

    arm64/entry: Don't call preempt_schedule_irq() with NMIs masked
    kunpeng inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I90N2C
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/broonie/misc.git/commit/?h=arm64-nmi&id=6465ab3af5ccc2122dc468ed4c7e9c66864c9f6f
    
    ----------------------------------------------------------------------
    
    As we do for pseudo NMIs don't call preempt_schedule_irq() when
    architechted NMIs are masked. If they are masked then we are calling from
    a preempting context so skip preemption.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Jie Liu <liujie375@h-partners.com>
    Signed-off-by: Shi Yang <shiyang50@huawei.com>

--- arch/arm64/include/asm/sysreg.h
+++ arch/arm64/include/asm/sysreg.h
@@ -340,6 +340,8 @@
 #define SYS_SPSR_EL1			sys_reg(3, 0, 4, 0, 0)
 #define SYS_ELR_EL1			sys_reg(3, 0, 4, 0, 1)
 
+#define SYS_ALLINT			sys_reg(3, 0, 4, 3, 0)
+
 #define SYS_ICC_PMR_EL1			sys_reg(3, 0, 4, 6, 0)
 
 #define SYS_AFSR0_EL1			sys_reg(3, 0, 5, 1, 0)
@@ -364,6 +366,7 @@
 #define SYS_PAR_EL1_FST			GENMASK(6, 1)
 
 #define HCRX_EL2_TALLINT_MASK		GENMASK(6, 6)
+#define ALLINT_ALLINT			BIT(13)
 
 /*** Statistical Profiling Extension ***/
 
