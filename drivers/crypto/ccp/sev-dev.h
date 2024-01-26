baed169e57328a935b7e64d7bc1e9911f3b285e3
Brijesh Singh <brijesh.singh@amd.com>
2024-01-25 22:11:13 -0600

    crypto: ccp: Add support to initialize the AMD-SP for SEV-SNP
    Before SNP VMs can be launched, the platform must be appropriately
    configured and initialized via the SNP_INIT command.
    
    During the execution of SNP_INIT command, the firmware configures
    and enables SNP security policy enforcement in many system components.
    Some system components write to regions of memory reserved by early
    x86 firmware (e.g. UEFI). Other system components write to regions
    provided by the operation system, hypervisor, or x86 firmware.
    Such system components can only write to HV-fixed pages or Default
    pages. They will error when attempting to write to pages in other page
    states after SNP_INIT enables their SNP enforcement.
    
    Starting in SNP firmware v1.52, the SNP_INIT_EX command takes a list of
    system physical address ranges to convert into the HV-fixed page states
    during the RMP initialization. If INIT_RMP is 1, hypervisors should
    provide all system physical address ranges that the hypervisor will
    never assign to a guest until the next RMP re-initialization.
    
    For instance, the memory that UEFI reserves should be included in the
    range list. This allows system components that occasionally write to
    memory (e.g. logging to UEFI reserved regions) to not fail due to
    RMP initialization and SNP enablement.
    
    Note that SNP_INIT(_EX) must not be executed while non-SEV guests are
    executing, otherwise it is possible that the system could reset or hang.
    The psp_init_on_probe module parameter was added for SEV/SEV-ES support
    and the init_ex_path module parameter to allow for time for the
    necessary file system to be mounted/available.
    
    SNP_INIT(_EX) does not use the file associated with init_ex_path. So, to
    avoid running into issues where SNP_INIT(_EX) is called while there are
    other running guests, issue it during module probe regardless of the
    psp_init_on_probe setting, but maintain the previous deferrable handling
    for SEV/SEV-ES initialization.
    
      [ mdr: Squash in psp_init_on_probe changes from Tom, reduce
        proliferation of 'probe' function parameter where possible.
        bp: Fix 32-bit allmodconfig build. ]
    
    [Backport Changes]
    1. In drivers/crypto/ccp/sev-dev.c, in function sev_pci_init() retained a
    variable named error as it is passed as an argument to the function
    csv_platform_cmd_set_secure_memory_region(). This function was introduced
    as part of the Hygon inclusion changes in commit a725122 ("crypto:
    ccp: Add SET_SMR/SET_SMCR commands for CSV3"). Retaining this ensures
    continued support for Hygon family CPUs.
    2. Used KABI_EXTEND macro in drivers/crypto/ccp/sev-dev.h to add field to
    struct sev_device
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Co-developed-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Co-developed-by: Jarkko Sakkinen <jarkko@profian.com>
    Signed-off-by: Jarkko Sakkinen <jarkko@profian.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/20240126041126.1927228-14-michael.roth@amd.com

--- drivers/crypto/ccp/sev-dev.h
+++ drivers/crypto/ccp/sev-dev.h
@@ -57,6 +57,7 @@ struct sev_device {
 
 	/* Management of the Hygon RING BUFFER mode */
 	struct csv_ringbuffer_queue ring_buffer[CSV_COMMAND_PRIORITY_NUM];
+	KABI_EXTEND(bool snp_initialized)
 };
 
 int sev_dev_init(struct psp_device *psp);
