806f322871520e3e665bed9c990b63812ff8a34e
Paolo Bonzini <pbonzini@redhat.com>
2024-02-09 13:37:33 -0500

    KVM: SEV: fix compat ABI for KVM_MEMORY_ENCRYPT_OP
    mainline inclusion
    from mainline-v6.9-rc3
    commit f3c80061c0d35c60709088ccb019305796d3f6ff
    category: Feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICQGHL
    CVE: NA
    
    Reference: https://github.com/torvalds/linux/commit/f3c80061c0d35c60709088ccb019305796d3f6ff
    
    --------------------------------
    
    commit f3c80061c0d35c60709088ccb019305796d3f6ff upstream
    
    The data structs for KVM_MEMORY_ENCRYPT_OP have different sizes for 32- and 64-bit
    userspace, but they do not make any attempt to convert from one ABI to the other
    when 32-bit userspace is running on 64-bit kernels.  This configuration never
    worked, and SEV is only for 64-bit kernels so we're not breaking ABI on 32-bit
    kernels.
    
    Fix this by adding the appropriate padding; no functional change intended
    for 64-bit userspace.
    
    Reviewed-by: Michael Roth <michael.roth@amd.com>
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: PKumarAditya <PappalaKumar.Aditya@amd.com>
    Signed-off-by: PrithivishS <sprithiv@amd.com>

--- arch/x86/include/uapi/asm/kvm.h
+++ arch/x86/include/uapi/asm/kvm.h
@@ -699,6 +699,7 @@ enum sev_cmd_id {
 #ifndef __GENKSYMS__
 struct kvm_sev_cmd {
 	__u32 id;
+	__u32 pad0;
 	__u64 data;
 	__u32 error;
 	__u32 sev_fd;
@@ -711,8 +712,10 @@ struct kvm_sev_launch_start {
 	__u32 policy;
 	__u64 dh_uaddr;
 	__u32 dh_len;
+	__u32 pad0;
 	__u64 session_uaddr;
 	__u32 session_len;
+	__u32 pad1;
 };
 #endif
 
@@ -720,6 +723,7 @@ struct kvm_sev_launch_start {
 struct kvm_sev_launch_update_data {
 	__u64 uaddr;
 	__u32 len;
+	__u32 pad0;
 };
 #endif
 
@@ -728,10 +732,13 @@ struct kvm_sev_launch_update_data {
 struct kvm_sev_launch_secret {
 	__u64 hdr_uaddr;
 	__u32 hdr_len;
+	__u32 pad0;
 	__u64 guest_uaddr;
 	__u32 guest_len;
+	__u32 pad1;
 	__u64 trans_uaddr;
 	__u32 trans_len;
+	__u32 pad2;
 };
 #endif
 
@@ -739,6 +746,7 @@ struct kvm_sev_launch_secret {
 struct kvm_sev_launch_measure {
 	__u64 uaddr;
 	__u32 len;
+	__u32 pad0;
 };
 #endif
 
@@ -755,6 +763,7 @@ struct kvm_sev_dbg {
 	__u64 src_uaddr;
 	__u64 dst_uaddr;
 	__u32 len;
+	__u32 pad0;
 };
 #endif
 
@@ -763,20 +772,26 @@ struct kvm_sev_attestation_report {
 	__u8 mnonce[16];
 	__u64 uaddr;
 	__u32 len;
+	__u32 pad0;
 };
 #endif
 
 #ifndef __GENKSYMS__
 struct kvm_sev_send_start {
 	__u32 policy;
+	__u32 pad0;
 	__u64 pdh_cert_uaddr;
 	__u32 pdh_cert_len;
+	__u32 pad1;
 	__u64 plat_certs_uaddr;
 	__u32 plat_certs_len;
+	__u32 pad2;
 	__u64 amd_certs_uaddr;
 	__u32 amd_certs_len;
+	__u32 pad3;
 	__u64 session_uaddr;
 	__u32 session_len;
+	__u32 pad4;
 };
 #endif
 
@@ -784,10 +799,13 @@ struct kvm_sev_send_start {
 struct kvm_sev_send_update_data {
 	__u64 hdr_uaddr;
 	__u32 hdr_len;
+	__u32 pad0;
 	__u64 guest_uaddr;
 	__u32 guest_len;
+	__u32 pad1;
 	__u64 trans_uaddr;
 	__u32 trans_len;
+	__u32 pad2;
 };
 #endif
 
@@ -797,8 +815,10 @@ struct kvm_sev_receive_start {
 	__u32 policy;
 	__u64 pdh_uaddr;
 	__u32 pdh_len;
+	__u32 pad0;
 	__u64 session_uaddr;
 	__u32 session_len;
+	__u32 pad1;
 };
 #endif
 
@@ -806,10 +826,13 @@ struct kvm_sev_receive_start {
 struct kvm_sev_receive_update_data {
 	__u64 hdr_uaddr;
 	__u32 hdr_len;
+	__u32 pad0;
 	__u64 guest_uaddr;
 	__u32 guest_len;
+	__u32 pad1;
 	__u64 trans_uaddr;
 	__u32 trans_len;
+	__u32 pad2;
 };
 #endif
 
