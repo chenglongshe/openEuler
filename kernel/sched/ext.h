bbf0f2ec14a9b4d1f104ee9ce1870d8614719068
Tejun Heo <tj@kernel.org>
2024-06-18 10:09:18 -1000

    sched_ext: Allow BPF schedulers to disallow specific tasks from joining SCHED_EXT
    BPF schedulers might not want to schedule certain tasks - e.g. kernel
    threads. This patch adds p->scx.disallow which can be set by BPF schedulers
    in such cases. The field can be changed anytime and setting it in
    ops.prep_enable() guarantees that the task can never be scheduled by
    sched_ext.
    
    scx_qmap is updated with the -d option to disallow a specific PID:
    
      # echo $$
      1092
      # grep -E '(policy)|(ext\.enabled)' /proc/self/sched
      policy                                       :                    0
      ext.enabled                                  :                    0
      # ./set-scx 1092
      # grep -E '(policy)|(ext\.enabled)' /proc/self/sched
      policy                                       :                    7
      ext.enabled                                  :                    0
    
    Run "scx_qmap -p -d 1092" in another terminal.
    
      # cat /sys/kernel/sched_ext/nr_rejected
      1
      # grep -E '(policy)|(ext\.enabled)' /proc/self/sched
      policy                                       :                    0
      ext.enabled                                  :                    0
      # ./set-scx 1092
      setparam failed for 1092 (Permission denied)
    
    - v4: Refreshed on top of tip:sched/core.
    
    - v3: Update description to reflect /sys/kernel/sched_ext interface change.
    
    - v2: Use atomic_long_t instead of atomic64_t for scx_kick_cpus_pnt_seqs to
          accommodate 32bit archs.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Suggested-by: Barret Rhoden <brho@google.com>
    Reviewed-by: David Vernet <dvernet@meta.com>
    Acked-by: Josh Don <joshdon@google.com>
    Acked-by: Hao Luo <haoluo@google.com>
    Acked-by: Barret Rhoden <brho@google.com>
    Change-Id: I239bef37bae7c5b050d3a171846d86144f8916b7

--- kernel/sched/ext.h
+++ kernel/sched/ext.h
@@ -34,6 +34,7 @@ void scx_pre_fork(struct task_struct *p);
 int scx_fork(struct task_struct *p);
 void scx_post_fork(struct task_struct *p);
 void scx_cancel_fork(struct task_struct *p);
+int scx_check_setscheduler(struct task_struct *p, int policy);
 bool task_should_scx(struct task_struct *p);
 void init_sched_ext_class(void);
 
@@ -71,6 +72,7 @@ static inline void scx_pre_fork(struct task_struct *p) {}
 static inline int scx_fork(struct task_struct *p) { return 0; }
 static inline void scx_post_fork(struct task_struct *p) {}
 static inline void scx_cancel_fork(struct task_struct *p) {}
+static inline int scx_check_setscheduler(struct task_struct *p, int policy) { return 0; }
 static inline bool task_on_scx(const struct task_struct *p) { return false; }
 static inline void init_sched_ext_class(void) {}
 
