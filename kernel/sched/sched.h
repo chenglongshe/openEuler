bd28d3f1bad0fc73b21bec32494c0e14b9585778
Tejun Heo <tj@kernel.org>
2024-06-18 10:09:16 -1000

    sched: Add sched_class->switching_to() and expose check_class_changing/changed()
    When a task switches to a new sched_class, the prev and new classes are
    notified through ->switched_from() and ->switched_to(), respectively, after
    the switching is done.
    
    A new BPF extensible sched_class will have callbacks that allow the BPF
    scheduler to keep track of relevant task states (like priority and cpumask).
    Those callbacks aren't called while a task is on a different sched_class.
    When a task comes back, we wanna tell the BPF progs the up-to-date state
    before the task gets enqueued, so we need a hook which is called before the
    switching is committed.
    
    This patch adds ->switching_to() which is called during sched_class switch
    through check_class_changing() before the task is restored. Also, this patch
    exposes check_class_changing/changed() in kernel/sched/sched.h. They will be
    used by the new BPF extensible sched_class to implement implicit sched_class
    switching which is used e.g. when falling back to CFS when the BPF scheduler
    fails or unloads.
    
    This is a prep patch and doesn't cause any behavior changes. The new
    operation and exposed functions aren't used yet.
    
    v3: Refreshed on top of tip:sched/core.
    
    v2: Improve patch description w/ details on planned use.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: David Vernet <dvernet@meta.com>
    Acked-by: Josh Don <joshdon@google.com>
    Acked-by: Hao Luo <haoluo@google.com>
    Acked-by: Barret Rhoden <brho@google.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2437,6 +2437,7 @@ struct sched_class {
 	 * cannot assume the switched_from/switched_to pair is serialized by
 	 * rq->lock. They are however serialized by p->pi_lock.
 	 */
+	void (*switching_to) (struct rq *this_rq, struct task_struct *task);
 	void (*switched_from)(struct rq *this_rq, struct task_struct *task);
 	void (*switched_to)  (struct rq *this_rq, struct task_struct *task);
 	void (*reweight_task)(struct rq *this_rq, struct task_struct *task,
@@ -3729,6 +3730,8 @@ static inline void init_sched_mm_cid(struct task_struct *t) { }
 
 extern u64 avg_vruntime(struct cfs_rq *cfs_rq);
 extern int entity_eligible(struct cfs_rq *cfs_rq, struct sched_entity *se);
+
+extern void check_class_changing(struct rq *rq, struct task_struct *p, const struct sched_class *prev_class);
 
 #ifdef CONFIG_BPF_SCHED
 bool bpf_sched_is_cpu_allowed(struct task_struct *p, int cpu);
