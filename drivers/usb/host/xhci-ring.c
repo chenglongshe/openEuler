caa432d958a029c5d2344afc94de196a8ae44f89
Niklas Neronin <niklas.neronin@linux.intel.com>
2024-06-26 15:48:31 +0300

    usb: xhci: remove infinite loop prevention
    mainline inclusion
    from mainline-v6.11-rc1
    commit 132dcf65fc2d5a308fc44833dbb7f73a211a02e9
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IDAZAS
    CVE: NA
    
    ----------------------------------------
    
    If a buggy HW reports some unpredicted event (for example, an overrun event
    following a MSE event while the EP ring is actually not empty), the driver
    will never find the TD, and it will loop until the TD list is empty.
    
    Before commits [1][2], the spin lock was released when giving back a URB in
    the do-while loop. This could cause more TD to be added to TD list, causing
    an infinite loop.
    
    Because of commits [1][2] the spin lock is not released any more, thus the
    infinite loop prevention is unnecessary and is removed.
    
    [1], commit 0c03d89d0c71 ("xhci: Giveback urb in finish_td directly")
    [2], commit 36dc01657b49 ("usb: host: xhci: Support running urb giveback in
    			   tasklet context")
    
    Signed-off-by: Niklas Neronin <niklas.neronin@linux.intel.com>
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Link: https://lore.kernel.org/r/20240626124835.1023046-18-mathias.nyman@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -2274,7 +2274,6 @@ static int handle_tx_event(struct xhci_hcd *xhci,
 	struct xhci_ep_ctx *ep_ctx;
 	struct list_head *tmp;
 	u32 trb_comp_code;
-	int td_num = 0;
 	bool handling_skipped_tds = false;
 
 	slot_id = TRB_TO_SLOT_ID(le32_to_cpu(event->flags));
@@ -2319,12 +2318,6 @@ static int handle_tx_event(struct xhci_hcd *xhci,
 				 slot_id, ep_index);
 			goto err_out;
 		}
-	}
-
-	/* Count current td numbers if ep->skip is set */
-	if (ep->skip) {
-		list_for_each(tmp, &ep_ring->td_list)
-			td_num++;
 	}
 
 	/* Look for common error cases */
@@ -2487,18 +2480,8 @@ static int handle_tx_event(struct xhci_hcd *xhci,
 			goto cleanup;
 		}
 
-		/* We've skipped all the TDs on the ep ring when ep->skip set */
-		if (ep->skip && td_num == 0) {
-			ep->skip = false;
-			xhci_dbg(xhci, "All tds on the ep_ring skipped. Clear skip flag for slot %u ep %u.\n",
-				 slot_id, ep_index);
-			goto cleanup;
-		}
-
 		td = list_first_entry(&ep_ring->td_list, struct xhci_td,
 				      td_list);
-		if (ep->skip)
-			td_num--;
 
 		/* Is this a TRB in the currently executing TD? */
 		ep_seg = trb_in_td(xhci, ep_ring->deq_seg, ep_ring->dequeue,
