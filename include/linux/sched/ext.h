b3a3c45133548819cdfa2ea110b64e5537beafdf
Tejun Heo <tj@kernel.org>
2024-07-08 14:30:55 -1000

    sched_ext: Implement DSQ iterator
    DSQs are very opaque in the consumption path. The BPF scheduler has no way
    of knowing which tasks are being considered and which is picked. This patch
    adds BPF DSQ iterator.
    
    - Allows iterating tasks queued on a DSQ in the dispatch order or reverse
      from anywhere using bpf_for_each(scx_dsq) or calling the iterator kfuncs
      directly.
    
    - Has ordering guarantee where only tasks which were already queued when the
      iteration started are visible and consumable during the iteration.
    
    v5: - Add a comment to the naked list_empty(&dsq->list) test in
          consume_dispatch_q() to explain the reasoning behind the lockless test
          and by extension why nldsq_next_task() isn't used there.
    
        - scx_qmap changes separated into its own patch.
    
    v4: - bpf_iter_scx_dsq_new() declaration in common.bpf.h was using the wrong
          type for the last argument (bool rev instead of u64 flags). Fix it.
    
    v3: - Alexei pointed out that the iterator is too big to allocate on stack.
          Added a prep patch to reduce the size of the cursor. Now
          bpf_iter_scx_dsq is 48 bytes and bpf_iter_scx_dsq_kern is 40 bytes on
          64bit.
    
        - u32_before() comparison factored out.
    
    v2: - scx_bpf_consume_task() is separated out into a separate patch.
    
        - DSQ seq and iter flags don't need to be u64. Use u32.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: David Vernet <dvernet@meta.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Cc: bpf@vger.kernel.org

--- include/linux/sched/ext.h
+++ include/linux/sched/ext.h
@@ -59,6 +59,7 @@ struct scx_dispatch_q {
 	struct list_head	list;	/* tasks in dispatch order */
 	struct rb_root		priq;	/* used to order by p->scx.dsq_vtime */
 	u32			nr;
+	u32			seq;	/* used by BPF iter */
 	u64			id;
 	struct rhash_head	hash_node;
 	struct llist_node	free_node;
@@ -118,6 +119,7 @@ enum scx_kf_mask {
 
 struct scx_dsq_list_node {
 	struct list_head	node;
+	bool			is_bpf_iter_cursor;
 };
 
 /*
@@ -128,6 +130,7 @@ struct sched_ext_entity {
 	struct scx_dispatch_q	*dsq;
 	struct scx_dsq_list_node dsq_list;	/* dispatch order */
 	struct rb_node		dsq_priq;	/* p->scx.dsq_vtime order */
+	u32			dsq_seq;
 	u32			dsq_flags;	/* protected by DSQ lock */
 	u32			flags;		/* protected by rq lock */
 	u32			weight;
