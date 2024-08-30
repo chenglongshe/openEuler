9fffffc20e21448cd58c679cdaf9ba69f8f7f67e
Shida Zhang <zhangshida@kylinos.cn>
2024-08-30 13:37:38 +0800

    ext4: fix a potential assertion failure due to improperly dirtied buffer
    mainline inclusion
    from mainline-v6.12-rc1
    commit cb3de5fc876ee9ef2b830c9e6cdafac5c90903ef
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IB731V?from=project-issue
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=cb3de5fc876ee9ef2b830c9e6cdafac5c90903ef
    
    --------------------------------------------------------
    
    On an old kernel version(4.19, ext3, data=journal, pagesize=64k),
    an assertion failure will occasionally be triggered by the line below:
    -----------
    jbd2_journal_commit_transaction
    {
    ...
    J_ASSERT_BH(bh, !buffer_dirty(bh));
    /*
    * The buffer on BJ_Forget list and not jbddirty means
    ...
    }
    -----------
    
    The same condition may also be applied to the lattest kernel version.
    
    When blocksize < pagesize and we truncate a file, there can be buffers in
    the mapping tail page beyond i_size. These buffers will be filed to
    transaction's BJ_Forget list by ext4_journalled_invalidatepage() during
    truncation. When the transaction doing truncate starts committing, we can
    grow the file again. This calls __block_write_begin() which allocates new
    blocks under these buffers in the tail page we go through the branch:
    
                            if (buffer_new(bh)) {
                                    clean_bdev_bh_alias(bh);
                                    if (folio_test_uptodate(folio)) {
                                            clear_buffer_new(bh);
                                            set_buffer_uptodate(bh);
                                            mark_buffer_dirty(bh);
                                            continue;
                                    }
                                    ...
                            }
    
    Hence buffers on BJ_Forget list of the committing transaction get marked
    dirty and this triggers the jbd2 assertion.
    
    Teach ext4_block_write_begin() to properly handle files with data
    journalling by avoiding dirtying them directly. Instead of
    folio_zero_new_buffers() we use ext4_journalled_zero_new_buffers() which
    takes care of handling journalling. We also don't need to mark new uptodate
    buffers as dirty in ext4_block_write_begin(). That will be either done
    either by block_commit_write() in case of success or by
    folio_zero_new_buffers() in case of failure.
    
    Reported-by: Baolin Liu <liubaolin@kylinos.cn>
    Suggested-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Shida Zhang <zhangshida@kylinos.cn>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Link: https://patch.msgid.link/20240830053739.3588573-4-zhangshida@kylinos.cn
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Conflicts:
    	fs/ext4/ext4.h
    	fs/ext4/inline.c
    	fs/ext4/inode.c
    [The conflicts were due to not merge commit 86b38c273cc6]
    Signed-off-by: Shida Zhang <zhangshida@kylinos.cn>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -3753,7 +3753,8 @@ static inline int ext4_buffer_uptodate(struct buffer_head *bh)
 	return buffer_uptodate(bh);
 }
 
-extern int ext4_block_write_begin(struct page *page, loff_t pos, unsigned len,
+extern int ext4_block_write_begin(handle_t *handle, struct page *page,
+				  loff_t pos, unsigned len,
 				  get_block_t *get_block);
 #endif	/* __KERNEL__ */
 
