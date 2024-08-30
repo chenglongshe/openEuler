964b044d01f6796803439f522abef8a303863c1e
Shida Zhang <zhangshida@kylinos.cn>
2024-08-30 13:37:39 +0800

    ext4: remove the special buffer dirty handling in do_journal_get_write_access
    mainline inclusion
    from mainline-v6.12-rc1
    commit 183aa1d3baea18b199505455a0247b13de826e2f
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IB731V?from=project-issue
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=183aa1d3baea18b199505455a0247b13de826e2f
    
    --------------------------------------------------------
    
    This kinda revert the commit 56d35a4cd13e("ext4: Fix dirtying of
    journalled buffers in data=journal mode") made by Jan 14 years ago,
    since the do_get_write_access() itself can deal with the extra
    unexpected buf dirting things in a proper way now.
    
    Suggested-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Shida Zhang <zhangshida@kylinos.cn>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Link: https://patch.msgid.link/20240830053739.3588573-5-zhangshida@kylinos.cn
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Conflicts:
    	fs/ext4/inode.c
    [The conflicts were due to not merge commit 86b38c273cc6]
    Signed-off-by: Shida Zhang <zhangshida@kylinos.cn>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -1011,26 +1011,10 @@ int ext4_walk_page_buffers(handle_t *handle,
 int do_journal_get_write_access(handle_t *handle,
 				struct buffer_head *bh)
 {
-	int dirty = buffer_dirty(bh);
-	int ret;
-
 	if (!buffer_mapped(bh) || buffer_freed(bh))
 		return 0;
-	/*
-	 * ext4_block_write_begin() could have dirtied some buffers. Clean
-	 * the dirty bit as jbd2_journal_get_write_access() could complain
-	 * otherwise about fs integrity issues. Setting of the dirty bit
-	 * by ext4_block_write_begin() isn't a real problem here as we clear
-	 * the bit before releasing a page lock and thus writeback cannot
-	 * ever write the buffer.
-	 */
-	if (dirty)
-		clear_buffer_dirty(bh);
 	BUFFER_TRACE(bh, "get write access");
-	ret = ext4_journal_get_write_access(handle, bh);
-	if (!ret && dirty)
-		ret = ext4_handle_dirty_metadata(handle, NULL, bh);
-	return ret;
+	return ext4_journal_get_write_access(handle, bh);
 }
 
 int ext4_block_write_begin(handle_t *handle, struct page *page,
