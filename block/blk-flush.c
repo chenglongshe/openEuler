2e79a17df52d2bebe328afc2d2a17721b4e23d1d
Yu Kuai <yukuai3@huawei.com>
2024-09-03 17:03:03 +0800

    block-io-hierarchy: core hierarchy stats implementation
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IAGRKP
    CVE: NA
    
    --------------------------------
    
    Include main structures definition and provede helpers for different IO
    stages to record IO stats.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>

--- block/blk-flush.c
+++ block/blk-flush.c
@@ -75,6 +75,7 @@
 #include "blk-mq.h"
 #include "blk-mq-tag.h"
 #include "blk-mq-sched.h"
+#include "blk-io-hierarchy/stats.h"
 
 /* PREFLUSH/FUA sequences */
 enum {
@@ -378,6 +379,8 @@ static bool blk_kick_flush(struct request_queue *q, struct blk_flush_queue *fq,
 	flush_rq->rq_flags |= RQF_FLUSH_SEQ;
 	flush_rq->rq_disk = first_rq->rq_disk;
 	flush_rq->end_io = flush_end_io;
+
+	blk_rq_hierarchy_stats_init(flush_rq);
 	blk_rq_init_bi_alloc_time(flush_rq, first_rq);
 	if (q->mq_ops)
 		blk_mq_get_alloc_task(flush_rq, first_rq->bio);
