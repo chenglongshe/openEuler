5060d7a4d087d9c09aedfaab19f17f5d2fb619c2
Bao Chenglong <994270571@qq.com>
2024-09-30 16:38:54 +0800

    riscv, bpf: Optimize cmpxchg insns with Zacas extension
    OSPP inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/open-source-summer/issues/I9JPN2
    
    ---------------------------------
    
    This patch optimizes the atomic_xchg operation using
    the Zacas extension, whereas the original code
    implemented this functionality with the LR and
    SC instructions.
    
    Signed-off-by: Bao Chenglong <994270571@qq.com>

--- arch/riscv/net/bpf_jit_comp64.c
+++ arch/riscv/net/bpf_jit_comp64.c
@@ -549,17 +549,24 @@ static void emit_atomic(u8 rd, u8 rs, s16 off, s32 imm, bool is64,
 	/* r0 = atomic_cmpxchg(dst_reg + off16, r0, src_reg); */
 	case BPF_CMPXCHG:
 		r0 = bpf_to_rv_reg(BPF_REG_0, ctx);
-		emit(is64 ? rv_addi(RV_REG_T2, r0, 0) :
-		     rv_addiw(RV_REG_T2, r0, 0), ctx);
-		emit(is64 ? rv_lr_d(r0, 0, rd, 0, 0) :
-		     rv_lr_w(r0, 0, rd, 0, 0), ctx);
-		jmp_offset = ninsns_rvoff(8);
-		emit(rv_bne(RV_REG_T2, r0, jmp_offset >> 1), ctx);
-		emit(is64 ? rv_sc_d(RV_REG_T3, rs, rd, 0, 1) :
-		     rv_sc_w(RV_REG_T3, rs, rd, 0, 1), ctx);
-		jmp_offset = ninsns_rvoff(-6);
-		emit(rv_bne(RV_REG_T3, 0, jmp_offset >> 1), ctx);
-		emit(rv_fence(0x3, 0x3), ctx);
+		if (rvzacas_enabled()) {
+			emit(is64 ? rvzacas_amocas_d(r0, rs, rd, 1, 1) :
+			     rvzacas_amocas_w(r0, rs, rd, 1, 1), ctx);
+			if (!is64)
+				emit_zext_32(r0, ctx);
+		} else {
+			emit(is64 ? rv_addi(RV_REG_T2, r0, 0) :
+			     rv_addiw(RV_REG_T2, r0, 0), ctx);
+			emit(is64 ? rv_lr_d(r0, 0, rd, 0, 0) :
+			     rv_lr_w(r0, 0, rd, 0, 0), ctx);
+			jmp_offset = ninsns_rvoff(8);
+			emit(rv_bne(RV_REG_T2, r0, jmp_offset >> 1), ctx);
+			emit(is64 ? rv_sc_d(RV_REG_T3, rs, rd, 0, 1) :
+			     rv_sc_w(RV_REG_T3, rs, rd, 0, 1), ctx);
+			jmp_offset = ninsns_rvoff(-6);
+			emit(rv_bne(RV_REG_T3, 0, jmp_offset >> 1), ctx);
+			emit(rv_fence(0x3, 0x3), ctx);
+		}
 		break;
 	}
 }
