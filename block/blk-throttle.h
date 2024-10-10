97fee3908a9e47d6b4dfd48062fd1ce7014f8146
Yu Kuai <yukuai3@huawei.com>
2024-10-10 21:47:53 +0800

    blk-throttle: remove THROTL_TG_HAS_IOPS_LIMIT
    mainline inclusion
    from mainline-v6.1-rc1
    commit 85496749904016f36b69332f73a1cf3ecfee828f
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IAUKH4
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=85496749904016f36b69332f73a1cf3ecfee828f
    
    --------------------------------
    
    Currently, "tg->has_rules" and "tg->flags & THROTL_TG_HAS_IOPS_LIMIT"
    both try to bypass bios that don't need to be throttled, however, they are
    a little redundant and both not perfect:
    
    1) "tg->has_rules" only distinguish read and write, but not iops and bps
       limit.
    2) "tg->flags & THROTL_TG_HAS_IOPS_LIMIT" only check if iops limit
       exist, read and write is not distinguished, and bps limit is not
       checked.
    
    tg->has_rules will extended to distinguish bps and iops in the following
    patch. There is no need to keep the flag.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Link: https://lore.kernel.org/r/20220921095309.1481289-2-yukuai1@huaweicloud.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Conflicts:
    	block/blk-throttle.h
    [commit ("8f9e7b65f833 block: cancel all throttled bios in del_gendisk()")
    is not backported.]
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>

--- block/blk-throttle.h
+++ block/blk-throttle.h
@@ -55,7 +55,6 @@ struct throtl_service_queue {
 enum tg_state_flags {
 	THROTL_TG_PENDING	= 1 << 0,	/* on parent's pending tree */
 	THROTL_TG_WAS_EMPTY	= 1 << 1,	/* bio_lists[] became non-empty */
-	THROTL_TG_HAS_IOPS_LIMIT = 1 << 2,	/* tg has iops limit */
 };
 
 enum {
@@ -179,11 +178,6 @@ bool __blk_throtl_bio(struct bio *bio);
 static inline bool blk_throtl_bio(struct bio *bio)
 {
 	struct throtl_grp *tg = blkg_to_tg(bio->bi_blkg);
-
-	/* no need to throttle bps any more if the bio has been throttled */
-	if (bio_flagged(bio, BIO_BPS_THROTTLED) &&
-	    !(tg->flags & THROTL_TG_HAS_IOPS_LIMIT))
-		return false;
 
 	if (!tg->has_rules[bio_data_dir(bio)])
 		return false;
