6db8849c46473e0331c3b5733fa686f37621882b
Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
2024-10-15 18:56:05 +0100

    fork: do not invoke uffd on fork if error occurs
    stable inclusion
    from stable-v6.11.7
    commit 92b472945dbf8abc020e9259c0088026f7027dfc
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/IB37AL
    CVE: CVE-2024-50220
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=92b472945dbf8abc020e9259c0088026f7027dfc
    
    --------------------------------
    
    [ Upstream commit f64e67e5d3a45a4a04286c47afade4b518acd47b ]
    
    Patch series "fork: do not expose incomplete mm on fork".
    
    During fork we may place the virtual memory address space into an
    inconsistent state before the fork operation is complete.
    
    In addition, we may encounter an error during the fork operation that
    indicates that the virtual memory address space is invalidated.
    
    As a result, we should not be exposing it in any way to external machinery
    that might interact with the mm or VMAs, machinery that is not designed to
    deal with incomplete state.
    
    We specifically update the fork logic to defer khugepaged and ksm to the
    end of the operation and only to be invoked if no error arose, and
    disallow uffd from observing fork events should an error have occurred.
    
    This patch (of 2):
    
    Currently on fork we expose the virtual address space of a process to
    userland unconditionally if uffd is registered in VMAs, regardless of
    whether an error arose in the fork.
    
    This is performed in dup_userfaultfd_complete() which is invoked
    unconditionally, and performs two duties - invoking registered handlers
    for the UFFD_EVENT_FORK event via dup_fctx(), and clearing down
    userfaultfd_fork_ctx objects established in dup_userfaultfd().
    
    This is problematic, because the virtual address space may not yet be
    correctly initialised if an error arose.
    
    The change in commit d24062914837 ("fork: use __mt_dup() to duplicate
    maple tree in dup_mmap()") makes this more pertinent as we may be in a
    state where entries in the maple tree are not yet consistent.
    
    We address this by, on fork error, ensuring that we roll back state that
    we would otherwise expect to clean up through the event being handled by
    userland and perform the memory freeing duty otherwise performed by
    dup_userfaultfd_complete().
    
    We do this by implementing a new function, dup_userfaultfd_fail(), which
    performs the same loop, only decrementing reference counts.
    
    Note that we perform mmgrab() on the parent and child mm's, however
    userfaultfd_ctx_put() will mmdrop() this once the reference count drops to
    zero, so we will avoid memory leaks correctly here.
    
    Link: https://lkml.kernel.org/r/cover.1729014377.git.lorenzo.stoakes@oracle.com
    Link: https://lkml.kernel.org/r/d3691d58bb58712b6fb3df2be441d175bd3cdf07.1729014377.git.lorenzo.stoakes@oracle.com
    Fixes: d24062914837 ("fork: use __mt_dup() to duplicate maple tree in dup_mmap()")
    Signed-off-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Reported-by: Jann Horn <jannh@google.com>
    Reviewed-by: Jann Horn <jannh@google.com>
    Reviewed-by: Liam R. Howlett <Liam.Howlett@Oracle.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Linus Torvalds <torvalds@linuxfoundation.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Wang Jinchao <wangjinchao@xfusion.com>

--- include/linux/userfaultfd_k.h
+++ include/linux/userfaultfd_k.h
@@ -85,6 +85,7 @@ static inline bool userfaultfd_armed(struct vm_area_struct *vma)
 
 extern int dup_userfaultfd(struct vm_area_struct *, struct list_head *);
 extern void dup_userfaultfd_complete(struct list_head *);
+void dup_userfaultfd_fail(struct list_head *);
 
 extern void mremap_userfaultfd_prep(struct vm_area_struct *,
 				    struct vm_userfaultfd_ctx *);
@@ -152,6 +153,10 @@ static inline int dup_userfaultfd(struct vm_area_struct *vma,
 }
 
 static inline void dup_userfaultfd_complete(struct list_head *l)
+{
+}
+
+static inline void dup_userfaultfd_fail(struct list_head *l)
 {
 }
 
