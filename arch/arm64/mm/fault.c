c5d1cdc1cac2d4cb24c778ed3930f5fa92f90eae
Ma Wupeng <mawupeng1@huawei.com>
2024-11-06 14:50:30 +0800

    arm64: send sig fault for user task when apei_claim_sea fails
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IB0OV7
    
    --------------------------------
    
    During arm64_do_kernel_sea, kernel will panic if no info is reported to
    kernel since apei_claim_sea will fail. However, for user task, sig fault
    can be send to which can avoid kernel panic.
    
    Signed-off-by: Ma Wupeng <mawupeng1@huawei.com>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -729,6 +729,9 @@ static int do_bad(unsigned long addr, unsigned int esr, struct pt_regs *regs)
 static bool arm64_do_kernel_sea(void __user *addr, unsigned int esr,
 				struct pt_regs *regs, int sig, int code)
 {
+	unsigned long pc;
+	int err;
+
 	if (!IS_ENABLED(CONFIG_ARCH_HAS_COPY_MC))
 		return false;
 
@@ -738,11 +741,19 @@ static bool arm64_do_kernel_sea(void __user *addr, unsigned int esr,
 	if (user_mode(regs))
 		return false;
 
-	if (apei_claim_sea(regs) < 0)
-		return false;
-
+	pc = regs->pc;
 	if (!fixup_exception_mc(regs))
 		return false;
+
+	err = apei_claim_sea(regs);
+	if (err < 0) {
+		pr_emerg("apei claim sea failed. addr: %#lx, esr: %#x\n",
+			 (unsigned long)addr, esr);
+		if (!current->mm) {
+			regs->pc = pc;
+			return false;
+		}
+	}
 
 	if (current->flags & PF_KTHREAD)
 		return true;
