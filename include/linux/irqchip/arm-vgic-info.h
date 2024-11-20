7edf5522f398891fb86053a553fdd4744df479e2
Zhou Wang <wangzhou1@hisilicon.com>
2024-11-20 17:02:05 +0800

    Revert "irqchip: gicv3: Add workaround for hip09 erratum 162200803"
    driver inclusion
    category: other
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9KBKD
    
    ------------------------------------------------------------------
    
    This reverts commit 12e11032205ea40d7046cdecce3b775f69034e2e.
    
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>
    Signed-off-by: caijian <caijian11@h-partners.com>

--- include/linux/irqchip/arm-vgic-info.h
+++ include/linux/irqchip/arm-vgic-info.h
@@ -38,7 +38,6 @@ struct gic_kvm_info {
 	/* vtimer irqbypass support */
 	bool            has_vtimer;
 #endif
-	u64		flags;
 	/* Deactivation impared, subpar stuff */
 	bool		no_hw_deactivation;
 };
