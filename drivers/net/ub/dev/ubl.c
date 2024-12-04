openeuler-mirror/kernel

commit eb44201ee79cffdcf1b16759a54940dca047aae5

ubl: add CONFIG_UBL definition and UBL interface
kernel inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I850RQ
CVE: NA

-----------------------------------------------------------

This patch adds UB link interfaces to support it.

UB is a new interconnection protocol that defines its own Layer 2 protocol.
The format of a complete UB packet is as follows. UBL HDR includes UB LINK,
CC and NPI. UB LINK replaces Ether header in packets. The UB Network header
consists of CC, NPI, and traditional Network packet headers.

|<-------- UBL HDR --------->|
+--------------+------+------+---------+-----------+---------+
|    UB LINK   |  CC  |  NPI | Network | TPH / TAH | Payload |
+--------------+------+------+---------+-----------+---------+
               |<----- UB Network ---->|

- UB LINK: Format specified by the data link layer of the UB LINK.
- CC: Congestion Control.
- NPI: Network Partition Identifier.
- Network: Traditinal L3 Header.

At network layer, the UB Network Header is encapsulated. However,
the packet type cannot be known by parsing packet from user, which
leads to restrictions on the use of socket.

This patch adds sw_ctype field to indicate the packet type. As shown
in the following figure, the sw_ctype field will be add at the header
of packet before entering the dirver.

+----------+----+-----+---------+-----------+---------+
| sw_ctype | CC | NPI | Network | TPH / TAH | Payload |
+----------+----+-----+---------+-----------+---------+

-sw_ctype: L3 Header type.

When packets are sent to hardware, the sw_ctype field will be deleted
and the original packet will be restored.

+----+-----+---------+-----------+---------+
| CC | NPI | Network | TPH / TAH | Payload |
+----+-----+---------+-----------+---------+

Signed-off-by: Junxin Chen <chenjunxin1@huawei.com>
Signed-off-by: Haiqing Fang <fanghaiqing@huawei.com>

drivers/net/ub/dev/ubl.c
@@ -1,0 +1,169 @@
+// SPDX-License-Identifier: GPL-2.0+
+/* Copyright (c) 2023-2023 Hisilicon Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * UBL      An implementation of the UB protocol suite for the LINUX
+ *      operating system.
+ *
+ * UB link device handling.
+ *
+ * Version:	@(#)ubl.c	1.0.1	23/02/14
+ *
+ */
+
+#include <linux/netdevice.h>
+#include <net/pkt_sched.h>
+
+#include "ubl.h"
+
+static __be16 ubl_type_to_proto(u8 type)
+{
+	__be16 proto;
+
+	switch (type) {
+	case UB_IPV4_CFG_TYPE:
+		proto = htons(ETH_P_IP);
+		break;
+	case UB_IPV6_CFG_TYPE:
+		proto = htons(ETH_P_IPV6);
+		break;
+	case UB_NOIP_CFG_TYPE:
+	default:
+		proto = htons(ETH_P_UB);
+		break;
+	}
+
+	return proto;
+}
+
+/**
+ * ubl_create_header - create the ubl header
+ * @skb:	buffer to alter
+ * @dev:	source device
+ * @type:	ubl type field
+ * @daddr:	not used in ubl
+ * @saddr:	not used in ubl
+ * @len:   packet length (<= skb->len)
+ *
+ */
+int ubl_create_header(struct sk_buff *skb, struct net_device *dev,
+		      unsigned short type, const void *daddr,
+		      const void *saddr, unsigned int len)
+{
+	u8 ctype = UB_NOIP_CFG_TYPE;
+	int ret = -UBL_HLEN;
+	struct ublhdr *ubl;
+
+	if (type == ETH_P_IP || type == ETH_P_IPV6) {
+		ubl = (struct ublhdr *)skb_push(skb, UBL_HLEN);
+		memset(ubl, 0, sizeof(struct ublhdr));
+		ubl->h_npi = htonl(UB_DEFAULT_NPI);
+		ctype = (type == ETH_P_IP) ? UB_IPV4_CFG_TYPE : UB_IPV6_CFG_TYPE;
+		ret = UBL_HLEN;
+	} else if (type == ETH_P_UB) {
+		/* if type is ETH_P_UB, then do nothing. */
+		ret = 0;
+	}
+	ubl_add_sw_ctype(skb, ctype);
+
+	return ret;
+}
+EXPORT_SYMBOL(ubl_create_header);
+
+/**
+ * ubl_header_parse_protocol - parse packets protocol before send it to driver.
+ * @skb: buffer to alter
+ *
+ * parse packets based on packet data if skb->protocol is ETH_P_ALL or 0.
+ */
+static __be16 ubl_header_parse_protocol(const struct sk_buff *skb)
+{
+	return ubl_type_to_proto(skb->data[0]);
+}
+
+const static struct header_ops ubl_header_ops ____cacheline_aligned = {
+	.create		= ubl_create_header,
+	.parse_protocol	= ubl_header_parse_protocol,
+};
+
+/**
+ * ubl_setup - setup ub link network device
+ * @dev: network device
+ *
+ * Fill in the fields of the device structure with ubl-generic values.
+ */
+void ubl_setup(struct net_device *dev)
+{
+	dev->header_ops         = &ubl_header_ops;
+	dev->type               = ARPHRD_UB;
+	dev->hard_header_len    = UBL_HLEN;
+	dev->min_header_len     = UBL_HLEN;
+	dev->mtu                = UB_DATA_LEN;
+	dev->min_mtu            = UB_MIN_MTU;
+	dev->max_mtu            = UB_DATA_LEN;
+	dev->addr_len           = UBL_ALEN;
+	dev->tx_queue_len       = DEFAULT_TX_QUEUE_LEN;
+	dev->flags              = (IFF_NOARP | IFF_POINTOPOINT);
+	dev->priv_flags         |= IFF_TX_SKB_SHARING;
+}
+EXPORT_SYMBOL(ubl_setup);
+
+/**
+ * alloc_ubndev_mqs - Allocates and sets up an ub-n device
+ * @sizeof_priv: Size of additional driver-private structure to be allocated
+ *	for this ubn device
+ * @txqs: The number of TX queues this device has.
+ * @rxqs: The number of RX queues this device has.
+ *
+ * Fill in the fields of the device structure with ubn-generic
+ * values. Basically does everything except registering the device.
+ *
+ * Constructs a new net device, complete with a private data area of
+ * size (sizeof_priv).  A 32-byte (not bit) alignment is enforced for
+ * this private data area.
+ */
+
+struct net_device *alloc_ubndev_mqs(int sizeof_priv, unsigned int txqs,
+				    unsigned int rxqs)
+{
+	return alloc_netdev_mqs(sizeof_priv, "ubn%d", NET_NAME_UNKNOWN,
+				ubl_setup, txqs, rxqs);
+}
+EXPORT_SYMBOL(alloc_ubndev_mqs);
+
+/**
+ * ubl_type_trans - obtains skb->protocol and adds sw_ptype to the packet
+ * @skb: buffer to alter
+ * @dev: source device
+ * @type: packet type
+ *
+ * Obtains the packet type and translates it to skb->protocol and adds sw_ptype
+ * to the packet data.
+ */
+__be16 ubl_type_trans(struct sk_buff *skb, struct net_device *dev, u8 type)
+{
+	skb->dev = dev;
+	ubl_add_sw_ctype(skb, type);
+	skb_reset_mac_header(skb);
+	if (type == UB_IPV4_CFG_TYPE || type == UB_IPV6_CFG_TYPE)
+		skb_pull_inline(skb, UBL_HLEN + 1);
+	else if (type != UB_NOIP_CFG_TYPE)
+		net_warn_ratelimited("An unknown packet is received by %s, type is %u\n",
+				     dev->name, type);
+
+	return ubl_type_to_proto(type);
+}
+EXPORT_SYMBOL(ubl_type_trans);
+
+MODULE_AUTHOR("Huawei Tech. Co., Ltd.");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("UB link level");
+MODULE_VERSION(UBL_MOD_VERSION);

arch/arm64/configs/openeuler_defconfig
@@ -2903,6 +2903,7 @@ CONFIG_NET_VENDOR_NEBULA_MATRIX=y
 CONFIG_M1600=m
 # CONFIG_FDDI is not set
 # CONFIG_HIPPI is not set
+CONFIG_UBL=y
 # CONFIG_NET_SB1000 is not set
 CONFIG_PHYLIB=y
 CONFIG_SWPHY=y

drivers/net/Kconfig
@@ -444,6 +444,8 @@ source "drivers/net/fddi/Kconfig"
 
 source "drivers/net/hippi/Kconfig"
 
+source "drivers/net/ub/Kconfig"
+
 source "drivers/net/ipa/Kconfig"
 
 config NET_SB1000

drivers/net/Makefile
@@ -68,6 +68,7 @@ obj-$(CONFIG_WAN) += wan/
 obj-$(CONFIG_WLAN) += wireless/
 obj-$(CONFIG_WIMAX) += wimax/
 obj-$(CONFIG_IEEE802154) += ieee802154/
+obj-$(CONFIG_UBL) += ub/
 
 obj-$(CONFIG_VMXNET3) += vmxnet3/
 obj-$(CONFIG_XEN_NETDEV_FRONTEND) += xen-netfront.o

drivers/net/ub/Kconfig
@@ -1,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# UBL configuration
+#
+
+config UBL
+	default n
+	tristate "UB link support"
+	help
+	  This enables UB link support. Say 'Y' or 'm' if your
+	  device works in UB.

drivers/net/ub/Makefile
@@ -1,0 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0+
+#
+# Makefile for the HISILICON network device drivers.
+#
+
+#### compile ubl
+obj-$(CONFIG_UBL) += dev/

drivers/net/ub/dev/Makefile
@@ -1,0 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0+
+#
+# Makefile for the HISILICON network device drivers.
+#
+
+#### compile ubl
+obj-$(CONFIG_UBL) += ubl.o

drivers/net/ub/dev/ubl.h
@@ -1,0 +1,104 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/* Copyright (c) 2023-2023 Hisilicon Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ */
+
+#ifndef __LINUX_UBL_H
+#define __LINUX_UBL_H
+
+#include <linux/if_arp.h>
+#include <linux/if_ether.h>
+#include <linux/types.h>
+#include <linux/skbuff.h>
+
+#define UBL_MOD_VERSION "1.0.0"
+
+#define UBL_HARD_HLEN		4
+#define UBL_HLEN		6
+#define UBL_ALEN		16
+
+#define UBL_LCRC_LEN		2
+#define UBL_BCRC_LEN		4
+#define UBL_LINK_LEN		2
+
+#define UB_DEFAULT_NPI		1
+
+#define UB_MAX_BLOCK_NUM	16
+#define UB_FLIT_NUM_OF_BLOCK	32
+#define UB_BYTE_OF_FLIT		20
+#define UB_BYTE_OF_BLK		(UB_BYTE_OF_FLIT * UB_FLIT_NUM_OF_BLOCK)
+
+#define UB_BLOCK_CRC_LEN	(UBL_BCRC_LEN + UBL_LCRC_LEN)
+#define UB_MAX_MTU_PER_BLK	(UB_BYTE_OF_BLK - UBL_LINK_LEN - UB_BLOCK_CRC_LEN)
+#define UB_DATA_LEN		1500
+#define UB_MAX_MTU		((UB_MAX_BLOCK_NUM * UB_MAX_MTU_PER_BLK) - \
+				UBL_HLEN - UBL_HARD_HLEN + UBL_LINK_LEN)
+#define UB_MIN_MTU		68
+
+#define UB_IPV4_CFG_TYPE	3
+#define UB_IPV6_CFG_TYPE	4
+#define UB_NOIP_CFG_TYPE	5
+#define UB_UNKNOWN_CFG_TYPE	255
+
+/**
+ *	struct ublhdr - ub link header
+ *	@h_cc: cc
+ *	@h_npi: npi
+ */
+struct ublhdr {
+	__be16 h_cc;
+	__be32 h_npi;
+} __packed;
+
+/**
+ * ubl_add_sw_ctype - add software packet type for skb->data
+ * @skb: buffer to alter
+ * @ctype: indicates the packet type
+ *
+ * The packet type cannot be known by parsing packe from user,
+ * which leads to restrictions on the use of socket.
+ * Add cs_type field to indicate the packet type. And sw_ctype
+ * exists only during software prcessing.
+ * +----------+----+-----+-----------+
+ * | sw_ctype | CC | NPI | L3 Packet |
+ * +----------+----+-----+-----------+
+ */
+static inline void ubl_add_sw_ctype(struct sk_buff *skb, u8 ctype)
+{
+	u8 *pkt_cfg = (u8 *)skb_push(skb, sizeof(u8));
+
+	*pkt_cfg = ctype;
+}
+
+/**
+ * ubl_rmv_sw_ctype - delete software packet type for skb->data
+ * @skb: buffer to alter
+ *
+ * Before the packet is sent to the hardware, remove sw_ctype field
+ * and restore the original packet.
+ */
+static inline void ubl_rmv_sw_ctype(struct sk_buff *skb)
+{
+	skb_pull_inline(skb, sizeof(u8));
+}
+
+int ubl_create_header(struct sk_buff *skb, struct net_device *dev,
+		      unsigned short type, const void *daddr,
+		      const void *saddr, unsigned int len);
+void ubl_setup(struct net_device *dev);
+__be16 ubl_type_trans(struct sk_buff *skb, struct net_device *dev, u8 type);
+struct net_device *alloc_ubndev_mqs(int sizeof_priv, unsigned int txqs,
+				    unsigned int rxqs);
+#define alloc_ubndev_mq(sizeof_priv, count) \
+	alloc_ubndev_mqs((sizeof_priv), (count), (count))
+
+#endif

chenglongshe/openEuler

commit e131f72e204415f259e7a0dca8a372721e0be873

UBL: Move static to the beginning of struct definition
driver inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IB951S
CVE: NA

-----------------------------------------------------------

Move static to the beginning of struct definition to
eliminate compile warnings from the compile options
-Wold-style-declaration which demands static should be
in the beginning of definition.

Fixes: eb44201ee79c ("ubl: add CONFIG_UBL definition and UBL interface")
Signed-off-by: mufengyan <mufengyan@hisilicon.com>

drivers/net/ub/dev/ubl.c
@@ -111,7 +111,7 @@ static __be16 ubl_header_parse_protocol(const struct sk_buff *skb)
 	return ubl_type_to_proto(skb->data[0]);
 }
 
-const static struct header_ops ubl_header_ops ____cacheline_aligned = {
+static const struct header_ops ubl_header_ops ____cacheline_aligned = {
 	.create		= ubl_create_header,
 	.parse_protocol	= ubl_header_parse_protocol,
 };