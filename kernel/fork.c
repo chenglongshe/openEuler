6ff8e80bfa77fccd6295aaa00e1b302e4f54d2ab
Jinjie Ruan <ruanjinjie@huawei.com>
2024-12-09 16:07:27 +0800

    fork: Return early if ctx is NULL for dup_userfaultfd()
    hulk inclusion
    category: performance
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/IBA1TU
    
    --------------------------------
    
    Check the vma's userfaultfd ctx ahead to dup_mmap(), so we can
    avoid the function call overhead if the ctx is NULL.
    
    The Unixbench single core process create has 2 ~ 3% improve with
    this and following patch.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -731,9 +731,14 @@ static __latent_entropy int dup_mmap(struct mm_struct *mm,
 		if (retval)
 			goto fail_nomem_policy;
 		tmp->vm_mm = mm;
-		retval = dup_userfaultfd(tmp, &uf);
-		if (retval)
-			goto fail_nomem_anon_vma_fork;
+
+#ifdef CONFIG_USERFAULTFD
+		if (unlikely(tmp->vm_userfaultfd_ctx.ctx)) {
+			retval = dup_userfaultfd(tmp, &uf);
+			if (retval)
+				goto fail_nomem_anon_vma_fork;
+		}
+#endif
 		if (tmp->vm_flags & VM_WIPEONFORK) {
 			/*
 			 * VM_WIPEONFORK gets a clean slate in the child.
