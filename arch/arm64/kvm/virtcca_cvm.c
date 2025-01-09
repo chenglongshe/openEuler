3a56b96b555a5e3a131b42df8af2f3c1cd0c4924
liupingwei <liupingwei0317@outlook.com>
2025-01-09 11:13:41 +0800

    virtCCA feature : add uefi grub boot support for cvm.
    virtcca inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBGNCV
    
    --------------------------------
    
    VirtCCA Feature:
    Add uefi grub boot support for cvm.
    
    Signed-off-by: liupingwei <liupingwei0317@outlook.com>

--- arch/arm64/kvm/virtcca_cvm.c
+++ arch/arm64/kvm/virtcca_cvm.c
@@ -24,6 +24,11 @@ static unsigned long *cvm_vmid_bitmap;
 DEFINE_STATIC_KEY_FALSE(virtcca_cvm_is_available);
 #define SIMD_PAGE_SIZE 0x3000
 
+#define UEFI_LOADER_START 0x0
+#define UEFI_MAX_SIZE 0x8000000
+#define UEFI_DTB_START 0x40000000
+#define DTB_MAX_SIZE 0x200000
+
 int kvm_enable_virtcca_cvm(struct kvm *kvm)
 {
 	if (!static_key_enabled(&virtcca_cvm_is_available))
@@ -651,6 +656,17 @@ static int kvm_populate_ipa_cvm_range(struct kvm *kvm,
 	ipa_base1 = round_down(args->populate_ipa_base1, l2_granule);
 	ipa_end2 = round_up(args->populate_ipa_base2 + args->populate_ipa_size2, l2_granule);
 
+	/* uefi boot, uefi image and uefi ram from 0 to 128M */
+	if (ipa_base1 == UEFI_LOADER_START) {
+		phys_addr_t ipa_base2 = round_down(args->populate_ipa_base2, l2_granule);
+		phys_addr_t ipa_end1 = round_up(args->populate_ipa_base1 + args->populate_ipa_size1, l2_granule);
+		int uefi_ret = kvm_populate_ram_region(kvm, l2_granule, ipa_base1, ipa_end1, args);
+		if (!uefi_ret) {
+			uefi_ret = kvm_populate_ram_region(kvm, l2_granule, ipa_base2, ipa_end2, args);
+		}
+		return uefi_ret;
+	}
+	/* direct boot */
 	return kvm_populate_ram_region(kvm, l2_granule, ipa_base1, ipa_end2, args);
 }
 
@@ -836,16 +852,24 @@ int kvm_load_user_data(struct kvm *kvm, unsigned long arg)
 
 		if (!is_numa_ipa_range_valid(numa_info))
 			return -EINVAL;
-		if (user_data.loader_start < numa_node->ipa_start ||
-			user_data.dtb_end > ipa_end)
+		// uefi image and uefi ram from 0 to 128M
+		if (user_data.loader_start != UEFI_LOADER_START && user_data.loader_start < numa_node->ipa_start ||
+			user_data.dtb_end > ipa_end) {
 			return -EINVAL;
+		}
 		for (i = 0; i < numa_info->numa_cnt; i++)
 			total_size += numa_info->numa_nodes[i].ipa_size;
 		if (total_size != user_data.ram_size)
 			return -EINVAL;
 	}
 
-	if (user_data.image_end <= user_data.loader_start ||
+	/* uefi boot */
+	if (user_data.loader_start == UEFI_LOADER_START) {
+		if (user_data.image_end > UEFI_LOADER_START + UEFI_MAX_SIZE ||
+			user_data.initrd_start != UEFI_DTB_START ||
+			user_data.dtb_end != UEFI_DTB_START + DTB_MAX_SIZE)
+			return -EINVAL;
+	} else if (user_data.image_end <= user_data.loader_start ||
 		user_data.initrd_start < user_data.image_end ||
 		user_data.dtb_end < user_data.initrd_start ||
 		user_data.ram_size < user_data.dtb_end - user_data.loader_start)
