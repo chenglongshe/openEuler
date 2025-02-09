5e86bcf3382edb46240ffc4dc300ad64712cf581
liujie_answer <liujie5@linkdatatechnology.com>
2025-02-09 11:48:46 +0800

    [PATCH] SCSI: Linkdata: Supports Linkdata HBA/RAID Controllers
    Linkdata inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBD4UN
    CVE: NA
    
    ------------------------------------------
    
    This commit is to support Linkdata HBA/RAID controllers.
    
    Signed-off-by: liujie_answer <liujie5@linkdatatechnology.com>

---
+++ drivers/scsi/linkdata/ps3stor/ps3_drv_ver.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _PS3_DRV_VER_H_
+#define _PS3_DRV_VER_H_
+
+#define PS3_PRIVATE_VERSION            "00.000.00.01"
+#define PS3_DRV_VERSION                "0.0.0.0"
+#define PS3_DRV_COMMIT_ID              "5473974 bbe55a7"
+#define PS3_DRV_BUILD_TIME             "Feb 19 2025 10:36:57"
+#define PS3_DRV_TOOLCHAIN_ID            ""
+
+#define PS3_DRV_PRODUCT_SUPPORT "RAID/HBA"
+#define PS3_DRV_LICENSE "GPL"
+#define PS3_DRV_AUTHOR "ps3stor"
+#define PS3_DRV_DESCRIPTION "ps3stor driver"
+
+#endif
