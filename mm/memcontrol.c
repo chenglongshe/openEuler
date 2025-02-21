0761c2daf167f8e15bb31f962e54fd2264f46510
Kairui Song <kasong@tencent.com>
2025-02-21 15:27:47 +0800

    mm/swap_cgroup: decouple swap cgroup recording and clearing
    mainline inclusion
    from mainline-v6.11-rc6
    commit 6769183166b33b1a5de8f938d1ff4d5f4be0f428
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBKK9D
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=6769183166b33b1a5de8f938d1ff4d5f4be0f428
    
    --------------------------------
    
    The current implementation of swap cgroup tracking is a bit complex and
    fragile:
    
    On charging path, swap_cgroup_record always records an actual memcg id,
    and it depends on the caller to make sure all entries passed in must
    belong to one single folio.  As folios are always charged or uncharged as
    a whole, and always charged and uncharged in order, swap_cgroup doesn't
    need an extra lock.
    
    On uncharging path, swap_cgroup_record always sets the record to zero.
    These entries won't be charged again until uncharging is done.  So there
    is no extra lock needed either.  Worth noting that swap cgroup clearing
    may happen without folio involved, eg.  exiting processes will zap its
    page table without swapin.
    
    The xchg/cmpxchg provides atomic operations and barriers to ensure no
    tearing or synchronization issue of these swap cgroup records.
    
    It works but quite error-prone.  Things can be much clear and robust by
    decoupling recording and clearing into two helpers.  Recording takes the
    actual folio being charged as argument, and clearing always set the record
    to zero, and refine the debug sanity checks to better reflect their usage
    
    Benchmark even showed a very slight improvement as it saved some
    extra arguments and lookups:
    
    make -j96 with defconfig on tmpfs in 1.5G memory cgroup using 4k folios:
    Before: sys 9617.23 (stdev 37.764062)
    After : sys 9541.54 (stdev 42.973976)
    
    make -j96 with defconfig on tmpfs in 2G memory cgroup using 64k folios:
    Before: sys 7358.98 (stdev 54.927593)
    After : sys 7337.82 (stdev 39.398956)
    
    Link: https://lkml.kernel.org/r/20241218114633.85196-5-ryncsn@gmail.com
    Signed-off-by: Kairui Song <kasong@tencent.com>
    Suggested-by: Chris Li <chrisl@kernel.org>
    Cc: Barry Song <baohua@kernel.org>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Yosry Ahmed <yosryahmed@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Conflicts:
        mm/memcontrol.c
    [Context conflicts in memcontrol.c due to miss commit f8f931bba0f9]
    [Context conflicts in memcontrol.c due to miss commit a095a940f784]
    Signed-off-by: Guangke Wang <dev01403@linx-info.com>
    ---

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -8966,7 +8966,6 @@ void mem_cgroup_swapout(struct folio *folio, swp_entry_t entry)
 {
 	struct mem_cgroup *memcg, *swap_memcg;
 	unsigned int nr_entries;
-	unsigned short oldid;
 
 	VM_BUG_ON_FOLIO(folio_test_lru(folio), folio);
 	VM_BUG_ON_FOLIO(folio_ref_count(folio), folio);
@@ -8993,10 +8992,9 @@ void mem_cgroup_swapout(struct folio *folio, swp_entry_t entry)
 	/* Get references for the tail pages, too */
 	if (nr_entries > 1)
 		mem_cgroup_id_get_many(swap_memcg, nr_entries - 1);
-	oldid = swap_cgroup_record(entry, mem_cgroup_id(swap_memcg),
-				   nr_entries);
-	VM_BUG_ON_FOLIO(oldid, folio);
 	mod_memcg_state(swap_memcg, MEMCG_SWAP, nr_entries);
+
+	swap_cgroup_record(folio, entry);
 
 	folio_unqueue_deferred_split(folio);
 	folio->memcg_data = 0;
@@ -9038,7 +9036,6 @@ int __mem_cgroup_try_charge_swap(struct folio *folio, swp_entry_t entry)
 	unsigned int nr_pages = folio_nr_pages(folio);
 	struct page_counter *counter;
 	struct mem_cgroup *memcg;
-	unsigned short oldid;
 
 	if (do_memsw_account())
 		return mem_cgroup_check_swap_for_v1(folio, entry);
@@ -9067,9 +9064,9 @@ int __mem_cgroup_try_charge_swap(struct folio *folio, swp_entry_t entry)
 	/* Get references for the tail pages, too */
 	if (nr_pages > 1)
 		mem_cgroup_id_get_many(memcg, nr_pages - 1);
-	oldid = swap_cgroup_record(entry, mem_cgroup_id(memcg), nr_pages);
-	VM_BUG_ON_FOLIO(oldid, folio);
 	mod_memcg_state(memcg, MEMCG_SWAP, nr_pages);
+
+	swap_cgroup_record(folio, entry);
 
 	return 0;
 }
@@ -9084,7 +9081,7 @@ void __mem_cgroup_uncharge_swap(swp_entry_t entry, unsigned int nr_pages)
 	struct mem_cgroup *memcg;
 	unsigned short id;
 
-	id = swap_cgroup_record(entry, 0, nr_pages);
+	id = swap_cgroup_clear(entry, nr_pages);
 	rcu_read_lock();
 	memcg = mem_cgroup_from_id(id);
 	if (memcg) {
