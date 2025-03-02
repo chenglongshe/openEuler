2d39fb31c02eee77128d71e4f669b01ae68b8b44
zhaolichang <zhaolichang@huawei.com>
2025-03-02 13:06:05 +0800

    【OLK-6.6】tlbi: Do not force the broadcasting of TLBI and ICache, and add TLB flush helpers based on IPI.
    Do not force the broadcasting of TLBI and ICache, and add TLB flush helpers based on IPI.
    
    Signed-off-by: zhaolichang <zhaolichang@huawei.com>

--- arch/arm64/include/asm/mmu_context.h
+++ arch/arm64/include/asm/mmu_context.h
@@ -261,8 +261,17 @@ static inline void
 switch_mm(struct mm_struct *prev, struct mm_struct *next,
 	  struct task_struct *tsk)
 {
-	if (prev != next)
+	unsigned int __maybe_unused cpu = smp_processor_id();
+
+	if (prev != next) {
 		__switch_mm(next);
+#ifdef CONFIG_ARM64_TLBI_IPI
+		if (unlikely(test_tlbi_ipi_switch())) {
+			cpumask_clear_cpu(cpu, mm_cpumask(prev));
+			local_flush_tlb_mm(prev);
+		}
+#endif
+	}
 
 	/*
 	 * Update the saved TTBR0_EL1 of the scheduled-in task as the previous
