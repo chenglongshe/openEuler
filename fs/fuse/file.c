b45ae4cb76bb855dfae6bd9840c897b8a10f02c8
Lei Huang <lei.huang@linux.intel.com>
2025-03-11 16:26:36 +0800

    fuse: Fix missing FOLL_PIN for direct-io
    Our user space filesystem relies on fuse to provide POSIX interface.
    In our test, a known string is written into a file and the content
    is read back later to verify correct data returned. We observed wrong
    data returned in read buffer in rare cases although correct data are
    stored in our filesystem.
    
    Fuse kernel module calls iov_iter_get_pages2() to get the physical
    pages of the user-space read buffer passed in read(). The pages are
    not pinned to avoid page migration. When page migration occurs, the
    consequence are two-folds.
    
    1) Applications do not receive correct data in read buffer.
    2) fuse kernel writes data into a wrong place.
    
    Using iov_iter_extract_pages() to pin pages fixes the issue in our
    test.
    
    An auxiliary variable "struct page **pt_pages" is used in the patch
    to prepare the 2nd parameter for iov_iter_extract_pages() since
    iov_iter_get_pages2() uses a different type for the 2nd parameter.
    
    [SzM] add iov_iter_extract_will_pin(ii) and unpin only if true.
    
    Signed-off-by: Lei Huang <lei.huang@linux.intel.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Conflicts:
    	fs/fuse/file.c
    	fs/fuse/fuse_i.h

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -648,7 +648,8 @@ static void fuse_release_user_pages(struct fuse_args_pages *ap, ssize_t nres,
 	for (i = 0; i < ap->num_pages; i++) {
 		if (should_dirty)
 			set_page_dirty_lock(ap->pages[i]);
-		put_page(ap->pages[i]);
+		if (ap->args.is_pinned)
+			unpin_user_page(ap->pages[i]);
 	}
 
 	if (nres > 0 && ap->args.invalidate_vmap)
@@ -1472,10 +1473,13 @@ static int fuse_get_user_pages(struct fuse_args_pages *ap, struct iov_iter *ii,
 	while (nbytes < *nbytesp && ap->num_pages < max_pages) {
 		unsigned npages;
 		size_t start;
-		ret = iov_iter_get_pages2(ii, &ap->pages[ap->num_pages],
-					*nbytesp - nbytes,
-					max_pages - ap->num_pages,
-					&start);
+		struct page **pt_pages;
+
+		pt_pages = &ap->pages[ap->num_pages];
+		ret = iov_iter_extract_pages(ii, &pt_pages,
+					     *nbytesp - nbytes,
+					     max_pages - ap->num_pages,
+					     0, &start);
 		if (ret < 0)
 			break;
 
@@ -1496,6 +1500,7 @@ static int fuse_get_user_pages(struct fuse_args_pages *ap, struct iov_iter *ii,
 		flush_kernel_vmap_range(ap->args.vmap_base, nbytes);
 
 	ap->args.invalidate_vmap = !write && flush_or_invalidate;
+	ap->args.is_pinned = iov_iter_extract_will_pin(ii);
 	ap->args.user_pages = true;
 	if (write)
 		ap->args.in_pages = true;
