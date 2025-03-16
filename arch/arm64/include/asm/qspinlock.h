070773558abc14e4a23144d90d867f054876868c
Zengruan Ye <yezengruan@huawei.com>
2025-03-16 11:28:34 +0800

    KVM: arm64: Add interface to support PV qspinlock
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBTO64
    CVE: NA
    
    ----------------------------------------------------
    
    As kernel has used this interface, so lets support it.
    
    Signed-off-by: Zengruan Ye <yezengruan@huawei.com>
    Reviewed-by: Zhanghailiang <zhang.zhanghailiang@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: lishusen <lishusen2@huawei.com>

--- arch/arm64/include/asm/qspinlock.h
+++ arch/arm64/include/asm/qspinlock.h
@@ -33,6 +33,41 @@ static inline void queued_spin_unlock(struct qspinlock *lock)
 }
 #endif
 
+#if !defined(CONFIG_NUMA_AWARE_SPINLOCKS) && defined(CONFIG_PARAVIRT_SPINLOCKS)
+#include <linux/jump_label.h>
+#include <asm/cpufeature.h>
+#include <asm-generic/qspinlock_types.h>
+#include <asm/paravirt.h>
+
+#define _Q_PENDING_LOOPS       (1 << 9)
+
+extern void native_queued_spin_lock_slowpath(struct qspinlock *lock, u32 val);
+extern void __pv_init_lock_hash(void);
+extern void __pv_queued_spin_lock_slowpath(struct qspinlock *lock, u32 val);
+
+#define queued_spin_unlock queued_spin_unlock
+/**
+ * queued_spin_unlock - release a queued spinlock
+ * @lock : Pointer to queued spinlock structure
+ *
+ * A smp_store_release() on the least-significant byte.
+ */
+static inline void native_queued_spin_unlock(struct qspinlock *lock)
+{
+	smp_store_release(&lock->locked, 0);
+}
+
+static inline void queued_spin_lock_slowpath(struct qspinlock *lock, u32 val)
+{
+	pv_queued_spin_lock_slowpath(lock, val);
+}
+
+static inline void queued_spin_unlock(struct qspinlock *lock)
+{
+	pv_queued_spin_unlock(lock);
+}
+#endif /* CONFIG_PARAVIRT_SPINLOCKS */
+
 #include <asm-generic/qspinlock.h>
 
 #endif /* _ASM_ARM64_QSPINLOCK_H */
