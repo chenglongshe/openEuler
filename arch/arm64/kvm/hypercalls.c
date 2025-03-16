e8fa6f62eec09d328101a52f531d0340bcf706c0
Zengruan Ye <yezengruan@huawei.com>
2025-03-16 15:23:05 +0800

    KVM: arm64: Implement PV_SCHED_KICK_CPU call
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBTO64
    CVE: NA
    
    ----------------------------------------------------
    
    Implement the service call for waking up a WFI state vCPU.
    
    Signed-off-by: Zengruan Ye <yezengruan@huawei.com>
    Reviewed-by: Zhanghailiang <zhang.zhanghailiang@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: lishusen <lishusen2@huawei.com>

--- arch/arm64/kvm/hypercalls.c
+++ arch/arm64/kvm/hypercalls.c
@@ -362,6 +362,9 @@ int kvm_smccc_call_handler(struct kvm_vcpu *vcpu)
 		vcpu->arch.pvsched.base = INVALID_GPA;
 		val[0] = SMCCC_RET_SUCCESS;
 		break;
+	case ARM_SMCCC_HV_PV_SCHED_KICK_CPU:
+		val[0] = kvm_pvsched_kick_vcpu(vcpu);
+		break;
 #endif /* CONFIG_PARAVIRT_SCHED */
 	case ARM_SMCCC_VENDOR_HYP_CALL_UID_FUNC_ID:
 		val[0] = ARM_SMCCC_VENDOR_HYP_UID_KVM_REG_0;
