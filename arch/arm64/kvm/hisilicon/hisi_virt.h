ca73ea957ae1376c0a269ead49e08fd0bb7606dc
Xiang Chen <chenxiang66@hisilicon.com>
2025-03-21 16:06:21 +0800

    kvm: hisi_virt: Update TLBI broadcast feature for hip12
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBTXY2
    
    ------------------------------------------------------------------------
    
    Compared with hip09, there are some differences on TLBI broadcast
    feature for hip12 including:
    - No need to translate logical cluster id to physical cluster id;
    - The minimum granularity of TLBI broadcast is cluster;
    - Some fields of register LSUDVMBM changes;
    
    So update for corresponding changes.
    
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: Jinqian Yang <yangjinqian1@huawei.com>

--- arch/arm64/kvm/hisilicon/hisi_virt.h
+++ arch/arm64/kvm/hisilicon/hisi_virt.h
@@ -14,8 +14,14 @@ enum hisi_cpu_type {
 	HI_IP09,
 	HI_IP10,
 	HI_IP10C,
+	HI_IP12,
 	UNKNOWN_HI_TYPE
 };
+
+/* HIP12 */
+#define TB_MN_BASE_HIP12 0x00400390000
+#define MN_DIS0_OFFSET_HIP12 0xc10
+#define MN_DIS0_DVM_BD_SHIFT_HIP12 16
 
 /* HIP10 */
 #define AIDR_EL1_DVMBM_MASK	GENMASK_ULL(13, 12)
@@ -63,6 +69,22 @@ enum hisi_cpu_type {
 #define DVMBM_DIE2_CLUSTER_SHIFT	0
 
 #define DVMBM_MAX_DIES			32
+
+/*
+ * MPIDR_EL1 layout on HIP12
+ *
+ * Aff3[3:2]	- socket ID	[0-3]
+ * Aff3[1:0]	- die ID	[0,1]
+ * Aff2		- cluster ID	[0-5]
+ * Aff1		- core ID	[0-15]
+ * Aff0		- thread ID	[0,1]
+ */
+
+#define DVMBM_DIE1_VDIE_SHIFT_HIP12		57
+#define DVMBM_DIE2_VDIE_SHIFT_HIP12		53
+#define DVMBM_DIE1_CLUSTER_SHIFT_HIP12		6
+#define DVMBM_DIE2_CLUSTER_SHIFT_HIP12		0
+#define DVMBM_MAX_DIES_HIP12				8
 
 void probe_hisi_cpu_type(void);
 bool hisi_ncsnp_supported(void);
