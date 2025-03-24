9e4d091cb3c24f7d6630becb723a8dcf326a210a
Jinjie Ruan <ruanjinjie@huawei.com>
2025-03-24 21:53:44 +0800

    arm64: entry: Support hardware xcall
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/release-management/issues/IBV2E4
    
    --------------------------------
    
    Support hardware xcall. Hardware xcall provides a separate entry for el0
    syscall handling, so we can use it to customize and respond to system
    call relatively quickly.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -2428,24 +2428,33 @@ static bool has_xint_support(const struct arm64_cpu_capabilities *entry, int __u
 }
 #endif
 
-#ifdef CONFIG_ARCH_SUPPORTS_XINT
-static bool test_has_xfunc(void)
+#if defined(CONFIG_ARCH_SUPPORTS_XINT) || defined(CONFIG_ARCH_SUPPORTS_XCALL)
+static bool test_has_xfunc(bool is_xint)
 {
 	u64 new, old = read_sysreg(actlr_el1);
 
-	write_sysreg(old | ACTLR_ELx_XINT, actlr_el1);
+	if (is_xint)
+		write_sysreg(old | ACTLR_ELx_XINT, actlr_el1);
+	else
+		write_sysreg(old | ACTLR_ELx_XCALL, actlr_el1);
+
 	isb();
 	new = read_sysreg(actlr_el1);
-	if (new & ACTLR_ELx_XINT) {
+	if (is_xint && (new & ACTLR_ELx_XINT)) {
 		write_sysreg(old, actlr_el1);
 		hw_xint_support = true;
+		return true;
+	}
+
+	if (!is_xint && (new & ACTLR_ELx_XCALL)) {
+		write_sysreg(old, actlr_el1);
 		return true;
 	}
 
 	return false;
 }
 
-static void enable_xfunc(void)
+static void enable_xfunc(bool is_xint)
 {
 	u64 actlr_el1, actlr_el2;
 	u64 el;
@@ -2453,7 +2462,7 @@ static void enable_xfunc(void)
 	el = read_sysreg(CurrentEL);
 	if (el == CurrentEL_EL2) {
 		actlr_el2 = read_sysreg(actlr_el2);
-		actlr_el2 |= ACTLR_ELx_XINT;
+		actlr_el2 |= (is_xint ? ACTLR_ELx_XINT : ACTLR_ELx_XCALL);
 		write_sysreg(actlr_el2, actlr_el2);
 		isb();
 		actlr_el2 = read_sysreg(actlr_el2);
@@ -2461,13 +2470,15 @@ static void enable_xfunc(void)
 	}
 
 	actlr_el1 = read_sysreg(actlr_el1);
-	actlr_el1 |= ACTLR_ELx_XINT;
+	actlr_el1 |= (is_xint ? ACTLR_ELx_XINT : ACTLR_ELx_XCALL);
 	write_sysreg(actlr_el1, actlr_el1);
 	isb();
 	actlr_el1 = read_sysreg(actlr_el1);
 	pr_info("actlr_el1: %llx, cpu:%d\n", actlr_el1, smp_processor_id());
 }
+#endif
 
+#ifdef CONFIG_ARCH_SUPPORTS_XINT
 static bool test_has_xint(const struct arm64_cpu_capabilities *entry, int scope)
 {
 	if (!IS_ENABLED(CONFIG_ARM64_NMI))
@@ -2479,12 +2490,24 @@ static bool test_has_xint(const struct arm64_cpu_capabilities *entry, int scope)
 	}
 #endif
 
-	return test_has_xfunc();
+	return test_has_xfunc(true);
 }
 
 static void xint_enable(const struct arm64_cpu_capabilities *__unused)
 {
-	enable_xfunc();
+	enable_xfunc(true);
+}
+#endif
+
+#ifdef CONFIG_ARCH_SUPPORTS_XCALL
+static bool test_has_xcall(const struct arm64_cpu_capabilities *entry, int scope)
+{
+	return test_has_xfunc(false);
+}
+
+static void xcall_enable(const struct arm64_cpu_capabilities *__unused)
+{
+	enable_xfunc(false);
 }
 #endif
 
@@ -3043,6 +3066,15 @@ static const struct arm64_cpu_capabilities arm64_features[] = {
 		.type = ARM64_CPUCAP_SYSTEM_FEATURE,
 		.matches = test_has_xint,
 		.cpu_enable = xint_enable,
+	},
+#endif
+#ifdef CONFIG_ARCH_SUPPORTS_XCALL
+	{
+		.desc = "Hardware xcall Support",
+		.capability = ARM64_HAS_HW_XCALL,
+		.type = ARM64_CPUCAP_SYSTEM_FEATURE,
+		.matches = test_has_xcall,
+		.cpu_enable = xcall_enable,
 	},
 #endif
 	{},
