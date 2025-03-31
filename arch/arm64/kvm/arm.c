0264cdd7bcaf3baf24e099d31dc09694a9561629
Jing Zhang <jingzhangos@google.com>
2025-03-31 10:13:10 +0800

    KVM: arm64: Allow userspace to get the writable masks for feature ID registers
    mainline inclusion
    from mainline-v6.7-rc1
    commit 3f9cd0ca848413fd368278310d2cdd6c2bef48b2
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBN3WI
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?h=v6.14-rc3&id=3f9cd0ca848413fd368278310d2cdd6c2bef48b2
    
    ----------------------------------------------------------------------
    
    While the Feature ID range is well defined and pretty large, it isn't
    inconceivable that the architecture will eventually grow some other
    ranges that will need to similarly be described to userspace.
    
    Add a VM ioctl to allow userspace to get writable masks for feature ID
    registers in below system register space:
    op0 = 3, op1 = {0, 1, 3}, CRn = 0, CRm = {0 - 7}, op2 = {0 - 7}
    This is used to support mix-and-match userspace and kernels for writable
    ID registers, where userspace may want to know upfront whether it can
    actually tweak the contents of an idreg or not.
    
    Add a new capability (KVM_CAP_ARM_SUPPORTED_FEATURE_ID_RANGES) that
    returns a bitmap of the valid ranges, which can subsequently be
    retrieved, one at a time by setting the index of the set bit as the
    range identifier.
    
    Suggested-by: Marc Zyngier <maz@kernel.org>
    Suggested-by: Cornelia Huck <cohuck@redhat.com>
    Signed-off-by: Jing Zhang <jingzhangos@google.com>
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    Reviewed-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20231003230408.3405722-2-oliver.upton@linux.dev
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -436,6 +436,9 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 		r = static_key_enabled(&virtcca_cvm_is_available);
 		break;
 #endif
+	case KVM_CAP_ARM_SUPPORTED_REG_MASK_RANGES:
+		r = BIT(0);
+		break;
 	default:
 		r = 0;
 	}
@@ -1897,6 +1900,13 @@ int kvm_arch_vm_ioctl(struct file *filp, unsigned int ioctl, unsigned long arg)
 		return 0;
 	}
 #endif
+	case KVM_ARM_GET_REG_WRITABLE_MASKS: {
+		struct reg_mask_range range;
+
+		if (copy_from_user(&range, argp, sizeof(range)))
+			return -EFAULT;
+		return kvm_vm_ioctl_get_reg_writable_masks(kvm, &range);
+	}
 	default:
 		return -EINVAL;
 	}
