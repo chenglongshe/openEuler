f12cf7f6c4a28ed07b2c94190cd57bcc52482882
Vishal Moola (Oracle) <vishal.moola@gmail.com>
2025-03-31 11:18:32 +0800

    hugetlb: check for anon_vma prior to folio allocation
    mainline inclusion
    from mainline-v6.9-rc6
    commit 37641efaa3faa4b8292aba4bbd7d71c0b703a239
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBMW50
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=37641efaa3faa4b8292aba4bbd7d71c0b703a239
    
    --------------------------------
    
    Commit 9acad7ba3e25 ("hugetlb: use vmf_anon_prepare() instead of
    anon_vma_prepare()") may bailout after allocating a folio if we do not
    hold the mmap lock.  When this occurs, vmf_anon_prepare() will release the
    vma lock.  Hugetlb then attempts to call restore_reserve_on_error(), which
    depends on the vma lock being held.
    
    We can move vmf_anon_prepare() prior to the folio allocation in order to
    avoid calling restore_reserve_on_error() without the vma lock.
    
    Link: https://lkml.kernel.org/r/ZiFqSrSRLhIV91og@fedora
    Fixes: 9acad7ba3e25 ("hugetlb: use vmf_anon_prepare() instead of anon_vma_prepare()")
    Reported-by: syzbot+ad1b592fc4483655438b@syzkaller.appspotmail.com
    Signed-off-by: Vishal Moola (Oracle) <vishal.moola@gmail.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Jiaming Sun <dev01402@linx-info.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -6112,6 +6112,12 @@ static vm_fault_t hugetlb_no_page(struct mm_struct *mm,
 							VM_UFFD_MISSING);
 		}
 
+		if (!(vma->vm_flags & VM_MAYSHARE)) {
+			ret = vmf_anon_prepare(vmf);
+			if (unlikely(ret))
+				goto out;
+		}
+
 		folio = alloc_hugetlb_folio(vma, haddr, 0);
 		if (IS_ERR(folio)) {
 			/*
@@ -6148,15 +6154,12 @@ static vm_fault_t hugetlb_no_page(struct mm_struct *mm,
 				 */
 				restore_reserve_on_error(h, vma, haddr, folio);
 				folio_put(folio);
+				ret = VM_FAULT_SIGBUS;
 				goto out;
 			}
 			new_pagecache_folio = true;
 		} else {
 			folio_lock(folio);
-
-			ret = vmf_anon_prepare(vmf);
-			if (unlikely(ret))
-				goto backout_unlocked;
 			anon_rmap = 1;
 		}
 	} else {
