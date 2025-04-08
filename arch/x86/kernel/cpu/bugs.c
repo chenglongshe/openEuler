4011a4adb9aa788ffe14cc232e4d17ed2e4560f5
Josh Poimboeuf <jpoimboe@kernel.org>
2025-04-08 14:47:30 -0700

    x86/bugs: Rename entry_ibpb() to write_ibpb()
    mainline inclusion
    from mainline-
    commit 13235d6d50bba99931c4392c0f813cfae0de3eac
    category: feature
    bugzilla:
    CVE: NA
    
    Reference: https://github.com/torvalds/linux/commit/13235d6d50bba99931c4392c0f813cfae0de3eac
    
    --------------------------------
    
    commit 13235d6d50bba99931c4392c0f813cfae0de3eac upstream
    
    There's nothing entry-specific about entry_ibpb().  In preparation for
    calling it from elsewhere, rename it to write_ibpb().
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/1e54ace131e79b760de3fe828264e26d0896e3ac.1744148254.git.jpoimboe@kernel.org
    Signed-off-by: chaithanyaLagisetty <LagisettyVenkata.NagaChaitanya@amd.com>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -1125,7 +1125,7 @@ do_cmd_auto:
 		setup_clear_cpu_cap(X86_FEATURE_RETHUNK);
 
 		/*
-		 * There is no need for RSB filling: entry_ibpb() ensures
+		 * There is no need for RSB filling: write_ibpb() ensures
 		 * all predictions, including the RSB, are invalidated,
 		 * regardless of IBPB implementation.
 		 */
@@ -2637,7 +2637,7 @@ static void __init srso_select_mitigation(void)
 				setup_clear_cpu_cap(X86_FEATURE_RETHUNK);
 
 				/*
-				 * There is no need for RSB filling: entry_ibpb() ensures
+				 * There is no need for RSB filling: write_ibpb() ensures
 				 * all predictions, including the RSB, are invalidated,
 				 * regardless of IBPB implementation.
 				 */
@@ -2656,7 +2656,7 @@ static void __init srso_select_mitigation(void)
 				srso_mitigation = SRSO_MITIGATION_IBPB_ON_VMEXIT;
 
 				/*
-				 * There is no need for RSB filling: entry_ibpb() ensures
+				 * There is no need for RSB filling: write_ibpb() ensures
 				 * all predictions, including the RSB, are invalidated,
 				 * regardless of IBPB implementation.
 				 */
