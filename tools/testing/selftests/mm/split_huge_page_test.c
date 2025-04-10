33ad9f2117da49f0802533dc3012a1e086723434
Zi Yan <ziy@nvidia.com>
2025-04-10 13:49:24 +0800

    selftests/mm: test splitting file-backed THP to any lower order
    mainline inclusion
    from mainline-v6.14-rc6
    commit ad4c9bb5415232b452157002a48d58a1dc92d3c0
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBV6VZ
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=ad4c9bb5415232b452157002a48d58a1dc92d3c0
    
    --------------------------------
    
    selftests/mm: test splitting file-backed THP to any lower order
    Now split_huge_page*() supports shmem THP split to any lower order.  Test
    it.
    
    The test now reads file content out after split to check if the split
    corrupts the file data.
    
    Link: https://lkml.kernel.org/r/20250122161928.1240637-3-ziy@nvidia.com
    Signed-off-by: Zi Yan <ziy@nvidia.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Tested-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: John Hubbard <jhubbard@nvidia.com>
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Kirill A. Shuemov <kirill.shutemov@linux.intel.com>
    Cc: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Yang Shi <yang@os.amperecomputing.com>
    Cc: Yu Zhao <yuzhao@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Conflicts:
       tools/testing/selftests/mm/split_huge_page_test.c
    [Context conflicts in split_huge_page_test.c due to miss commit dfe61db4a132]
    Signed-off-by: Wang Lian <dev01404@linx-info.com>

--- tools/testing/selftests/mm/split_huge_page_test.c
+++ tools/testing/selftests/mm/split_huge_page_test.c
@@ -201,7 +201,7 @@ void split_pte_mapped_thp(void)
 	close(kpageflags_fd);
 }
 
-void split_file_backed_thp(void)
+void split_file_backed_thp(int order)
 {
 	int status;
 	int fd;
@@ -253,7 +253,7 @@ void split_file_backed_thp(void)
 	}
 
 	/* split the file-backed THP */
-	write_debugfs(PATH_FMT, testfile, pgoff_start, pgoff_end, 0);
+	write_debugfs(PATH_FMT, testfile, pgoff_start, pgoff_end, order);
 
 	/* check file content after split */
 	status = lseek(fd, 0, SEEK_SET);
@@ -291,7 +291,7 @@ void split_file_backed_thp(void)
 		ksft_exit_fail_msg("cannot remove tmp dir: %s\n", strerror(errno));
 
 	ksft_print_msg("Please check dmesg for more information\n");
-	ksft_test_result_pass("File-backed THP split test done\n");
+	ksft_test_result_pass("File-backed THP split to order %d test done\n", order);
 	return;
 
 close_file:
@@ -456,7 +456,7 @@ int main(int argc, char **argv)
 	if (argc > 1)
 		optional_xfs_path = argv[1];
 
-	ksft_set_plan(3+9);
+	ksft_set_plan(1+8+1+9+9);
 
 	pagesize = getpagesize();
 	pageshift = ffs(pagesize) - 1;
@@ -468,7 +468,8 @@ int main(int argc, char **argv)
 
 	split_pmd_thp();
 	split_pte_mapped_thp();
-	split_file_backed_thp();
+	for (i = 0; i < 9; i++)
+		split_file_backed_thp(i);
 
 	created_tmp = prepare_thp_fs(optional_xfs_path, fs_loc_template,
 			&fs_loc);
