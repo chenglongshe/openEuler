e46713f465a8f92742a5778bd680274d72642fc3
Frank Sae <Frank.Sae@motor-comm.com>
2025-04-10 11:19:00 +0800

    yt6801: Implement the poll functions
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBFE17
    CVE: NA
    
    --------------------------------
    
    Implement the fxgmac_request_irqs to request legacy or msix irqs, msix
      irqs.
    Implement the fxgmac_create_skb to  create skb for rx.
    Implement the fxgmac_isr function to handle legacy irq.
    Implement the fxgmac_dma_isr function to handle tx and rx irq.
    Implement the fxgmac_all_poll for legacy irq.
    Implement the fxgmac_one_poll_rx and fxgmac_one_poll_tx for msix irq.
    Implement the fxgmac_tx_poll and fxgmac_rx_poll to handle tx and rx.
    
    Signed-off-by: Frank Sae <Frank.Sae@motor-comm.com>

--- drivers/net/ethernet/motorcomm/yt6801/yt6801_desc.h
+++ drivers/net/ethernet/motorcomm/yt6801/yt6801_desc.h
@@ -13,9 +13,23 @@
 #define FXGMAC_GET_DESC_DATA(ring, idx)	((ring)->desc_data_head + (idx))
 #define FXGMAC_GET_ENTRY(x, size)	(((x) + 1) & ((size) - 1))
 
+void fxgmac_desc_tx_reset(struct fxgmac_desc_data *desc_data);
+void fxgmac_desc_rx_reset(struct fxgmac_desc_data *desc_data);
 void fxgmac_desc_data_unmap(struct fxgmac_pdata *priv,
 			    struct fxgmac_desc_data *desc_data);
 
 int fxgmac_channels_rings_alloc(struct fxgmac_pdata *priv);
 void fxgmac_channels_rings_free(struct fxgmac_pdata *priv);
+int fxgmac_tx_skb_map(struct fxgmac_channel *channel, struct sk_buff *skb);
+int fxgmac_rx_buffe_map(struct fxgmac_pdata *priv, struct fxgmac_ring *ring,
+			struct fxgmac_desc_data *desc_data);
+void fxgmac_dump_tx_desc(struct fxgmac_pdata *priv, struct fxgmac_ring *ring,
+			 unsigned int idx, unsigned int count,
+			 unsigned int flag);
+void fxgmac_dump_rx_desc(struct fxgmac_pdata *priv, struct fxgmac_ring *ring,
+			 unsigned int idx);
+
+int fxgmac_is_tx_complete(struct fxgmac_dma_desc *dma_desc);
+int fxgmac_is_last_desc(struct fxgmac_dma_desc *dma_desc);
+
 #endif /* YT6801_DESC_H */
