100265ccd184f1441909f3d9919944d85778bdc8
Jinqian Yang <yangjinqian1@huawei.com>
2025-04-15 14:24:57 +0800

    kvm: arm64: avoid sending multi-SGIs in IPIV
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBPH85
    
    ------------------------------------------------------------------------
    
    IPIV optimizes unicast scenarios and does not support multicast or
    broadcast. To prevent GuestOS from generating multicast and broadcast,
    the MPIDR  of vCPUs is modified to ensure that the [aff3, aff2, aff1]
    fields are uniquely assigned for each vCPU within a virtual machine,
    while all aff0 fields are set to zero. This configuration guarantees
    the uniqueness of vCPU affinity identifiers at the architecture level,
    thereby suppressing the generation of SGI multicast and broadcast
    signals by the GuestOS.
    
    Signed-off-by: Jinqian Yang <yangjinqian1@huawei.com>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -711,6 +711,8 @@ static u64 reset_actlr(struct kvm_vcpu *vcpu, const struct sys_reg_desc *r)
 	return actlr;
 }
 
+extern struct static_key_false ipiv_enable;
+
 static u64 reset_mpidr(struct kvm_vcpu *vcpu, const struct sys_reg_desc *r)
 {
 	u64 mpidr;
@@ -725,6 +727,15 @@ static u64 reset_mpidr(struct kvm_vcpu *vcpu, const struct sys_reg_desc *r)
 	mpidr = (vcpu->vcpu_id & 0x0f) << MPIDR_LEVEL_SHIFT(0);
 	mpidr |= ((vcpu->vcpu_id >> 4) & 0xff) << MPIDR_LEVEL_SHIFT(1);
 	mpidr |= ((vcpu->vcpu_id >> 12) & 0xff) << MPIDR_LEVEL_SHIFT(2);
+
+	if (static_branch_unlikely(&ipiv_enable)) {
+		/*
+		 * To avoid sending multi-SGIs in guest OS, make aff1/aff2 unique
+		 */
+		mpidr = (vcpu->vcpu_id & 0x0f) << MPIDR_LEVEL_SHIFT(1);
+		mpidr |= ((vcpu->vcpu_id >> 4) & 0xff) << MPIDR_LEVEL_SHIFT(2);
+	}
+
 	mpidr |= (1ULL << 31);
 	vcpu_write_sys_reg(vcpu, mpidr, MPIDR_EL1);
 
