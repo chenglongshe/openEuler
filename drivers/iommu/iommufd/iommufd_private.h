ff28ec9682a2c4e420983ba49e65b6a28a7d2d80
Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
2025-04-18 17:28:12 +0800

    iommufd: Associate kvm pointer to iommufd ctx
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBMHO0
    CVE: NA
    
    --------------------------------
    
    Introduce an API to set the KVM pointer to the iommufd ctx
    and set the same when a vfio dev is bind to iommufd.
    
    Signed-off-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>
    Signed-off-by: lishusen <lishusen2@huawei.com>

--- drivers/iommu/iommufd/iommufd_private.h
+++ drivers/iommu/iommufd/iommufd_private.h
@@ -18,6 +18,7 @@ struct iommu_domain;
 struct iommu_group;
 struct iommu_option;
 struct iommufd_device;
+struct kvm;
 
 struct iommufd_ctx {
 	struct file *file;
@@ -29,6 +30,8 @@ struct iommufd_ctx {
 	/* Compatibility with VFIO no iommu */
 	u8 no_iommu_mode;
 	struct iommufd_ioas *vfio_ioas;
+	/* Associated KVM pointer */
+	struct kvm *kvm;
 };
 
 /*
