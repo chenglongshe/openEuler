d93b5c33a68e8ab25a270ddf70ef545b1a0ec59a
Zeng Heng <zengheng4@huawei.com>
2025-04-21 11:15:15 +0800

    fs/resctrl: Introduce the resctrl_feat_type
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8T2RT
    
    --------------------------------
    
    In order to expand the new features of MPAM in the following patches, enum
    resctrl_feat_type is newly added.
    
    When the configuration is updated, the CDP type and configuration feature
    will be traversed and updated, so the staged_config array used to deliver
    the configuration is expanded into a two-dimensional array.
    
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>

--- arch/x86/include/asm/resctrl.h
+++ arch/x86/include/asm/resctrl.h
@@ -223,6 +223,29 @@ void resctrl_cpu_detect(struct cpuinfo_x86 *c);
 bool resctrl_arch_get_cdp_enabled(enum resctrl_res_level l);
 int resctrl_arch_set_cdp_enabled(enum resctrl_res_level l, bool enable);
 
+static inline bool resctrl_arch_feat_capable(enum resctrl_res_level level,
+					     enum resctrl_feat_type feat)
+{
+	if (feat == FEAT_PBM) {
+		if (level == RDT_RESOURCE_L3 ||
+		    level == RDT_RESOURCE_L2)
+			return true;
+
+	} else if (feat == FEAT_MAX) {
+		if (level == RDT_RESOURCE_MBA ||
+		    level == RDT_RESOURCE_SMBA)
+			return true;
+	}
+
+	return false;
+}
+
+static inline const char *resctrl_arch_set_feat_lab(enum resctrl_feat_type feat,
+						    unsigned long fflags)
+{
+	return "";
+}
+
 #else
 
 static inline void resctrl_sched_in(struct task_struct *tsk) {}
