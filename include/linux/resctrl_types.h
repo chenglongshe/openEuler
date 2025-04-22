ab8dd6cb119267d4c8b6cfddacc1798bf59337e2
Zeng Heng <zengheng4@huawei.com>
2025-04-22 21:27:24 +0800

    arm64/mpam: Add limit feature
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC35SV
    
    --------------------------------
    
    Allow users can configure the mata hard-limit feature and cache soft-limit
    feature by the schemata {MB,L3}HDL:$domain_id=$value interface under the
    resctrl fs.
    
    When MAX bandwidth is exceeded, the hard-limit feature not allows the
    partition use any more bandwidth until the memory bandwidth measurement
    for the partition falls below MAX.
    
    When CMAX cache capacity is exceeded, the soft-limit allows the partition
    to allocate capacity beyond CMAX, but only from invalid lines or lines
    belonging to disabled PARTIDs.
    
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>

--- include/linux/resctrl_types.h
+++ include/linux/resctrl_types.h
@@ -80,12 +80,16 @@ enum resctrl_conf_type {
  * @FEAT_MAX:	Controls the maximum fraction of the resource capacity.
  * @FEAT_MIN:	Controls the minimum fraction of the resource capacity.
  * @FEAT_INTPRI:Controls the priority for the resource.
+ * @FEAT_LIMIT: Allows some allocations by a PARTID when its resource
+ *		use is above the maximum capacity partition.
  */
 enum resctrl_feat_type {
 	FEAT_PBM,
 	FEAT_MAX,
 	FEAT_MIN,
 	FEAT_INTPRI,
+	FEAT_LIMIT,
+
 	FEAT_NUM_TYPES,
 };
 
