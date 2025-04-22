37b41fe5ad699e6ca4ce03b155af5875ad76587a
Zeng Heng <zengheng4@huawei.com>
2025-04-22 21:27:21 +0800

    arm64/mpam: Add PRIO feature
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC35SV
    
    --------------------------------
    
    The Priority Partition feature controls the priority of requests
    attributed to the target PARTID.
    
    In addition to the MATA msc implementing priority features, the L3
    controller can also have the same features. Here the *MBPRI* and *L3PRI*
    interface are provided in the schemata file to allow users to configure
    and query QoS partition settings.
    
    Before transmitting the priority value to msc, it needs to check whether
    the setting from user is validate or not.
    
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>

--- drivers/platform/mpam/mpam_resctrl.c
+++ drivers/platform/mpam/mpam_resctrl.c
@@ -70,6 +70,7 @@ bool resctrl_arch_mon_capable(void)
 }
 
 static bool min_capable[RDT_NUM_RESOURCES];
+static bool intpri_capable[RDT_NUM_RESOURCES];
 bool resctrl_arch_feat_capable(enum resctrl_res_level level,
 			       enum resctrl_feat_type feat)
 {
@@ -87,6 +88,9 @@ bool resctrl_arch_feat_capable(enum resctrl_res_level level,
 	case FEAT_MIN:
 		return min_capable[level];
 
+	case FEAT_INTPRI:
+		return intpri_capable[level];
+
 	default:
 		break;
 	}
@@ -100,6 +104,9 @@ const char *resctrl_arch_get_feat_lab(enum resctrl_feat_type feat,
 	switch (feat) {
 	case FEAT_MIN:
 		return "MIN";
+
+	case FEAT_INTPRI:
+		return "PRI";
 
 	default:
 		break;
@@ -824,6 +831,7 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		/* TODO: Scaling is not yet supported */
 		r->cache.cbm_len = class->props.cpbm_wd;
 		r->cache.arch_has_sparse_bitmasks = true;
+		r->cache.intpri_wd = class->props.intpri_wd;
 
 		/* mpam_devices will reject empty bitmaps */
 		r->cache.min_cbm_bits = 1;
@@ -849,6 +857,9 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 
 		if (mpam_has_feature(mpam_feat_cmin, &class->props))
 			min_capable[r->rid] = true;
+
+		if (mpam_has_feature(mpam_feat_intpri_part, &class->props))
+			intpri_capable[r->rid] = true;
 
 		/*
 		 * MBWU counters may be 'local' or 'total' depending on where
@@ -881,6 +892,7 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		r->membw.delay_linear = true;
 		r->membw.throttle_mode = THREAD_THROTTLE_UNDEFINED;
 		r->membw.bw_gran = get_mba_granularity(cprops);
+		r->membw.intpri_wd = class->props.intpri_wd;
 
 		/* Round up to at least 1% */
 		if (!r->membw.bw_gran)
@@ -893,6 +905,9 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 
 		if (mpam_has_feature(mpam_feat_mbw_min, cprops))
 			min_capable[r->rid] = true;
+
+		if (mpam_has_feature(mpam_feat_intpri_part, cprops))
+			intpri_capable[r->rid] = true;
 
 		if (has_mbwu && class->type == MPAM_CLASS_MEMORY) {
 			mbm_total_class = class;
@@ -1012,6 +1027,10 @@ u32 resctrl_arch_get_config(struct rdt_resource *r, struct rdt_domain *d,
 			  (feat == FEAT_MIN)) {
 			configured_by = mpam_feat_cmin;
 			break;
+		} else if (mpam_has_feature(mpam_feat_intpri_part, cprops) &&
+			  (feat == FEAT_INTPRI)) {
+			configured_by = mpam_feat_intpri_part;
+			break;
 		}
 		return -EINVAL;
 
@@ -1028,6 +1047,10 @@ u32 resctrl_arch_get_config(struct rdt_resource *r, struct rdt_domain *d,
 			  (feat == FEAT_MIN)) {
 			configured_by = mpam_feat_mbw_min;
 			break;
+		} else if (mpam_has_feature(mpam_feat_intpri_part, cprops) &&
+			  (feat == FEAT_INTPRI)) {
+			configured_by = mpam_feat_intpri_part;
+			break;
 		}
 		fallthrough;
 	default:
@@ -1036,11 +1059,16 @@ u32 resctrl_arch_get_config(struct rdt_resource *r, struct rdt_domain *d,
 
 	if (!r->alloc_capable || partid >= resctrl_arch_get_num_closid(r) ||
 	    !mpam_has_feature(configured_by, cfg)) {
-		if (configured_by == mpam_feat_cmin)
+		if ((configured_by == mpam_feat_cmin) ||
+		    (configured_by == mpam_feat_mbw_min))
 			return 0;
 
-		if (configured_by == mpam_feat_mbw_min)
-			return 0;
+		if (configured_by == mpam_feat_intpri_part) {
+			if (!mpam_has_feature(mpam_feat_intpri_part_0_low, cprops))
+				return 0;
+
+			return (u32)GENMASK(cprops->intpri_wd - 1, 0);
+		}
 
 		return r->default_ctrl;
 	}
@@ -1058,6 +1086,8 @@ u32 resctrl_arch_get_config(struct rdt_resource *r, struct rdt_domain *d,
 		return mbw_max_to_percent(cfg->mbw_max, cprops->bwa_wd);
 	case mpam_feat_mbw_min:
 		return mbw_max_to_percent(cfg->mbw_min, cprops->bwa_wd);
+	case mpam_feat_intpri_part:
+		return cfg->intpri;
 	default:
 		return -EINVAL;
 	}
@@ -1103,6 +1133,11 @@ int resctrl_arch_update_one(struct rdt_resource *r, struct rdt_domain *d, u32 cl
 			cfg.ca_min = percent_to_mbw_max(cfg_val, cprops->cmax_wd);
 			mpam_set_feature(mpam_feat_cmin, &cfg);
 			break;
+		} else if (mpam_has_feature(mpam_feat_intpri_part, cprops) &&
+			  (f == FEAT_INTPRI)) {
+			cfg.intpri = cfg_val;
+			mpam_set_feature(mpam_feat_intpri_part, &cfg);
+			break;
 		}
 		return -EINVAL;
 
@@ -1121,7 +1156,13 @@ int resctrl_arch_update_one(struct rdt_resource *r, struct rdt_domain *d, u32 cl
 			cfg.mbw_min = percent_to_mbw_max(cfg_val, cprops->bwa_wd);
 			mpam_set_feature(mpam_feat_mbw_min, &cfg);
 			break;
+		} else if (mpam_has_feature(mpam_feat_intpri_part, cprops) &&
+			  (f == FEAT_INTPRI)) {
+			cfg.intpri = cfg_val;
+			mpam_set_feature(mpam_feat_intpri_part, &cfg);
+			break;
 		}
+
 		fallthrough;
 	default:
 		return -EINVAL;
