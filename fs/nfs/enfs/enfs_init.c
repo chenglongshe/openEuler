66e5d55a0550c21640617c203ef824e10aec6621
闫海涛 <yanhaitao2@huawei.com>
2025-04-27 21:22:16 +0800

    调整目录

--- fs/nfs/enfs/enfs_init.c
+++ fs/nfs/enfs/enfs_init.c
@@ -1,11 +1,3 @@
-#include
-#include
-#include
-#include
-#include
-#include
-#include
-#include
 #include "enfs.h"
 #include "enfs_multipath_parse.h"
 #include "enfs_multipath_client.h"
@@ -21,18 +13,13 @@ MODULE_PARM_DESC(enfs_debug, "enfs debugging mask");
 struct enfs_adapter_ops enfs_adapter = {
 	.name = "enfs",
 	.owner = THIS_MODULE,
-	//.alloc_mount_option      = nfs_multipath_alloc_options,
 	.parse_mount_options = nfs_multipath_parse_options,
 	.free_mount_options = nfs_multipath_free_options,
-	//.dup_mount_options       = nfs_multipath_dup_options,
 	.client_info_init = nfs_multipath_client_info_init,
 	.client_info_free = nfs_multipath_client_info_free,
 	.client_info_match = nfs_multipath_client_info_match,
 	.nfs4_client_info_match = nfs4_multipath_client_info_match,
 	.client_info_show = nfs_multipath_client_info_show,
-	// .client_info_clone       = nfs_multipath_client_info_clone,
-	// .get_best_conn           = nfs_multipath_get_best_conn,
-	// .conn_set_unavailable    = nfs_multipath_set_conn_disconnect,
 	.remount_ip_list = enfs_remount,
 	.set_mount_data = enfs_set_mount_data,
 	.trigger_get_capability = enfs_trigger_get_capability,
