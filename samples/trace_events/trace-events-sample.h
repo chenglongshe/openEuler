1a601108d642786f11edd83f2f3739ab1797c61e
Steven Rostedt <rostedt@goodmis.org>
2025-04-27 14:25:37 +0000

    tracing: Verify event formats that have "%*p.."
    mainline inclusion
    from mainline-v6.15-rc1
    commit ea8d7647f9ddf1f81e2027ed305299797299aa03
    category: bugfix
    bugzilla: 190584
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=ea8d7647f9dd
    
    --------------------------------
    
    The trace event verifier checks the formats of trace events to make sure
    that they do not point at memory that is not in the trace event itself or
    in data that will never be freed. If an event references data that was
    allocated when the event triggered and that same data is freed before the
    event is read, then the kernel can crash by reading freed memory.
    
    The verifier runs at boot up (or module load) and scans the print formats
    of the events and checks their arguments to make sure that dereferenced
    pointers are safe. If the format uses "%*p.." the verifier will ignore it,
    and that could be dangerous. Cover this case as well.
    
    Also add to the sample code a use case of "%*pbl".
    
    Link: https://lore.kernel.org/all/bcba4d76-2c3f-4d11-baf0-02905db953dd@oracle.com/
    
    Cc: stable@vger.kernel.org
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Fixes: 5013f454a352c ("tracing: Add check of trace event print fmts for dereferencing pointers")
    Link: https://lore.kernel.org/20250327195311.2d89ec66@gandalf.local.home
    Reported-by: Libo Chen <libo.chen@oracle.com>
    Reviewed-by: Libo Chen <libo.chen@oracle.com>
    Tested-by: Libo Chen <libo.chen@oracle.com>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Conflicts:
    	kernel/trace/trace_events.c
    	samples/trace_events/trace-events-sample.h
    [The conflicts were due to some minor issues]
    Signed-off-by: Pu Lehui <pulehui@huawei.com>

--- samples/trace_events/trace-events-sample.h
+++ samples/trace_events/trace-events-sample.h
@@ -315,7 +315,8 @@ TRACE_EVENT(foo_bar,
 		__assign_cpumask(cpum, cpumask_bits(mask));
 	),
 
-	TP_printk("foo %s %d %s %s %s %s (%s) (%s) %s", __entry->foo, __entry->bar,
+	TP_printk("foo %s %d %s %s %s %s (%s) (%s) %s [%d] %*pbl",
+		  __entry->foo, __entry->bar,
 
 /*
  * Notice here the use of some helper functions. This includes:
@@ -360,7 +361,10 @@ TRACE_EVENT(foo_bar,
 				__get_dynamic_array_len(list) / sizeof(int),
 				sizeof(int)),
 		  __get_str(str), __get_bitmask(cpus), __get_cpumask(cpum),
-		  __get_str(vstr))
+		  __get_str(vstr),
+		  __get_dynamic_array_len(cpus),
+		  __get_dynamic_array_len(cpus),
+		  __get_dynamic_array(cpus))
 );
 
 /*
