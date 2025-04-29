intel-lab-lkp/linux

jump_label,module: Don't alloc static_key_mod for __ro_after_init keys
When a static_key is marked ro_after_init, its state will never change
(after init), therefore jump_label_update() will never need to iterate
the entries, and thus module load won't actually need to track this --
avoiding the static_key::next write.

Therefore, mark these keys such that jump_label_add_module() might
recognise them and avoid the modification.

Use the special state: 'static_key_linked(key) && !static_key_mod(key)'
to denote such keys.

jump_label_add_module() does not exist under CONFIG_JUMP_LABEL=n, so the
newly-introduced jump_label_ro() can be defined as a nop for that
configuration.

Link: http://lore.kernel.org/r/20230705204142.GB2813335@hirez.programming.kicks-ass.net
Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
[Added comments and build fix]
Signed-off-by: Valentin Schneider <vschneid@redhat.com>
Acked-by: Josh Poimboeuf <jpoimboe@kernel.org>

include/linux/jump_label.h
@@ -216,6 +216,7 @@ extern struct jump_entry __start___jump_table[];
 extern struct jump_entry __stop___jump_table[];
 
 extern void jump_label_init(void);
+extern void jump_label_ro(void);
 extern void jump_label_lock(void);
 extern void jump_label_unlock(void);
 extern void arch_jump_label_transform(struct jump_entry *entry,
@@ -264,6 +265,8 @@ static __always_inline void jump_label_init(void)
 {
 	static_key_initialized = true;
 }
+
+static __always_inline void jump_label_ro(void) { }
 
 static __always_inline bool static_key_false(struct static_key *key)
 {

chenglongshe/openEuler

mainline inclusion
from mainline-v6.10-rc1
commit 91a1d97ef482c1e4c9d4c1c656a53b0f6b16d0ed
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IBWQ24
Reference: https://github.com/torvalds/linux/commit/91a1d97ef482c1e4c9d4c1c656a53b0f6b16d0ed
--------------------------------
[ Upstream commit 91a1d97ef482c1e4c9d4c1c656a53b0f6b16d0ed ]
newly-introduced jump_label_init_ro() can be defined as a nop for that
[ mingo: Renamed jump_label_ro() to jump_label_init_ro() ]
Signed-off-by: Ingo Molnar <mingo@kernel.org>
Link: https://lore.kernel.org/r/20240313180106.2917308-2-vschneid@redhat.com
Signed-off-by: Yiwei Zhuang <zhuangyiwei@huawei.com>

include/linux/jump_label.h
@@ -216,6 +216,7 @@ extern struct jump_entry __start___jump_table[];
 extern struct jump_entry __stop___jump_table[];
 
 extern void jump_label_init(void);
+extern void jump_label_init_ro(void);
 extern void jump_label_lock(void);
 extern void jump_label_unlock(void);
 extern void arch_jump_label_transform(struct jump_entry *entry,
@@ -267,6 +268,8 @@ static __always_inline void jump_label_init(void)
 {
 	static_key_initialized = true;
 }
+
+static __always_inline void jump_label_init_ro(void) { }
 
 static __always_inline bool static_key_false(struct static_key *key)
 {