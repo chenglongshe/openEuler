1dbbc572ee63b536d06f4f3984b6fa9bc99a8557
Amir Goldstein <amir73il@gmail.com>
2025-05-08 08:59:51 +0800

    fuse: implement read/write passthrough
    mainline inclusion
    from mainline-v6.9-rc1
    commit 57e1176e6086673d31bf0a0dc58e144c8e65e589
    category: feature
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/IBHLU4
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=57e1176e6086673d31bf0a0dc58e144c8e65e589
    
    --------------------------------
    
    Use the backing file read/write helpers to implement read/write
    passthrough to a backing file.
    
    After read/write, we invalidate a/c/mtime/size attributes.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Yifan Qiao <qiaoyifan4@huawei.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1477,4 +1477,7 @@ static inline struct file *fuse_file_passthrough(struct fuse_file *ff)
 #endif
 }
 
+ssize_t fuse_passthrough_read_iter(struct kiocb *iocb, struct iov_iter *iter);
+ssize_t fuse_passthrough_write_iter(struct kiocb *iocb, struct iov_iter *iter);
+
 #endif /* _FS_FUSE_I_H */
