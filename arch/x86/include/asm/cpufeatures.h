openeuler-mirror/kernel

commit a9efa75d809979bc1daf3ec0169be74a29dfa8ed

x86/cpufeatures: Fix abi breakage caused by NCAPINTS in cpufeature header file.
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/src-openeuler/kernel/issues/I7RQ67

--------------------------------

Fix abi breakage according to the previous solution:

commit ac376dd8c1af ("x86/cpufeatures: Fix abi breakage
caused by NCAPINTS in cpufeature header file.")

Signed-off-by: Jialin Zhang <zhangjialin11@huawei.com>
Signed-off-by: Yanan Wang <wangyanan55@huawei.com>
Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>

arch/x86/include/asm/cpufeatures.h
@@ -13,7 +13,7 @@
 /*
  * Defines x86 CPU feature bits
  */
-#define NCAPINTS			20	   /* N 32-bit words worth of info */
+#define NCAPINTS			19	   /* N 32-bit words worth of info */
 #define NBUGINTS			2	   /* N 32-bit bug flags */
 
 /*
@@ -409,6 +409,11 @@
 #define X86_FEATURE_SUCCOR		(17*32+ 1) /* Uncorrectable error containment and recovery */
 #define X86_FEATURE_SMCA		(17*32+ 3) /* Scalable MCA */
 
+/* AMD-defined SRSO vulnerability features, CPUID level 0x80000021 (EAX), word 20 */
+#define X86_FEATURE_SBPB		(17*32+24)
+#define X86_FEATURE_IBPB_BRTYPE		(17*32+25)
+#define X86_FEATURE_SRSO_NO		(17*32+26)
+
 /* AMD-defined memory encryption features, CPUID level 0x8000001f (EAX), word 19 */
 #define X86_FEATURE_SME			(17*32+ 27) /* AMD Secure Memory Encryption */
 #define X86_FEATURE_SEV			(17*32+ 28) /* AMD Secure Encrypted Virtualization */
@@ -439,10 +444,6 @@
 #define X86_FEATURE_CORE_CAPABILITIES	(18*32+30) /* "" IA32_CORE_CAPABILITIES MSR */
 #define X86_FEATURE_SPEC_CTRL_SSBD	(18*32+31) /* "" Speculative Store Bypass Disable */
 
-
-#define X86_FEATURE_SBPB		(19*32+27) /* "" Selective Branch Prediction Barrier */
-#define X86_FEATURE_IBPB_BRTYPE		(19*32+28) /* "" MSR_PRED_CMD[IBPB] flushes all branch type predictions */
-#define X86_FEATURE_SRSO_NO		(19*32+29) /* "" CPU is not affected by SRSO */
 
 /*
  * BUG word(s)

arch/x86/kernel/cpu/scattered.c
@@ -50,6 +50,9 @@ static const struct cpuid_bit cpuid_bits[] = {
 	{ X86_FEATURE_VM_PAGE_FLUSH,    CPUID_EAX,  2, 0x8000001f, 0 },
 	{ X86_FEATURE_SEV_ES,           CPUID_EAX,  3, 0x8000001f, 0 },
 	{ X86_FEATURE_SME_COHERENT,     CPUID_EAX, 10, 0x8000001f, 0 },
+	{ X86_FEATURE_SBPB,		CPUID_EAX, 27, 0x80000021, 0 },
+	{ X86_FEATURE_IBPB_BRTYPE,	CPUID_EAX, 28, 0x80000021, 0 },
+	{ X86_FEATURE_SRSO_NO,		CPUID_EAX, 29, 0x80000021, 0 },
 	{ 0, 0, 0, 0, 0 }
 };
 

arch/x86/kvm/cpuid.c
@@ -560,6 +560,10 @@ void kvm_set_cpu_caps(void)
 	    !boot_cpu_has(X86_FEATURE_AMD_SSBD))
 		kvm_cpu_cap_set(X86_FEATURE_VIRT_SSBD);
 
+	if (cpu_feature_enabled(X86_FEATURE_SBPB))
+		kvm_cpu_cap_set(X86_FEATURE_SBPB);
+	if (cpu_feature_enabled(X86_FEATURE_IBPB_BRTYPE))
+		kvm_cpu_cap_set(X86_FEATURE_IBPB_BRTYPE);
 	if (cpu_feature_enabled(X86_FEATURE_SRSO_NO))
 		kvm_cpu_cap_set(X86_FEATURE_SRSO_NO);
 

arch/x86/kvm/cpuid.h
@@ -14,6 +14,7 @@
  */
 enum kvm_only_cpuid_leafs {
 	CPUID_12_EAX	 = NCAPINTS,
+	CPUID_8000_0021_EAX,
 	NR_KVM_CPU_CAPS,
 
 	NKVMCAPINTS = NR_KVM_CPU_CAPS - NCAPINTS,
@@ -25,6 +26,11 @@ enum kvm_only_cpuid_leafs {
 #define KVM_X86_FEATURE_SGX1		KVM_X86_FEATURE(CPUID_12_EAX, 0)
 #define KVM_X86_FEATURE_SGX2		KVM_X86_FEATURE(CPUID_12_EAX, 1)
 #define KVM_X86_FEATURE_SGX_EDECCSSA	KVM_X86_FEATURE(CPUID_12_EAX, 11)
+
+/* AMD-defined SRSO vulnerability features, CPUID level 0x80000021 (EAX), word 20 */
+#define KVM_X86_FEATURE_SBPB		KVM_X86_FEATURE(CPUID_8000_0021_EAX, 27)
+#define KVM_X86_FEATURE_IBPB_BRTYPE	KVM_X86_FEATURE(CPUID_8000_0021_EAX, 28)
+#define KVM_X86_FEATURE_SRSO_NO		KVM_X86_FEATURE(CPUID_8000_0021_EAX, 29)
 
 extern u32 kvm_cpu_caps[NR_KVM_CPU_CAPS] __read_mostly;
 void kvm_set_cpu_caps(void);
@@ -118,6 +124,13 @@ static __always_inline u32 __feature_translate(int x86_feature)
 		return KVM_X86_FEATURE_SGX2;
 	else if (x86_feature == X86_FEATURE_SGX_EDECCSSA)
 		return KVM_X86_FEATURE_SGX_EDECCSSA;
+
+	if (x86_feature == X86_FEATURE_SBPB)
+		return KVM_X86_FEATURE_SBPB;
+	else if (x86_feature == X86_FEATURE_IBPB_BRTYPE)
+		return KVM_X86_FEATURE_IBPB_BRTYPE;
+	else if (x86_feature == X86_FEATURE_SRSO_NO)
+		return KVM_X86_FEATURE_SRSO_NO;
 
 	return x86_feature;
 }

arch/x86/include/asm/cpufeature.h
@@ -31,7 +31,6 @@ enum cpuid_leafs
 	CPUID_7_ECX,
 	CPUID_8000_0007_EBX,
 	CPUID_7_EDX,
-	CPUID_8000_0021_EAX,
 };
 
 #ifdef CONFIG_X86_FEATURE_NAMES
@@ -90,9 +89,8 @@ extern const char * const x86_bug_flags[NBUGINTS*32];
 	   CHECK_BIT_IN_MASK_WORD(REQUIRED_MASK, 16, feature_bit) ||	\
 	   CHECK_BIT_IN_MASK_WORD(REQUIRED_MASK, 17, feature_bit) ||	\
 	   CHECK_BIT_IN_MASK_WORD(REQUIRED_MASK, 18, feature_bit) ||	\
-	   CHECK_BIT_IN_MASK_WORD(REQUIRED_MASK, 19, feature_bit) ||	\
 	   REQUIRED_MASK_CHECK					  ||	\
-	   BUILD_BUG_ON_ZERO(NCAPINTS != 20))
+	   BUILD_BUG_ON_ZERO(NCAPINTS != 19))
 
 #define DISABLED_MASK_BIT_SET(feature_bit)				\
 	 ( CHECK_BIT_IN_MASK_WORD(DISABLED_MASK,  0, feature_bit) ||	\
@@ -114,9 +112,8 @@ extern const char * const x86_bug_flags[NBUGINTS*32];
 	   CHECK_BIT_IN_MASK_WORD(DISABLED_MASK, 16, feature_bit) ||	\
 	   CHECK_BIT_IN_MASK_WORD(DISABLED_MASK, 17, feature_bit) ||	\
 	   CHECK_BIT_IN_MASK_WORD(DISABLED_MASK, 18, feature_bit) ||	\
-	   CHECK_BIT_IN_MASK_WORD(DISABLED_MASK, 19, feature_bit) ||	\
 	   DISABLED_MASK_CHECK					  ||	\
-	   BUILD_BUG_ON_ZERO(NCAPINTS != 20))
+	   BUILD_BUG_ON_ZERO(NCAPINTS != 19))
 
 #define cpu_has(c, bit)							\
 	(__builtin_constant_p(bit) && REQUIRED_MASK_BIT_SET(bit) ? 1 :	\

arch/x86/include/asm/disabled-features.h
@@ -110,7 +110,6 @@
 			 DISABLE_ENQCMD)
 #define DISABLED_MASK17	0
 #define DISABLED_MASK18	0
-#define DISABLED_MASK19	0
-#define DISABLED_MASK_CHECK BUILD_BUG_ON_ZERO(NCAPINTS != 20)
+#define DISABLED_MASK_CHECK BUILD_BUG_ON_ZERO(NCAPINTS != 19)
 
 #endif /* _ASM_X86_DISABLED_FEATURES_H */

arch/x86/include/asm/required-features.h
@@ -101,7 +101,6 @@
 #define REQUIRED_MASK16	0
 #define REQUIRED_MASK17	0
 #define REQUIRED_MASK18	0
-#define REQUIRED_MASK19	0
-#define REQUIRED_MASK_CHECK BUILD_BUG_ON_ZERO(NCAPINTS != 20)
+#define REQUIRED_MASK_CHECK BUILD_BUG_ON_ZERO(NCAPINTS != 19)
 
 #endif /* _ASM_X86_REQUIRED_FEATURES_H */

arch/x86/kernel/cpu/common.c
@@ -963,9 +963,6 @@ void get_cpu_cap(struct cpuinfo_x86 *c)
 	if (c->extended_cpuid_level >= 0x8000000a)
 		c->x86_capability[CPUID_8000_000A_EDX] = cpuid_edx(0x8000000a);
 
-	if (c->extended_cpuid_level >= 0x80000021)
-		c->x86_capability[CPUID_8000_0021_EAX] = cpuid_eax(0x80000021);
-
 	init_scattered_cpuid_features(c);
 	init_speculation_control(c);
 

chenglongshe/openEuler

commit 9d1ac9eddaa978c5418bf76903e06686962663a6

x86/cpufeatures: reading CPUID_80000021_EAX and enable it for KVM
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IA644G
CVE: NA

--------------------------------

CPUID.80000021H:EAX.no-nested-data-bp [bit 0]
CPUID.80000021H:EAX.lfence-always-serializing [bit 2]
CPUID.80000021H:EAX.null-sel-clr-base [bit 6]
CPUID.80000021H:EAX.auto-ibrs [bit 8]

The a9efa75d8099 (x86/cpufeatures: Fix abi breakage caused by NCAPINTS in
cpufeature header file.) droping the CPUID check for
6c0893ee6540 x86/cpu, kvm: Add support for CPUID_80000021_EAX

Fixes: a9efa75d8099 (x86/cpufeatures: Fix abi breakage caused by NCAPINTS in cpufeature header file.)

Signed-off-by: Wenkuan Wang <Wenkuan.Wang@amd.com>

arch/x86/include/asm/cpufeatures.h
@@ -422,6 +422,11 @@
 #define X86_FEATURE_SMCA		(17*32+ 3) /* Scalable MCA */
 
 /* AMD-defined SRSO vulnerability features, CPUID level 0x80000021 (EAX), word 20 */
+#define X86_FEATURE_NO_NESTED_DATA_BP	(17*32+ 10) /* "" No Nested Data Breakpoints */
+#define X86_FEATURE_LFENCE_RDTSC	(17*32+ 12) /* "" LFENCE always serializing / synchronizes RDTSC */
+#define X86_FEATURE_NULL_SEL_CLR_BASE	(17*32+ 16) /* "" Null Selector Clears Base */
+#define X86_FEATURE_AUTOIBRS		(17*32+ 18) /* "" Automatic IBRS */
+#define X86_FEATURE_NO_SMM_CTL_MSR	(17*32+ 19) /* "" SMM_CTL MSR is not present */
 #define X86_FEATURE_SBPB		(17*32+24)
 #define X86_FEATURE_IBPB_BRTYPE		(17*32+25)
 #define X86_FEATURE_SRSO_NO		(17*32+26)
@@ -456,13 +461,6 @@
 #define X86_FEATURE_ARCH_CAPABILITIES	(18*32+29) /* IA32_ARCH_CAPABILITIES MSR (Intel) */
 #define X86_FEATURE_CORE_CAPABILITIES	(18*32+30) /* "" IA32_CORE_CAPABILITIES MSR */
 #define X86_FEATURE_SPEC_CTRL_SSBD	(18*32+31) /* "" Speculative Store Bypass Disable */
-
-/* AMD-defined Extended Feature 2 EAX, CPUID level 0x80000021 (EAX), word 20 */
-#define X86_FEATURE_NO_NESTED_DATA_BP	(20*32+ 0) /* "" No Nested Data Breakpoints */
-#define X86_FEATURE_LFENCE_RDTSC	(20*32+ 2) /* "" LFENCE always serializing / synchronizes RDTSC */
-#define X86_FEATURE_NULL_SEL_CLR_BASE	(20*32+ 6) /* "" Null Selector Clears Base */
-#define X86_FEATURE_AUTOIBRS		(20*32+ 8) /* "" Automatic IBRS */
-#define X86_FEATURE_NO_SMM_CTL_MSR	(20*32+ 9) /* "" SMM_CTL MSR is not present */
 
 /*
  * BUG word(s)

arch/x86/kernel/cpu/scattered.c
@@ -52,6 +52,11 @@ static const struct cpuid_bit cpuid_bits[] = {
 	{ X86_FEATURE_VM_PAGE_FLUSH,    CPUID_EAX,  2, 0x8000001f, 0 },
 	{ X86_FEATURE_SEV_ES,           CPUID_EAX,  3, 0x8000001f, 0 },
 	{ X86_FEATURE_SME_COHERENT,     CPUID_EAX, 10, 0x8000001f, 0 },
+	{ X86_FEATURE_NO_NESTED_DATA_BP,	CPUID_EAX, 0, 0x80000021, 0 },
+	{ X86_FEATURE_LFENCE_RDTSC,			CPUID_EAX, 2, 0x80000021, 0 },
+	{ X86_FEATURE_NULL_SEL_CLR_BASE,	CPUID_EAX, 6, 0x80000021, 0 },
+	{ X86_FEATURE_AUTOIBRS,				CPUID_EAX, 8, 0x80000021, 0 },
+	{ X86_FEATURE_NO_SMM_CTL_MSR,		CPUID_EAX, 9, 0x80000021, 0 },
 	{ X86_FEATURE_SBPB,		CPUID_EAX, 27, 0x80000021, 0 },
 	{ X86_FEATURE_IBPB_BRTYPE,	CPUID_EAX, 28, 0x80000021, 0 },
 	{ X86_FEATURE_SRSO_NO,		CPUID_EAX, 29, 0x80000021, 0 },

arch/x86/kvm/cpuid.c
@@ -586,6 +586,15 @@ void kvm_set_cpu_caps(void)
 	    !boot_cpu_has(X86_FEATURE_AMD_SSBD))
 		kvm_cpu_cap_set(X86_FEATURE_VIRT_SSBD);
 
+	if (boot_cpu_has(X86_FEATURE_NO_NESTED_DATA_BP))
+		kvm_cpu_cap_set(X86_FEATURE_NO_NESTED_DATA_BP);
+	if (boot_cpu_has(X86_FEATURE_LFENCE_RDTSC))
+		kvm_cpu_cap_set(X86_FEATURE_LFENCE_RDTSC);
+	if (boot_cpu_has(X86_FEATURE_NULL_SEL_CLR_BASE))
+		kvm_cpu_cap_set(X86_FEATURE_NULL_SEL_CLR_BASE);
+	if (boot_cpu_has(X86_FEATURE_AUTOIBRS))
+		kvm_cpu_cap_set(X86_FEATURE_AUTOIBRS);
+
 	if (cpu_feature_enabled(X86_FEATURE_SBPB))
 		kvm_cpu_cap_set(X86_FEATURE_SBPB);
 	if (cpu_feature_enabled(X86_FEATURE_IBPB_BRTYPE))
@@ -598,11 +607,6 @@ void kvm_set_cpu_caps(void)
 	 * features it emulates and/or exposes for L1.
 	 */
 	kvm_cpu_cap_mask(CPUID_8000_000A_EDX, 0);
-
-	kvm_cpu_cap_mask(CPUID_8000_0021_EAX,
-		F(NO_NESTED_DATA_BP) | F(LFENCE_RDTSC) | 0 /* SmmPgCfgLock */ |
-		F(NULL_SEL_CLR_BASE) | F(AUTOIBRS) | 0 /* PrefetchCtlMsr */
-	);
 
 	/*
 	 * Synthesize "LFENCE is serializing" into the AMD-defined entry in

arch/x86/kvm/cpuid.h
@@ -41,6 +41,10 @@ enum kvm_only_cpuid_leafs {
 #define KVM_X86_FEATURE_SGX_EDECCSSA	KVM_X86_FEATURE(CPUID_12_EAX, 11)
 
 /* AMD-defined SRSO vulnerability features, CPUID level 0x80000021 (EAX), word 20 */
+#define KVM_X86_FEATURE_NO_NESTED_DATA_BP	KVM_X86_FEATURE(CPUID_8000_0021_EAX, 0)
+#define KVM_X86_FEATURE_LFENCE_RDTSC		KVM_X86_FEATURE(CPUID_8000_0021_EAX, 2)
+#define KVM_X86_FEATURE_NULL_SEL_CLR_BASE	KVM_X86_FEATURE(CPUID_8000_0021_EAX, 6)
+#define KVM_X86_FEATURE_AUTOIBRS			KVM_X86_FEATURE(CPUID_8000_0021_EAX, 8)
 #define KVM_X86_FEATURE_SBPB		KVM_X86_FEATURE(CPUID_8000_0021_EAX, 27)
 #define KVM_X86_FEATURE_IBPB_BRTYPE	KVM_X86_FEATURE(CPUID_8000_0021_EAX, 28)
 #define KVM_X86_FEATURE_SRSO_NO		KVM_X86_FEATURE(CPUID_8000_0021_EAX, 29)
@@ -162,7 +166,15 @@ static __always_inline u32 __feature_translate(int x86_feature)
 	else if (x86_feature == X86_FEATURE_SGX_EDECCSSA)
 		return KVM_X86_FEATURE_SGX_EDECCSSA;
 
-	if (x86_feature == X86_FEATURE_SBPB)
+	if (x86_feature == X86_FEATURE_NO_NESTED_DATA_BP)
+		return KVM_X86_FEATURE_NO_NESTED_DATA_BP;
+	else if (x86_feature == X86_FEATURE_LFENCE_RDTSC)
+		return KVM_X86_FEATURE_LFENCE_RDTSC;
+	else if (x86_feature == X86_FEATURE_NULL_SEL_CLR_BASE)
+		return KVM_X86_FEATURE_NULL_SEL_CLR_BASE;
+	else if (x86_feature == X86_FEATURE_AUTOIBRS)
+		return KVM_X86_FEATURE_AUTOIBRS;
+	else if (x86_feature == X86_FEATURE_SBPB)
 		return KVM_X86_FEATURE_SBPB;
 	else if (x86_feature == X86_FEATURE_IBPB_BRTYPE)
 		return KVM_X86_FEATURE_IBPB_BRTYPE;