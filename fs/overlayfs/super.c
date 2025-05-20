be8b832a056ff6d04220c2bae99c3f330e1f54ce
Amir Goldstein <amir73il@gmail.com>
2025-05-20 10:52:00 +0800

    fs: factor out backing_file_{read,write}_iter() helpers
    mainline inclusion
    from mainline-v6.8-rc1
    commit a6293b3e285cd0d7692141d7981a5f144f0e2f0b
    category: feature
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/IBHLU4
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=a6293b3e285cd0d7692141d7981a5f144f0e2f0b
    
    --------------------------------
    
    Overlayfs submits files io to backing files on other filesystems.
    Factor out some common helpers to perform io to backing files, into
    fs/backing-file.c.
    
    Suggested-by: Miklos Szeredi <miklos@szeredi.hu>
    Link: https://lore.kernel.org/r/CAJfpeguhmZbjP3JLqtUy0AdWaHOkAPWeP827BBWwRFEAUgnUcQ@mail.gmail.com
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Yifan Qiao <qiaoyifan4@huawei.com>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -1556,14 +1556,10 @@ static int __init ovl_init(void)
 	if (ovl_inode_cachep == NULL)
 		return -ENOMEM;
 
-	err = ovl_aio_request_cache_init();
-	if (!err) {
-		err = register_filesystem(&ovl_fs_type);
-		if (!err)
-			return 0;
+	err = register_filesystem(&ovl_fs_type);
+	if (!err)
+		return 0;
 
-		ovl_aio_request_cache_destroy();
-	}
 	kmem_cache_destroy(ovl_inode_cachep);
 
 	return err;
@@ -1579,7 +1575,6 @@ static void __exit ovl_exit(void)
 	 */
 	rcu_barrier();
 	kmem_cache_destroy(ovl_inode_cachep);
-	ovl_aio_request_cache_destroy();
 }
 
 module_init(ovl_init);
