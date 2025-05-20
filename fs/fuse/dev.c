openeuler-mirror/kernel

commit 1fc14d09b9f01b271181b25cc434d200e220c4d3

ipc: add fast_ipc module to enable fast process switching
euleros inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IC4GP7
CVE: NA

----------------------------------------

Add the fast_ipc module to support fast switching and
 communication between processes.

Signed-off-by: chenrenhui <chenrenhui1@huawei.com>

ipc/fast_ipc.c
@@ -1,0 +1,296 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#ifndef pr_fmt
+# define pr_fmt(fmt) "fast_ipc: " fmt
+#endif
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/slab.h>
+#include <linux/sched.h>
+#include <linux/printk.h>
+#include <linux/preempt.h>
+#include <linux/sched/signal.h>
+#include <linux/fast_ipc.h>
+#include <linux/sched/debug.h>
+
+#define IPC_DEBUG(fmt, ...)
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("yangyun");
+MODULE_DESCRIPTION("fast ipc");
+MODULE_VERSION("1.0");
+
+static inline void bind_info_lock(struct fast_ipc_bind_info *bind_info)
+{
+	spin_lock(&bind_info->lock);
+}
+
+static inline void bind_info_unlock(struct fast_ipc_bind_info *bind_info)
+{
+	spin_unlock(&bind_info->lock);
+}
+
+static inline int fast_ipc_check_task_consistency(struct task_struct *client,
+		struct task_struct *server)
+{
+	if (client->pid == server->pid) {
+		pr_err("error: client(%s/%d) and server(%s/%d) is same\n", client->comm,
+			   client->pid, server->comm, server->pid);
+		return -EPERM;
+	}
+
+	return 0;
+}
+
+static inline ssize_t
+fast_ipc_call_check(struct fast_ipc_bind_info *bind_info,
+		struct task_struct *tsk)
+{
+	ssize_t ret = 0;
+	struct task_struct *server_task;
+
+	if (!bind_info)
+		return -ENOENT;
+
+	if (bind_info->client_task) {
+		pr_err("error: bind already with client task: %s/%d, current is : %s/%d",
+			   bind_info->client_task->comm, bind_info->client_task->pid,
+			   tsk->comm, tsk->pid);
+		return -EEXIST;
+	}
+
+	server_task = bind_info->server_task;
+	if (!server_task) {
+		pr_err("error: server thread is not exsit\n");
+		return -ESRCH;
+	}
+
+	return ret;
+}
+
+static inline void fast_ipc_client_init(
+		struct fast_ipc_bind_info *bind_info, struct task_struct *tsk)
+{
+	bind_info->client_task = tsk;
+}
+
+
+static inline void fast_ipc_client_exit(
+		struct fast_ipc_bind_info *bind_info, struct task_struct *tsk)
+{
+	bind_info->client_task = NULL;
+}
+
+static inline int fast_ipc_get_client_exit_code(
+		const struct fast_ipc_bind_info *bind_info)
+{
+	return bind_info->client_need_exit ? -ESRCH : 0;
+}
+
+static inline void fast_ipc_wakeup_client_task(
+		struct fast_ipc_bind_info *bind_info)
+{
+	struct task_struct *client_task;
+
+	client_task = bind_info->client_task;
+	bind_info->client_need_exit = true;
+	wake_up_process(client_task);
+}
+
+void fast_ipc_wakeup_server_task(struct fast_ipc_bind_info *bind_info)
+{
+	struct task_struct *server_task;
+
+	server_task = bind_info->server_task;
+	bind_info->server_need_exit = true;
+	wake_up_process(server_task);
+}
+EXPORT_SYMBOL_GPL(fast_ipc_wakeup_server_task);
+
+void *fast_ipc_bind(struct task_struct *server_task)
+{
+	struct fast_ipc_bind_info *bind_info = NULL;
+
+	bind_info = kcalloc(1, sizeof(struct fast_ipc_bind_info), GFP_KERNEL);
+	if (!bind_info)
+		return ERR_PTR(-ENOMEM);
+
+	bind_info->server_task = server_task;
+
+	return (void *) bind_info;
+}
+EXPORT_SYMBOL_GPL(fast_ipc_bind);
+
+void fast_ipc_release(struct fast_ipc_bind_info *bind_info)
+{
+	if (bind_info) {
+		if (bind_info->client_task && bind_info->is_calling)
+			fast_ipc_wakeup_client_task(bind_info);
+		kfree(bind_info);
+	}
+}
+EXPORT_SYMBOL_GPL(fast_ipc_release);
+
+void fast_ipc_unbind(struct fast_ipc_bind_info *bind_info,
+		struct task_struct *server_task)
+{
+	if (bind_info) {
+		if (bind_info->server_task == server_task) {
+			bind_info->server_task = NULL;
+			if (bind_info->client_task && bind_info->is_calling)
+				fast_ipc_wakeup_client_task(bind_info);
+			kfree(bind_info);
+		}
+	}
+
+}
+EXPORT_SYMBOL_GPL(fast_ipc_unbind);
+
+ssize_t fast_ipc_do_call(struct fast_ipc_bind_info *bind_info,
+		struct task_struct *tsk)
+{
+	struct task_struct *server_task;
+	ssize_t ret;
+
+	ret = fast_ipc_call_check(bind_info, tsk);
+	if (ret) {
+		pr_err("fast ipc call check and init failed, errno: %ld\n", ret);
+		return ret;
+	}
+
+	fast_ipc_client_init(bind_info, tsk);
+
+	server_task = bind_info->server_task;
+
+	bind_info->client_need_exit = false;
+	bind_info->is_calling = true;
+
+	preempt_disable(); /* optimize performance if preemption occurs */
+	smp_mb();
+	wake_up_process(server_task);
+	preempt_enable();
+	IPC_DEBUG("[cpu/%d][%s/%d]  ipc do call server(%s/%d)\n",
+			  smp_processor_id(), tsk->comm, tsk->pid, server_task->comm,
+			  server_task->pid);
+
+	set_current_state(TASK_INTERRUPTIBLE);
+	while (bind_info->is_calling) {
+		IPC_DEBUG("[cpu/%d][%s/%d] client begin schedule\n", smp_processor_id(),
+				  tsk->comm, tsk->pid);
+		schedule();
+		IPC_DEBUG("[cpu/%d][%s/%d] client schedule end\n", smp_processor_id(),
+				  tsk->comm, tsk->pid);
+		if (signal_pending(current)) {
+			ret = -EINTR;
+			pr_err("[cpu/%d][%s/%d] client has signal pending break\n",
+				   smp_processor_id(), tsk->comm, tsk->pid);
+			break;
+		}
+		set_current_state(TASK_INTERRUPTIBLE);
+	}
+	set_current_state(TASK_RUNNING);
+
+	if (bind_info->is_calling) {
+		pr_err("[cpu/%d][%s/%d] server is still calling, but client is waken up\n",
+			   smp_processor_id(), tsk->comm, tsk->pid);
+		pr_err("[cpu/%d][%s/%d] servertask(%s/%d) is running on cpu %d\n",
+			   smp_processor_id(), tsk->comm, tsk->pid, server_task->comm,
+			   server_task->pid, task_cpu(server_task));
+	}
+
+	fast_ipc_client_exit(bind_info, tsk);
+
+	if (ret == -EINTR)
+		return ret;
+	ret = fast_ipc_get_client_exit_code(bind_info);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(fast_ipc_do_call);
+
+long fast_ipc_ret_call(struct fast_ipc_bind_info *bind_info,
+		struct task_struct *tsk)
+{
+	struct task_struct *client_task;
+
+	if (!bind_info->is_calling)
+		return 0;
+
+	bind_info_lock(bind_info);
+	client_task = bind_info->client_task;
+	if (!client_task) {
+		bind_info_unlock(bind_info);
+		return -ESRCH;
+	}
+
+	bind_info_unlock(bind_info);
+
+	bind_info->is_calling = false;
+	preempt_disable();
+	/* memory barrier for preempt */
+	smp_mb();
+	wake_up_process(client_task);
+	preempt_enable();
+	IPC_DEBUG("[CPU/%d][%s/%d] client task pid: %d, state: %d\n",
+			  smp_processor_id(), tsk->comm, tsk->pid, client_task->pid,
+			  client_task->state);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(fast_ipc_ret_call);
+
+long fast_ipc_wait_call(struct fast_ipc_bind_info *bind_info,
+		struct task_struct *tsk)
+{
+	long ret = 0;
+	sigset_t pending_signals;
+
+	for (;;) {
+		set_current_state(TASK_INTERRUPTIBLE);
+		if (bind_info->is_calling)
+			break;
+
+		if (bind_info->server_need_exit) {
+			ret = -ENODEV;
+			break;
+		}
+
+		schedule();
+
+		if (signal_pending_state(TASK_INTERRUPTIBLE, tsk)
+			&& !bind_info->is_calling) {
+			if (fatal_signal_pending(tsk)) {
+				pr_err("[CPU/%d][%s/%d] current task has SIGKILL\n",
+					   smp_processor_id(), tsk->comm, tsk->pid);
+			}
+
+			pending_signals = current->pending.signal;
+			ret = -ERESTARTSYS;
+			break;
+		}
+	}
+
+	set_current_state(TASK_RUNNING);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(fast_ipc_wait_call);
+
+static int __init
+fast_ipc_init(void)
+{
+	pr_info("fast ipc init\n");
+	return 0;
+}
+
+static void __exit
+fast_ipc_exit(void)
+{
+	pr_info("fast ipc exit\n");
+}
+
+
+module_init(fast_ipc_init);
+module_exit(fast_ipc_exit);

Kconfig
@@ -21,6 +21,8 @@ source "drivers/Kconfig"
 
 source "fs/Kconfig"
 
+source "ipc/Kconfig"
+
 source "security/Kconfig"
 
 source "crypto/Kconfig"

arch/arm64/configs/openeuler_defconfig
@@ -6660,6 +6660,7 @@ CONFIG_FILE_MITIGATION_FALSE_SHARING=y
 # end of File systems
 
 CONFIG_RESCTRL=y
+CONFIG_FAST_IPC=m
 
 #
 # Security options

include/linux/fast_ipc.h
@@ -1,0 +1,45 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ * Description: fast IPC header
+ * Author: yangyun
+ * Create: 2024-05-31
+ */
+#ifndef __FAST_IPC_H_
+#define __FAST_IPC_H_
+
+struct fast_ipc_bind_info {
+	unsigned int data_size;
+
+	struct task_struct *client_task;
+	struct task_struct *server_task;
+
+	bool is_calling;
+	bool client_need_exit;
+	bool server_need_exit;
+
+	atomic_t nr_call;
+
+	spinlock_t lock;
+	struct list_head node;
+};
+
+void fast_ipc_wakeup_server_task(struct fast_ipc_bind_info *bind_info);
+
+void *fast_ipc_bind(struct task_struct *server_task);
+
+void fast_ipc_unbind(struct fast_ipc_bind_info *bind_info,
+		struct task_struct *server_task);
+
+ssize_t fast_ipc_do_call(struct fast_ipc_bind_info *bind_info,
+		struct task_struct *tsk);
+
+long fast_ipc_ret_call(struct fast_ipc_bind_info *bind_info,
+		struct task_struct *tsk);
+
+long fast_ipc_wait_call(struct fast_ipc_bind_info *bind_info,
+		struct task_struct *tsk);
+
+void fast_ipc_release(struct fast_ipc_bind_info *bind_info);
+
+#endif

ipc/Kconfig
@@ -1,0 +1,6 @@
+config FAST_IPC
+    tristate "Fast IPC support"
+    default n
+    depends on ARM64
+    help
+       Fast Inter-process call, used to switch threads quickly.

ipc/Makefile
@@ -9,4 +9,4 @@ obj-$(CONFIG_SYSVIPC_SYSCTL) += ipc_sysctl.o
 obj-$(CONFIG_POSIX_MQUEUE) += mqueue.o msgutil.o
 obj-$(CONFIG_IPC_NS) += namespace.o
 obj-$(CONFIG_POSIX_MQUEUE_SYSCTL) += mq_sysctl.o
-
+obj-$(CONFIG_FAST_IPC) += fast_ipc.o

chenglongshe/openEuler

commit d7820d345f399d4ab4e722d434f7305a8c82ea1b

ipc: fix error kill signal handle in fastpath
euleros inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IC7KJ2
CVE: NA

--------------------------------

When a kill signal recived by fusepath client,
the server will abort, we need ignore the client
action.
Fixes: 1fc14d09b9f0 ("ipc: add fast_ipc module to enable fast process switching")

Signed-off-by: wuyifeng10 <wuyifeng10@huawei.com>

fs/fuse/dev.c
@@ -2873,7 +2873,9 @@ static long fuse_ipc_ret_call(struct file *file, struct fuse_conn *fc,
 	if (num_written < 0) {
 		pr_err("[cpu/%d] [%s/%d]fuse_ipc_write failed %ld\n",
 			   smp_processor_id(), tsk->comm, tsk->pid, num_written);
-		return num_written;
+		/* no return, we report the error msg to client in
+		 * fuse_ipc_write and continue handle the next request
+		 */
 	}
 
 	ret = fast_ipc_ret_call(ipc_info->bind_info, tsk);

ipc/fast_ipc.c
@@ -10,6 +10,7 @@
 #include <linux/types.h>
 #include <linux/slab.h>
 #include <linux/sched.h>
+#include <linux/delay.h>
 #include <linux/printk.h>
 #include <linux/preempt.h>
 #include <linux/sched/signal.h>
@@ -185,11 +186,14 @@ ssize_t fast_ipc_do_call(struct fast_ipc_bind_info *bind_info,
 				  tsk->comm, tsk->pid);
 		if (signal_pending(current)) {
 			ret = -EINTR;
-			pr_err("[cpu/%d][%s/%d] client has signal pending break\n",
+			pr_err("[cpu/%d][%s/%d] client has signal, wait server finish\n",
 				   smp_processor_id(), tsk->comm, tsk->pid);
-			break;
+			msleep(10);
+			set_current_state(TASK_RUNNING);
+			/* for next loop, server change the is_calling flags */
+			if (!bind_info->is_calling)
+				pr_err("server finish\n");
 		}
-		set_current_state(TASK_INTERRUPTIBLE);
 	}
 	set_current_state(TASK_RUNNING);
 
@@ -216,12 +220,15 @@ long fast_ipc_ret_call(struct fast_ipc_bind_info *bind_info,
 {
 	struct task_struct *client_task;
 
-	if (!bind_info->is_calling)
+	if (!bind_info->is_calling) {
+		pr_err("confusing bug is_no_calling\n");
 		return 0;
+	}
 
 	bind_info_lock(bind_info);
 	client_task = bind_info->client_task;
 	if (!client_task) {
+		pr_err("confusing bug no_client\n");
 		bind_info_unlock(bind_info);
 		return -ESRCH;
 	}