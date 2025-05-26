1a792a5fca425dfceba9d8d13615584858af5c7f
Stefan Raspl <raspl@linux.ibm.com>
2025-05-26 18:16:27 +0800

    net/smc: Register SMC-D as ISM client
    mainline inclusion
    from mainline-v6.3-rc1
    commit 8747716f3942a610efdd12e3655df47269c268ac
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC914B
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=8747716f3942a610efdd12e3655df47269c268ac
    
    --------------------------------
    
    Register the smc module with the new ism device driver API.
    This is the second part of a bigger overhaul of the interfaces between SMC
    and ISM.
    
    Signed-off-by: Stefan Raspl <raspl@linux.ibm.com>
    Signed-off-by: Jan Karcher <jaka@linux.ibm.com>
    Signed-off-by: Wenjia Zhang <wenjia@linux.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Conflicts:
    	net/smc/af_smc.c
    [conflicts due to not merge 194730a9beb5 ("net/smc: Make SMC statistics network namespace aware")]
    Signed-off-by: Wang Liang <wangliang74@huawei.com>

--- net/smc/af_smc.c
+++ net/smc/af_smc.c
@@ -3262,12 +3262,14 @@ static int __init smc_init(void)
 	if (rc)
 		return rc;
 
-	smc_ism_init();
+	rc = smc_ism_init();
+	if (rc)
+		goto out_pernet_subsys;
 	smc_clc_init();
 
 	rc = smc_nl_init();
 	if (rc)
-		goto out_pernet_subsys;
+		goto out_ism;
 
 	rc = smc_pnet_init();
 	if (rc)
@@ -3365,6 +3367,8 @@ out_pnet:
 	smc_pnet_exit();
 out_nl:
 	smc_nl_exit();
+out_ism:
+	smc_ism_exit();
 out_pernet_subsys:
 	unregister_pernet_subsys(&smc_net_ops);
 
