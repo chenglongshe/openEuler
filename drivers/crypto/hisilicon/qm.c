7bb83c910add3af13fe60689de2d427a110939e1
Weili Qian <qianweili@huawei.com>
2025-05-27 17:22:04 +0800

    crypto: hisilicon/qm - fix vfs_num error
    driver inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC9UQB
    CVE: NA
    
    ----------------------------------------------------------------------
    
    In sriov_enable and sriov_disable, when vfs_num is 0, calling
    the qm_clear_vft_config function is meaningless and the loop is
    not executed. Therefore, Call qm_clear_vft_config and set vfs_num to 0.
    If the VF configuration fails to be cleared, the remaining
    VF configurations are cleared.
    
    Fixes: fa2bf6e35091 ("crypto: hisilicon/qm - return failure if vfs_num exceeds total VFs")
    Signed-off-by: Weili Qian <qianweili@huawei.com>
    Signed-off-by: JiangShui Yang <yangjiangshui@h-partners.com>

--- drivers/crypto/hisilicon/qm.c
+++ drivers/crypto/hisilicon/qm.c
@@ -4147,13 +4147,14 @@ int hisi_qm_sriov_enable(struct pci_dev *pdev, int max_vfs)
 		goto err_put_sync;
 	}
 
+	qm->vfs_num = num_vfs;
 	ret = pci_enable_sriov(pdev, num_vfs);
 	if (ret) {
 		pci_err(pdev, "Can't enable VF!\n");
 		qm_clear_vft_config(qm);
+		qm->vfs_num = 0;
 		goto err_put_sync;
 	}
-	qm->vfs_num = num_vfs;
 
 	pci_info(pdev, "VF enabled, vfs_num(=%d)!\n", num_vfs);
 
@@ -4175,6 +4176,7 @@ EXPORT_SYMBOL_GPL(hisi_qm_sriov_enable);
 int hisi_qm_sriov_disable(struct pci_dev *pdev, bool is_frozen)
 {
 	struct hisi_qm *qm = pci_get_drvdata(pdev);
+	int ret;
 
 	if (pci_vfs_assigned(pdev)) {
 		pci_err(pdev, "Failed to disable VFs as VFs are assigned!\n");
@@ -4188,11 +4190,14 @@ int hisi_qm_sriov_disable(struct pci_dev *pdev, bool is_frozen)
 	}
 
 	pci_disable_sriov(pdev);
+	ret = qm_clear_vft_config(qm);
+	if (ret)
+		pci_err(pdev, "Failed to clear vft config!\n");
 
 	qm->vfs_num = 0;
 	qm_pm_put_sync(qm);
 
-	return qm_clear_vft_config(qm);
+	return 0;
 }
 EXPORT_SYMBOL_GPL(hisi_qm_sriov_disable);
 
