62c094888ef97bad1658cdd940e326c94283bea1
Cheng Yu <serein.chengyu@huawei.com>
2025-05-29 10:33:24 +0800

    sched: Support NUMA parallel scheduling for multiple processes
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICBBNL
    
    --------------------------------
    
    For architectures with multiple NUMA node levels and large distances
    between nodes, a better approach is to support processes running in
    parallel on each NUMA node.
    
    The usage is restricted to the following scenarios:
    1. No CPU binding for user-space processes;
    2. It is applicable to distributed applications, such as business
       architectures with one master and multiple slaves running in
       parallel;
    3. The existing "qos dynamic affinity" and "qos smart grid" features
       must not be used simultaneously.
    
    Signed-off-by: Cheng Yu <serein.chengyu@huawei.com>

--- arch/arm64/mm/numa.c
+++ arch/arm64/mm/numa.c
@@ -12,6 +12,7 @@
 #include <linux/memblock.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/perf_event.h>
 
 #include <asm/acpi.h>
 #include <asm/sections.h>
@@ -777,3 +778,75 @@ void __init arm64_numa_init(void)
 
 	numa_init(dummy_numa_init);
 }
+
+#ifdef CONFIG_SCHED_PARAL
+static atomic_t paral_nid_last = ATOMIC_INIT(-1);
+
+int probe_pmu_numa_event(void)
+{
+	struct perf_event *event;
+	struct perf_event_attr attr = {};
+	int type = perf_pmu_type_of_name("hisi_sccl3_hha0");
+
+	if (type == -1)
+		return -EINVAL;
+
+	attr.type = type;
+	attr.config = 0x02;
+	attr.size = sizeof(struct perf_event_attr);
+	attr.pinned = 1;
+	attr.disabled = 1;
+	attr.sample_period = 0;
+
+	event = perf_event_create_kernel_counter(&attr, smp_processor_id(),
+							NULL, NULL, NULL);
+	if (IS_ERR(event))
+		return PTR_ERR(event);
+
+	perf_event_release_kernel(event);
+
+	return 0;
+}
+
+static inline int update_sched_paral_nid(void)
+{
+	int onid, nnid;
+
+	do {
+		onid = atomic_read(&paral_nid_last);
+		nnid = (onid >= INT_MAX) ? 0 : (onid + 1);
+	} while (atomic_cmpxchg(&paral_nid_last, onid, nnid) != onid);
+
+	return nnid;
+}
+
+void set_task_paral_node(struct task_struct *p)
+{
+	int nid;
+	int i = 0;
+	const cpumask_t *cpus_mask;
+
+	if (is_global_init(current))
+		return;
+
+	if (p->flags & PF_KTHREAD || p->tgid != p->pid)
+		return;
+
+	while (i < nr_node_ids) {
+		nid = update_sched_paral_nid() % nr_node_ids;
+		cpus_mask = cpumask_of_node(nid);
+
+		if (cpumask_empty(cpus_mask) ||
+			!cpumask_subset(cpus_mask, p->cpus_ptr)) {
+			i++;
+			continue;
+		}
+
+		cpumask_copy(p->prefer_cpus, cpus_mask);
+		break;
+	}
+}
+#else
+void set_task_paral_node(struct task_struct *p) {}
+int probe_pmu_numa_event(void) { return -1; }
+#endif
