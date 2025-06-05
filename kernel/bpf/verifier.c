39f2054e5baeb9dabe643a46dd395b8584c0147e
Eduard Zingerman <eddyz87@gmail.com>
2025-06-05 09:54:25 +0000

    bpf: check changes_pkt_data property for extension programs
    mainline inclusion
    from mainline-v6.13-rc3
    commit 81f6d0530ba031b5f038a091619bf2ff29568852
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/IC5J6M
    CVE: CVE-2024-58100
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=81f6d0530ba031b5f038a091619bf2ff29568852
    
    --------------------------------
    
    When processing calls to global sub-programs, verifier decides whether
    to invalidate all packet pointers in current state depending on the
    changes_pkt_data property of the global sub-program.
    
    Because of this, an extension program replacing a global sub-program
    must be compatible with changes_pkt_data property of the sub-program
    being replaced.
    
    This commit:
    - adds changes_pkt_data flag to struct bpf_prog_aux:
      - this flag is set in check_cfg() for main sub-program;
      - in jit_subprogs() for other sub-programs;
    - modifies bpf_check_attach_btf_id() to check changes_pkt_data flag;
    - moves call to check_attach_btf_id() after the call to check_cfg(),
      because it needs changes_pkt_data flag to be set:
    
        bpf_check:
          ...                             ...
        - check_attach_btf_id             resolve_pseudo_ldimm64
          resolve_pseudo_ldimm64   -->    bpf_prog_is_offloaded
          bpf_prog_is_offloaded           check_cfg
          check_cfg                     + check_attach_btf_id
          ...                             ...
    
    The following fields are set by check_attach_btf_id():
    - env->ops
    - prog->aux->attach_btf_trace
    - prog->aux->attach_func_name
    - prog->aux->attach_func_proto
    - prog->aux->dst_trampoline
    - prog->aux->mod
    - prog->aux->saved_dst_attach_type
    - prog->aux->saved_dst_prog_type
    - prog->expected_attach_type
    
    Neither of these fields are used by resolve_pseudo_ldimm64() or
    bpf_prog_offload_verifier_prep() (for netronome and netdevsim
    drivers), so the reordering is safe.
    
    Suggested-by: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>
    Link: https://lore.kernel.org/r/20241210041100.1898468-6-eddyz87@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Conflicts:
    	include/linux/bpf.h
    	kernel/bpf/verifier.c
    [Context conflicts]
    Signed-off-by: Chen Zhongjin <chenzhongjin@huawei.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -9158,6 +9158,7 @@ check_state:
 		}
 	}
 	ret = 0; /* cfg looks good */
+	env->prog->aux->changes_pkt_data = env->subprog_info[0].changes_pkt_data;
 
 err_free:
 	kvfree(insn_state);
@@ -11714,6 +11715,7 @@ static int jit_subprogs(struct bpf_verifier_env *env)
 		}
 		func[i]->aux->num_exentries = num_exentries;
 		func[i]->aux->tail_call_reachable = env->subprog_info[i].tail_call_reachable;
+		func[i]->aux->changes_pkt_data = env->subprog_info[i].changes_pkt_data;
 		func[i] = bpf_int_jit_compile(func[i]);
 		if (!func[i]->jited) {
 			err = -ENOTSUPP;
@@ -12597,6 +12599,12 @@ int bpf_check_attach_target(struct bpf_verifier_log *log,
 					"Extension programs should be JITed\n");
 				return -EINVAL;
 			}
+			if (prog->aux->changes_pkt_data &&
+			    !aux->func[subprog]->aux->changes_pkt_data) {
+				bpf_log(log,
+					"Extension program changes packet data, while original does not\n");
+				return -EINVAL;
+			}
 		}
 		if (!tgt_prog->jited) {
 			bpf_log(log, "Can attach to only JITed progs\n");
@@ -12955,10 +12963,6 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr,
 	if (ret < 0)
 		goto skip_full_check;
 
-	ret = check_attach_btf_id(env);
-	if (ret)
-		goto skip_full_check;
-
 	ret = resolve_pseudo_ldimm64(env);
 	if (ret < 0)
 		goto skip_full_check;
@@ -12971,6 +12975,10 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr,
 
 	ret = check_cfg(env);
 	if (ret < 0)
+		goto skip_full_check;
+
+	ret = check_attach_btf_id(env);
+	if (ret)
 		goto skip_full_check;
 
 	ret = do_check_subprogs(env);
