OERV-RVCI/rvck-olk

commit 587296e12d7d64c971a182ca1d28de8cdbe29ca9

iommu/arm-smmu-v3: Add mpam helpers to query and set state
maillist inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IC03L1

Reference: https://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git/commit/?h=mpam/snapshot%2bextras/v6.14-rc1&id=3b263c0f5b05d8c60fe8921c055c99ea280374c9

--------------------------------

To allow an iommu_group to be moved between resctrl groups as if it
were a CPU thread, the mpam driver needs to be able to set the partid
and pmg for the iommu_group.

Use the properties in the STE, as these only apply to one stream.

The MPAM driver also needs to know the maximum partid and pmg
values that the SMMU can generate. This allows it to determine
the system-wide common supported range of values. Add a helper
to return this id register.

Tested-by: Amit Singh Tomar <amitsinght@marvell.com>
Signed-off-by: James Morse <james.morse@arm.com>
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -3998,6 +3998,96 @@ static int arm_smmu_def_domain_type(struct device *dev)
 	return ret;
 }
 
+static int arm_smmu_group_set_mpam(struct iommu_group *group, u16 partid,
+				   u8 pmg)
+{
+	int i;
+	u32 sid;
+	unsigned long flags;
+	struct arm_smmu_ste *step;
+	struct iommu_domain *domain;
+	struct arm_smmu_device *smmu;
+	struct arm_smmu_master *master;
+	struct arm_smmu_cmdq_batch cmds;
+	struct arm_smmu_domain *smmu_domain;
+	struct arm_smmu_cmdq_ent cmd = {
+		.opcode	= CMDQ_OP_CFGI_STE,
+		.cfgi	= {
+			.leaf	= true,
+		},
+	};
+	struct arm_smmu_master_domain *master_domain;
+
+	domain = iommu_get_domain_for_group(group);
+	smmu_domain = to_smmu_domain(domain);
+	if (!(smmu_domain->smmu->features & ARM_SMMU_FEAT_MPAM))
+		return -EIO;
+	smmu = smmu_domain->smmu;
+
+	arm_smmu_cmdq_batch_init(smmu, &cmds);
+
+	spin_lock_irqsave(&smmu_domain->devices_lock, flags);
+	list_for_each_entry(master_domain, &smmu_domain->devices,
+			    devices_elm) {
+		master = master_domain->master;
+
+		for (i = 0; i < master->num_streams; i++) {
+			sid = master->streams[i].id;
+			step = arm_smmu_get_step_for_sid(smmu, sid);
+
+			/* These need locking if the VMSPtr is ever used */
+			step->data[4] = FIELD_PREP(STRTAB_STE_4_PARTID, partid);
+			step->data[5] = FIELD_PREP(STRTAB_STE_5_PMG, pmg);
+
+			cmd.cfgi.sid = sid;
+			arm_smmu_cmdq_batch_add(smmu, &cmds, &cmd);
+		}
+
+		master->partid = partid;
+		master->pmg = pmg;
+	}
+	spin_unlock_irqrestore(&smmu_domain->devices_lock, flags);
+
+	arm_smmu_cmdq_batch_submit(smmu, &cmds);
+
+	return 0;
+}
+
+static int arm_smmu_group_get_mpam(struct iommu_group *group, u16 *partid,
+				   u8 *pmg)
+{
+	int err = -EINVAL;
+	unsigned long flags;
+	struct iommu_domain *domain;
+	struct arm_smmu_master *master;
+	struct arm_smmu_domain *smmu_domain;
+	struct arm_smmu_master_domain *master_domain;
+
+	domain = iommu_get_domain_for_group(group);
+	smmu_domain = to_smmu_domain(domain);
+	if (!(smmu_domain->smmu->features & ARM_SMMU_FEAT_MPAM))
+		return -EIO;
+
+	if (!partid && !pmg)
+		return 0;
+
+	spin_lock_irqsave(&smmu_domain->devices_lock, flags);
+	list_for_each_entry(master_domain, &smmu_domain->devices,
+			    devices_elm) {
+		master = master_domain->master;
+		if (master) {
+			if (partid)
+				*partid = master->partid;
+			if (pmg)
+				*pmg = master->pmg;
+			err = 0;
+		}
+	}
+	spin_unlock_irqrestore(&smmu_domain->devices_lock, flags);
+
+	return err;
+}
+
 static struct iommu_ops arm_smmu_ops = {
 	.identity_domain	= &arm_smmu_identity_domain,
 	.blocked_domain		= &arm_smmu_blocked_domain,
@@ -4014,6 +4104,8 @@ static struct iommu_ops arm_smmu_ops = {
 	.remove_dev_pasid	= arm_smmu_remove_dev_pasid,
 	.dev_enable_feat	= arm_smmu_dev_enable_feature,
 	.dev_disable_feat	= arm_smmu_dev_disable_feature,
+	.get_group_qos_params	= arm_smmu_group_get_mpam,
+	.set_group_qos_params	= arm_smmu_group_set_mpam,
 	.page_response		= arm_smmu_page_response,
 	.def_domain_type	= arm_smmu_def_domain_type,
 	.viommu_alloc		= arm_vsmmu_alloc,

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
@@ -303,6 +303,7 @@ static inline u32 arm_smmu_strtab_l2_idx(u32 sid)
 
 #define STRTAB_STE_1_S2FWB		(1UL << 25)
 #define STRTAB_STE_1_S1STALLD		(1UL << 27)
+#define STRTAB_STE_1_S1MPAM		(1UL << 26)
 
 #define STRTAB_STE_1_EATS		GENMASK_ULL(29, 28)
 #define STRTAB_STE_1_EATS_ABT		0UL
@@ -331,6 +332,10 @@ static inline u32 arm_smmu_strtab_l2_idx(u32 sid)
 #define STRTAB_STE_2_S2R		(1UL << 58)
 
 #define STRTAB_STE_3_S2TTB_MASK		GENMASK_ULL(51, 4)
+
+#define STRTAB_STE_4_PARTID		GENMASK_ULL(31, 16)
+
+#define STRTAB_STE_5_PMG		GENMASK_ULL(7, 0)
 
 /* These bits can be controlled by userspace for STRTAB_STE_0_CFG_NESTED */
 #define STRTAB_STE_0_NESTING_ALLOWED                                         \
@@ -855,6 +860,8 @@ struct arm_smmu_master {
 	bool				sva_enabled;
 	bool				iopf_enabled;
 	unsigned int			ssid_bits;
+	u16				partid;
+	u8				pmg;
 };
 
 /* SMMU private data for an IOMMU domain */

drivers/iommu/iommu.c
@@ -2155,6 +2155,12 @@ struct iommu_domain *iommu_get_domain_for_dev(struct device *dev)
 }
 EXPORT_SYMBOL_GPL(iommu_get_domain_for_dev);
 
+struct iommu_domain *iommu_get_domain_for_group(struct iommu_group *group)
+{
+	return group->domain;
+}
+EXPORT_SYMBOL_GPL(iommu_get_domain_for_group);
+
 /*
  * For IOMMU_DOMAIN_DMA implementations which already provide their own
  * guarantees that the group and its default domain are valid and correct.

include/linux/iommu.h
@@ -703,6 +703,12 @@ struct iommu_ops {
 	int (*dev_enable_feat)(struct device *dev, enum iommu_dev_features f);
 	int (*dev_disable_feat)(struct device *dev, enum iommu_dev_features f);
 
+	/* Per group IOMMU features */
+	int (*get_group_qos_params)(struct iommu_group *group, u16 *partition,
+				    u8 *perf_mon_grp);
+	int (*set_group_qos_params)(struct iommu_group *group, u16 partition,
+				    u8 perf_mon_grp);
+
 	void (*page_response)(struct device *dev, struct iopf_fault *evt,
 			      struct iommu_page_response *msg);
 
@@ -971,6 +977,7 @@ extern void iommu_detach_device(struct iommu_domain *domain,
 extern int iommu_sva_unbind_gpasid(struct iommu_domain *domain,
 				   struct device *dev, ioasid_t pasid);
 extern struct iommu_domain *iommu_get_domain_for_dev(struct device *dev);
+extern struct iommu_domain *iommu_get_domain_for_group(struct iommu_group *group);
 extern struct iommu_domain *iommu_get_dma_domain(struct device *dev);
 extern size_t iommu_pgsize(struct iommu_domain *domain, unsigned long iova,
 			   phys_addr_t paddr, size_t size, size_t *count);

chenglongshe/openEuler

commit aaa0e8e57543b91256389f722c893544761b632a

iommu/arm-smmu-v3: Fix global-out-of-bounds access in arm_smmu_group_get_mpam()
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IC03L1

--------------------------------

During the testing of the SMMU functions related to MPAM, KASAN reports an
access fault:

BUG: KASAN: global-out-of-bounds in arm_smmu_group_get_mpam
Read of size 4 at addr ffffd8dbdf3bf0d8 by task rmdir/9954
Call trace:
 arm_smmu_group_get_mpam+0x4c/0x150
 iommu_group_get_qos_params+0xa8/0xe8
 resctrl_arch_match_iommu_closid+0x80/0x118
 rdt_move_group_iommus.isra.0+0x198/0x1c0
 rdtgroup_rmdir_ctrl+0x58/0x1e8
 rdtgroup_rmdir+0x198/0x290
 kernfs_iop_rmdir+0xa0/0xe8
 vfs_rmdir.part.0+0xd8/0x290
 do_rmdir+0x288/0x2e0

The DMA of some devices like the HISI PTT trace can only work under
Some devices' DMA (such as Hisi PTT trace) can only work in pass-through
mode. Therefore, in the __iommu_domain_alloc(), the SMMU does not allocate
a smmu_domain struct for them. Naturally, these types of devices do not
support the MPAM feature.

Consequently, for devices of type IOMMU_DOMAIN_IDENTITY and
IOMMU_DOMAIN_BLOCKED, they are skipped in arm_smmu_group_get_mpam() or
returned fail in arm_smmu_group_set_mpam().

Fixes: 587296e12d7d ("iommu/arm-smmu-v3: Add mpam helpers to query and set state")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -4004,6 +4004,7 @@ static int arm_smmu_group_set_mpam(struct iommu_group *group, u16 partid,
 	int i;
 	u32 sid;
 	unsigned long flags;
+	unsigned int alloc_type;
 	struct arm_smmu_ste *step;
 	struct iommu_domain *domain;
 	struct arm_smmu_device *smmu;
@@ -4019,6 +4020,12 @@ static int arm_smmu_group_set_mpam(struct iommu_group *group, u16 partid,
 	struct arm_smmu_master_domain *master_domain;
 
 	domain = iommu_get_domain_for_group(group);
+
+	alloc_type = domain->type & IOMMU_DOMAIN_ALLOC_FLAGS;
+	if (alloc_type == IOMMU_DOMAIN_IDENTITY ||
+	    alloc_type == IOMMU_DOMAIN_BLOCKED)
+		return -EINVAL;
+
 	smmu_domain = to_smmu_domain(domain);
 
 	if (!smmu_domain->smmu)
@@ -4062,12 +4069,19 @@ static int arm_smmu_group_get_mpam(struct iommu_group *group, u16 *partid,
 {
 	int err = -EINVAL;
 	unsigned long flags;
+	unsigned int alloc_type;
 	struct iommu_domain *domain;
 	struct arm_smmu_master *master;
 	struct arm_smmu_domain *smmu_domain;
 	struct arm_smmu_master_domain *master_domain;
 
 	domain = iommu_get_domain_for_group(group);
+
+	alloc_type = domain->type & IOMMU_DOMAIN_ALLOC_FLAGS;
+	if (alloc_type == IOMMU_DOMAIN_IDENTITY ||
+	    alloc_type == IOMMU_DOMAIN_BLOCKED)
+		return 0;
+
 	smmu_domain = to_smmu_domain(domain);
 
 	if (!smmu_domain->smmu)