09862fd61111e2698a82eba0bd7b66fa89ec3f30
Ming-Guang Zhang <2648455538@qq.com>
2025-06-08 14:43:22 +0800

    Signed-off-by: Ming-Guang Zhang <2648455538@qq.com>
    Added error handling in the max14577_reg_get_current_limit function to check the return value of max14577_read_reg.

--- drivers/regulator/max14577-regulator.c
+++ drivers/regulator/max14577-regulator.c
@@ -44,7 +44,9 @@ static int max14577_reg_get_current_limit(struct regulator_dev *rdev)
 	if (rdev_get_id(rdev) != MAX14577_CHARGER)
 		return -EINVAL;
 
-	max14577_read_reg(rmap, MAX14577_CHG_REG_CHG_CTRL4, &reg_data);
+	int ret = max14577_read_reg(rmap, MAX14577_CHG_REG_CHG_CTRL4, &reg_data);
+	if (ret < 0)
+		return ret;
 
 	if ((reg_data & CHGCTRL4_MBCICHWRCL_MASK) == 0)
 		return limits->min;
