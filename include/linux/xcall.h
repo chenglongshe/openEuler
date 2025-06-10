bc65f5a3f3238a2e200732fa006975ac2746916d
Jinjie Ruan <ruanjinjie@huawei.com>
2025-06-10 16:07:38 +0800

    arm64: Reserve a kabi in task_struct exclusively for xcall
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/release-management/issues/IC9Q31
    
    --------------------------------
    
    Refactor the xcall_enable logic to ensure only one reserved kabi
    in task_struct is used by xcall, which facilitates future xcall
    expansion.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

---
+++ include/linux/xcall.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_XCALL_H
+#define _LINUX_XCALL_H
+
+struct xcall_info {
+	/* Must be first! */
+	DECLARE_BITMAP(xcall_enable, __NR_syscalls);
+};
+#endif
