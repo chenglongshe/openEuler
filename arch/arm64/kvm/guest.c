04173114809715925d74d0af3568d6e39b5eba90
xuraoqing <xuraoqing@huawei.com>
2025-06-18 12:28:56 +0800

    [v8-38-43]arm64: RME: Configure max SVE vector length for a Realm
    community inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBY08N
    Reference: https://lore.kernel.org/kvm/20250416134208.383984-1-steven.price@arm.com/T/
    
    --------------------------------
    
    From: Jean-Philippe Brucker <jean-philippe@linaro.org>
    
    Obtain the max vector length configured by userspace on the vCPUs, and
    write it into the Realm parameters. By default the vCPU is configured
    with the max vector length reported by RMM, and userspace can reduce it
    with a write to KVM_REG_ARM64_SVE_VLS.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Steven Price <steven.price@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    ---
    Changes since v6:
     * Rename max_vl/realm_max_vl to vl/last_vl - there is nothing "maximum"
       about them, we're just checking that all realms have the same vector
       length
    
    Signed-off-by: Yiwei Zhuang <zhuangyiwei@huawei.com>
    Conflicts:arch/arm64/kvm/guest.c
    Signed-off-by: xuraoqing <xuraoqing@huawei.com>

--- arch/arm64/kvm/guest.c
+++ arch/arm64/kvm/guest.c
@@ -398,7 +398,7 @@ static int set_sve_vls(struct kvm_vcpu *vcpu, const struct kvm_one_reg *reg)
 	if (!vcpu_has_sve(vcpu))
 		return -ENOENT;
 
-	if (kvm_arm_vcpu_sve_finalized(vcpu))
+	if (kvm_arm_vcpu_sve_finalized(vcpu) || kvm_realm_is_created(vcpu->kvm))
 		return -EPERM; /* too late! */
 
 	if (WARN_ON(vcpu->arch.sve_state))
@@ -850,6 +850,11 @@ static bool validate_realm_set_reg(struct kvm_vcpu *vcpu,
 		u64 off = core_reg_offset_from_id(reg->id);
 
 		return kvm_realm_validate_core_reg(off);
+	} else {
+		switch (reg->id) {
+		case KVM_REG_ARM64_SVE_VLS:
+			return true;
+		}
 	}
 
 	return false;
