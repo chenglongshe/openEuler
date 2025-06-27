openeuler-mirror/kernel

commit 14554f1c8b052a9996a03b03516df3eecb200414

iommu/arm-smmu-v3: Reducing the CMD_SYNC times
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I9G9TI

----------------------------------------

Benifiting from the hardware same address blocking mechanism.
We only need to send the CMD_SYNC when creating pte entry other
than lvl 3, this can reduce the amounts of CMD_SYNC times sent
to avoiding the errata. Detecting the hardware ability by read
the SMMU_USER_CONFIG1.

Fixes: 46f0c5798ba5 ("iommu/arm-smmu-v3: Enable iotlb_sync_map according to SMMU_IIDR")
Signed-off-by: Zhang Zekun <zhangzekun11@huawei.com>

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -2203,6 +2203,14 @@ static void arm_smmu_tlb_inv_page_nosync(struct iommu_iotlb_gather *gather,
 static void arm_smmu_tlb_inv_walk(unsigned long iova, size_t size,
 				  size_t granule, void *cookie)
 {
+#ifdef CONFIG_HISILICON_ERRATUM_162100602
+	struct arm_smmu_domain *smmu_domain = cookie;
+
+	if (!size && smmu_domain->smmu->options & ARM_SMMU_OPT_SYNC_BATCH) {
+		arm_smmu_tlb_inv_range_domain(iova, granule, granule, true, cookie);
+		return;
+	}
+#endif
 	arm_smmu_tlb_inv_range_domain(iova, size, granule, false, cookie);
 }
 
@@ -2734,6 +2742,9 @@ static int arm_smmu_iotlb_sync_map(struct iommu_domain *domain,
 	size_t granule_size;
 
 	if (!(smmu_domain->smmu->options & ARM_SMMU_OPT_SYNC_MAP))
+		return 0;
+
+	if (smmu_domain->smmu->options & ARM_SMMU_OPT_SYNC_BATCH)
 		return 0;
 
 	granule_size = 1 <<  __ffs(smmu_domain->domain.pgsize_bitmap);
@@ -4241,6 +4252,45 @@ static void arm_smmu_get_httu(struct arm_smmu_device *smmu, u32 reg)
 			 fw_features);
 }
 
+#ifdef CONFIG_HISILICON_ERRATUM_162100602
+static void hisi_smmu_check_errata(struct arm_smmu_device *smmu)
+{
+	u32 reg, i;
+
+	if (!(smmu->options & ARM_SMMU_OPT_SYNC_MAP))
+		return;
+
+	smmu->options |= ARM_SMMU_OPT_SYNC_MAP;
+
+	reg = readl_relaxed(smmu->base + ARM_SMMU_USER_CFG1);
+	reg = reg & GENMASK(15, 0);
+	for (i = 0; i < 8; i++) {
+		unsigned long val;
+
+		val = (reg >> 2 * i) & GENMASK(1, 0);
+		switch (PAGE_SIZE) {
+		case SZ_4K:
+			if (!val)
+				return;
+			break;
+		case SZ_16K:
+			if (!val || val == 0x1)
+				return;
+			break;
+		case SZ_64K:
+			if (!val || val == 0x1 || val == 0x3)
+				return;
+			break;
+		default:
+			return;
+		}
+	}
+	smmu->options |= ARM_SMMU_OPT_SYNC_BATCH;
+}
+#else
+static void hisi_smmu_check_errata(struct arm_smmu_device *smmu) {}
+#endif
+
 static int arm_smmu_device_hw_probe(struct arm_smmu_device *smmu)
 {
 	u32 reg;
@@ -4454,6 +4504,8 @@ static int arm_smmu_device_hw_probe(struct arm_smmu_device *smmu)
 	case IDR5_OAS_48_BIT:
 		smmu->oas = 48;
 	}
+
+	hisi_smmu_check_errata(smmu);
 
 	if (arm_smmu_ops.pgsize_bitmap == -1UL)
 		arm_smmu_ops.pgsize_bitmap = smmu->pgsize_bitmap;

drivers/iommu/io-pgtable-arm.c
@@ -407,6 +407,11 @@ static int __arm_lpae_map(struct arm_lpae_io_pgtable *data, unsigned long iova,
 		pte = arm_lpae_install_table(cptep, ptep, 0, data);
 		if (pte)
 			__arm_lpae_free_pages(cptep, tblsz, cfg, data->iop.cookie);
+
+#ifdef CONFIG_HISILICON_ERRATUM_162100602
+		if (lvl <= 2)
+			io_pgtable_tlb_flush_walk(&data->iop, iova, 0, ARM_LPAE_GRANULE(data));
+#endif
 	} else if (!cfg->coherent_walk && !(pte & ARM_LPAE_PTE_SW_SYNC)) {
 		__arm_lpae_sync_pte(ptep, 1, cfg);
 	}

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
@@ -139,6 +139,8 @@
 #define ARM_SMMU_GERROR_IRQ_CFG1	0x70
 #define ARM_SMMU_GERROR_IRQ_CFG2	0x74
 
+#define ARM_SMMU_USER_CFG1		0xe04
+
 #define ARM_SMMU_IDR6			0x190
 #define IDR6_LOG2NUMP			GENMASK(27, 24)
 #define IDR6_LOG2NUMQ			GENMASK(19, 16)
@@ -708,6 +710,7 @@ struct arm_smmu_device {
 #define ARM_SMMU_OPT_MSIPOLL		(1 << 2)
 #define ARM_SMMU_OPT_CMDQ_FORCE_SYNC	(1 << 3)
 #define ARM_SMMU_OPT_SYNC_MAP		(1 << 4)
+#define ARM_SMMU_OPT_SYNC_BATCH		(1 << 5)
 	u32				options;
 
 #ifdef CONFIG_ARM_SMMU_V3_ECMDQ

chenglongshe/openEuler

commit 1eeb2758e2bf8127226138de7553848c657a4b8b

iommu/arm-smmu-v3: Fix the extra atc tlb flush
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IBZUQU

------------------------------------------

The tlb invalidation code for smmu pagetable prefetch problem might
cause a extra tlb invalidation for atc. Prevent it from happening.

Fixes: 14554f1c8b05 ("iommu/arm-smmu-v3: Reducing the CMD_SYNC times")
Signed-off-by: Zhang Zekun <zhangzekun11@huawei.com>

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -2508,7 +2508,7 @@ static void arm_smmu_tlb_inv_walk(unsigned long iova, size_t size,
 #ifdef CONFIG_HISILICON_ERRATUM_162100602
 	struct arm_smmu_domain *smmu_domain = cookie;
 
-	if (!size && smmu_domain->smmu->options & ARM_SMMU_OPT_SYNC_BATCH) {
+	if (!size) {
 		arm_smmu_tlb_inv_range_domain(iova, granule, granule, true, cookie);
 		return;
 	}
@@ -2729,6 +2729,9 @@ static int arm_smmu_domain_finalise(struct arm_smmu_domain *smmu_domain,
 		pgtbl_cfg.quirks |= IO_PGTABLE_QUIRK_ARM_BBML1;
 	else if (smmu->features & ARM_SMMU_FEAT_BBML2)
 		pgtbl_cfg.quirks |= IO_PGTABLE_QUIRK_ARM_BBML2;
+
+	if (smmu->options & ARM_SMMU_OPT_SYNC_BATCH)
+		pgtbl_cfg.quirks |= IO_PGTABLE_QUIRK_HISI_ERRATA;
 
 	pgtbl_ops = alloc_io_pgtable_ops(fmt, &pgtbl_cfg, smmu_domain);
 	if (!pgtbl_ops)

drivers/iommu/io-pgtable-arm.c
@@ -450,7 +450,7 @@ static int __arm_lpae_map(struct arm_lpae_io_pgtable *data, unsigned long iova,
 			__arm_lpae_free_pages(cptep, tblsz, cfg, data->iop.cookie);
 
 #ifdef CONFIG_HISILICON_ERRATUM_162100602
-		if (lvl <= 2)
+		if (lvl <= 2 && (cfg->quirks & IO_PGTABLE_QUIRK_HISI_ERRATA))
 			io_pgtable_tlb_flush_walk(&data->iop, iova, 0, ARM_LPAE_GRANULE(data));
 #endif
 	} else if (!cfg->coherent_walk && !(pte & ARM_LPAE_PTE_SW_SYNC)) {
@@ -1357,7 +1357,8 @@ arm_64_lpae_alloc_pgtable_s1(struct io_pgtable_cfg *cfg, void *cookie)
 			    IO_PGTABLE_QUIRK_ARM_OUTER_WBWA |
 			    IO_PGTABLE_QUIRK_ARM_HD |
 			    IO_PGTABLE_QUIRK_ARM_BBML1 |
-			    IO_PGTABLE_QUIRK_ARM_BBML2))
+			    IO_PGTABLE_QUIRK_ARM_BBML2 |
+			    IO_PGTABLE_QUIRK_HISI_ERRATA))
 		return NULL;
 
 	data = arm_lpae_alloc_pgtable(cfg);
@@ -1462,7 +1463,8 @@ arm_64_lpae_alloc_pgtable_s2(struct io_pgtable_cfg *cfg, void *cookie)
 	if (cfg->quirks & ~(IO_PGTABLE_QUIRK_ARM_HD |
 			    IO_PGTABLE_QUIRK_ARM_BBML1 |
 			    IO_PGTABLE_QUIRK_ARM_BBML2 |
-			    IO_PGTABLE_QUIRK_ARM_S2FWB))
+			    IO_PGTABLE_QUIRK_ARM_S2FWB |
+			    IO_PGTABLE_QUIRK_HISI_ERRATA))
 		return NULL;
 
 	data = arm_lpae_alloc_pgtable(cfg);

include/linux/io-pgtable.h
@@ -105,6 +105,7 @@ struct io_pgtable_cfg {
 	#define IO_PGTABLE_QUIRK_ARM_BBML1		BIT(8)
 	#define IO_PGTABLE_QUIRK_ARM_BBML2		BIT(9)
 	#define IO_PGTABLE_QUIRK_ARM_S2FWB		BIT(10)
+	#define IO_PGTABLE_QUIRK_HISI_ERRATA            BIT(11)
 	unsigned long			quirks;
 	unsigned long			pgsize_bitmap;
 	unsigned int			ias;