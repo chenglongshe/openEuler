dcbef73dc4d90ce28c3b6b32466ac2d7e8f7c8a3
nicunshu <nicunshu@huawei.com>
2025-06-28 10:17:48 +0800

    mm: gmem: change conflict flag
    euleros inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICHFJN
    
    ---------------------------------------------
    
    change conflict mmap flag used in gmem
    Fixes: 4c627cebab85 ("mm: gmem: Introduce GMEM")
    
    Signed-off-by: nicunshu <nicunshu@huawei.com>

--- include/uapi/asm-generic/mman-common.h
+++ include/uapi/asm-generic/mman-common.h
@@ -33,7 +33,7 @@
 #define MAP_UNINITIALIZED 0x4000000	/* For anonymous mmap, memory could be
 					 * uninitialized */
 
-#define MAP_PEER_SHARED		0x8000000
+#define MAP_PEER_SHARED		0x1000000
 
 /*
  * Flags for mlock
