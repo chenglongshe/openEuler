8f75627ce0a6e91eb62c44124576c4fd067db43c
Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
2025-07-01 20:47:39 +0800

    xsched: Add base vstream support
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC5EHB
    
    --------------------------------
    
    Add sys_vstream_manage() syscall.
    Add the basic function framework.
    Add basic header files.
    Add new Kconfig.xsched with XCU_SCHEDULER and XCU_VSTREAM configurations.
    Create new dir kernel/xsched with vstream.c file with
    base xsched syscalls stubs.
    
    Add Makefile in kernel/xsched. Update main kernel Makefile to include
    kernel/xsched in build.
    
    Signed-off-by: Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
    Signed-off-by: Hui Tang <tanghui20@.huawei.com>

---
+++ kernel/xsched/vstream.c
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Vstream manage for XPU device
+ *
+ * Copyright (C) 2025-2026 Huawei Technologies Co., Ltd
+ *
+ * Author: Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+#include <linux/syscalls.h>
+#include <linux/vstream.h>
+
+#ifdef CONFIG_XCU_VSTREAM
+
+int vstream_alloc(struct vstream_args *arg)
+{
+	return 0;
+}
+
+int vstream_free(struct vstream_args *arg)
+{
+	return 0;
+}
+
+int vstream_kick(struct vstream_args *arg)
+{
+	return 0;
+}
+
+/*
+ * vstream_manage_cmd table
+ */
+static vstream_manage_t(*vstream_command_table[MAX_COMMAND + 1]) = {
+	vstream_alloc, // VSTREAM_ALLOC
+	vstream_free, // VSTREAM_FREE
+	vstream_kick, // VSTREAM_KICK
+	NULL // MAX_COMMAND
+};
+
+SYSCALL_DEFINE2(vstream_manage, struct vstream_args __user *, arg, int, cmd)
+{
+	int res = 0;
+	struct vstream_args vstream_arg;
+
+	if (copy_from_user(&vstream_arg, arg, sizeof(struct vstream_args))) {
+		pr_err("copy_from_user failed\n");
+		return -EFAULT;
+	}
+
+	res = vstream_command_table[cmd](&vstream_arg);
+	if (copy_to_user(arg, &vstream_arg, sizeof(struct vstream_args))) {
+		pr_err("copy_to_user failed\n");
+		return -EFAULT;
+	}
+
+	pr_debug("vstream_manage: cmd %d\n", cmd);
+	return res;
+}
+#else
+SYSCALL_DEFINE2(vstream_manage, struct vstream_args __user *, arg, int, cmd)
+{
+	return 0;
+}
+#endif
