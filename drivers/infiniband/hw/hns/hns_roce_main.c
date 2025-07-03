afa60bc51b276dea5be28afb6b17b9e2775f3ede
Junxian Huang <huangjunxian6@hisilicon.com>
2025-07-03 15:46:54 +0800

    RDMA/hns: Fix unmatched condition in error flow
    driver inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICLOZA
    
    ----------------------------------------------------------------------
    
    pgdir_mutex is always initialized now, and it should also be always
    destroyed in error flow.
    
    Fixes: 998d0c366a26 ("[BigDipperV3R9,NeZha][ROCE] RDMA/hns: Fix accessing uninitialized resources")
    Signed-off-by: Junxian Huang <huangjunxian6@hisilicon.com>
    Signed-off-by: Donghua Huang <huangdonghua3@h-partners.com>

--- drivers/infiniband/hw/hns/hns_roce_main.c
+++ drivers/infiniband/hw/hns/hns_roce_main.c
@@ -1299,9 +1299,7 @@ static int hns_roce_setup_hca(struct hns_roce_dev *hr_dev)
 
 err_uar_table_free:
 	ida_destroy(&hr_dev->uar_ida.ida);
-	if (hr_dev->caps.flags & HNS_ROCE_CAP_FLAG_CQ_RECORD_DB ||
-	    hr_dev->caps.flags & HNS_ROCE_CAP_FLAG_QP_RECORD_DB)
-		mutex_destroy(&hr_dev->pgdir_mutex);
+	mutex_destroy(&hr_dev->pgdir_mutex);
 	mutex_destroy(&hr_dev->db_unfree_list_mutex);
 	mutex_destroy(&hr_dev->mtr_unfree_list_mutex);
 	mutex_destroy(&hr_dev->uctx_list_mutex);
