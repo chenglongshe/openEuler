openeuler-mirror/kernel

commit 6d14be01fcb032c478cd4669b32526fda0393195

urma: upload kernel patch for 20240318_happy_birthday
driver inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I91DSN
CVE: NA

--------------------------------

Upload kernel patch for 20240318_happy_birthday, detailed
fixes are as follows:
1. Modify urma_tjetty_cfg_t and urma_tjfr_cfg_t.
2. Rmv delete ctx, use close fd
3. Fix mutex lock competition long-term blocking
4. Rearrange order of function call of uobj_fd_alloc_commit
5. Add uvs_admin query_res
6. Fix perftest bug

Fixes: 329bf7f33128 ("ubcore: fix the bug of tp negotiation concurrency")
Signed-off-by: WenChen <chenwen54@huawei.com>
Signed-off-by: Jie Lei <leijie31@huawei.com>
Signed-off-by: Weibo Zhao <zhaoweibo3@huawei.com>

drivers/ub/urma/ubcore/ubcore_ctp.c
@@ -38,7 +38,9 @@ struct ubcore_ctp *ubcore_create_ctp(struct ubcore_device *dev, struct ubcore_ct
 	}
 	ctp->ub_dev = dev;
 	ctp->ctp_cfg = *cfg;
-	atomic_set(&ctp->use_cnt, 1);
+	atomic_set(&ctp->use_cnt, 0);
+	kref_init(&ctp->ref_cnt);
+	init_completion(&ctp->comp);
 
 	ret = ubcore_hash_table_find_add(&dev->ht[UBCORE_HT_CTP], &ctp->hnode, ctp->ctpn);
 	if (ret != 0) {
@@ -52,6 +54,25 @@ struct ubcore_ctp *ubcore_create_ctp(struct ubcore_device *dev, struct ubcore_ct
 	return ctp;
 }
 
+static void ubcore_ctp_kref_release(struct kref *ref_cnt)
+{
+	struct ubcore_ctp *ctp = container_of(ref_cnt, struct ubcore_ctp, ref_cnt);
+
+	complete(&ctp->comp);
+}
+
+void ubcore_ctp_kref_put(struct ubcore_ctp *ctp)
+{
+	(void)kref_put(&ctp->ref_cnt, ubcore_ctp_kref_release);
+}
+
+void ubcore_ctp_get(void *obj)
+{
+	struct ubcore_ctp *ctp = obj;
+
+	kref_get(&ctp->ref_cnt);
+}
+
 int ubcore_destroy_ctp(struct ubcore_ctp *ctp)
 {
 	struct ubcore_device *dev = ctp->ub_dev;
@@ -61,13 +82,8 @@ int ubcore_destroy_ctp(struct ubcore_ctp *ctp)
 	if (dev == NULL || dev->ops == NULL || dev->ops->destroy_ctp == NULL)
 		return -EINVAL;
 
-	if (atomic_dec_return(&ctp->use_cnt) > 0) {
-		ubcore_log_err("ctp in use");
-		return -EBUSY;
-	}
-
-	ubcore_hash_table_remove(&dev->ht[UBCORE_HT_CTP], &ctp->hnode);
-
+	ubcore_ctp_kref_put(ctp);
+	wait_for_completion(&ctp->comp);
 	ret = dev->ops->destroy_ctp(ctp);
 	if (ret != 0) {
 		(void)ubcore_hash_table_find_add(&dev->ht[UBCORE_HT_CTP], &ctp->hnode, ctp->ctpn);
@@ -83,4 +99,34 @@ int ubcore_destroy_ctp(struct ubcore_ctp *ctp)
 struct ubcore_ctp *ubcore_find_ctp(struct ubcore_device *dev, uint32_t idx)
 {
 	return ubcore_hash_table_lookup(&dev->ht[UBCORE_HT_CTP], idx, &idx);
+}
+
+struct ubcore_ctp *ubcore_find_get_ctp(struct ubcore_device *dev, uint32_t idx)
+{
+	return ubcore_hash_table_lookup_get(&dev->ht[UBCORE_HT_CTP], idx, &idx);
+}
+
+struct ubcore_ctp *ubcore_find_remove_ctp(struct ubcore_device *dev, uint32_t idx)
+{
+	struct ubcore_ctp *ctp;
+
+	spin_lock(&dev->ht[UBCORE_HT_CTP].lock);
+	if (&dev->ht[UBCORE_HT_CTP].head == NULL) {
+		spin_unlock(&dev->ht[UBCORE_HT_CTP].lock);
+		return NULL;
+	}
+	ctp = ubcore_hash_table_lookup_nolock(&dev->ht[UBCORE_HT_CTP], idx, &idx);
+	if (ctp == NULL) {
+		spin_unlock(&dev->ht[UBCORE_HT_CTP].lock);
+		return NULL;
+	}
+	if (atomic_read(&ctp->use_cnt) > 0) {
+		spin_unlock(&dev->ht[UBCORE_HT_CTP].lock);
+		ubcore_log_err("Failed to remove ctp");
+		return NULL;
+	}
+	ubcore_hash_table_remove_nolock(&dev->ht[UBCORE_HT_CTP], &ctp->hnode);
+	spin_unlock(&dev->ht[UBCORE_HT_CTP].lock);
+
+	return ctp;
 }

drivers/ub/urma/ubcore/ubcore_tpg.c
@@ -24,33 +24,57 @@
 #include "ubcore_tp.h"
 #include "ubcore_tpg.h"
 
+static int ubcore_free_wait_tpg(struct ubcore_device *dev, struct ubcore_tpg *tpg)
+{
+	ubcore_tpg_kref_put(tpg);
+	wait_for_completion(&tpg->comp);
+	return dev->ops->destroy_tpg(tpg);
+}
+
 struct ubcore_tpg *ubcore_create_tpg(struct ubcore_device *dev, struct ubcore_tpg_cfg *cfg)
 {
 	struct ubcore_tpg *tpg;
 	uint32_t i;
-	int ret;
 
 	if (dev->ops == NULL || dev->ops->create_tpg == NULL)
-		return NULL;
+		return ERR_PTR(-EINVAL);
 
 	tpg = dev->ops->create_tpg(dev, cfg, NULL);
-	if (tpg == NULL) {
+	if (IS_ERR_OR_NULL(tpg)) {
 		ubcore_log_err("Failed to create tpg");
-		return NULL;
+		if (tpg == NULL)
+			return ERR_PTR(-ENOEXEC);
+		return tpg;
 	}
 	tpg->ub_dev = dev;
 	tpg->tpg_cfg = *cfg;
 	for (i = 0; i < cfg->tp_cnt; i++)
 		tpg->tp_list[i] = NULL;
-	atomic_set(&tpg->use_cnt, 1);
+	atomic_set(&tpg->use_cnt, 0);
+	kref_init(&tpg->ref_cnt);
+	init_completion(&tpg->comp);
+	mutex_init(&tpg->mutex);
 
-	ret = ubcore_hash_table_find_add(&dev->ht[UBCORE_HT_TPG], &tpg->hnode, tpg->tpgn);
-	if (ret != 0) {
-		(void)dev->ops->destroy_tpg(tpg);
-		tpg = NULL;
-		ubcore_log_err("Failed to add tpg to the tpg table");
-	}
 	return tpg;
+}
+
+static void ubcore_tpg_kref_release(struct kref *ref_cnt)
+{
+	struct ubcore_tpg *tpg = container_of(ref_cnt, struct ubcore_tpg, ref_cnt);
+
+	complete(&tpg->comp);
+}
+
+void ubcore_tpg_kref_put(struct ubcore_tpg *tpg)
+{
+	(void)kref_put(&tpg->ref_cnt, ubcore_tpg_kref_release);
+}
+
+void ubcore_tpg_get(void *obj)
+{
+	struct ubcore_tpg *tpg = obj;
+
+	kref_get(&tpg->ref_cnt);
 }
 
 int ubcore_destroy_tpg(struct ubcore_tpg *tpg)
@@ -61,14 +85,7 @@ int ubcore_destroy_tpg(struct ubcore_tpg *tpg)
 	if (dev->ops == NULL || dev->ops->destroy_tpg == NULL)
 		return -EINVAL;
 
-	if (atomic_dec_return(&tpg->use_cnt) > 0) {
-		ubcore_log_err("tpg in use");
-		return -EBUSY;
-	}
-
-	ubcore_hash_table_remove(&dev->ht[UBCORE_HT_TPG], &tpg->hnode);
-
-	ret = dev->ops->destroy_tpg(tpg);
+	ret = ubcore_free_wait_tpg(dev, tpg);
 	if (ret != 0) {
 		(void)ubcore_hash_table_find_add(&dev->ht[UBCORE_HT_TPG], &tpg->hnode, tpg->tpgn);
 		/* inc tpg use cnt? */
@@ -81,6 +98,74 @@ int ubcore_destroy_tpg(struct ubcore_tpg *tpg)
 struct ubcore_tpg *ubcore_find_tpg(struct ubcore_device *dev, uint32_t tpgn)
 {
 	return ubcore_hash_table_lookup(&dev->ht[UBCORE_HT_TPG], tpgn, &tpgn);
+}
+
+struct ubcore_tpg *ubcore_find_get_tpg(struct ubcore_device *dev, uint32_t tpgn)
+{
+	return ubcore_hash_table_lookup_get(&dev->ht[UBCORE_HT_TPG], tpgn, &tpgn);
+}
+
+struct ubcore_tpg *ubcore_find_remove_tpg(struct ubcore_device *dev, uint32_t tpgn)
+{
+	struct ubcore_tpg *tpg;
+
+	spin_lock(&dev->ht[UBCORE_HT_TPG].lock);
+	if (&dev->ht[UBCORE_HT_TPG].head == NULL) {
+		spin_unlock(&dev->ht[UBCORE_HT_TPG].lock);
+		return NULL;
+	}
+	tpg = ubcore_hash_table_lookup_nolock(&dev->ht[UBCORE_HT_TPG], tpgn, &tpgn);
+	if (tpg == NULL) {
+		spin_unlock(&dev->ht[UBCORE_HT_TPG].lock);
+		return NULL;
+	}
+	if (atomic_read(&tpg->use_cnt) > 0) {
+		spin_unlock(&dev->ht[UBCORE_HT_TPG].lock);
+		ubcore_log_err("Failed to remove tpg");
+		return NULL;
+	}
+	ubcore_hash_table_remove_nolock(&dev->ht[UBCORE_HT_TPG], &tpg->hnode);
+	spin_unlock(&dev->ht[UBCORE_HT_TPG].lock);
+
+	return tpg;
+}
+
+int ubcore_add_tp(struct ubcore_device *dev, struct ubcore_tp *tp)
+{
+	return ubcore_hash_table_find_add(&dev->ht[UBCORE_HT_UB_TP],
+		&tp->hnode, tp->tpn);
+}
+
+struct ubcore_tp *ubcore_find_get_tp(struct ubcore_device *dev, uint32_t tpn)
+{
+	return ubcore_hash_table_lookup_get(&dev->ht[UBCORE_HT_UB_TP], tpn, &tpn);
+}
+
+struct ubcore_tp *ubcore_find_remove_tp_node(struct ubcore_device *dev, uint32_t tpn)
+{
+	struct ubcore_tp *tp;
+
+	spin_lock(&dev->ht[UBCORE_HT_UB_TP].lock);
+	if (&dev->ht[UBCORE_HT_UB_TP].head == NULL) {
+		spin_unlock(&dev->ht[UBCORE_HT_UB_TP].lock);
+		return NULL;
+	}
+	tp = ubcore_hash_table_lookup_nolock(&dev->ht[UBCORE_HT_UB_TP], tpn, &tpn);
+	if (tp == NULL) {
+		spin_unlock(&dev->ht[UBCORE_HT_UB_TP].lock);
+		return NULL;
+	}
+
+	if (atomic_dec_return(&tp->use_cnt) > 0) {
+		spin_unlock(&dev->ht[UBCORE_HT_UB_TP].lock);
+		ubcore_log_warn("Failed to remove tp:%u and use cnt:%u",
+			tp->tpn, (uint32_t)atomic_read(&tp->use_cnt));
+		return NULL;
+	}
+	ubcore_hash_table_remove_nolock(&dev->ht[UBCORE_HT_UB_TP], &tp->hnode);
+	spin_unlock(&dev->ht[UBCORE_HT_UB_TP].lock);
+
+	return tp;
 }
 
 static void ubcore_set_tp_init_flag(union ubcore_tp_flag *flag, union ubcore_tp_cfg_flag in)
@@ -116,58 +201,97 @@ static void ubcore_store_tp_init_cfg(struct ubcore_tpg *tpg, struct ubcore_tp *t
 int ubcore_create_multi_tp(struct ubcore_device *dev, struct ubcore_tpg *tpg,
 	struct ubcore_tp_cfg *cfg)
 {
-	struct ubcore_tp *tp[UBCORE_MAX_TP_CNT_IN_GRP];
+	struct ubcore_tp *tp[UBCORE_MAX_TP_CNT_IN_GRP] = {0};
 	uint32_t tp_cnt = tpg->tpg_cfg.tp_cnt;
+	int created_tp_cnt;
 	uint32_t i;
+	uint32_t j;
 	int ret;
 
 	if (dev->ops == NULL || dev->ops->create_multi_tp == NULL ||
 		dev->ops->destroy_multi_tp == NULL)
 		return -EINVAL;
 
-	ret = dev->ops->create_multi_tp(dev, tp_cnt, cfg, NULL, tp);
-	if (ret != (int)tp_cnt) {
-		if (ret > 0)
-			(void)dev->ops->destroy_multi_tp(ret, tp);
+	created_tp_cnt = dev->ops->create_multi_tp(dev, tp_cnt, cfg, NULL, tp);
+	if (created_tp_cnt != (int)tp_cnt) {
+		if (created_tp_cnt > 0)
+			(void)dev->ops->destroy_multi_tp(created_tp_cnt, tp);
 		ubcore_log_err("Failed to create multi tp");
 		return -ENOSPC;
 	}
 
 	/* add tp to tpg */
 	for (i = 0; i < tp_cnt; i++) {
-		if (tp[i] == NULL) {
+		struct ubcore_tp *single_tp = tp[i];
+
+		if (single_tp == NULL) {
 			ubcore_log_warn("create multi tp, buf tp is null_ptr");
 			continue;
 		}
-		ubcore_store_tp_init_cfg(tpg, tp[i], dev, &cfg[i]);
-		tpg->tp_list[i] = tp[i];
+		kref_init(&single_tp->ref_cnt);
+		init_completion(&single_tp->comp);
+		ubcore_store_tp_init_cfg(tpg, single_tp, dev, &cfg[i]);
+		tpg->tp_list[i] = single_tp;
+
+		ret = ubcore_add_tp(dev, single_tp);
+		if (ret != 0) {
+			ubcore_log_err("Failed to add tp:%u to the tp table and tpg:%u, dev:%s",
+				single_tp->tpn, tpg->tpgn, dev->dev_name);
+			for (j = 0; j < i; j++)
+				(void)ubcore_find_remove_tp_node(dev, tp[j]->tpn);
+
+			(void)dev->ops->destroy_multi_tp(created_tp_cnt, tp);
+			ubcore_log_err("Failed to create multi tp");
+			return -EPERM;
+		}
+		ubcore_log_debug("success to add tp:%u to the tp table and tpg:%u, dev:%s",
+				single_tp->tpn, tpg->tpgn, dev->dev_name);
 	}
 
 	return 0;
 }
 
-uint32_t ubcore_destroy_multi_tp(struct ubcore_device *dev, struct ubcore_tpg *tpg)
+uint32_t ubcore_destroy_multi_tp_from_index(struct ubcore_device *dev, struct ubcore_tpg *tpg,
+	uint32_t from_index)
 {
 	struct ubcore_tp *tp[UBCORE_MAX_TP_CNT_IN_GRP];
-	uint32_t tp_cnt = tpg->tpg_cfg.tp_cnt;
+	uint32_t tp_cnt = tpg->tpg_cfg.tp_cnt - from_index;
+	struct ubcore_tp *single_tp;
 	int ret;
 	int i;
 
 	if (dev->ops == NULL || dev->ops->destroy_multi_tp == NULL)
 		return 0;
 
-	for (i = 0; i < (int)tp_cnt; i++)
-		tp[i] = tpg->tp_list[i];
+	mutex_lock(&tpg->mutex);
+	for (i = 0; i < (int)tp_cnt; i++) {
+		tp[i] = tpg->tp_list[i + (int)from_index];
+		tpg->tp_list[i + (int)from_index] = NULL;
+		ubcore_log_info("tp set null %d", i + (int)from_index);
+	}
+	mutex_unlock(&tpg->mutex);
+
+	for (i = 0; i < (int)tp_cnt; i++) {
+		ubcore_tp_kref_put(tp[i]);
+		wait_for_completion(&tp[i]->comp);
+
+		single_tp = ubcore_find_remove_tp_node(dev, tp[i]->tpn);
+		if (single_tp == NULL)
+			ubcore_log_err("failed to find tp with tpn %u and tpgn %u, dev:%s",
+				tp[i]->tpn, tp[i]->tpg->tpgn, dev->dev_name);
+	}
 
 	/* todonext: modify to error, and reset first */
 	ret = dev->ops->destroy_multi_tp(tp_cnt, tp);
 	if (ret != (int)tp_cnt)
 		ubcore_log_err("Failed to destroy multi tp %d", ret);
 
-	for (i = 0; (i < ret) && (i < UBCORE_MAX_TP_CNT_IN_GRP); i++)
-		tpg->tp_list[i] = NULL;
-
 	return (ret > 0 ? (uint32_t)ret : 0);
+}
+
+uint32_t ubcore_destroy_multi_tp(struct ubcore_device *dev, struct ubcore_tpg *tpg)
+{
+	return ubcore_destroy_multi_tp_from_index(dev, tpg, 0);
 }
 
 uint32_t ubcore_modify_tp_in_tpg(struct ubcore_device *dev, struct ubcore_tpg *tpg,
@@ -179,6 +303,11 @@ uint32_t ubcore_modify_tp_in_tpg(struct ubcore_device *dev, struct ubcore_tpg *t
 	if (dev->ops == NULL || dev->ops->modify_multi_tp == NULL)
 		return 0;
 
+	mutex_lock(&tpg->mutex);
+	for (i = 0; i < (int)tpg->tpg_cfg.tp_cnt; i++)
+		ubcore_tp_get(tpg->tp_list[i]);
+	mutex_unlock(&tpg->mutex);
+
 	ret = dev->ops->modify_multi_tp(tpg->tpg_cfg.tp_cnt, tpg->tp_list, attr, mask, failed_tp);
 	if (ret != (int)tpg->tpg_cfg.tp_cnt)
 		ubcore_log_err("Failed to modify multi tp %d and tpgn %u ", ret, tpg->tpgn);
@@ -186,6 +315,8 @@ uint32_t ubcore_modify_tp_in_tpg(struct ubcore_device *dev, struct ubcore_tpg *t
 	for (i = 0; (i < ret && i < UBCORE_MAX_TP_CNT_IN_GRP); i++)
 		ubcore_modify_tp_attr(tpg->tp_list[i], &attr[i], mask[i]);
 
+	for (i = 0; i < (int)tpg->tpg_cfg.tp_cnt; i++)
+		ubcore_tp_kref_put(tpg->tp_list[i]);
 	return (ret > 0 ? (uint32_t)ret : 0);
 }
 
@@ -199,6 +330,25 @@ struct ubcore_tp *ubcore_find_tp_in_tpg(struct ubcore_tpg *tpg, uint32_t tpn)
 
 		return tpg->tp_list[i];
 	}
+
+	return NULL;
+}
+
+struct ubcore_tp *ubcore_find_get_tp_in_tpg(struct ubcore_tpg *tpg, uint32_t tpn)
+{
+	uint32_t i;
+
+	mutex_lock(&tpg->mutex);
+	for (i = 0; i < tpg->tpg_cfg.tp_cnt; i++) {
+		if (tpg->tp_list[i] == NULL || tpg->tp_list[i]->tpn != tpn)
+			continue;
+
+		ubcore_tp_get(tpg->tp_list[i]);
+		mutex_unlock(&tpg->mutex);
+
+		return tpg->tp_list[i];
+	}
+	mutex_unlock(&tpg->mutex);
 
 	return NULL;
 }

drivers/ub/urma/ubcore/ubcore_utp.c
@@ -21,9 +21,10 @@
 #include <linux/slab.h>
 #include "ubcore_log.h"
 #include "ubcore_hash_table.h"
+#include "ubcore_device.h"
 #include "ubcore_utp.h"
 
-static int utp_get_active_mtu(struct ubcore_device *dev, uint8_t port_num,
+int utp_get_active_mtu(struct ubcore_device *dev, uint8_t port_num,
 	enum ubcore_mtu *mtu)
 {
 	struct ubcore_device_status st = { 0 };
@@ -45,24 +46,32 @@ static int utp_get_active_mtu(struct ubcore_device *dev, uint8_t port_num,
 	return 0;
 }
 
+static void ubcore_utp_kref_release(struct kref *ref_cnt)
+{
+	struct ubcore_utp *utp = container_of(ref_cnt, struct ubcore_utp, ref_cnt);
+
+	complete(&utp->comp);
+}
+
+void ubcore_utp_kref_put(struct ubcore_utp *utp)
+{
+	(void)kref_put(&utp->ref_cnt, ubcore_utp_kref_release);
+}
+
+void ubcore_utp_get(void *obj)
+{
+	struct ubcore_utp *utp = obj;
+
+	kref_get(&utp->ref_cnt);
+}
+
 struct ubcore_utp *ubcore_create_utp(struct ubcore_device *dev, struct ubcore_utp_cfg *cfg)
 {
 	struct ubcore_utp *utp;
-	enum ubcore_mtu mtu;
 	int ret;
 
 	if (dev->ops == NULL || dev->ops->create_utp == NULL)
 		return NULL;
-
-	if (((int32_t)cfg->mtu) == 0) {
-		ret = utp_get_active_mtu(dev, (uint8_t)cfg->port_id, &mtu);
-		if (ret < 0) {
-			ubcore_log_warn("Failed to get active mtu, use default 1024");
-			mtu = UBCORE_MTU_1024;
-		}
-		cfg->mtu = mtu;
-		ubcore_log_info("Global cfg not config, device mtu is %d", (int32_t)cfg->mtu);
-	}
 
 	ubcore_log_info("Utp mtu config to %u", (uint32_t)cfg->mtu);
 
@@ -73,7 +82,9 @@ struct ubcore_utp *ubcore_create_utp(struct ubcore_device *dev, struct ubcore_ut
 	}
 	utp->ub_dev = dev;
 	utp->utp_cfg = *cfg;
-	atomic_set(&utp->use_cnt, 1);
+	atomic_set(&utp->use_cnt, 0);
+	kref_init(&utp->ref_cnt);
+	init_completion(&utp->comp);
 
 	ret = ubcore_hash_table_find_add(&dev->ht[UBCORE_HT_UTP], &utp->hnode, utp->utpn);
 	if (ret != 0) {
@@ -96,13 +107,8 @@ int ubcore_destroy_utp(struct ubcore_utp *utp)
 	if (dev->ops == NULL || dev->ops->destroy_utp == NULL)
 		return -EINVAL;
 
-	if (atomic_dec_return(&utp->use_cnt) > 0) {
-		ubcore_log_err("utp in use");
-		return -EBUSY;
-	}
-
-	ubcore_hash_table_remove(&dev->ht[UBCORE_HT_UTP], &utp->hnode);
-
+	ubcore_utp_kref_put(utp);
+	wait_for_completion(&utp->comp);
 	ret = dev->ops->destroy_utp(utp);
 	if (ret != 0) {
 		(void)ubcore_hash_table_find_add(&dev->ht[UBCORE_HT_UTP], &utp->hnode, utp->utpn);
@@ -118,4 +124,34 @@ int ubcore_destroy_utp(struct ubcore_utp *utp)
 struct ubcore_utp *ubcore_find_utp(struct ubcore_device *dev, uint32_t idx)
 {
 	return ubcore_hash_table_lookup(&dev->ht[UBCORE_HT_UTP], idx, &idx);
+}
+
+struct ubcore_utp *ubcore_find_get_utp(struct ubcore_device *dev, uint32_t idx)
+{
+	return ubcore_hash_table_lookup_get(&dev->ht[UBCORE_HT_UTP], idx, &idx);
+}
+
+struct ubcore_utp *ubcore_find_remove_utp(struct ubcore_device *dev, uint32_t idx)
+{
+	struct ubcore_utp *utp;
+
+	spin_lock(&dev->ht[UBCORE_HT_UTP].lock);
+	if (&dev->ht[UBCORE_HT_UTP].head == NULL) {
+		spin_unlock(&dev->ht[UBCORE_HT_UTP].lock);
+		return NULL;
+	}
+	utp = ubcore_hash_table_lookup_nolock(&dev->ht[UBCORE_HT_UTP], idx, &idx);
+	if (utp == NULL) {
+		spin_unlock(&dev->ht[UBCORE_HT_UTP].lock);
+		return NULL;
+	}
+	if (atomic_read(&utp->use_cnt) > 0) {
+		spin_unlock(&dev->ht[UBCORE_HT_UTP].lock);
+		ubcore_log_err("Failed to remove utp");
+		return NULL;
+	}
+	ubcore_hash_table_remove_nolock(&dev->ht[UBCORE_HT_UTP], &utp->hnode);
+	spin_unlock(&dev->ht[UBCORE_HT_UTP].lock);
+
+	return utp;
 }

drivers/ub/hw/hns3/hns3_udma_jetty.c
@@ -300,7 +300,7 @@ static int alloc_common_jetty_id(struct udma_dev *udma_dev, struct udma_jetty *j
 			free_common_qpn(udma_dev, jetty->jetty_id);
 		return ret;
 	}
-	jetty->ubcore_jetty.id = jetty->jetty_id;
+	jetty->ubcore_jetty.jetty_id.id = jetty->jetty_id;
 
 	return ret;
 }

drivers/ub/hw/hns3/hns3_udma_jfr.c
@@ -369,7 +369,7 @@ static int alloc_jfrc(struct udma_dev *dev, struct udma_jfr *jfr)
 	}
 
 	jfr->srqn = (uint32_t)id;
-	jfr->ubcore_jfr.id = jfr->jfrn;
+	jfr->ubcore_jfr.jfr_id.id = jfr->jfrn;
 
 	ret = udma_table_get(dev, &jfr_table->table, jfr->srqn);
 	if (ret) {

drivers/ub/hw/hns3/hns3_udma_jfs.c
@@ -178,8 +178,7 @@ static int alloc_jfs_id(struct udma_dev *udma_dev, struct udma_jfs *jfs)
 		dev_err(udma_dev->dev, "failed to store JFS, ret = %d.\n", ret);
 		goto store_err;
 	}
-
-	jfs->ubcore_jfs.id = jfs->jfs_id;
+	jfs->ubcore_jfs.jfs_id.id = jfs->jfs_id;
 
 	return ret;
 store_err:

drivers/ub/urma/ubcore/ubcore_cdev_file.c
@@ -37,8 +37,6 @@ typedef ssize_t (*ubcore_store_attr_cb)(struct ubcore_device *dev,
 	const char *buf, size_t len);
 typedef ssize_t (*ubcore_show_port_attr_cb)(struct ubcore_device *dev,
 	char *buf, uint8_t port_id);
-typedef ssize_t (*ubcore_show_eid_attr_cb)(struct ubcore_device *dev,
-	char *buf, uint16_t idx, struct net *net);
 
 static inline struct ubcore_device *get_ubcore_device(struct ubcore_logic_device *ldev)
 {
@@ -383,7 +381,8 @@ static DEVICE_ATTR_RO(ceq_cnt);
 
 static ssize_t utp_cnt_show_cb(struct ubcore_device *dev, char *buf)
 {
-	return snprintf(buf, UBCORE_MAX_VALUE_LEN, "%u\n", dev->attr.dev_cap.utp_cnt);
+	return snprintf(buf, UBCORE_MAX_VALUE_LEN, "%u\n",
+		dev->attr.dev_cap.max_utp_cnt);
 }
 
 static ssize_t utp_cnt_show(struct device *dev, struct device_attribute *attr, char *buf)
@@ -487,6 +486,50 @@ static ssize_t driver_name_show(struct device *dev, struct device_attribute *att
 
 static DEVICE_ATTR_RO(driver_name);
 
+/* One eid line has upto 51 bytes with the format:
+ * "4294967295 0000:0000:0000:0000:0000:ffff:7f00:0001\n"
+ * sysfs buf size is PAGESIZE, upto 80 eid lines are supported in the sysfs
+ */
+#define UBCORE_MAX_EID_LINE 51
+
+static ssize_t eid_show_cb(struct ubcore_device *dev, char *buf, struct net *net)
+{
+	struct ubcore_eid_entry *e;
+	ssize_t len = 0;
+	uint32_t i;
+
+	if (dev->eid_table.eid_entries == NULL)
+		return -EINVAL;
+
+	spin_lock(&dev->eid_table.lock);
+	for (i = 0; i < dev->eid_table.eid_cnt; i++) {
+		e = &dev->eid_table.eid_entries[i];
+		if (!e->valid || !net_eq(e->net, net))
+			continue;
+		len += snprintf(buf + len, UBCORE_MAX_EID_LINE,
+			"%u "EID_FMT"\n", i, EID_ARGS(e->eid));
+		if (len >= (ssize_t)(PAGE_SIZE - UBCORE_MAX_EID_LINE))
+			break;
+	}
+	spin_unlock(&dev->eid_table.lock);
+	return len;
+}
+
+static ssize_t eid_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct ubcore_logic_device *ldev = dev_get_drvdata(dev);
+	struct ubcore_device *ub_dev = get_ubcore_device(ldev);
+
+	if (!ldev || !ub_dev || !buf) {
+		ubcore_log_err("Invalid argument.\n");
+		return -EINVAL;
+	}
+
+	return eid_show_cb(ub_dev, buf, read_pnet(&ldev->net));
+}
+static DEVICE_ATTR_RO(eid);
+
+
 static struct attribute *ubcore_dev_attrs[] = {
 	&dev_attr_ubdev.attr,
 	&dev_attr_guid.attr,
@@ -521,6 +564,7 @@ static struct attribute *ubcore_dev_attrs[] = {
 	&dev_attr_virtualization.attr,
 	&dev_attr_transport_type.attr,
 	&dev_attr_driver_name.attr,
+	&dev_attr_eid.attr,
 	NULL,
 };
 
@@ -700,101 +744,6 @@ int ubcore_create_port_attr_files(struct ubcore_logic_device *ldev,
 		"port%hhu", port_id);
 }
 
-static ssize_t ubcore_show_eid_attr(struct ubcore_eid_kobj *eid, struct ubcore_eid_attribute *attr,
-	char *buf, ubcore_show_eid_attr_cb show_cb)
-{
-	struct ubcore_logic_device *ldev = eid->ldev;
-	struct ubcore_device *dev = get_ubcore_device(ldev);
-
-	if (!ldev || !dev) {
-		ubcore_log_err("Invalid argument in show_eid_attr.\n");
-		return -EINVAL;
-	}
-
-	return show_cb(dev, buf, eid->eid_idx, read_pnet(&ldev->net));
-}
-
-static ssize_t show_eid_cb(struct ubcore_device *dev, char *buf, uint16_t idx, struct net *net)
-{
-	struct ubcore_eid_entry *e;
-	union ubcore_eid eid;
-
-	e = &dev->eid_table.eid_entries[idx];
-	if (e->valid && net_eq(e->net, net)) {
-		return snprintf(buf, (UBCORE_EID_STR_LEN + 1) + 1, EID_FMT"\n",
-			EID_ARGS(e->eid));
-	} else {
-		memset(&eid, 0, sizeof(union ubcore_eid));
-		return snprintf(buf, (UBCORE_EID_STR_LEN + 1) + 1, EID_FMT"\n",
-			EID_ARGS(eid));
-	}
-}
-
-static ssize_t eid_show(struct ubcore_eid_kobj *eid, struct ubcore_eid_attribute *attr, char *buf)
-{
-	return ubcore_show_eid_attr(eid, attr, buf, show_eid_cb);
-}
-
-static EID_ATTR_RO(eid);
-
-static struct attribute *ubcore_eid_attrs[] = {
-	&eid_attr_eid.attr,
-	NULL,
-};
-
-static ssize_t ubcore_eid_attr_show(struct kobject *kobj, struct attribute *attr, char *buf)
-{
-	struct ubcore_eid_attribute *eid_attr =
-		container_of(attr, struct ubcore_eid_attribute, attr);
-	struct ubcore_eid_kobj *eid = container_of(kobj, struct ubcore_eid_kobj, kobj);
-
-	if (!eid_attr->show)
-		return -EIO;
-
-	return eid_attr->show(eid, eid_attr, buf);
-}
-
-static ssize_t ubcore_eid_attr_store(struct kobject *kobj, struct attribute *attr,
-	const char *buf, size_t count)
-{
-	struct ubcore_eid_attribute *eid_attr =
-		container_of(attr, struct ubcore_eid_attribute, attr);
-	struct ubcore_eid_kobj *eid = container_of(kobj, struct ubcore_eid_kobj, kobj);
-
-	if (!eid_attr->store)
-		return -EIO;
-	return eid_attr->store(eid, eid_attr, buf, count);
-}
-
-static const struct sysfs_ops ubcore_eid_sysfs_ops = {
-	.show	= ubcore_eid_attr_show,
-	.store	= ubcore_eid_attr_store
-};
-
-static void ubcore_eid_release(struct kobject *kobj)
-{
-}
-
-// ATTRIBUTE_GROUPS defined in 3.11, but must be consistent with kobj_type->default_groups
-ATTRIBUTE_GROUPS(ubcore_eid);
-
-static struct kobj_type ubcore_eid_type = {
-	.release       = ubcore_eid_release,
-	.sysfs_ops     = &ubcore_eid_sysfs_ops,
-	.default_groups = ubcore_eid_groups
-};
-
-int ubcore_create_eid_attr_files(struct ubcore_logic_device *ldev, uint32_t eid_num)
-{
-	struct ubcore_eid_kobj *eid;
-
-	eid = &ldev->eid[eid_num];
-	eid->ldev = ldev;
-	eid->eid_idx = eid_num;
-
-	return kobject_init_and_add(&eid->kobj, &ubcore_eid_type, &ldev->dev->kobj,
-		"eid%u", eid_num);
-}
 
 int ubcore_create_dev_attr_files(struct ubcore_logic_device *ldev)
 {
@@ -814,41 +763,14 @@ void ubcore_remove_port_attr_files(struct ubcore_logic_device *ldev, uint8_t por
 	kobject_put(&ldev->port[port_id].kobj);
 }
 
-void ubcore_remove_eid_attr_files(struct ubcore_logic_device *ldev, uint32_t eid_num)
-{
-	kobject_put(&ldev->eid[eid_num].kobj);
-}
-
 void ubcore_remove_dev_attr_files(struct ubcore_logic_device *ldev)
 {
 	sysfs_remove_group(&ldev->dev->kobj, &ubcore_dev_attr_group);
 }
 
-static int ubcore_create_eid_table(struct ubcore_logic_device *ldev, struct ubcore_device *dev)
-{
-	struct ubcore_eid_kobj *eid_list;
-
-	eid_list = kcalloc(
-		1, dev->attr.dev_cap.max_eid_cnt * sizeof(struct ubcore_eid_kobj), GFP_ATOMIC);
-	if (eid_list == NULL)
-		return -ENOMEM;
-
-	ldev->eid = eid_list;
-	return 0;
-}
-
-static void ubcore_destroy_eid_table(struct ubcore_logic_device *ldev)
-{
-	if (ldev->eid != NULL) {
-		kfree(ldev->eid);
-		ldev->eid = NULL;
-	}
-}
-
 int ubcore_fill_logic_device_attr(struct ubcore_logic_device *ldev,
 	struct ubcore_device *dev)
 {
-	uint32_t e1, e2; /* eid */
 	uint8_t p1, p2; /* port */
 
 	if (ubcore_create_dev_attr_files(ldev) != 0) {
@@ -862,21 +784,7 @@ int ubcore_fill_logic_device_attr(struct ubcore_logic_device *ldev,
 			goto err_port_attr;
 	}
 
-	/* create /sys/class/ubcore/<dev->dev_name>/eid* */
-	if (ubcore_create_eid_table(ldev, dev) != 0)
-		goto err_port_attr;
-
-	for (e1 = 0; e1 < dev->attr.dev_cap.max_eid_cnt; e1++) {
-		if (ubcore_create_eid_attr_files(ldev, e1) != 0)
-			goto err_eid_attr;
-	}
 	return 0;
-
-err_eid_attr:
-	for (e2 = 0; e2 < e1; e2++)
-		ubcore_remove_eid_attr_files(ldev, e2);
-
-	ubcore_destroy_eid_table(ldev);
 
 err_port_attr:
 	for (p2 = 0; p2 < p1; p2++)
@@ -889,13 +797,7 @@ err_port_attr:
 void ubcore_unfill_logic_device_attr(struct ubcore_logic_device *ldev,
 	struct ubcore_device *dev)
 {
-	uint32_t e;
 	uint8_t p;
-
-	for (e = 0; e < dev->attr.dev_cap.max_eid_cnt; e++)
-		ubcore_remove_eid_attr_files(ldev, e);
-
-	ubcore_destroy_eid_table(ldev);
 
 	for (p = 0; p < dev->attr.port_cnt; p++)
 		ubcore_remove_port_attr_files(ldev, p);

chenglongshe/openEuler

commit 9ff992884b99ae21848bd8e68614e1c1439c16ef

drivers:ub: fix invalid null-pointer validation on variable address
h3c inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICLB22

--------------------------------

UB module implements null checks on var address in several places.
These should be changed to check the variables directly.

Fixes: 6d14be01fcb0 (urma: upload kernel patch for 20240318_happy_birthday)
Signed-off-by: zhangzhengming <zhang.zhengming@h3c.com>

drivers/ub/urma/ubcore/ubcore_ctp.c
@@ -111,7 +111,7 @@ struct ubcore_ctp *ubcore_find_remove_ctp(struct ubcore_device *dev, uint32_t id
 	struct ubcore_ctp *ctp;
 
 	spin_lock(&dev->ht[UBCORE_HT_CTP].lock);
-	if (&dev->ht[UBCORE_HT_CTP].head == NULL) {
+	if (dev->ht[UBCORE_HT_CTP].head == NULL) {
 		spin_unlock(&dev->ht[UBCORE_HT_CTP].lock);
 		return NULL;
 	}

drivers/ub/urma/ubcore/ubcore_tpg.c
@@ -97,7 +97,7 @@ int ubcore_find_remove_tpg(struct ubcore_device *dev, uint32_t tpgn)
 	struct ubcore_tpg *tpg;
 
 	spin_lock(&dev->ht[UBCORE_HT_TPG].lock);
-	if (&dev->ht[UBCORE_HT_TPG].head == NULL) {
+	if (dev->ht[UBCORE_HT_TPG].head == NULL) {
 		spin_unlock(&dev->ht[UBCORE_HT_TPG].lock);
 		return -1;
 	}
@@ -128,7 +128,7 @@ struct ubcore_tp *ubcore_find_remove_tp_node(struct ubcore_device *dev, uint32_t
 	struct ubcore_tp *tp;
 
 	spin_lock(&dev->ht[UBCORE_HT_TP].lock);
-	if (&dev->ht[UBCORE_HT_TP].head == NULL) {
+	if (dev->ht[UBCORE_HT_TP].head == NULL) {
 		spin_unlock(&dev->ht[UBCORE_HT_TP].lock);
 		return NULL;
 	}

drivers/ub/urma/ubcore/ubcore_utp.c
@@ -127,7 +127,7 @@ void ubcore_find_remove_utp(struct ubcore_device *dev, uint32_t idx)
 	struct ubcore_utp *utp;
 
 	spin_lock(&dev->ht[UBCORE_HT_UTP].lock);
-	if (&dev->ht[UBCORE_HT_UTP].head == NULL) {
+	if (dev->ht[UBCORE_HT_UTP].head == NULL) {
 		spin_unlock(&dev->ht[UBCORE_HT_UTP].lock);
 		return;
 	}