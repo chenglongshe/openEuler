a6b3995405ab9ca89abc114393600e3ac604e177
Jinjiang Tu <tujinjiang@huawei.com>
2025-07-17 14:27:14 +0800

    mm/pfn_range_alloc: introduce set_linear_mapping_invalid
    hulk inclusion
    category: cleanup
    bugzilla: https://gitee.com/openeuler/kernel/issues/ID3Q7D
    
    --------------------------------
    
    Introduce set_linear_mapping_invalid to mark the range of linear mapping
    to be invalid or valid.
    
    Signed-off-by: Jinjiang Tu <tujinjiang@huawei.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -4220,12 +4220,20 @@ void vma_pgtable_walk_end(struct vm_area_struct *vma);
 extern unsigned long contig_mem_pool_percent;
 struct folio *pfn_range_alloc(unsigned int nr_pages, int nid);
 int pfn_range_free(struct folio *folio);
+int set_linear_mapping_invalid(unsigned long start_pfn, unsigned long end_pfn,
+										bool set_invalid);
 #else
 static inline struct folio *pfn_range_alloc(unsigned int nr_pages, int nid)
 {
 	return ERR_PTR(-EINVAL);
 }
 static inline int pfn_range_free(struct folio *folio)
+{
+	return -EINVAL;
+}
+static inline
+int set_linear_mapping_invalid(unsigned long start_pfn, unsigned long end_pfn,
+										bool set_invalid)
 {
 	return -EINVAL;
 }
