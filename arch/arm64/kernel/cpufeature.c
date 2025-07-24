3f903fda5de0cd57b492ff160f76242b707b2164
Jinjie Ruan <ruanjinjie@huawei.com>
2025-07-24 20:36:11 +0800

    arm64: Add hardware xcall framework support
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/release-management/issues/IBV2E4
    
    --------------------------------
    
    In the svc exception handling process of the ARM64, some auxiliary
    functions such as debug/trace and core functions such as sched/signal
    need to be considered. As a result, the svc exception handling process
    is very "lengthy". In some short syscalls (such as lseek(), getpid(),
    etc.) handles the syscall function itself with a small percentage of
    instructions, which also leads to inefficient execution of such
    syscalls.
    
    The idea of xcall can be regarded as a fast svc exception handling path.
    In the processing process, only the context save and restore and
    syscall functions are considered. It can be understood that xcall is a
    fast syscall processing mechanism between vdso and normal syscalls.
    
    The svc instruction with the immediate 0xffff is used to transfer xcall
    information. At the synchronization exception handling entry of the
    arm64 vectors, the ESR_EL1 register is used to distinguish xcall from
    normal syscalls.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -2432,17 +2432,37 @@ static void mpam_extra_caps(void)
 #ifdef CONFIG_FAST_SYSCALL
 #include <asm/xcall.h>
 DEFINE_STATIC_KEY_FALSE(xcall_enable);
+DEFINE_STATIC_KEY_FALSE(has_hwcap_actlr_xcall);
+
+static bool is_arch_xcall_xint_support(void)
+{
+	/* List of CPUs that support Xcall/Xint */
+	static const struct midr_range xcall_xint_cpus[] = {
+		MIDR_ALL_VERSIONS(MIDR_HISI_HIP12),
+		{ /* sentinel */ }
+	};
+
+	if (is_midr_in_range_list(read_cpuid_id(), xcall_xint_cpus))
+		return true;
+
+	return false;
+}
 
 static int __init xcall_setup(char *str)
 {
-	static_branch_enable(&xcall_enable);
+	if (!is_arch_xcall_xint_support())
+		static_branch_enable(&xcall_enable);
+
 	return 1;
 }
 __setup("xcall", xcall_setup);
 
 static bool has_xcall_support(const struct arm64_cpu_capabilities *entry, int __unused)
 {
-	return static_branch_unlikely(&xcall_enable);
+	if (static_branch_unlikely(&xcall_enable))
+		return true;
+
+	return false;
 }
 #endif
 
@@ -2467,13 +2487,12 @@ static bool has_xint_support(const struct arm64_cpu_capabilities *entry, int __u
 #ifdef CONFIG_ACTLR_XCALL_XINT
 static bool has_arch_xcall_xint_support(const struct arm64_cpu_capabilities *entry, int scope)
 {
-	/* List of CPUs that support Xcall/Xint */
-	static const struct midr_range xcall_xint_cpus[] = {
-		MIDR_ALL_VERSIONS(MIDR_HISI_HIP12),
-		{ /* sentinel */ }
-	};
+	if (is_arch_xcall_xint_support()) {
+		static_branch_enable(&has_hwcap_actlr_xcall);
+		return true;
+	}
 
-	return is_midr_in_range_list(read_cpuid_id(), xcall_xint_cpus);
+	return false;
 }
 
 static void enable_xcall_xint_vectors(void)
@@ -2504,7 +2523,7 @@ static void enable_xcall_xint_vectors(void)
 	isb();
 }
 
-static void cpu_enable_arch_xcall_xint(const struct arm64_cpu_capabilities *__unused)
+static void cpu_enable_arch_xint(void)
 {
 	int cpu = smp_processor_id();
 	u64 actlr_el1, actlr_el2;
@@ -2517,20 +2536,24 @@ static void cpu_enable_arch_xcall_xint(const struct arm64_cpu_capabilities *__un
 		 */
 		write_sysreg_s(read_sysreg_s(SYS_HCR_EL2) | HCR_TACR, SYS_HCR_EL2);
 		actlr_el2 = read_sysreg(actlr_el2);
-		actlr_el2 |= (ACTLR_ELx_XINT | ACTLR_ELx_XCALL);
+		actlr_el2 |= ACTLR_ELx_XINT;
 		write_sysreg(actlr_el2, actlr_el2);
 		isb();
 		actlr_el2 = read_sysreg(actlr_el2);
 		pr_info("actlr_el2: %llx, cpu:%d\n", actlr_el2, cpu);
 	} else {
 		actlr_el1 = read_sysreg(actlr_el1);
-		actlr_el1 |= (ACTLR_ELx_XINT | ACTLR_ELx_XCALL);
+		actlr_el1 |= ACTLR_ELx_XINT;
 		write_sysreg(actlr_el1, actlr_el1);
 		isb();
 		actlr_el1 = read_sysreg(actlr_el1);
 		pr_info("actlr_el1: %llx, cpu:%d\n", actlr_el1, cpu);
 	}
+}
 
+static void cpu_enable_arch_xcall_xint(const struct arm64_cpu_capabilities *__unused)
+{
+	cpu_enable_arch_xint();
 	enable_xcall_xint_vectors();
 }
 #endif
