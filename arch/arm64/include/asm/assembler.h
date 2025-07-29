openeuler-mirror/kernel

commit 1c203efa0a421d6a2368653f017143a430137be9

arm64/asm: Introduce assembly macros for managing ALLINT
kunpeng inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I90N2C
CVE: NA

Reference: https://git.kernel.org/pub/scm/linux/kernel/git/broonie/misc.git/commit/?h=arm64-nmi&id=4f8ecfa5830f4605a34d0982937351940147e4ef

----------------------------------------------------------------------

In order to allow assembly code to ensure that not even superpriorty
interrupts can preempt it provide macros for enabling and disabling
ALLINT.ALLINT.  This is not integrated into the existing DAIF macros
since we do not always wish to manage ALLINT along with DAIF and the
use of DAIF in the naming of the existing macros might lead to surprises
if ALLINT is also managed.

Signed-off-by: Mark Brown <broonie@kernel.org>
Signed-off-by: Jie Liu <liujie375@h-partners.com>

arch/arm64/include/asm/assembler.h
@@ -34,6 +34,22 @@
 	wx\n	.req	w\n
 	.endr
 
+	.macro	disable_allint
+#ifdef CONFIG_ARM64_NMI
+alternative_if ARM64_HAS_NMI
+	msr_s	SYS_ALLINT_SET, xzr
+alternative_else_nop_endif
+#endif
+	.endm
+
+	.macro	enable_allint
+#ifdef CONFIG_ARM64_NMI
+alternative_if ARM64_HAS_NMI
+	msr_s	SYS_ALLINT_CLR, xzr
+alternative_else_nop_endif
+#endif
+	.endm
+
 	.macro disable_daif
 	msr	daifset, #0xf
 	.endm

chenglongshe/openEuler

commit 23d64a6f15cabffc157513e2b98ba5a74f7d7394

arm64/nmi: fix using wrong nmi check
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I90N2C
CVE: NA

----------------------------------------------------------------------

If ARM64_HAS_NMI is enabled, it doesn't mean NMI is enabled. When NMI
is enabled, ARM64_USES_NMI is set to true, so use it instead.

Fixes: 1c203efa0a42 ("arm64/asm: Introduce assembly macros for managing ALLINT")
Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

arch/arm64/include/asm/assembler.h
@@ -36,7 +36,7 @@
 
 	.macro	disable_allint
 #ifdef CONFIG_ARM64_NMI
-alternative_if ARM64_HAS_NMI
+alternative_if ARM64_USES_NMI
 	msr_s	SYS_ALLINT_SET, xzr
 alternative_else_nop_endif
 #endif
@@ -44,7 +44,7 @@ alternative_else_nop_endif
 
 	.macro	enable_allint
 #ifdef CONFIG_ARM64_NMI
-alternative_if ARM64_HAS_NMI
+alternative_if ARM64_USES_NMI
 	msr_s	SYS_ALLINT_CLR, xzr
 alternative_else_nop_endif
 #endif