fedafcf48c02b16bc74572b0ab26406cc72c2084
Gu Bowen <gubowen5@huawei.com>
2025-07-31 16:57:56 +0800

    bpf-rvi: Add bpf_mem_percpu kfunc
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICA1GK
    
    --------------------------------
    
    Add kfunc to get the number of per-cpu pages.
    
    Co-developed-by: GONG Ruiqi <gongruiqi1@huawei.com>
    Signed-off-by: GONG Ruiqi <gongruiqi1@huawei.com>
    Signed-off-by: Gu Bowen <gubowen5@huawei.com>

--- kernel/bpf-rvi/common_kfuncs.c
+++ kernel/bpf-rvi/common_kfuncs.c
@@ -234,6 +234,11 @@ __bpf_kfunc unsigned long bpf_mem_failure(void)
 	return atomic_long_read(&num_poisoned_pages) << (PAGE_SHIFT - 10);
 }
 
+__bpf_kfunc unsigned long bpf_mem_percpu(void)
+{
+	return pcpu_nr_pages();
+}
+
 BTF_SET8_START(bpf_common_kfuncs_ids)
 BTF_ID_FLAGS(func, bpf_mem_cgroup_from_task, KF_RET_NULL | KF_RCU)
 BTF_ID_FLAGS(func, bpf_task_active_pid_ns, KF_TRUSTED_ARGS)
@@ -257,6 +262,7 @@ BTF_ID_FLAGS(func, bpf_mem_totalcma)
 BTF_ID_FLAGS(func, bpf_mem_freecma)
 BTF_ID_FLAGS(func, bpf_hugetlb_report_meminfo, KF_TRUSTED_ARGS)
 BTF_ID_FLAGS(func, bpf_mem_failure)
+BTF_ID_FLAGS(func, bpf_mem_percpu)
 BTF_SET8_END(bpf_common_kfuncs_ids)
 
 static const struct btf_kfunc_id_set bpf_common_kfuncs_set = {
