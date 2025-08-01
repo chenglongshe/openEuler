54fdeb9cb59a4a54a7b389ce644b36a3a1988905
GONG Ruiqi <gongruiqi1@huawei.com>
2025-08-01 17:54:12 +0800

    bpf-rvi: pidns: Add for_each_task_in_pidns and loadavg-related kfuncs
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICA1GK
    
    --------------------------------
    
    Add kfuncs to get the active pid namespace of a task, and to get the
    number of tasks and the last pid of a given pid namespace.
    
    Signed-off-by: GONG Ruiqi <gongruiqi1@huawei.com>

--- kernel/bpf-rvi/common_kfuncs.c
+++ kernel/bpf-rvi/common_kfuncs.c
@@ -2,6 +2,9 @@
 /* Copyright (c) 2025 Huawei Technologies Co., Ltd
  */
 #include <linux/memcontrol.h>
+#include <linux/idr.h>
+#include <linux/pid_namespace.h>
+#include <linux/sched.h>
 #include <linux/btf_ids.h>
 #include <linux/bpf.h>
 
@@ -14,8 +17,39 @@ __bpf_kfunc struct mem_cgroup *bpf_mem_cgroup_from_task(struct task_struct *p)
 	return mem_cgroup_from_task(p);
 }
 
+/*
+ * Loadavg related kfuncs
+ */
+
+__bpf_kfunc struct pid_namespace *bpf_task_active_pid_ns(struct task_struct *task)
+{
+	return task_active_pid_ns(task);
+}
+
+__bpf_kfunc u64 bpf_pidns_nr_tasks(struct pid_namespace *ns)
+{
+	struct pid_iter iter;
+	u32 nr_running = 0, nr_threads = 0;
+
+	for_each_task_in_pidns(iter, ns) {
+		nr_threads++;
+		if (task_is_running(iter.task))
+			nr_running++;
+	}
+
+	return (u64)nr_running << 32 | nr_threads;
+}
+
+__bpf_kfunc u32 bpf_pidns_last_pid(struct pid_namespace *ns)
+{
+	return idr_get_cursor(&ns->idr) - 1;
+}
+
 BTF_SET8_START(bpf_common_kfuncs_ids)
 BTF_ID_FLAGS(func, bpf_mem_cgroup_from_task, KF_RET_NULL | KF_RCU)
+BTF_ID_FLAGS(func, bpf_task_active_pid_ns, KF_TRUSTED_ARGS)
+BTF_ID_FLAGS(func, bpf_pidns_nr_tasks)
+BTF_ID_FLAGS(func, bpf_pidns_last_pid)
 BTF_SET8_END(bpf_common_kfuncs_ids)
 
 static const struct btf_kfunc_id_set bpf_common_kfuncs_set = {
