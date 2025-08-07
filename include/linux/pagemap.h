torvalds/linux

commit 77d075221ae777296e2b18a0a4f5fea6f75daf2c

mm/readahead: add readahead_control->dropbehind member
If ractl->dropbehind is set to true, then folios created are marked as
dropbehind as well.

Link: https://lkml.kernel.org/r/20241220154831.1086649-6-axboe@kernel.dk
Signed-off-by: Jens Axboe <axboe@kernel.dk>
Reviewed-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
Cc: Brian Foster <bfoster@redhat.com>
Cc: Chris Mason <clm@meta.com>
Cc: Christoph Hellwig <hch@lst.de>
Cc: Johannes Weiner <hannes@cmpxchg.org>
Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

include/linux/pagemap.h
@@ -1358,6 +1358,7 @@ struct readahead_control {
 	pgoff_t _index;
 	unsigned int _nr_pages;
 	unsigned int _batch_count;
+	bool dropbehind;
 	bool _workingset;
 	unsigned long _pflags;
 };

mm/readahead.c
@@ -181,7 +181,13 @@ static void read_pages(struct readahead_control *rac)
 static struct folio *ractl_alloc_folio(struct readahead_control *ractl,
 				       gfp_t gfp_mask, unsigned int order)
 {
-	return filemap_alloc_folio(gfp_mask, order);
+	struct folio *folio;
+
+	folio = filemap_alloc_folio(gfp_mask, order);
+	if (folio && ractl->dropbehind)
+		__folio_set_dropbehind(folio);
+
+	return folio;
 }
 
 /**

chenglongshe/openEuler

commit a56dfdb7c6dadf59103f3ccdd6235360d2e706df

mm/readahead: fix kabi breakage in struct readahead_control
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICPOEB
CVE: NA

----------------------------------------------------------------------

Fix kabi breakage in struct readahead_control.

Fixes: 77d075221ae7 ("mm/readahead: add readahead_control->dropbehind member")
Signed-off-by: Long Li <leo.lilong@huawei.com>

include/linux/pagemap.h
@@ -1375,14 +1375,13 @@ struct readahead_control {
 	struct file *file;
 	struct address_space *mapping;
 	struct file_ra_state *ra;
-	KABI_RESERVE(1)
+	KABI_USE(1, bool dropbehind)
 	KABI_RESERVE(2)
 	KABI_RESERVE(3)
 /* private: use the readahead_* accessors instead */
 	pgoff_t _index;
 	unsigned int _nr_pages;
 	unsigned int _batch_count;
-	bool dropbehind;
 	bool _workingset;
 	unsigned long _pflags;
 };