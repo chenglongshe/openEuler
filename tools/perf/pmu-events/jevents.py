4d930e2869fbd3f9b603076b225badbcf5777dda
Qinxin Xia <xiaqinxin@huawei.com>
2025-08-08 16:19:16 +0800

    perf jevents: Add support for HiSilicon MN PMU aliasing
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICRSOQ
    
    ----------------------------------------------------------------------
    
    a) Add support for Hisilicon MN PMU perf metric:
        · Average latency of dvmsync/no-sync snoop request;
        · Average latency of dvmsync/no-sync request.
    
    Signed-off-by: Qinxin Xia <xiaqinxin@huawei.com>
    Signed-off-by: Qizhi Zhang <zhangqizhi3@h-partners.com>
    Signed-off-by: JiangShui Yang <yangjiangshui@h-partners.com>

--- tools/perf/pmu-events/jevents.py
+++ tools/perf/pmu-events/jevents.py
@@ -284,6 +284,7 @@ class JsonEvent:
           'hisi_sccl,hha': 'hisi_sccl,hha',
           'hisi_sccl,l3c': 'hisi_sccl,l3c',
           'hisi_scl,noc': 'hisi_scl,noc',
+          'hisi_scl,mn': 'hisi_scl,mn',
           'imx8_ddr': 'imx8_ddr',
           'L3PMC': 'amd_l3',
           'DFPMC': 'amd_df',
