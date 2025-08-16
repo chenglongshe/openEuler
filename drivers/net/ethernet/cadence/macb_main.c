torvalds/linux

commit 61183b056b49e2937ff92a1424291ba36a6f6d05

net: macb: add missed tasklet_kill
This driver forgets to kill tasklet in remove.
Add the call to fix it.

Fixes: 032dc41ba6e2 ("net: macb: Handle HRESP error")
Signed-off-by: Chuhong Yuan <hslester96@gmail.com>
Signed-off-by: David S. Miller <davem@davemloft.net>

drivers/net/ethernet/cadence/macb_main.c
@@ -4422,6 +4422,7 @@ static int macb_remove(struct platform_device *pdev)
 		mdiobus_free(bp->mii_bus);
 
 		unregister_netdev(dev);
+		tasklet_kill(&bp->hresp_err_tasklet);
 		pm_runtime_disable(&pdev->dev);
 		pm_runtime_dont_use_autosuspend(&pdev->dev);
 		if (!pm_runtime_suspended(&pdev->dev)) {

chenglongshe/openEuler

commit a449179fc3f4b3b707fbdbe396c212511e308440

net: macb: fix unregister_netdev call order in macb_remove()
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/src-openeuler/kernel/issues/ICS636
CVE: NA

--------------------------------

macb_remove() was calling unregister_netdev() after phy_exit(),
which could trigger kernfs_remove_by_name_ns() warnings during driver
removal.

Fixes: 61183b056b49 ("net: macb: add missed tasklet_kill")
Signed-off-by: luogf <15388634752@163.com>

drivers/net/ethernet/cadence/macb_main.c
@@ -5166,11 +5166,11 @@ static int macb_remove(struct platform_device *pdev)
 
 	if (dev) {
 		bp = netdev_priv(dev);
+		unregister_netdev(dev);
 		phy_exit(bp->sgmii_phy);
 		mdiobus_unregister(bp->mii_bus);
 		mdiobus_free(bp->mii_bus);
 
-		unregister_netdev(dev);
 		tasklet_kill(&bp->hresp_err_tasklet);
 		pm_runtime_disable(&pdev->dev);
 		pm_runtime_dont_use_autosuspend(&pdev->dev);