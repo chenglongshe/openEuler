OERV-RVCI/rvck-olk

commit 2671ba2219683b30ced931875147193cbdd69aab

kvm: hisi_virt: Update TLBI broadcast feature for hip12
virt inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IBTXY2

------------------------------------------------------------------------

Compared with hip09, there are some differences on TLBI broadcast
feature for hip12 including:
- No need to translate logical cluster id to physical cluster id;
- The minimum granularity of TLBI broadcast is cluster;
- Some fields of register LSUDVMBM changes;

So update for corresponding changes.

MPIDR_EL1 layout on HIP12:
Aff3[3:2]	- socket ID	[0-3]
Aff3[1:0]	- die ID	[0,1]
Aff2		- cluster ID	[0-5]
Aff1		- core ID	[0-15]
Aff0		- thread ID	[0,1]

Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
Signed-off-by: Jinqian Yang <yangjinqian1@huawei.com>

arch/arm64/kvm/hisilicon/hisi_virt.c
@@ -20,6 +20,7 @@ static const char * const hisi_cpu_type_str[] = {
 	"HIP09",
 	"HIP10",
 	"HIP10C",
+	"HIP12",
 	"Unknown"
 };
 
@@ -30,7 +31,8 @@ static const char * const oem_str[] = {
 	"HIP08   ",	/* Hisi 1620 */
 	"HIP09   ",	/* HIP09 */
 	"HIP10   ",	/* HIP10 */
-	"HIP10C  "	/* HIP10C */
+	"HIP10C  ",	/* HIP10C */
+	"HIP12   "	/* HIP12 */
 };
 
 /*
@@ -396,6 +398,56 @@ out_update:
 	kvm->arch.tlbi_dvmbm = val;
 }
 
+static void kvm_update_vm_lsudvmbm_hip12(struct kvm *kvm)
+{
+	u64 mpidr, aff3, aff2;
+	u64 vm_aff3s[DVMBM_MAX_DIES_HIP12];
+	u64 val;
+	int cpu, nr_dies;
+
+	nr_dies = kvm_dvmbm_get_dies_info(kvm, vm_aff3s, DVMBM_MAX_DIES_HIP12);
+	if (nr_dies > 2) {
+		val = DVMBM_RANGE_ALL_DIES << DVMBM_RANGE_SHIFT;
+		goto out_update;
+	}
+
+	if (nr_dies == 1) {
+		val = DVMBM_RANGE_ONE_DIE << DVMBM_RANGE_SHIFT	|
+		      vm_aff3s[0] << DVMBM_DIE1_VDIE_SHIFT_HIP12;
+
+		/* fulfill bits [11:6] */
+		for_each_cpu(cpu, kvm->arch.sched_cpus) {
+			mpidr = cpu_logical_map(cpu);
+			aff2 = MPIDR_AFFINITY_LEVEL(mpidr, 2);
+
+			val |= 1ULL << (aff2 + DVMBM_DIE1_CLUSTER_SHIFT_HIP12);
+		}
+
+		goto out_update;
+	}
+
+	/* nr_dies == 2 */
+	val = DVMBM_RANGE_TWO_DIES << DVMBM_RANGE_SHIFT	|
+	      DVMBM_GRAN_CLUSTER << DVMBM_GRAN_SHIFT	|
+	      vm_aff3s[0] << DVMBM_DIE1_VDIE_SHIFT_HIP12    |
+	      vm_aff3s[1] << DVMBM_DIE2_VDIE_SHIFT_HIP12;
+
+	/* and fulfill bits [11:0] */
+	for_each_cpu(cpu, kvm->arch.sched_cpus) {
+		mpidr = cpu_logical_map(cpu);
+		aff3 = MPIDR_AFFINITY_LEVEL(mpidr, 3);
+		aff2 = MPIDR_AFFINITY_LEVEL(mpidr, 2);
+
+		if (aff3 == vm_aff3s[0])
+			val |= 1ULL << (aff2 + DVMBM_DIE1_CLUSTER_SHIFT_HIP12);
+		else
+			val |= 1ULL << (aff2 + DVMBM_DIE2_CLUSTER_SHIFT_HIP12);
+	}
+
+out_update:
+	kvm->arch.tlbi_dvmbm = val;
+}
+
 void kvm_tlbi_dvmbm_vcpu_load(struct kvm_vcpu *vcpu)
 {
 	struct kvm *kvm = vcpu->kvm;
@@ -440,7 +492,10 @@ void kvm_tlbi_dvmbm_vcpu_load(struct kvm_vcpu *vcpu)
 	 * Re-calculate LSUDVMBM_EL2 for this VM and kick all vcpus
 	 * out to reload the LSUDVMBM configuration.
 	 */
-	kvm_update_vm_lsudvmbm(kvm);
+	if (cpu_type == HI_IP12)
+		kvm_update_vm_lsudvmbm_hip12(kvm);
+	else
+		kvm_update_vm_lsudvmbm(kvm);
 	kvm_make_all_cpus_request(kvm, KVM_REQ_RELOAD_TLBI_DVMBM);
 
 out_unlock:

arch/arm64/kvm/hisilicon/hisi_virt.h
@@ -14,6 +14,7 @@ enum hisi_cpu_type {
 	HI_IP09,
 	HI_IP10,
 	HI_IP10C,
+	HI_IP12,
 	UNKNOWN_HI_TYPE
 };
 
@@ -63,6 +64,13 @@ enum hisi_cpu_type {
 #define DVMBM_DIE2_CLUSTER_SHIFT	0
 
 #define DVMBM_MAX_DIES			32
+
+/* HIP12 */
+#define DVMBM_DIE1_VDIE_SHIFT_HIP12		57
+#define DVMBM_DIE2_VDIE_SHIFT_HIP12		53
+#define DVMBM_DIE1_CLUSTER_SHIFT_HIP12		6
+#define DVMBM_DIE2_CLUSTER_SHIFT_HIP12		0
+#define DVMBM_MAX_DIES_HIP12				8
 
 void probe_hisi_cpu_type(void);
 bool hisi_ncsnp_supported(void);

chenglongshe/openEuler

commit 53d5e0e8028f47e8240771aa39d5d977daac64cf

kvm: hisi_virt: Correct the correspondence between the aff3 of mpidr and bitmap register
driver inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICURZK

---------------------------------------------------------------------

On HIP12, mpidr indicates the die ID and socket ID, which are bit[2:0] and
bit[4:3] of aff3 respectively. The fileds representing the socket ID and die
ID in the bitmap register are both 2 bits. Therefore, the socket ID and die
ID obtained from the mpidr do not correspond to the socket ID and die ID fields
indicated by the bitmap register.

Perform necessary conversion when writing the bitmap register based on the
die ID and socket ID obtained from the mpidr.

Fixes: 2671ba221968 ("kvm: hisi_virt: Update TLBI broadcast feature for hip12")
Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>
Signed-off-by: Jinqian Yang <yangjinqian1@huawei.com>

arch/arm64/kvm/hisilicon/hisi_virt.c
@@ -8,6 +8,7 @@
 #include <linux/init.h>
 #include <linux/kvm_host.h>
 #include "hisi_virt.h"
+#include <linux/bitfield.h>
 
 static enum hisi_cpu_type cpu_type = UNKNOWN_HI_TYPE;
 
@@ -490,7 +491,10 @@ static void kvm_update_vm_lsudvmbm_hip12(struct kvm *kvm)
 
 	if (nr_dies == 1) {
 		val = DVMBM_RANGE_ONE_DIE << DVMBM_RANGE_SHIFT	|
-		      vm_aff3s[0] << DVMBM_DIE1_VDIE_SHIFT_HIP12;
+		      FIELD_PREP(DVMBM_DIE1_VDIE_MASK_HIP12,
+				 FIELD_GET(MPIDR_AFF3_VDIE_ID_MASK, vm_aff3s[0])) |
+		      FIELD_PREP(DVMBM_DIE1_SOCKET_MASK_HIP12,
+				 FIELD_GET(MPIDR_AFF3_SOCKET_ID_MASK, vm_aff3s[0]));
 
 		/* fulfill bits [11:6] */
 		for_each_cpu(cpu, kvm->arch.sched_cpus) {
@@ -506,8 +510,14 @@ static void kvm_update_vm_lsudvmbm_hip12(struct kvm *kvm)
 	/* nr_dies == 2 */
 	val = DVMBM_RANGE_TWO_DIES << DVMBM_RANGE_SHIFT	|
 	      DVMBM_GRAN_CLUSTER << DVMBM_GRAN_SHIFT	|
-	      vm_aff3s[0] << DVMBM_DIE1_VDIE_SHIFT_HIP12    |
-	      vm_aff3s[1] << DVMBM_DIE2_VDIE_SHIFT_HIP12;
+	      FIELD_PREP(DVMBM_DIE1_VDIE_MASK_HIP12,
+			 FIELD_GET(MPIDR_AFF3_VDIE_ID_MASK, vm_aff3s[0]))	|
+	      FIELD_PREP(DVMBM_DIE1_SOCKET_MASK_HIP12,
+			 FIELD_GET(MPIDR_AFF3_SOCKET_ID_MASK, vm_aff3s[0]))	|
+	      FIELD_PREP(DVMBM_DIE2_VDIE_MASK_HIP12,
+			 FIELD_GET(MPIDR_AFF3_VDIE_ID_MASK, vm_aff3s[1]))	|
+	      FIELD_PREP(DVMBM_DIE2_SOCKET_MASK_HIP12,
+			 FIELD_GET(MPIDR_AFF3_SOCKET_ID_MASK, vm_aff3s[1]));
 
 	/* and fulfill bits [11:0] */
 	for_each_cpu(cpu, kvm->arch.sched_cpus) {

arch/arm64/kvm/hisilicon/hisi_virt.h
@@ -69,12 +69,25 @@ enum hisi_cpu_type {
 
 #define DVMBM_MAX_DIES			32
 
-/* HIP12 */
-#define DVMBM_DIE1_VDIE_SHIFT_HIP12		57
-#define DVMBM_DIE2_VDIE_SHIFT_HIP12		53
+/*
+ * MPIDR_EL1 layout on HIP12
+ *
+ * Aff3[4:3]	- socket ID	[0-3]
+ * Aff3[2:0]	- vdie ID	[0,1]
+ * Aff2[2:0]	- cluster ID	[0-5]
+ * Aff1[3:0]	- core ID	[0-15]
+ * Aff0[0]	- thread ID	[0,1]
+ */
+
+#define DVMBM_DIE1_SOCKET_MASK_HIP12		GENMASK_ULL(60, 59)
+#define DVMBM_DIE2_SOCKET_MASK_HIP12		GENMASK_ULL(56, 55)
+#define DVMBM_DIE1_VDIE_MASK_HIP12		GENMASK_ULL(58, 57)
+#define DVMBM_DIE2_VDIE_MASK_HIP12		GENMASK_ULL(54, 53)
 #define DVMBM_DIE1_CLUSTER_SHIFT_HIP12		6
 #define DVMBM_DIE2_CLUSTER_SHIFT_HIP12		0
-#define DVMBM_MAX_DIES_HIP12				8
+#define DVMBM_MAX_DIES_HIP12			8
+#define MPIDR_AFF3_VDIE_ID_MASK			GENMASK_ULL(2, 0)
+#define MPIDR_AFF3_SOCKET_ID_MASK		GENMASK_ULL(4, 3)
 
 void probe_hisi_cpu_type(void);
 bool hisi_ncsnp_supported(void);