openeuler-mirror/kernel

commit 92cd2e7fa82d917cc2d49e0118f142799ec65bc3

mm:vmscan: add the missing check of page_cache_over_limit
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I4SK3S
CVE: NA

--------------------------------

Function shrink_shepherd is used to queue each work on cpu to shrink
page cache, and it will be called periodically, but if there is no
page_cache_over_limit check before shrink page cache, it will result
in periodic memory reclamation even the number of page cache below
limit, so add basic check before shrink page cache.

Signed-off-by: Chen Wandun <chenwandun@huawei.com>
Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

mm/vmscan.c
@@ -4056,6 +4056,10 @@ static void shrink_page_cache_work(struct work_struct *w)
 	if (vm_cache_reclaim_s == 0 || !vm_cache_reclaim_enable)
 		return;
 
+	if (mem_reliable_is_enabled() &&
+			(!vm_cache_limit_mbytes || !page_cache_over_limit()))
+		return;
+
 	/* It should wait more time if we hardly reclaim the page cache */
 	nr_pages = shrink_page_cache(GFP_KERNEL);
 	if ((nr_pages < SWAP_CLUSTER_MAX) && vm_cache_reclaim_enable)

chenglongshe/openEuler

commit 16244cb52962f72b936d7fbd676079e9f21ce804

mm: vmscan: enable shrink_page_cache_work limit
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I4SK3S
CVE: NA

--------------------------------

Commit 92cd2e7fa82d ("mm:vmscan: add the missing check of
page_cache_over_limit") add limit check for shrink_page_cache_work
to stop page cache below limit. However this limit is only checked
iff memory reliable is enable, since page cache limit can be
enabled without memory reliable. Enable this limit check in all
scenarios.

Fixes: 92cd2e7fa82d ("mm:vmscan: add the missing check of page_cache_over_limit")
Signed-off-by: Wupeng Ma <mawupeng1@huawei.com>

mm/vmscan.c
@@ -4060,8 +4060,7 @@ static void shrink_page_cache_work(struct work_struct *w)
 	if (vm_cache_reclaim_s == 0 || !vm_cache_reclaim_enable)
 		return;
 
-	if (mem_reliable_is_enabled() &&
-			(!vm_cache_limit_mbytes || !page_cache_over_limit()))
+	if (!vm_cache_limit_mbytes || !page_cache_over_limit())
 		return;
 
 	/* It should wait more time if we hardly reclaim the page cache */