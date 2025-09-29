fb6bec01245cc4e356938976e71ba3817867c686
Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
2025-09-29 03:41:27 +0000

    xsched: Add xsched RT class
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC5EHB
    
    -----------------------------------------
    
    Add rt class callbacks implementation:
    - dequeue_ctx
    - enqueue_ctx
    - pick_next_ctx
    - put_prev_ctx
    - submit_prepare_ctx
    - select_work
    - check_preempt
    
    Add xsched_rt.c in /kernel/xsched Makefile.
    Add RT class callbacks support in core.c.
    
    Signed-off-by: Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
    Signed-off-by: Hui Tang <tanghui20@.huawei.com>
    Signed-off-by: Liu Kai <liukai284@huawei.com>
    Signed-off-by: Xia Fukun <xiafukun@huawei.com>

--- include/linux/xsched.h
+++ include/linux/xsched.h
@@ -37,7 +37,32 @@
 
 #define __GET_VS_TASK_TYPE(t) ((t)&0xFF)
 
+#define __GET_VS_TASK_PRIO_RT(t) (((t) >> 8) & 0xFF)
+
 #define GET_VS_TASK_TYPE(vs_ptr) __GET_VS_TASK_TYPE((vs_ptr)->task_type)
+
+#define GET_VS_TASK_PRIO_RT(vs_ptr) __GET_VS_TASK_PRIO_RT((vs_ptr)->task_type)
+
+/*
+ * A default kick slice for RT class XSEs.
+ */
+#define XSCHED_RT_KICK_SLICE 20
+/*
+ * A default kick slice for CFS class XSEs.
+ */
+#define XSCHED_CFS_KICK_SLICE 10
+
+enum xcu_sched_type {
+	XSCHED_TYPE_RT,
+	XSCHED_TYPE_DFLT = XSCHED_TYPE_RT,
+	XSCHED_TYPE_NUM,
+};
+
+enum xse_prio {
+	XSE_PRIO_LOW,
+	XSE_PRIO_HIGH,
+	NR_XSE_PRIO,
+};
 
 enum xsched_rq_state {
 	XRQ_STATE_INACTIVE = 0x00,
@@ -47,18 +72,61 @@ enum xsched_rq_state {
 	XRQ_STATE_WAIT_RUNNING = 0x08,
 };
 
+enum xse_state {
+	XSE_PREPARE,
+	XSE_READY,
+	XSE_RUNNING,
+	XSE_BLOCK,
+	XSE_DEAD,
+};
+
+enum xse_flag {
+	XSE_TIF_NONE,
+	XSE_TIF_PREEMPT,
+	XSE_TIF_BALANCE, /* Unused so far */
+};
+
+
+extern const struct xsched_class rt_xsched_class;
+
+#define xsched_first_class (&rt_xsched_class)
+
+#define for_each_xsched_class(class)                                           \
+	for (class = xsched_first_class; class; class = class->next)
+
+#define for_each_xse_prio(prio)                                                \
+	for (prio = XSE_PRIO_LOW; prio < NR_XSE_PRIO; prio++)
+
 #define for_each_vstream_in_ctx(vs, ctx)                                       \
 	list_for_each_entry((vs), &((ctx)->vstream_list), ctx_node)
 
+
+/* Manages xsched RT-like class linked list based runqueue.
+ *
+ * Now RT-like class runqueue structs is identical
+ * but will most likely grow different in the
+ * future as the Xsched evolves.
+ */
+struct xsched_rq_rt {
+	struct list_head rq[NR_XSE_PRIO];
+	unsigned int nr_running;
+	int prio_nr_running[NR_XSE_PRIO];
+	atomic_t prio_nr_kicks[NR_XSE_PRIO];
+	DECLARE_BITMAP(curr_prios, NR_XSE_PRIO);
+};
 
 /* Base XSched runqueue object structure that contains both mutual and
  * individual parameters for different scheduling classes.
  */
 struct xsched_rq {
 	struct xsched_entity *curr_xse;
+	const struct xsched_class *class;
 
 	int state;
 	int nr_running;
+
+	/* RT class run queue.*/
+	struct xsched_rq_rt rt;
 };
 
 enum xcu_state {
@@ -89,6 +157,9 @@ struct xsched_cu {
 	uint32_t id;
 	uint32_t state;
 
+	/* RT class kick counter. */
+	atomic_t pending_kicks_rt;
+
 	struct task_struct *worker;
 
 	struct xsched_rq xrq;
@@ -102,6 +173,16 @@ struct xsched_cu {
 
 	wait_queue_head_t wq_xcu_idle;
 	wait_queue_head_t wq_xcu_running;
+};
+
+struct xsched_entity_rt {
+	struct list_head list_node;
+	enum xse_state state;
+	enum xse_flag flag;
+	enum xse_prio prio;
+
+	ktime_t timeslice;
+	s64 kick_slice;
 };
 
 struct xsched_entity {
@@ -118,6 +199,9 @@ struct xsched_entity {
 	/* Amount of submitted kicks context, used for resched decision. */
 	atomic_t submitted_one_kick;
 
+	size_t total_scheduled;
+	size_t total_submitted;
+
 	/* File descriptor coming from an associated context
 	 * used for identifying a given xsched entity in
 	 * info and error prints.
@@ -127,8 +211,14 @@ struct xsched_entity {
 	/* Xsched class for this xse. */
 	const struct xsched_class *class;
 
+	/* RT class entity. */
+	struct xsched_entity_rt rt;
+
 	/* Pointer to context object. */
 	struct xsched_context *ctx;
+
+	/* Xsched entity execution statistics */
+	u64 last_exec_runtime;
 
 	/* Pointer to an XCU object that represents an XCU
 	 * on which this xse is to be processed or is being
@@ -140,14 +230,84 @@ struct xsched_entity {
 	spinlock_t xse_lock;
 };
 
+static inline bool xse_is_rt(const struct xsched_entity *xse)
+{
+	return xse && xse->class == &rt_xsched_class;
+}
+
+/* Returns a pointer to an atomic_t variable representing a counter
+ * of currently pending vstream kicks on a given XCU and for a
+ * given xsched class.
+ */
+static inline atomic_t *
+xsched_get_pending_kicks_class(const struct xsched_class *class,
+				struct xsched_cu *xcu)
+{
+	/* Right now for testing purposes we have only XCU running streams. */
+	if (!xcu) {
+		XSCHED_ERR("Try to get pending kicks with xcu=NULL.\n");
+		return NULL;
+	}
+
+	if (!class) {
+		XSCHED_ERR("Try to get pending kicks with class=NULL.\n");
+		return NULL;
+	}
+
+	if (class == &rt_xsched_class)
+		return &xcu->pending_kicks_rt;
+
+	XSCHED_ERR("Xsched entity has an invalid class @ %s\n", __func__);
+	return NULL;
+}
+
+/* Returns a pointer to an atomic_t variable representing a counter of
+ * currently pending vstream kicks for an XCU on which a given xsched
+ * entity is enqueued on and for a xsched class that assigned to a
+ * given xsched entity.
+ */
+static inline atomic_t *
+xsched_get_pending_kicks_xse(const struct xsched_entity *xse)
+{
+	if (!xse) {
+		XSCHED_ERR("Try to get pending kicks with xse=NULL\n");
+		return NULL;
+	}
+
+	if (!xse->xcu) {
+		XSCHED_ERR("Try to get pending kicks with xse->xcu=NULL\n");
+		return NULL;
+	}
+
+	return xsched_get_pending_kicks_class(xse->class, xse->xcu);
+}
+
 /* Increments pending kicks counter for an XCU that the given
  * xsched entity is attached to and for xsched entity's xsched
  * class.
  */
 static inline int xsched_inc_pending_kicks_xse(struct xsched_entity *xse)
 {
+	atomic_t *kicks_class = NULL;
+
+	kicks_class = xsched_get_pending_kicks_xse(xse);
+	if (!kicks_class)
+		return -EINVAL;
+
+	/* Incrementing pending kicks for XSE's sched class */
+	atomic_inc(kicks_class);
+
 	/* Icrement pending kicks for current XSE. */
 	atomic_inc(&xse->kicks_pending_ctx_cnt);
+
+	/* Incrementing prio based pending kicks counter for RT class */
+	if (xse_is_rt(xse)) {
+		atomic_inc(&xse->xcu->xrq.rt.prio_nr_kicks[xse->rt.prio]);
+		XSCHED_DEBUG("xcu increased pending kicks @ %s\n", __func__);
+	} else {
+		XSCHED_DEBUG("xse %u isn't rt class @ %s\n", xse->tgid,
+			    __func__);
+	}
 
 	return 0;
 }
@@ -158,8 +318,40 @@ static inline int xsched_inc_pending_kicks_xse(struct xsched_entity *xse)
  */
 static inline int xsched_dec_pending_kicks_xse(struct xsched_entity *xse)
 {
+	atomic_t *kicks_class = NULL;
+	atomic_t *kicks_prio_rt = NULL;
+
+	kicks_class = xsched_get_pending_kicks_xse(xse);
+	if (!kicks_class)
+		return -EINVAL;
+
+	if (!atomic_read(kicks_class)) {
+		XSCHED_ERR("Try to decrement pending kicks beyond 0!\n");
+		return -EINVAL;
+	}
+
+	/* Decrementing pending kicks for XSE's sched class. */
+	atomic_dec(kicks_class);
+
 	/* Decrementing pending kicks for current XSE. */
 	atomic_dec(&xse->kicks_pending_ctx_cnt);
+
+	/* Decrementing prio based pending kicks counter for RT class. */
+	if (xse_is_rt(xse)) {
+		kicks_prio_rt = &xse->xcu->xrq.rt.prio_nr_kicks[xse->rt.prio];
+
+		if (!atomic_read(kicks_prio_rt)) {
+			XSCHED_ERR(
+				"Tried to decrement prio pending kicks beyond 0!\n");
+			return -EINVAL;
+		}
+
+		atomic_dec(kicks_prio_rt);
+		XSCHED_DEBUG("xcu decreased pending kicks @ %s\n", __func__);
+	} else {
+		XSCHED_DEBUG("xse %u isn't rt class @ %s\n", xse->tgid,
+			    __func__);
+	}
 
 	return 0;
 }
@@ -169,7 +361,14 @@ static inline int xsched_dec_pending_kicks_xse(struct xsched_entity *xse)
  */
 static inline bool xsched_check_pending_kicks_xcu(struct xsched_cu *xcu)
 {
-	return 0;
+	atomic_t *kicks_rt;
+
+	kicks_rt = xsched_get_pending_kicks_class(&rt_xsched_class, xcu);
+
+	if (!kicks_rt)
+		return false;
+
+	return !!atomic_read(kicks_rt);
 }
 
 static inline int xse_integrity_check(const struct xsched_entity *xse)
@@ -222,6 +421,33 @@ static inline struct xsched_context *ctx_find_by_tgid(pid_t tgid)
 
 	return ret;
 }
+
+/* Xsched class. */
+struct xsched_class {
+	const struct xsched_class *next;
+
+	/* Removes a given XSE from it's runqueue. */
+	void (*dequeue_ctx)(struct xsched_entity *xse);
+
+	/* Places a given XSE on a runqueue on a given XCU. */
+	void (*enqueue_ctx)(struct xsched_entity *xse, struct xsched_cu *xcu);
+
+	/* Returns a next XSE to be submitted on a given XCU. */
+	struct xsched_entity *(*pick_next_ctx)(struct xsched_cu *xcu);
+
+	/* Put a XSE back into rq during preemption. */
+	void (*put_prev_ctx)(struct xsched_entity *xse);
+
+	/* Prepares a given XSE for submission on a given XCU. */
+	int (*submit_prepare_ctx)(struct xsched_entity *xse,
+				  struct xsched_cu *xcu);
+
+	/* Check context preemption. */
+	bool (*check_preempt)(struct xsched_entity *xse);
+
+	/* Select jobs from XSE to submit on XCU */
+	size_t (*select_work)(struct xsched_cu *xcu, struct xsched_entity *xse);
+};
 
 static inline void xsched_init_vsm(struct vstream_metadata *vsm,
 				struct vstream_info *vs, vstream_args_t *arg)
