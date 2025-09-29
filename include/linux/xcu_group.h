08c11f4f096e103705da2f9fa1a36c691fb5c6e5
Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
2025-09-29 03:41:25 +0000

    xsched: Add xsched_ctx_init_xse() implementation
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC5EHB
    
    -----------------------------------------
    
    Add xsched_ctx_init_xse() implementation:
    - Add 2 atomics kicks_pending_ctx_cnt and kicks_submited members to
    - xsched_entity structure.
    - Add GET_VS_TASK_TYPE macro helper.
    - Add xsched_xse_set_class() and bind_ctx_to_xcu() stubs.
    
    Add bind_ctx_to_xcu() implementation:
    - Add ctx_revmap hash table to save XCUs' history.
    - Add XCU_HASH_ORDER to set hashtable order.
    - Add additional data structure ctx_devid_revmap_data
      to xcu_group that is used to save XCU history in hashtable by devid.
    
    Signed-off-by: Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
    Signed-off-by: Hui Tang <tanghui20@.huawei.com>
    Signed-off-by: Liu Kai <liukai284@huawei.com>
    Signed-off-by: Xia Fukun <xiafukun@huawei.com>

--- include/linux/xcu_group.h
+++ include/linux/xcu_group.h
@@ -22,6 +22,17 @@ enum xcu_version {
 	XCU_HW_V2,
 };
 
+/**
+ * @group: value for this entry.
+ * @hash_node: hash node list.
+ * @dev_id: device id to bind with ctx.
+ */
+struct ctx_devid_revmap_data {
+	unsigned int dev_id;
+	struct xcu_group *group;
+	struct hlist_node hash_node;
+};
+
 struct xcu_op_handler_params {
 	int fd;
 	struct xcu_group *group;
