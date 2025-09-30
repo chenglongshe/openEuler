3b4199d3759f870e3d70d4a5e521bb5d0468f589
Alekseev Dmitry <alekseev.dmitry@huawei.com>
2025-09-30 07:06:34 +0000

    xsched/core: Add xcu_wait() implementation
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC5EHB
    
    -----------------------------------------
    
    The xpu device should be exclusive in sched slice. So add
    xcu_wait() implementation which return after xpu kernels
    executed completed. It is called after submit_kick().
    
    Signed-off-by: Alekseev Dmitry <alekseev.dmitry@huawei.com>
    Signed-off-by: Liu Kai <liukai284@huawei.com>
    Signed-off-by: Xia Fukun <xiafukun@huawei.com>

--- drivers/xcu/xcu_group.c
+++ drivers/xcu/xcu_group.c
@@ -137,7 +137,12 @@ int xcu_run(struct xcu_op_handler_params *params)
  */
 int xcu_wait(struct xcu_op_handler_params *params)
 {
-	return 0;
+	if (!params->group->opt || !params->group->opt->wait) {
+		XSCHED_ERR("No function [wait] called.\n");
+		return -EINVAL;
+	}
+
+	return params->group->opt->wait(params);
 }
 
 /* This function runs "complete" callback for a given xcu_group
@@ -215,6 +220,22 @@ int xcu_logic_free(struct xcu_op_handler_params *params)
 	}
 
 	return params->group->opt->logic_free(params);
+}
+
+/* This function runs a "sqe_op" callback for a given xcu_group
+ * and a given vstream that are passed within
+ * xcu_op_handler_params object.
+ *
+ * This handler provides an interface to set or get sqe info.
+ */
+int xcu_sqe_op(struct xcu_op_handler_params *params)
+{
+	if (!params->group->opt || !params->group->opt->sqe_op) {
+		XSCHED_ERR("No function [sqe_op] called.\n");
+		return -EINVAL;
+	}
+
+	return params->group->opt->sqe_op(params);
 }
 
 static struct xcu_group __xcu_group_root = {
