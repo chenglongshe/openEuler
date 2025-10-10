6379c1d3ab3af07369577b7ce049de0f3b098e68
Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
2025-10-10 07:44:48 +0000

    xsched: Add base vstream support
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC5EHB
    
    --------------------------------
    
    Add sys_vstream_manage() syscall.
    Add the basic function framework.
    Add basic header files.
    Add new Kconfig.xsched with XCU_SCHEDULER and XCU_VSTREAM configurations.
    Create new dir kernel/xsched with vstream.c file with
    base xsched syscalls stubs.
    
    Add Makefile in kernel/xsched. Update main kernel Makefile to include
    kernel/xsched in build.
    
    Signed-off-by: Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
    Signed-off-by: Hui Tang <tanghui20@.huawei.com>
    Signed-off-by: Liu Kai <liukai284@huawei.com>
    Signed-off-by: Xia Fukun <xiafukun@huawei.com>

---
+++ include/linux/vstream.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_VSTREAM_H
+#define _LINUX_VSTREAM_H
+
+#include <uapi/linux/xcu_vstream.h>
+
+typedef int vstream_manage_t(struct vstream_args *arg);
+
+#endif /* _LINUX_VSTREAM_H */
