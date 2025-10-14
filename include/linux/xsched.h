4f303b2c6af04854297edb079b06dd7313803980
Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
2025-10-14 06:39:16 +0000

    xsched: Introduce vstream management
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC5EHB
    
    -----------------------------------------
    
    Add vstream related data structures:
     - vstream_info.
    
    Add vstream related context and entity data structures:
     - xsched_entity
     - xsched_context
    
    Add xsched_init() implementation.
    Add vstream_alloc/free implementation.
    
    Signed-off-by: Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
    Signed-off-by: Artem Kuzin <artem.kuzin@huawei.com>
    Signed-off-by: Hui Tang <tanghui20@.huawei.com>
    Signed-off-by: Liu Kai <liukai284@huawei.com>
    Signed-off-by: Xia Fukun <xiafukun@huawei.com>

--- include/linux/xsched.h
+++ include/linux/xsched.h
@@ -3,6 +3,8 @@
 #define __LINUX_XSCHED_H__
 
 #include <linux/xcu_group.h>
+#include <linux/kref.h>
+#include <linux/vstream.h>
 #ifndef pr_fmt
 #define pr_fmt(fmt) fmt
 #endif
@@ -67,5 +69,77 @@ struct xsched_cu {
 	wait_queue_head_t wq_xcu_running;
 };
 
+struct xsched_entity {
+	uint32_t task_type;
+
+	bool on_rq;
+
+	pid_t owner_pid;
+	pid_t tgid;
+
+	/* File descriptor coming from an associated context
+	 * used for identifying a given xsched entity in
+	 * info and error prints.
+	 */
+	uint32_t fd;
+
+	/* Xsched class for this xse. */
+	const struct xsched_class *class;
+
+	/* Pointer to context object. */
+	struct xsched_context *ctx;
+
+	/* Pointer to an XCU object that represents an XCU
+	 * on which this xse is to be processed or is being
+	 * processed currently.
+	 */
+	struct xsched_cu *xcu;
+
+	/* General purpose xse lock. */
+	spinlock_t xse_lock;
+};
+
+struct xsched_context {
+	uint32_t fd;
+	uint32_t dev_id;
+	pid_t tgid;
+
+	struct list_head vstream_list;
+	struct list_head ctx_node;
+
+	struct xsched_entity xse;
+
+	spinlock_t ctx_lock;
+	struct mutex ctx_mutex;
+	struct kref kref;
+};
+
+extern struct list_head xsched_ctx_list;
+extern struct mutex xsched_ctx_list_mutex;
+
+/* Returns a pointer to xsched_context object corresponding to a given
+ * device file descriptor provided by fd argument.
+ */
+static inline struct xsched_context *ctx_find_by_tgid(pid_t tgid)
+{
+	struct xsched_context *ctx;
+	struct xsched_context *ret = NULL;
+
+	list_for_each_entry(ctx, &xsched_ctx_list, ctx_node) {
+		if (ctx->tgid == tgid) {
+			ret = ctx;
+			break;
+		}
+	}
+
+	return ret;
+}
+
 int xsched_xcu_register(struct xcu_group *group);
+void xsched_task_free(struct kref *kref);
+int xsched_ctx_init_xse(struct xsched_context *ctx, struct vstream_info *vs);
+int ctx_bind_to_xcu(vstream_info_t *vstream_info, struct xsched_context *ctx);
+int vstream_bind_to_xcu(vstream_info_t *vstream_info);
+struct xsched_cu *xcu_find(uint32_t *type,
+				uint32_t dev_id, uint32_t channel_id);
 #endif /* !__LINUX_XSCHED_H__ */
