openeuler-mirror/kernel

commit 2dfbfabfced809d6e9672a50cc9edd3435477df0

locking/qspinlock: Add CNA support for ARM64 without pvspinlock
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I8T8XV
CVE: NA

-------------------------------------------------

The CNA spinlock is enabled base on 'pv_ops' of pvspinlock, and is only
supported on x86_64 now, add support for arm64 without pvspinlock.

Signed-off-by: Wei Li <liwei391@huawei.com>

kernel/locking/qspinlock_cna.h
@@ -376,6 +376,14 @@ static inline void cna_lock_handoff(struct mcs_spinlock *node,
 	arch_mcs_lock_handoff(&next->locked, val);
 }
 
+#ifdef CONFIG_PARAVIRT_SPINLOCKS
+#define cna_queued_spin_lock_slowpath	pv_ops.lock.queued_spin_lock_slowpath
+#else
+void (*cna_queued_spin_lock_slowpath)(struct qspinlock *lock, u32 val) =
+		native_queued_spin_lock_slowpath;
+EXPORT_SYMBOL(cna_queued_spin_lock_slowpath);
+#endif
+
 /*
  * Constant (boot-param configurable) flag selecting the NUMA-aware variant
  * of spinlock.  Possible values: -1 (off, default) / 0 (auto) / 1 (on).
@@ -413,13 +421,13 @@ void __init cna_configure_spin_lock_slowpath(void)
 		return;
 
 	if (numa_spinlock_flag == 0 && (nr_node_ids < 2 ||
-		    pv_ops.lock.queued_spin_lock_slowpath !=
+		    cna_queued_spin_lock_slowpath !=
 			native_queued_spin_lock_slowpath))
 		return;
 
 	cna_init_nodes();
 
-	pv_ops.lock.queued_spin_lock_slowpath = __cna_queued_spin_lock_slowpath;
+	cna_queued_spin_lock_slowpath = __cna_queued_spin_lock_slowpath;
 
 	pr_info("Enabling CNA spinlock\n");
 }

arch/arm64/Kconfig
@@ -1545,6 +1545,21 @@ config NODES_SHIFT
 	  Specify the maximum number of NUMA Nodes available on the target
 	  system.  Increases memory reserved to accommodate various tables.
 
+config NUMA_AWARE_SPINLOCKS
+	bool "Numa-aware spinlocks"
+	depends on NUMA
+	depends on QUEUED_SPINLOCKS
+	default n
+	help
+	  Introduce NUMA (Non Uniform Memory Access) awareness into
+	  the slow path of spinlocks.
+
+	  In this variant of qspinlock, the kernel will try to keep the lock
+	  on the same node, thus reducing the number of remote cache misses,
+	  while trading some of the short term fairness for better performance.
+
+	  Say N if you want absolute first come first serve fairness.
+
 source "kernel/Kconfig.hz"
 
 config ARCH_SPARSEMEM_ENABLE

arch/arm64/include/asm/Kbuild
@@ -2,7 +2,6 @@
 generic-y += early_ioremap.h
 generic-y += mcs_spinlock.h
 generic-y += qrwlock.h
-generic-y += qspinlock.h
 generic-y += parport.h
 generic-y += user.h
 

arch/arm64/include/asm/qspinlock.h
@@ -1,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_ARM64_QSPINLOCK_H
+#define _ASM_ARM64_QSPINLOCK_H
+
+#ifdef CONFIG_NUMA_AWARE_SPINLOCKS
+#include <asm-generic/qspinlock_types.h>
+
+extern void cna_configure_spin_lock_slowpath(void);
+
+extern void (*cna_queued_spin_lock_slowpath)(struct qspinlock *lock, u32 val);
+extern void native_queued_spin_lock_slowpath(struct qspinlock *lock, u32 val);
+
+#define	queued_spin_unlock queued_spin_unlock
+/**
+ * queued_spin_unlock - release a queued spinlock
+ * @lock : Pointer to queued spinlock structure
+ *
+ * A smp_store_release() on the least-significant byte.
+ */
+static inline void native_queued_spin_unlock(struct qspinlock *lock)
+{
+	smp_store_release(&lock->locked, 0);
+}
+
+static inline void queued_spin_lock_slowpath(struct qspinlock *lock, u32 val)
+{
+	cna_queued_spin_lock_slowpath(lock, val);
+}
+
+static inline void queued_spin_unlock(struct qspinlock *lock)
+{
+	native_queued_spin_unlock(lock);
+}
+#endif
+
+#include <asm-generic/qspinlock.h>
+
+#endif /* _ASM_ARM64_QSPINLOCK_H */

arch/x86/kernel/alternative.c
@@ -1612,10 +1612,6 @@ void __init alternative_instructions(void)
 	 */
 	paravirt_set_cap();
 
-#if defined(CONFIG_NUMA_AWARE_SPINLOCKS)
-	cna_configure_spin_lock_slowpath();
-#endif
-
 	/*
 	 * First patch paravirt functions, such that we overwrite the indirect
 	 * call with the direct call.

init/main.c
@@ -1013,7 +1013,9 @@ void start_kernel(void)
 		      panic_param);
 
 	lockdep_init();
-
+#if defined(CONFIG_NUMA_AWARE_SPINLOCKS)
+	cna_configure_spin_lock_slowpath();
+#endif
 	/*
 	 * Need to run this when irqs are enabled, because it wants
 	 * to self-test [hard/soft]-irqs on/off lock inversion bugs

kernel/locking/qspinlock.c
@@ -289,7 +289,7 @@ static __always_inline u32  __pv_wait_head_or_lock(struct qspinlock *lock,
 #define pv_kick_node		__pv_kick_node
 #define pv_wait_head_or_lock	__pv_wait_head_or_lock
 
-#ifdef CONFIG_PARAVIRT_SPINLOCKS
+#if defined(CONFIG_PARAVIRT_SPINLOCKS) || defined(CONFIG_NUMA_AWARE_SPINLOCKS)
 #define queued_spin_lock_slowpath	native_queued_spin_lock_slowpath
 #endif
 

chenglongshe/openEuler

commit 97582048af16479098a8274a9c24f277586800d0

locking/qspinlock: Fix CNA NUMA node setup before numa_store_cpu_info()
Offering: HULK
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICXU74

----------------------------------------

CNA currently sets up per-CPU NUMA nodes from start_kernel(), when the
CPU to node mapping is not yet valid. This can lead to incorrect node
ids being stored in CNA structures.

Defer the initialization to late_initcall(), after SMP and NUMA setup
has completed. This ensures that cpu_to_node() returns the correct ids
before CNA is enabled.

Fixes: 2dfbfabfced8 ("locking/qspinlock: Add CNA support for ARM64 without pvspinlock")
Signed-off-by: Jiacheng Yu <yujiacheng3@huawei.com>

kernel/locking/qspinlock_cna.h
@@ -142,6 +142,8 @@ static int __init cna_init_nodes(void)
 	return 0;
 }
 
+late_initcall(cna_init_nodes);
+
 static __always_inline void cna_init_node(struct mcs_spinlock *node)
 {
 	bool priority = !in_task() || irqs_disabled() || rt_task(current);