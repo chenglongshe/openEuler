openeuler-mirror/kernel

watchdog: Support watchdog_sdei coexist with existing watchdogs
kunpeng inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I90N2C
CVE: NA

----------------------------------------------------------------------

Currently we cannot use watchdog_{perf, buddy} if CONFIG_SDEI_WATCHDOG=y.
Not all the platforms has watchdog_sdei so this patch tries to make
watchdog_sdei coexist with other watchdogs.

Only one watchdog will finally works. By default watchdog_sdei
will be used. If boot with "disable_sdei_nmi_watchdog", other
watchdogs will be used if probed.

Signed-off-by: Yicong Yang <yangyicong@hisilicon.com>
Signed-off-by: Jie Liu <liujie375@h-partners.com>

kernel/watchdog.c
@@ -562,8 +562,12 @@ static void watchdog_enable(unsigned int cpu)
 	/* Initialize timestamp */
 	update_touch_ts();
 	/* Enable the hardlockup detector */
-	if (watchdog_enabled & WATCHDOG_HARDLOCKUP_ENABLED)
-		watchdog_hardlockup_enable(cpu);
+	if (watchdog_enabled & WATCHDOG_HARDLOCKUP_ENABLED) {
+		if (disable_sdei_nmi_watchdog)
+			watchdog_hardlockup_enable(cpu);
+		else
+			sdei_watchdog_hardlockup_enable(cpu);
+	}
 }
 
 static void watchdog_disable(unsigned int cpu)
@@ -577,7 +581,10 @@ static void watchdog_disable(unsigned int cpu)
 	 * delay between disabling the timer and disabling the hardlockup
 	 * detector causes a false positive.
 	 */
-	watchdog_hardlockup_disable(cpu);
+	if (disable_sdei_nmi_watchdog)
+		watchdog_hardlockup_disable(cpu);
+	else
+		sdei_watchdog_hardlockup_disable(cpu);
 	hrtimer_cancel(hrtimer);
 	wait_for_completion(this_cpu_ptr(&softlockup_completion));
 }
@@ -1022,7 +1029,8 @@ void __init lockup_detector_init(void)
 	cpumask_copy(&watchdog_cpumask,
 		     housekeeping_cpumask(HK_TYPE_TIMER));
 
-	if (!watchdog_hardlockup_probe())
+	if ((!disable_sdei_nmi_watchdog && !sdei_watchdog_hardlockup_probe()) ||
+	    !watchdog_hardlockup_probe())
 		watchdog_hardlockup_available = true;
 	else
 		allow_lockup_detector_init_retry = true;

chenglongshe/openEuler

Signed-off-by: Shi Yang <shiyang50@huawei.com>

kernel/watchdog.c
@@ -487,8 +487,12 @@ static void watchdog_enable(unsigned int cpu)
 	/* Initialize timestamp */
 	update_touch_ts();
 	/* Enable the perf event */
-	if (watchdog_enabled & NMI_WATCHDOG_ENABLED)
-		nmi_watchdog_ops.watchdog_nmi_enable(cpu);
+	if (watchdog_enabled & NMI_WATCHDOG_ENABLED) {
+		if (disable_sdei_nmi_watchdog)
+			watchdog_nmi_enable(cpu);
+		else
+			sdei_watchdog_nmi_enable(cpu);
+	}
 }
 
 static void watchdog_disable(unsigned int cpu)
@@ -502,7 +506,10 @@ static void watchdog_disable(unsigned int cpu)
 	 * between disabling the timer and disabling the perf event causes
 	 * the perf NMI to detect a false positive.
 	 */
-	nmi_watchdog_ops.watchdog_nmi_disable(cpu);
+	if (disable_sdei_nmi_watchdog)
+		watchdog_nmi_disable(cpu);
+	else
+		sdei_watchdog_nmi_disable(cpu);
 	hrtimer_cancel(hrtimer);
 	wait_for_completion(this_cpu_ptr(&softlockup_completion));
 }