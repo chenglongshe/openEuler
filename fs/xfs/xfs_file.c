be83ff7a2f78cb5eb8860075ba691ae790928aa9
John Garry <john.g.garry@oracle.com>
2025-10-25 10:15:04 +0800

    xfs: Support setting FMODE_CAN_ATOMIC_WRITE
    mainline inclusion
    from mainline-v6.12-rc1
    commit 3af5298ce94bb672845adadf347874ba5bcfad2c
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ID2HML
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=3af5298ce94bb672845adadf347874ba5bcfad2c
    
    --------------------------------
    
    Set FMODE_CAN_ATOMIC_WRITE flag if we can atomic write for that inode.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: "Darrick J. Wong" <djwong@kernel.org>
    Signed-off-by: John Garry <john.g.garry@oracle.com>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reviewed-by: John Garry <john.g.garry@oracle.com>
    Tested-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>	 #On ppc64
    Conflicts:
    	fs/xfs/xfs_file.c
    Signed-off-by: Long Li <leo.lilong@huawei.com>

--- fs/xfs/xfs_file.c
+++ fs/xfs/xfs_file.c
@@ -1249,6 +1249,8 @@ xfs_file_open(
 		return -EIO;
 	file->f_mode |= FMODE_NOWAIT | FMODE_BUF_RASYNC | FMODE_BUF_WASYNC |
 			FMODE_DIO_PARALLEL_WRITE | FMODE_CAN_ODIRECT;
+	if (xfs_inode_can_atomicwrite(XFS_I(inode)))
+		file->f_mode |= FMODE_CAN_ATOMIC_WRITE;
 	return generic_file_open(inode, file);
 }
 
