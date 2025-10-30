f5001906bbf1dcf591c2a9215aa2df5fd0a1f635
leoliu-oc <leoliu-oc@zhaoxin.com>
2025-10-30 16:08:23 +0800

    hwrng: add Zhaoxin rng driver base on rep_xstore instruction
    zhaoxin inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IDAR40
    CVE: NA
    
    -------------------
    
    Add support for Zhaoxin hardware random number generator.
    This driver base on rep_xstore instruction and uses the same
    X86_FEATURE_XSTORE as via-rng driver. Therefore, modify the x86_cpu_id
    array in the via-rng driver, so that the corresponding driver can be
    correctly loader on respective platforms.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

--- drivers/char/hw_random/via-rng.c
+++ drivers/char/hw_random/via-rng.c
@@ -135,7 +135,7 @@ static int via_rng_init(struct hwrng *rng)
 	 * is always enabled if CPUID rng_en is set.  There is no
 	 * RNG configuration like it used to be the case in this
 	 * register */
-	if (((c->x86 == 6) && (c->x86_model >= 0x0f))  || (c->x86 > 6)){
+	if ((c->x86 == 6) && (c->x86_model >= 0x0f)) {
 		if (!boot_cpu_has(X86_FEATURE_XSTORE_EN)) {
 			pr_err(PFX "can't enable hardware RNG "
 				"if XSTORE is not enabled\n");
@@ -191,17 +191,17 @@ static struct hwrng via_rng = {
 	.data_read	= via_rng_data_read,
 };
 
-static struct x86_cpu_id via_rng_ids[] = {
-	{ X86_VENDOR_CENTAUR, 6, X86_MODEL_ANY, X86_FEATURE_XSTORE },
+static struct x86_cpu_id via_rng_cpu_ids[] = {
+	X86_MATCH_VENDOR_FAM_FEATURE(CENTAUR, 6, X86_FEATURE_XSTORE, NULL),
 	{}
 };
-MODULE_DEVICE_TABLE(x86cpu, via_rng_ids);
+MODULE_DEVICE_TABLE(x86cpu, via_rng_cpu_ids);
 
 static int __init mod_init(void)
 {
 	int err;
 
-	if (!x86_match_cpu(via_rng_ids))
+	if (!x86_match_cpu(via_rng_cpu_ids))
 		return -ENODEV;
 
 	pr_info("VIA RNG detected\n");
