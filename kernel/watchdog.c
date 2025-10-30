OERV-RVCI/rvck-olk

sdei_watchdog: use lockup_detector_retry_init() to init sdei watchdog
hulk inclusion
category: other
bugzilla: https://gitee.com/openeuler/kernel/issues/I8LQCC
CVE: NA

-------------------------------------------------

sdei watchdog needs to be initialized after sdei_init,
so commit 1509d06c9c41 ("init: only move down lockup_detector_init() when sdei_watchdog is enabled")
move down the lockup_detector_init().

Now Commit 930d8f8dbab9 ("watchdog/perf: adapt the watchdog_perf interface for async model")
provide an API lockup_detector_retry_init() for anyone
who needs to delayed init lockup detector, so use this
API to delay init sdei watchdog.

Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

kernel/watchdog.c
@@ -986,7 +986,12 @@ static void __init lockup_detector_delay_init(struct work_struct *work)
 {
 	int ret;
 
-	ret = watchdog_hardlockup_probe();
+	if (disable_sdei_nmi_watchdog) {
+		ret = watchdog_hardlockup_probe();
+	} else {
+		ret = sdei_watchdog_hardlockup_probe();
+	}
+
 	if (ret) {
 		pr_info("Delayed init of the lockup detector failed: %d\n", ret);
 		pr_info("Hard watchdog permanently disabled\n");
@@ -1042,8 +1047,7 @@ void __init lockup_detector_init(void)
 	cpumask_copy(&watchdog_cpumask,
 		     housekeeping_cpumask(HK_TYPE_TIMER));
 
-	if ((!disable_sdei_nmi_watchdog && !sdei_watchdog_hardlockup_probe()) ||
-	    (disable_sdei_nmi_watchdog && !watchdog_hardlockup_probe()))
+	if (disable_sdei_nmi_watchdog && !watchdog_hardlockup_probe())
 		watchdog_hardlockup_available = true;
 	else
 		allow_lockup_detector_init_retry = true;

chenglongshe/openEuler

Signed-off-by: Shi Yang <shiyang50@huawei.com>

kernel/watchdog.c
@@ -818,7 +818,12 @@ static void __init lockup_detector_delay_init(struct work_struct *work)
 {
 	int ret;
 
-	ret = watchdog_nmi_probe();
+	if (disable_sdei_nmi_watchdog) {
+		ret = watchdog_nmi_probe();
+	} else {
+		ret = sdei_watchdog_nmi_probe();
+	}
+
 	if (ret) {
 		pr_info("Delayed init of the lockup detector failed: %d\n", ret);
 		pr_info("Hard watchdog permanently disabled\n");
@@ -874,8 +879,7 @@ void __init lockup_detector_init(void)
 	cpumask_copy(&watchdog_cpumask,
 		     housekeeping_cpumask(HK_FLAG_TIMER));
 
-	if ((!disable_sdei_nmi_watchdog && !sdei_watchdog_nmi_probe()) ||
-	    (disable_sdei_nmi_watchdog && !watchdog_nmi_probe()))
+	if (disable_sdei_nmi_watchdog && !watchdog_nmi_probe())
 		nmi_watchdog_available = true;
 	else
 		allow_lockup_detector_init_retry = true;