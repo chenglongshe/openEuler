16f70116a83a6788ada0d68ed19980948469de71
leoliu-oc <leoliu-oc@zhaoxin.com>
2025-10-30 16:06:41 +0800

    Set ASYM_PACKING Flag on Zhaoxin KH-40000 platform
    zhaoxin inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IDAMEC
    CVE: NA
    
    --------------------
    
    Set ASYM_PACKING Flag on Zhaoxin KH-40000 platform
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

--- kernel/sched/topology.c
+++ kernel/sched/topology.c
@@ -1871,6 +1871,17 @@ build_sched_domains(const struct cpumask *cpu_map, struct sched_domain_attr *att
 		}
 	}
 
+#ifdef CONFIG_X86
+	if ((boot_cpu_data.x86_vendor == X86_VENDOR_CENTAUR ||
+	     boot_cpu_data.x86_vendor == X86_VENDOR_ZHAOXIN) &&
+	    (boot_cpu_data.x86 == 7 && boot_cpu_data.x86_model == 0x5b)) {
+		for_each_cpu(i, cpu_map) {
+			for (sd = *per_cpu_ptr(d.sd, i); sd; sd = sd->parent)
+				sd->flags |= SD_ASYM_PACKING;
+		}
+	}
+#endif
+
 	/* Calculate CPU capacity for physical packages and nodes */
 	for (i = nr_cpumask_bits-1; i >= 0; i--) {
 		if (!cpumask_test_cpu(i, cpu_map))
@@ -1881,6 +1892,36 @@ build_sched_domains(const struct cpumask *cpu_map, struct sched_domain_attr *att
 			init_sched_groups_capacity(i, sd);
 		}
 	}
+
+#ifdef CONFIG_X86
+	if ((boot_cpu_data.x86_vendor == X86_VENDOR_CENTAUR ||
+	     boot_cpu_data.x86_vendor == X86_VENDOR_ZHAOXIN) &&
+	    (boot_cpu_data.x86 == 7 && boot_cpu_data.x86_model == 0x5b)) {
+		for_each_cpu(i, cpu_map) {
+			for (sd = *per_cpu_ptr(d.sd, i); sd; sd = sd->parent) {
+				if (!(sd->flags & SD_NUMA) && sd->parent &&
+				    (sd->parent->flags & SD_NUMA)) {
+					struct sched_group *sg;
+					int prio;
+					sd = sd->parent;
+					sg = sd->groups;
+					prio = arch_asym_cpu_priority(sg->asym_prefer_cpu);
+					sg = sg->next;
+
+					while (sg != sd->groups) {
+						if (arch_asym_cpu_priority(sg->asym_prefer_cpu) !=
+						    prio)
+							break;
+						sg = sg->next;
+					}
+
+					if (sg != sd->groups)
+						sd->child->flags &= ~(SD_PREFER_SIBLING);
+				}
+			}
+		}
+	}
+#endif
 
 	/*
 	 * Allocate shared sd data at last level cache.  Must be done after
