72360df4a6877a65d90a62667decb591dc7cbb66
Li Ruilin <liruilin4@huawei.com>
2025-11-07 17:45:41 +0800

    obmm: Add address validation and checking
    euleros inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/release-management/issues/ID45O2
    CVE: NA
    
    ------------------------------------------------------------------
    
    Implement comprehensive address validation to ensure security
    and stability of memory import operation.
    
    Validations include:
    - Address range and boundary checking
    - Alignment verification for different architectures
    - Permission and access right validation
    - Memory region overlap detection
    - User/kernel space address separation
    
    These checks prevent invalid memory accesses and ensure
    robust operation of the OBMM framework.
    
    Signed-off-by: Li Ruilin <liruilin4@huawei.com>
    Signed-off-by: Gao Chao <gaochao24@huawei.com>

---
+++ drivers/ub/obmm/obmm_addr_check.h
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+ */
+#ifndef OBMM_ADDR_DUP_CHECK_H
+#define OBMM_ADDR_DUP_CHECK_H
+
+#include <linux/types.h>
+
+struct obmm_uba_range {
+	unsigned int cna;
+	unsigned int tokenid;
+	unsigned long start;
+	unsigned long end;
+};
+
+int occupy_uba_range(const struct obmm_uba_range *uba_range);
+int free_uba_range(const struct obmm_uba_range *uba_range);
+
+enum obmm_addr_user {
+	OBMM_ADDR_USER_DIRECT_IMPORT,
+};
+struct obmm_addr_info {
+	enum obmm_addr_user user;
+	void *data;
+};
+
+struct obmm_pa_range {
+	phys_addr_t start;
+	phys_addr_t end;
+	struct obmm_addr_info info;
+};
+
+int occupy_pa_range(const struct obmm_pa_range *pa_range);
+int free_pa_range(const struct obmm_pa_range *pa_range);
+
+/* @addr is the search key and @info stores output value */
+int query_pa_range(phys_addr_t addr, struct obmm_addr_info *info);
+/* @addr is the search key and @info stores the overwrite value */
+int update_pa_range(phys_addr_t addr, const struct obmm_addr_info *info);
+
+void module_addr_check_init(void);
+void module_addr_check_exit(void);
+
+#endif
