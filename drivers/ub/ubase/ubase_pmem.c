ff77bf73546fe9112657ee767a4a03112e2575c3
Fengyan Mu <mufengyan@huawei.com>
2025-11-10 15:08:50 +0800

    ub: ubase: Fix some compile warnings
    drivers inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/ID5SNV
    CVE: NA
    
    ------------------------------------------------------------------
    
    Fix some compile warnings including unused struct, incompatible
    definition of struct and missed "static" modifier.
    
    Fixes: da7d25293a38 ("ub: ubase: support pre-alloc 2M pages.")
    Signed-off-by: Fengyan Mu <mufengyan@huawei.com>

--- drivers/ub/ubase/ubase_pmem.c
+++ drivers/ub/ubase/ubase_pmem.c
@@ -232,16 +232,31 @@ static void ubase_unmap_udma_pmem(struct ubase_dev *udev)
 	pmem->dma_len = 0;
 }
 
-static const struct ubase_pmem_init_func {
-	int (*init)(struct ubase_dev *udev);
-	void (*uninit)(struct ubase_dev *udev);
-} ubase_pmem_init_map[] = {
-	{ ubase_init_comm_pmem_ctx, ubase_uninit_comm_pmem_ctx },
-	{ ubase_init_udma_pmem_ctx, ubase_uninit_udma_pmem_ctx },
-	{ ubase_alloc_comm_pmem, ubase_free_comm_pmem },
-	{ ubase_alloc_udma_pmem, ubase_free_udma_pmem },
-	{ ubase_map_comm_pmem, ubase_unmap_comm_pmem },
-	{ ubase_map_udma_pmem, ubase_unmap_udma_pmem },
+static struct ubase_pmem_init_func ubase_pmem_init_map[] = {
+	{ 
+		.init = ubase_init_comm_pmem_ctx,
+	  	.uninit = ubase_uninit_comm_pmem_ctx,
+	},
+	{ 
+		.init = ubase_init_udma_pmem_ctx,
+		.uninit = ubase_uninit_udma_pmem_ctx,
+	},
+	{
+		.init = ubase_alloc_comm_pmem,
+		.uninit = ubase_free_comm_pmem,
+	},
+	{
+		.init = ubase_alloc_udma_pmem,
+		.uninit = ubase_free_udma_pmem,
+	},
+	{
+		.init = ubase_map_comm_pmem,
+		.uninit = ubase_unmap_comm_pmem,
+	},
+	{
+		.init = ubase_map_udma_pmem,
+		.uninit = ubase_unmap_udma_pmem,
+	},
 };
 
 static int __ubase_prealloc_mem_init(struct ubase_dev *udev)
