5737bd7bb06265f10aefc9ef28662920232516f3
Jinjie Ruan <ruanjinjie@huawei.com>
2025-11-10 16:47:27 +0800

    xcall2.0: Support xcall1.0 for hardware xcall
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/release-management/issues/ID5CMS
    
    --------------------------------
    
    Due to the default HCR_EL2.TACR value is 1, enable ACTLR_XCALL system-wide
    by default to avoid the overhead of vCPU trapping out caused by accessing
    ACTLR_XCALL takes place during scheduling context switch.
    
    And Separate the control interface of xcall in userspace into
    the two parts. The first one aims to register xcall to individual TASK
    via /proc/[pid]/xcall. The second one amis to register xcall to
    individual BINARY file via /proc/xcall/comm. So it needs some cleanup to
    the code to implement the first one.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- arch/arm64/kernel/xcall/xcall.c
+++ arch/arm64/kernel/xcall/xcall.c
@@ -6,7 +6,6 @@
  */
 
 #include <linux/bitmap.h>
-#include <linux/percpu.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <asm/xcall.h>
@@ -25,45 +24,8 @@ static inline int sw_xcall_init_task(struct task_struct *p, struct task_struct *
 	return 0;
 }
 
-#ifdef CONFIG_ACTLR_XCALL_XINT
-static const void *default_syscall_table[__NR_syscalls + 1] = {
-	[0 ... __NR_syscalls] = no_xcall_entry,
-};
-
-asmlinkage DEFINE_PER_CPU(void *, __cpu_xcall_entry) = default_syscall_table;
-static inline int hw_xcall_init_task(struct task_struct *p, struct task_struct *orig)
-{
-	struct hw_xcall_info *p_xinfo, *orig_xinfo;
-
-	p->xinfo = kzalloc(sizeof(struct hw_xcall_info), GFP_KERNEL);
-	if (!p->xinfo)
-		return -ENOMEM;
-
-	p_xinfo = TASK_HW_XINFO(p);
-	spin_lock_init(&p_xinfo->lock);
-
-	if (!orig->xinfo) {
-		memcpy(p->xinfo, default_syscall_table, XCALL_ENTRY_SIZE);
-		atomic_set(&p_xinfo->xcall_scno_count, 0);
-	} else {
-		orig_xinfo = TASK_HW_XINFO(orig);
-		spin_lock(&orig_xinfo->lock);
-		memcpy(p->xinfo, orig->xinfo, XCALL_ENTRY_SIZE);
-		atomic_set(&p_xinfo->xcall_scno_count,
-			   atomic_read(&orig_xinfo->xcall_scno_count));
-		spin_unlock(&orig_xinfo->lock);
-	}
-
-	return 0;
-}
-#endif
-
 int xcall_init_task(struct task_struct *p, struct task_struct *orig)
 {
-#ifdef CONFIG_ACTLR_XCALL_XINT
-	if (system_uses_xcall_xint())
-		return hw_xcall_init_task(p, orig);
-#endif
 	if (static_branch_unlikely(&xcall_enable))
 		return sw_xcall_init_task(p, orig);
 
@@ -72,6 +34,6 @@ int xcall_init_task(struct task_struct *p, struct task_struct *orig)
 
 void xcall_task_free(struct task_struct *p)
 {
-	if (system_uses_xcall_xint() || static_branch_unlikely(&xcall_enable))
+	if (static_branch_unlikely(&xcall_enable))
 		kfree(p->xinfo);
 }
