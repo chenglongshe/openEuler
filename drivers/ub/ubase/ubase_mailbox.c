OERV-RVCI/rvck-olk

commit 8d68017f37faefe5f545dbfd994e1b5141ec06e1

ub: ubase: support for command process
drivers inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID41MP
CVE: NA

------------------------------------------------------------

This patch support for command proccess by command queue and
provides interfaces to other modules, such as udma and unic
etc. The command queue is used by the driver to interact with
the imp.

Signed-off-by: Xiaobo Zhang <zhangxiaobo21@huawei.com>
Signed-off-by: Xiongchuan Zhou <zhouxiongchuan@h-partners.com>

drivers/ub/ubase/ubase_mailbox.c
@@ -1,0 +1,542 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved.
+ *
+ */
+
+#include <linux/dmapool.h>
+#include <linux/slab.h>
+#include <linux/ummu_core.h>
+
+#include "ubase_cmd.h"
+#include "ubase_mailbox.h"
+
+int ubase_mbox_cmd_init(struct ubase_dev *udev)
+{
+	struct ubase_mbx_event_context *ctx = &udev->mb_cmd.ctx;
+
+	udev->mb_cmd.pool = dma_pool_create("ubase_mbox", udev->dev,
+					    UBASE_MAILBOX_SIZE,
+					    UBASE_MAILBOX_SIZE, 0);
+	if (!udev->mb_cmd.pool)
+		return -ENOMEM;
+
+	sema_init(&udev->mb_cmd.sem, 1);
+	init_completion(&ctx->done);
+
+	return 0;
+}
+
+void ubase_mbox_cmd_uninit(struct ubase_dev *udev)
+{
+	if (!udev->mb_cmd.pool)
+		return;
+
+	dma_pool_destroy(udev->mb_cmd.pool);
+	udev->mb_cmd.pool = NULL;
+}
+
+struct ubase_cmd_mailbox *__ubase_alloc_cmd_mailbox(struct ubase_dev *udev)
+{
+	struct ubase_cmd_mailbox *mailbox;
+
+	if (!udev->mb_cmd.pool) {
+		ubase_err(udev, "failed to alloc mailbox, pool is null.\n");
+		return NULL;
+	}
+
+	mailbox = kzalloc(sizeof(*mailbox), GFP_KERNEL);
+	if (!mailbox) {
+		ubase_err(udev, "failed to alloc mailbox.\n");
+		goto failed_alloc_mailbox;
+	}
+
+	mailbox->buf = dma_pool_zalloc(udev->mb_cmd.pool, GFP_KERNEL,
+				       &mailbox->dma);
+	if (!mailbox->buf) {
+		ubase_err(udev, "failed to alloc buffer of mailbox.\n");
+		goto failed_alloc_mailbox_buf;
+	}
+
+	return mailbox;
+
+failed_alloc_mailbox_buf:
+	kfree(mailbox);
+failed_alloc_mailbox:
+	return NULL;
+}
+
+struct ubase_cmd_mailbox *ubase_alloc_cmd_mailbox(struct auxiliary_device *aux_dev)
+{
+	struct ubase_dev *udev;
+
+	if (!aux_dev)
+		return NULL;
+
+	udev = __ubase_get_udev_by_adev(aux_dev);
+
+	return __ubase_alloc_cmd_mailbox(udev);
+}
+EXPORT_SYMBOL(ubase_alloc_cmd_mailbox);
+
+void __ubase_free_cmd_mailbox(struct ubase_dev *udev,
+			      struct ubase_cmd_mailbox *mailbox)
+{
+	if (!mailbox) {
+		ubase_err(udev, "Invalid mailbox.\n");
+		return;
+	}
+
+	dma_pool_free(udev->mb_cmd.pool, mailbox->buf, mailbox->dma);
+	kfree(mailbox);
+}
+
+void ubase_free_cmd_mailbox(struct auxiliary_device *aux_dev,
+			    struct ubase_cmd_mailbox *mailbox)
+{
+	struct ubase_dev *udev;
+
+	if (!aux_dev)
+		return;
+
+	udev = __ubase_get_udev_by_adev(aux_dev);
+
+	__ubase_free_cmd_mailbox(udev, mailbox);
+}
+EXPORT_SYMBOL(ubase_free_cmd_mailbox);
+
+static int ubase_post_mailbox(struct ubase_dev *udev,
+			      struct ubase_cmdq_desc *desc,
+			      u32 timeout, u8 *complete_status, bool is_read)
+{
+	union ubase_mbox *mb;
+	unsigned long end;
+	int ret;
+
+	end = msecs_to_jiffies(timeout) + jiffies;
+	mb = (union ubase_mbox *)desc->data;
+
+	while (1) {
+		desc->flag = cpu_to_le16(UBASE_CMD_FLAG_NO_INTR | UBASE_CMD_FLAG_IN);
+		if (is_read)
+			desc->flag |= cpu_to_le16(UBASE_CMD_FLAG_WR);
+
+		ret = ubase_send_cmd(udev, desc, 1);
+		if (!ret && !(is_read ? mb->query_hw_run : mb->hw_run))
+			break;
+
+		if (time_after(jiffies, end)) {
+			dev_err_ratelimited(udev->dev,
+					    "failed to wait mbox.\n");
+			return -ETIMEDOUT;
+		}
+
+		cond_resched();
+	}
+
+	if (!ret)
+		*complete_status =
+			(u8)(is_read ? mb->query_status : mb->status);
+
+	return ret;
+}
+
+static int ubase_poll_mbox_done(struct ubase_dev *udev, uint32_t timeout)
+{
+	struct ubase_cmdq_desc desc;
+	u8 status = 0;
+	int ret;
+
+	ubase_cmd_setup_basic_desc(&desc, UBASE_OPC_QUERY_MB_ST, true, 1);
+
+	ret = ubase_post_mailbox(udev, &desc, timeout, &status, true);
+	if (!ret) {
+		if (!status) {
+			ubase_info(udev,
+				   "failed to query ubase mailbox, status = %u.\n",
+				   status);
+			return -EBUSY;
+		}
+	} else
+		dev_err_ratelimited(udev->dev,
+				    "failed to check mbox status = %u, ret = %d.\n",
+				    status, ret);
+
+	return ret;
+}
+
+static void ubase_mbox_desc_init(struct ubase_dev *udev, union ubase_mbox *mb,
+				 u64 in_param, struct ubase_mbx_attr *attr)
+{
+	memset(mb, 0, sizeof(*mb));
+	mb->in_param_l = cpu_to_le32(lower_32_bits(in_param));
+	mb->in_param_h = cpu_to_le32(upper_32_bits(in_param));
+	mb->cmd = attr->op;
+	mb->tag = attr->tag;
+	mb->mbx_ue_id = attr->mbx_ue_id;
+}
+
+static int ubase_cmd_mbox_poll(struct ubase_dev *udev,
+			       struct ubase_mbx_attr *attr,
+			       struct ubase_cmd_mailbox *mailbox)
+{
+	struct ubase_cmdq_desc desc;
+	union ubase_mbox *mb;
+	u8 status = 0;
+	int ret;
+
+	if (udev->reset_stage == UBASE_RESET_STAGE_UNINIT)
+		return 0;
+
+	mb = (union ubase_mbox *)desc.data;
+
+	ubase_cmd_setup_basic_desc(&desc, UBASE_OPC_POST_MB, false, 1);
+
+	ubase_mbox_desc_init(udev, mb, (u64)mailbox->dma, attr);
+
+	mb->seq_num = CMD_MBX_POLL_VALUE;
+
+	ret = ubase_post_mailbox(udev, &desc, UBASE_MBX_TX_TIMEOUT, &status,
+				 false);
+	if (ret) {
+		ubase_err(udev,
+			  "failed to post mailbox 0x%x in poll mode, status = %u, ret = %d.\n",
+			  attr->op, status, ret);
+		return ret;
+	}
+
+	return ubase_poll_mbox_done(udev, UBASE_MBX_TX_TIMEOUT);
+}
+
+static int ubase_cmd_mbox_event(struct ubase_dev *udev,
+				struct ubase_mbx_attr *attr,
+				struct ubase_cmd_mailbox *mailbox)
+{
+	union ubase_mbox mb_out = {0};
+	union ubase_mbox mb_in = {0};
+	struct ubase_cmd_buf in, out;
+	int ret;
+
+	if (udev->reset_stage == UBASE_RESET_STAGE_UNINIT)
+		return 0;
+
+	ubase_mbox_desc_init(udev, &mb_in, (u64)mailbox->dma, attr);
+
+	mb_in.event_en = 1;
+	__ubase_fill_inout_buf(&in, UBASE_OPC_POST_MB, false,
+			       sizeof(union ubase_mbox), (void *)&mb_in);
+	__ubase_fill_inout_buf(&out, UBASE_OPC_POST_MB, false,
+			       sizeof(union ubase_mbox), (void *)&mb_out);
+
+	ret = ubase_post_mailbox_by_event(udev, &in, &out);
+	if (ret)
+		ubase_err(udev,
+			  "failed to post mailbox 0x%x in event mode, ret = %d.\n",
+			  attr->op, ret);
+
+	return ret;
+}
+
+int ubase_create_ctx_page(struct ubase_dev *udev,
+			  struct ubase_ctx_buf_cap *ctx_buf,
+			  struct ubase_ctx_page **ctx_page, u32 npage)
+{
+	int ret;
+
+	*ctx_page = kzalloc(sizeof(struct ubase_ctx_page), GFP_KERNEL);
+	if (!(*ctx_page))
+		return -ENOMEM;
+
+	(*ctx_page)->iova = ctx_buf->dma_ctx_buf_ba + npage * PAGE_SIZE;
+	refcount_set(&(*ctx_page)->refcount, 1);
+	(*ctx_page)->npage = npage;
+	ret = ummu_fill_pages(ctx_buf->slot, (*ctx_page)->iova,
+			      UBASE_IOVA_COMM_PFN_CNT);
+	if (ret) {
+		ubase_err(udev, "failed to fill pages in ummu, ret = %d\n", ret);
+		kfree(*ctx_page);
+		*ctx_page = NULL;
+	}
+
+	return ret;
+}
+
+void ubase_destroy_ctx_page(struct ubase_dev *udev,
+			    struct ubase_ctx_page *ctx_page,
+			    struct ubase_ctx_buf_cap *ctx_buf)
+{
+	int ret;
+
+	ret = ummu_drain_pages(ctx_buf->slot, ctx_page->iova,
+			       UBASE_IOVA_COMM_PFN_CNT);
+	if (ret)
+		ubase_err(udev,
+			  "failed to drain pages in ummu, npage = %u, ret = %d.\n",
+			  ctx_page->npage, ret);
+
+	kfree(ctx_page);
+}
+
+static int ubase_use_buf_ctx_page(struct ubase_dev *udev,
+				  struct ubase_ctx_buf_cap *ctx_buf, u32 tag)
+{
+	u32 cnt_per_page_shift = ctx_buf->cnt_per_page_shift;
+	u32 npage = tag >> cnt_per_page_shift;
+	struct ubase_ctx_page *ctx_page;
+	int ret;
+
+	mutex_lock(&ctx_buf->ctx_mutex);
+
+	ctx_page = (struct ubase_ctx_page *)xa_load(&ctx_buf->ctx_xa, npage);
+	if (!ctx_page) {
+		ret = ubase_create_ctx_page(udev, ctx_buf, &ctx_page, npage);
+		if (ret) {
+			ubase_err(udev, "failed to create context page, ret = %d.\n",
+				  ret);
+			goto err_create;
+		}
+
+		ret = xa_err(xa_store(&ctx_buf->ctx_xa, npage, ctx_page,
+				      GFP_KERNEL));
+		if (ret) {
+			ubase_err(udev, "failed to store page, ret = %d.\n",
+				  ret);
+			goto err_store;
+		}
+	}
+
+	refcount_inc(&ctx_page->refcount);
+	mutex_unlock(&ctx_buf->ctx_mutex);
+
+	return 0;
+err_store:
+	ubase_destroy_ctx_page(udev, ctx_page, ctx_buf);
+err_create:
+	mutex_unlock(&ctx_buf->ctx_mutex);
+
+	return ret;
+}
+
+static void ubase_free_buf_ctx_page(struct ubase_dev *udev,
+				    struct ubase_ctx_buf_cap *ctx_buf, u32 tag)
+{
+	struct ubase_ctx_page *ctx_page;
+	u32 cnt_per_page_shift;
+	u32 npage;
+
+	if (!ctx_buf)
+		return;
+
+	cnt_per_page_shift = ctx_buf->cnt_per_page_shift;
+	npage = tag >> cnt_per_page_shift;
+
+	mutex_lock(&ctx_buf->ctx_mutex);
+
+	ctx_page = (struct ubase_ctx_page *)xa_load(&ctx_buf->ctx_xa, npage);
+	if (!ctx_page) {
+		ubase_err(udev,
+			  "no find ctx page in free buf page, npage = %u.\n",
+			  npage);
+		mutex_unlock(&ctx_buf->ctx_mutex);
+		return;
+	}
+
+	refcount_dec(&ctx_page->refcount);
+	if (refcount_dec_if_one(&ctx_page->refcount)) {
+		ubase_info(udev,
+			   "refcout of ctx page is equal to one and the ctx_page is going to erased.\n");
+		xa_erase(&ctx_buf->ctx_xa, npage);
+		ubase_destroy_ctx_page(udev, ctx_page, ctx_buf);
+	}
+
+	mutex_unlock(&ctx_buf->ctx_mutex);
+}
+
+static struct ubase_ctx_buf_cap*
+ubase_parse_common_buf(struct ubase_mbx_attr *attr,
+		       struct mbx_op_match op_matches[],
+		       enum ubase_mb_type *type, u32 size)
+{
+	u32 i;
+
+	for (i = 0; i < size; i++) {
+		if (attr->op == op_matches[i].op) {
+			*type = op_matches[i].type;
+			return op_matches[i].ctx_caps;
+		}
+	}
+
+	return NULL;
+}
+
+static bool ubase_is_jfs_opcode(u8 op)
+{
+	switch (op) {
+	case UBASE_MB_CREATE_JFS_CONTEXT:
+	case UBASE_MB_MODIFY_JFS_CONTEXT:
+	case UBASE_MB_DESTROY_JFS_CONTEXT:
+	case UBASE_MB_QUERY_JFS_CONTEXT:
+		return true;
+	default:
+		return false;
+	}
+}
+
+static struct ubase_ctx_buf_cap*
+ubase_parse_ta_opcode_buf(struct ubase_dev *udev, struct ubase_mbx_attr *attr,
+			  enum ubase_mb_type *type)
+{
+	struct mbx_op_match ta_matches[] = {
+		{UBASE_MB_CREATE_JFS_CONTEXT, UBASE_MB_CREATE, &udev->ctx_buf.jfs},
+		{UBASE_MB_MODIFY_JFS_CONTEXT, UBASE_MB_MODIFY, &udev->ctx_buf.jfs},
+		{UBASE_MB_DESTROY_JFS_CONTEXT, UBASE_MB_DESTROY, &udev->ctx_buf.jfs},
+		{UBASE_MB_QUERY_JFS_CONTEXT, UBASE_MB_QUERY, &udev->ctx_buf.jfs},
+		{UBASE_MB_CREATE_JFC_CONTEXT, UBASE_MB_CREATE, &udev->ctx_buf.jfc},
+		{UBASE_MB_MODIFY_JFC_CONTEXT, UBASE_MB_MODIFY, &udev->ctx_buf.jfc},
+		{UBASE_MB_DESTROY_JFC_CONTEXT, UBASE_MB_DESTROY, &udev->ctx_buf.jfc},
+		{UBASE_MB_QUERY_JFC_CONTEXT, UBASE_MB_QUERY, &udev->ctx_buf.jfc},
+		{UBASE_MB_CREATE_JFR_CONTEXT, UBASE_MB_CREATE, &udev->ctx_buf.jfr},
+		{UBASE_MB_MODIFY_JFR_CONTEXT, UBASE_MB_MODIFY, &udev->ctx_buf.jfr},
+		{UBASE_MB_DESTROY_JFR_CONTEXT, UBASE_MB_DESTROY, &udev->ctx_buf.jfr},
+		{UBASE_MB_QUERY_JFR_CONTEXT, UBASE_MB_QUERY, &udev->ctx_buf.jfr},
+		{UBASE_MB_CREATE_JETTY_GROUP_CONTEXT, UBASE_MB_CREATE, &udev->ctx_buf.jtg},
+		{UBASE_MB_MODIFY_JETTY_GROUP_CONTEXT, UBASE_MB_MODIFY, &udev->ctx_buf.jtg},
+		{UBASE_MB_DESTROY_JETTY_GROUP_CONTEXT, UBASE_MB_DESTROY, &udev->ctx_buf.jtg},
+		{UBASE_MB_QUERY_JETTY_GROUP_CONTEXT, UBASE_MB_QUERY, &udev->ctx_buf.jtg},
+		{UBASE_MB_CREATE_RC_CONTEXT, UBASE_MB_CREATE, &udev->ctx_buf.rc},
+		{UBASE_MB_MODIFY_RC_CONTEXT, UBASE_MB_MODIFY, &udev->ctx_buf.rc},
+		{UBASE_MB_DESTROY_RC_CONTEXT, UBASE_MB_DESTROY, &udev->ctx_buf.rc},
+		{UBASE_MB_QUERY_RC_CONTEXT, UBASE_MB_QUERY, &udev->ctx_buf.rc},
+	};
+	u32 size = ARRAY_SIZE(ta_matches);
+
+	return ubase_parse_common_buf(attr, ta_matches, type, size);
+}
+
+static struct ubase_ctx_buf_cap*
+ubase_parse_opcode_buf(struct ubase_dev *udev, struct ubase_mbx_attr *attr,
+		       enum ubase_mb_type *type)
+{
+	if (ubase_is_jfs_opcode(attr->op) &&
+	    test_bit(UBASE_STATE_PREALLOC_OK_B, &udev->state_bits))
+		return NULL;
+
+	return ubase_parse_ta_opcode_buf(udev, attr, type);
+}
+
+static int ubase_check_buf_ctx_page(struct ubase_dev *udev,
+				    struct ubase_ctx_buf_cap *ctx_buf, u32 tag)
+{
+	u32 cnt_per_page_shift = ctx_buf->cnt_per_page_shift;
+	u32 npage = tag >> cnt_per_page_shift;
+	struct ubase_ctx_page *ctx_page;
+
+	mutex_lock(&ctx_buf->ctx_mutex);
+
+	ctx_page = (struct ubase_ctx_page *)xa_load(&ctx_buf->ctx_xa, npage);
+	if (!ctx_page) {
+		ubase_err(udev,
+			  "failed to find ctx page in free buf page, npage = %u.\n",
+			  npage);
+		mutex_unlock(&ctx_buf->ctx_mutex);
+		return -EINVAL;
+	}
+
+	mutex_unlock(&ctx_buf->ctx_mutex);
+
+	return 0;
+}
+
+static int ubase_hw_upgrade_ctx_event(struct ubase_dev *udev,
+				      struct ubase_mbx_attr *attr,
+				      struct ubase_cmd_mailbox *mailbox)
+{
+	int ret;
+
+	down(&udev->mb_cmd.sem);
+	ret = ubase_cmd_mbox_event(udev, attr, mailbox);
+	up(&udev->mb_cmd.sem);
+
+	return ret;
+}
+
+int ubase_hw_upgrade_ctx_poll(struct ubase_dev *udev,
+			      struct ubase_mbx_attr *attr,
+			      struct ubase_cmd_mailbox *mailbox)
+{
+	int ret;
+
+	down(&udev->mb_cmd.sem);
+	ret = ubase_cmd_mbox_poll(udev, attr, mailbox);
+	up(&udev->mb_cmd.sem);
+
+	return ret;
+}
+
+int __ubase_hw_upgrade_ctx(struct ubase_dev *udev,
+			   struct ubase_mbx_attr *attr,
+			   struct ubase_cmd_mailbox *mailbox)
+{
+	return ubase_hw_upgrade_ctx_event(udev, attr, mailbox);
+}
+
+int __ubase_hw_upgrade_ctx_ex(struct ubase_dev *udev,
+			      struct ubase_mbx_attr *attr,
+			      struct ubase_cmd_mailbox *mailbox)
+{
+	enum ubase_mb_type type = UBASE_MB_OTHER;
+	struct ubase_ctx_buf_cap *ctx_buf;
+	int ret;
+
+	ctx_buf = ubase_parse_opcode_buf(udev, attr, &type);
+	if (ctx_buf) {
+		if (type == UBASE_MB_CREATE) {
+			ret = ubase_use_buf_ctx_page(udev, ctx_buf, attr->tag);
+			if (ret) {
+				ubase_err(udev, "failed to write context va, ret = %d.\n",
+					  ret);
+				return ret;
+			}
+		} else if (type == UBASE_MB_QUERY) {
+			ret = ubase_check_buf_ctx_page(udev, ctx_buf, attr->tag);
+			if (ret) {
+				ubase_info(udev,
+					   "A query operation is performed before the create operation, 0 is returned by default, op = 0x%x.\n",
+					   attr->op);
+				return 0;
+			}
+		} else if (type == UBASE_MB_MODIFY || type == UBASE_MB_DESTROY) {
+			ret = ubase_check_buf_ctx_page(udev, ctx_buf, attr->tag);
+			if (ret) {
+				ubase_info(udev,
+					   "An access operation is performed before the create operation, op = 0x%x.\n",
+					   attr->op);
+				return ret;
+			}
+		}
+	}
+
+	ret = __ubase_hw_upgrade_ctx(udev, attr, mailbox);
+	if ((ret && type == UBASE_MB_CREATE) ||
+	    (!ret && type == UBASE_MB_DESTROY))
+		ubase_free_buf_ctx_page(udev, ctx_buf, attr->tag);
+
+	return ret;
+}
+
+int ubase_hw_upgrade_ctx_ex(struct auxiliary_device *aux_dev,
+			    struct ubase_mbx_attr *attr,
+			    struct ubase_cmd_mailbox *mailbox)
+{
+	struct ubase_dev *udev;
+
+	if (!aux_dev || !attr || !mailbox)
+		return -EINVAL;
+
+	udev = ubase_get_udev_by_adev(aux_dev);
+	if (!test_bit(UBASE_STATE_CTX_READY_B, &udev->state_bits))
+		return -EAGAIN;
+
+	return __ubase_hw_upgrade_ctx_ex(udev, attr, mailbox);
+}
+EXPORT_SYMBOL(ubase_hw_upgrade_ctx_ex);

drivers/ub/ubase/Makefile
@@ -10,7 +10,7 @@ ccflags-y += -I$(srctree)/drivers/ub/ubase/debugfs
 MODULE_NAME := ubase
 
 UBASE_OBJS := ubase_main.o ubase_dev.o ubase_cmd.o \
-	debugfs/ubase_debugfs.o ubase_eq.o ubase_ubus.o
+	debugfs/ubase_debugfs.o ubase_eq.o ubase_mailbox.o ubase_ubus.o
 
 $(MODULE_NAME)-objs := $(UBASE_OBJS)
 obj-$(CONFIG_UB_UBASE) := ubase.o

drivers/ub/ubase/ubase.h
@@ -13,6 +13,11 @@
 #define UBASE_CAP_LEN			3
 #define UBASE_MAX_TCG_NUM		(4)
 
+enum ubase_service_state {
+	UBASE_STATE_CRQ_SERVICE_SCHED,
+	UBASE_STATE_CRQ_HANDLING,
+};
+
 struct ubase_delay_work {
 	struct delayed_work	service_task;
 	unsigned long		state;
@@ -41,5 +46,22 @@ enum {
 
 #define ubase_get_cap_bit(udev, nr) \
 	test_bit(nr, (unsigned long *)((udev)->cap_bits))
+
+static inline void ubase_write_reg(void __iomem *base, u32 reg, u32 value)
+{
+	writel(value, base + reg);
+}
+
+static inline u32 ubase_read_reg(u8 __iomem *base, u32 reg)
+{
+	u8 __iomem *reg_addr = READ_ONCE(base);
+
+	return readl(reg_addr + reg);
+}
+
+#define ubase_write_dev(a, reg, value) \
+	ubase_write_reg((a)->io_base.addr, reg, value)
+#define ubase_read_dev(a, reg) \
+	ubase_read_reg((a)->io_base.addr, reg)
 
 #endif

drivers/ub/ubase/ubase_cmd.h
@@ -11,8 +11,88 @@
 
 #include "ubase_dev.h"
 
+#define UBASE_CMDQ_DESC_NUM_S		3
+#define UBASE_CMDQ_DESC_NUM		1024
+#define UBASE_CMDQ_TX_TIMEOUT		3000000
+#define UBASE_CMDQ_MBX_TX_TIMEOUT	30000
+#define UBASE_CMDQ_CLEAR_WAIT_TIME	200
+#define UBASE_CMDQ_WAIT_TIME		10
+
+#define UBASE_CMD_FLAG_IN		BIT(0)
+#define UBASE_CMD_FLAG_OUT		BIT(1)
+#define UBASE_CMD_FLAG_NEXT		BIT(2)
+#define UBASE_CMD_FLAG_WR		BIT(3)
+#define UBASE_CMD_FLAG_NO_INTR		BIT(4)
+#define UBASE_CMD_FLAG_ERR_INTR		BIT(5)
+#define UBASE_CMD_FLAG_GET_BD_NUM	BIT(6)
+
+#define UBASE_UE2UE_MSG_WAIT_TIME		3000
+
+#define UBASE_CRQ_SCHED_TIMEOUT		(HZ / 2)
+
+#define UBASE_CMD_HEADER_LENGTH		8
+#define UBASE_CMD_DATA_LENGTH		(UBASE_DESC_DATA_LEN * sizeof(__le32))
+#define UBASE_CMD_MAX_DESC_SIZE \
+	(UBASE_CMDQ_DESC_NUM * sizeof(struct ubase_cmdq_desc))
+
+#define UBASE_MOVE_CRQ_RING_PTR(crq) \
+	((crq)->ci = ((crq)->ci + 1) % (crq)->desc_num)
+
+enum ubase_cmd_state {
+	UBASE_STATE_CMD_DISABLE
+};
+
+struct ubase_query_version_cmd {
+	__le32 firmware;
+	__le32 hardware;
+	__le32 rsv;
+	__le32 caps[UBASE_CAP_LEN];
+};
+
+static inline void __ubase_fill_inout_buf(struct ubase_cmd_buf *buf, u16 opcode,
+					  bool is_read, u32 data_size, void *data)
+{
+	buf->opcode = opcode;
+	buf->is_read = is_read;
+	buf->data_size = data_size;
+	buf->data = data;
+}
+
+int ubase_cmd_init(struct ubase_dev *udev);
+void ubase_cmd_uninit(struct ubase_dev *udev);
+
+static inline void __ubase_cmd_enable(struct ubase_dev *udev)
+{
+	clear_bit(UBASE_STATE_CMD_DISABLE, &udev->hw.state);
+}
+
+static inline void __ubase_cmd_disable(struct ubase_dev *udev)
+{
+	set_bit(UBASE_STATE_CMD_DISABLE, &udev->hw.state);
+}
+
+void ubase_cmd_disable(struct ubase_dev *udev);
+
+void ubase_cmd_setup_basic_desc(struct ubase_cmdq_desc *desc,
+				enum ubase_opcode_type opcode, bool is_read,
+				u8 num);
+int ubase_send_cmd(struct ubase_dev *udev,
+		   struct ubase_cmdq_desc *desc, int num);
+
+int ubase_post_mailbox_by_event(struct ubase_dev *udev,
+				struct ubase_cmd_buf *in,
+				struct ubase_cmd_buf *out);
+int __ubase_cmd_send_in(struct ubase_dev *udev, struct ubase_cmd_buf *in);
+int __ubase_cmd_send_inout(struct ubase_dev *udev, struct ubase_cmd_buf *in,
+			   struct ubase_cmd_buf *out);
+
+int ubase_cmd_mbx_event_cb(struct notifier_block *nb, unsigned long action,
+			   void *data);
+
 int __ubase_register_crq_event(struct ubase_dev *udev,
 			       struct ubase_crq_event_nb *nb);
 void __ubase_unregister_crq_event(struct ubase_dev *udev, u16 opcode);
+
+void ubase_crq_service_task(struct ubase_delay_work *ubase_work);
 
 #endif

drivers/ub/ubase/ubase_dev.c
@@ -9,6 +9,7 @@
 
 #include "debugfs/ubase_debugfs.h"
 #include "ubase_cmd.h"
+#include "ubase_mailbox.h"
 #include "ubase_dev.h"
 
 #define UBASE_PERIOD_100MS 100
@@ -435,7 +436,7 @@ static const struct ubase_init_function ubase_init_func_map[] = {
 	},
 	{
 		"init cmd queue", UBASE_SUP_ALL, 1,
-		NULL, NULL
+		ubase_cmd_init, ubase_cmd_uninit
 	},
 	{
 		"query dev res", UBASE_SUP_ALL, 0,
@@ -443,7 +444,7 @@ static const struct ubase_init_function ubase_init_func_map[] = {
 	},
 	{
 		"init mailbox", UBASE_SUP_NO_PMU, 0,
-		NULL, NULL
+		ubase_mbox_cmd_init, ubase_mbox_cmd_uninit
 	},
 	{
 		"query chip info", UBASE_SUP_ALL, 0,

drivers/ub/ubase/ubase_dev.h
@@ -83,6 +83,12 @@ struct ubase_dma_buf {
 	size_t		size;
 };
 
+struct ubase_ctx_page {
+	dma_addr_t		iova;
+	u32			npage;
+	refcount_t		refcount;
+};
+
 struct ubase_ta_layer_ctx {
 	struct ubase_dma_buf	extdb_buf;
 	struct ubase_dma_buf	timer_buf;
@@ -242,7 +248,6 @@ static inline
 struct ubase_dev *__ubase_get_udev_by_adev(struct auxiliary_device *adev)
 {
 	struct ubase_adev *uadev = container_of(adev, struct ubase_adev, adev);
-
 	return uadev->udev;
 }
 

drivers/ub/ubase/ubase_mailbox.h
@@ -1,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved.
+ *
+ */
+
+#ifndef __UBASE_MAILBOX_H__
+#define __UBASE_MAILBOX_H__
+
+#include "ubase_comm_mbx.h"
+#include "ubase_dev.h"
+
+#define CMD_MBX_POLL_VALUE 0xffff
+#define UBASE_MAILBOX_SIZE 4096
+#define UBASE_MBX_TX_TIMEOUT 30000
+
+enum ubase_mb_type {
+	UBASE_MB_CREATE,
+	UBASE_MB_MODIFY,
+	UBASE_MB_DESTROY,
+	UBASE_MB_QUERY,
+	UBASE_MB_OTHER,
+};
+
+struct mbx_op_match {
+	u32			op;
+	enum ubase_mb_type	type;
+	struct ubase_ctx_buf_cap	*ctx_caps;
+};
+
+int ubase_mbox_cmd_init(struct ubase_dev *udev);
+void ubase_mbox_cmd_uninit(struct ubase_dev *udev);
+struct ubase_cmd_mailbox *__ubase_alloc_cmd_mailbox(struct ubase_dev *udev);
+void __ubase_free_cmd_mailbox(struct ubase_dev *udev,
+			      struct ubase_cmd_mailbox *mailbox);
+void ubase_destroy_ctx_page(struct ubase_dev *udev,
+			    struct ubase_ctx_page *ctx_page,
+			    struct ubase_ctx_buf_cap *ctx_buf);
+int ubase_hw_upgrade_ctx_poll(struct ubase_dev *udev,
+			      struct ubase_mbx_attr *attr,
+			      struct ubase_cmd_mailbox *mailbox);
+int __ubase_hw_upgrade_ctx(struct ubase_dev *udev,
+			   struct ubase_mbx_attr *attr,
+			   struct ubase_cmd_mailbox *mailbox);
+int __ubase_hw_upgrade_ctx_ex(struct ubase_dev *udev,
+			      struct ubase_mbx_attr *attr,
+			      struct ubase_cmd_mailbox *mailbox);
+int ubase_create_ctx_page(struct ubase_dev *udev,
+			  struct ubase_ctx_buf_cap *ctx_buf,
+			  struct ubase_ctx_page **ctx_page, u32 npage);
+
+#endif

chenglongshe/openEuler

commit 7b5a251157456eca539bc558ae245cecb7be0b5d

ub: ubase: Fix the problem that free pages when ubase driver destroy TA context
drivers inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ID5SNV
CVE: NA

------------------------------------------------------------------

Fix the problem that free pages when ubase driver destroy TA context
which we don't want to release, because the upper driver still need
to use them for further use.

Fixes: 8d68017f37fa ("ub: ubase: support for command process")
Signed-off-by: Fengyan Mu <mufengyan@huawei.com>

drivers/ub/ubase/ubase_mailbox.c
@@ -517,8 +517,7 @@ int __ubase_hw_upgrade_ctx_ex(struct ubase_dev *udev,
 	}
 
 	ret = __ubase_hw_upgrade_ctx(udev, attr, mailbox);
-	if ((ret && type == UBASE_MB_CREATE) ||
-	    (!ret && type == UBASE_MB_DESTROY))
+	if (ret && type == UBASE_MB_CREATE)
 		ubase_free_buf_ctx_page(udev, ctx_buf, attr->tag);
 
 	return ret;