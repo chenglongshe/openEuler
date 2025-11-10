4fc48b43346eb19f13838394e9e3528c39da35c1
Liao Chang <liaochang1@huawei.com>
2025-11-10 15:17:48 +0800

    xcall: Refactor the early exception entry for ACTLR.xcall
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/release-management/issues/ID5CMS
    
    --------------------------------
    
    Use the symbol el0t_64_svc_entry as the dispatcher of svc exception
    handler:
    
    - el0_slow_syscall: use kernel_entry and ret_to_user to do exception
      context switch, additionally use el0_svc to invoke syscall
      functions.
    
    - el0_fast_syscall: use hw_xcall_save_base_regs and
      hw_xcall_restore_base_regs to do low-overhead context switch,
      additionally use el0_svc to invoke syscall functions.
    
    - el0_xcall_syscall: use hw_xcall_save_base_regs and
      hw_xcall_restore_base_regs to do low-overhead context switch,
      additionally use el0_xcall to invoke dynamically load syscall
      functions.
    
    Signed-off-by: Liao Chang <liaochang1@huawei.com>
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- fs/proc/proc_xcall.c
+++ fs/proc/proc_xcall.c
@@ -12,9 +12,9 @@
 static int xcall_show(struct seq_file *m, void *v)
 {
 	struct inode *inode = m->private;
-	struct task_struct *p;
-	unsigned int rs, re;
 	struct xcall_info *xinfo;
+	struct task_struct *p;
+	int l = 0, r = 1;
 
 	if (!static_key_enabled(&xcall_enable))
 		return -EACCES;
@@ -27,14 +27,25 @@ static int xcall_show(struct seq_file *m, void *v)
 	if (!xinfo)
 		goto out;
 
-	for (rs = 0, bitmap_next_set_region(xinfo->xcall_enable, &rs, &re, __NR_syscalls);
-	     rs < re; rs = re + 1,
-	     bitmap_next_set_region(xinfo->xcall_enable, &rs, &re, __NR_syscalls)) {
-		if (rs == (re - 1))
-			seq_printf(m, "%d,", rs);
-		else
-			seq_printf(m, "%d-%d,", rs, re - 1);
+	while (r < __NR_syscalls) {
+		if (!xinfo->xcall_enable[l]) {
+			l++;
+			r = l + 1;
+			continue;
+		}
+
+		if (!xinfo->xcall_enable[r]) {
+			if (r == (l + 1))
+				seq_printf(m, "%d,", l);
+			else
+				seq_printf(m, "%d-%d,", l, r - 1);
+			l = r + 1;
+			r = l + 1;
+			continue;
+		}
+		r++;
 	}
+
 	seq_puts(m, "\n");
 out:
 	put_task_struct(p);
@@ -47,45 +58,28 @@ static int xcall_open(struct inode *inode, struct file *filp)
 	return single_open(filp, xcall_show, inode);
 }
 
-static int xcall_enable_one(struct xcall_info *xinfo, unsigned int sc_no)
-{
-	test_and_set_bit(sc_no, xinfo->xcall_enable);
-	return 0;
-}
-
-static int xcall_disable_one(struct xcall_info *xinfo, unsigned int sc_no)
-{
-	test_and_clear_bit(sc_no, xinfo->xcall_enable);
-	return 0;
-}
-
-static ssize_t xcall_write(struct file *file, const char __user *buf,
+static ssize_t xcall_write(struct file *file, const char __user *ubuf,
 				      size_t count, loff_t *offset)
 {
-	struct inode *inode = file_inode(file);
-	struct task_struct *p;
-	char buffer[5];
-	const size_t maxlen = sizeof(buffer) - 1;
 	unsigned int sc_no = __NR_syscalls;
+	struct task_struct *p;
+	char buf[5];
 	int ret = 0;
-	int is_clear = 0;
-	struct xcall_info *xinfo;
 
 	if (!static_key_enabled(&xcall_enable))
 		return -EACCES;
 
-	memset(buffer, 0, sizeof(buffer));
-	if (!count || copy_from_user(buffer, buf, count > maxlen ? maxlen : count))
-		return -EFAULT;
-
-	p = get_proc_task(inode);
-	if (!p || !p->xinfo)
+	p = get_proc_task(file_inode(file));
+	if (!p || !TASK_XINFO(p))
 		return -ESRCH;
 
-	if (buffer[0] == '!')
-		is_clear = 1;
+	memset(buf, '\0', 5);
+	if (!count || (count > 4) || copy_from_user(buf, ubuf, count)) {
+		ret = -EFAULT;
+		goto out;
+	}
 
-	if (kstrtouint(buffer + is_clear, 10, &sc_no)) {
+	if (kstrtouint((buf + (int)(buf[0] == '!')), 10, &sc_no)) {
 		ret = -EINVAL;
 		goto out;
 	}
@@ -95,13 +89,8 @@ static ssize_t xcall_write(struct file *file, const char __user *buf,
 		goto out;
 	}
 
-	xinfo = TASK_XINFO(p);
-	if (!is_clear && !test_bit(sc_no, xinfo->xcall_enable))
-		ret = xcall_enable_one(xinfo, sc_no);
-	else if (is_clear && test_bit(sc_no, xinfo->xcall_enable))
-		ret = xcall_disable_one(xinfo, sc_no);
-	else
-		ret = -EINVAL;
+	(TASK_XINFO(p))->xcall_enable[sc_no] = (int)(buf[0] != '!');
+	ret = 0;
 
 out:
 	put_task_struct(p);
