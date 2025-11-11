89fdd254c99b9563e75350fe89f616ff7ccc0f55
Hongbo Li <lihongbo22@huawei.com>
2025-11-11 09:41:41 +0000

    mfs: implement the mount procedure
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/release-management/issues/ID5W1P
    
    --------------------------------
    
    MFS is going to provides the three modes: none, local and remote.
      - none: As the stackable file system on the lower file system,
        and just transfer operations to the backend file system.
      - local: Working at local case which lower and cachedir layer
        are both local file system. And the miss event (not hit in
        page cache) will post the async events to the userspace.
      - remote: Working at remote case which the target data is in
        the remote server such as OBS or other private DFS without
        POSIX-like interface. And the miss event (not hit in local
        cache) will post the sync events to the userspace and waiting
        for response.
    
    MFS also needs mtree and cachedir mount options. mtree means the
    metadata source of MFS, cachedir means the data source of MFS. For
    remote mode, cachedir will be the local cache of the backend remote
    storage system.
    
    MFS provides a read-only view of the under file system. So some
    modified operation on metadata or data shouldn't be allowed. We
    will implement other file system operation step by step.
    
    Signed-off-by: Huang Xiaojia <huangxiaojia2@huawei.com>
    Signed-off-by: Hongbo Li <lihongbo22@huawei.com>

--- fs/mfs/internal.h
+++ fs/mfs/internal.h
@@ -5,9 +5,24 @@
 #define _MFS_INTERNAL_H
 
 #include <linux/fs.h>
+#include <linux/mm.h>
+#include <linux/container_of.h>
 #include <linux/spinlock_types.h>
+#include <linux/mfs.h>
 
 #define MFS_NAME "mfs"
+
+struct mfs_sb_info {
+	int mode;
+	char *mtree;
+	char *cachedir;
+	struct path lower;
+	struct path cache;
+
+	int minor;
+
+	struct super_block *sb;
+};
 
 struct mfs_inode {
 	struct inode *lower;
@@ -21,5 +36,53 @@ struct mfs_dentry_info {
 	struct path lower;
 	struct path cache;
 };
+
+#define MFS_SB(sb) ((struct mfs_sb_info *)(sb)->s_fs_info)
+#define MFS_I(ptr) container_of(ptr, struct mfs_inode, vfs_inode)
+#define MFS_D(dent) ((struct mfs_dentry_info *)(dent)->d_fsdata)
+
+static inline struct inode *mfs_lower_inode(const struct inode *i)
+{
+	return MFS_I(i)->lower;
+}
+
+static inline void pathcpy(struct path *dst, const struct path *src)
+{
+	dst->dentry = src->dentry;
+	dst->mnt = src->mnt;
+}
+
+static inline void mfs_install_path(const struct dentry *dent,
+					 struct path *lpath,
+					 struct path *cpath)
+{
+	spin_lock(&MFS_D(dent)->lock);
+	pathcpy(&MFS_D(dent)->lower, lpath);
+	pathcpy(&MFS_D(dent)->cache, cpath);
+	spin_unlock(&MFS_D(dent)->lock);
+}
+
+static inline void mfs_release_path(const struct dentry *dent)
+{
+	struct path lpath, cpath;
+
+	if (!dent || !dent->d_fsdata)
+		return;
+	spin_lock(&MFS_D(dent)->lock);
+	pathcpy(&lpath, &MFS_D(dent)->lower);
+	pathcpy(&cpath, &MFS_D(dent)->cache);
+	MFS_D(dent)->lower.dentry = NULL;
+	MFS_D(dent)->lower.mnt = NULL;
+	MFS_D(dent)->cache.dentry = NULL;
+	MFS_D(dent)->cache.mnt = NULL;
+	path_put(&lpath);
+	path_put(&cpath);
+	spin_unlock(&MFS_D(dent)->lock);
+}
+
+struct inode *mfs_iget(struct super_block *sb, struct inode *lower_inode,
+			  struct path *cache_path);
+int mfs_alloc_dentry_info(struct dentry *dentry);
+void mfs_free_dentry_info(struct dentry *dentry);
 
 #endif
