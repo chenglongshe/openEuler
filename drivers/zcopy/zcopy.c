8459d979a2dd861cade997e428f868df4a748741
Liu Mingrui <liumingrui@huawei.com>
2025-11-14 09:16:21 +0000

    zcopy: Add debug inerface dump_pagetable
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/release-management/issues/ID3TGE
    
    --------------------------------
    
    Add a new interface dump_pagetable for debug, user can use it
    to dump the pagetables of vaddr.
    
    Signed-off-by: Liu Mingrui <liumingrui@huawei.com>

--- drivers/zcopy/zcopy.c
+++ drivers/zcopy/zcopy.c
@@ -35,6 +35,7 @@ enum pgt_entry {
 
 enum {
 	IO_ATTACH	= 1,
+	IO_DUMP		= 3,
 	IO_MAX
 };
 
@@ -44,6 +45,11 @@ struct zcopy_ioctl_pswap {
 	int src_pid;
 	int dst_pid;
 	unsigned long size;
+};
+
+struct zcopy_ioctl_dump {
+	unsigned long size;
+	unsigned long addr;
 };
 
 struct zcopy_cdev {
@@ -63,6 +69,7 @@ static int (*__zcopy_pud_alloc)(struct mm_struct *, p4d_t *, unsigned long);
 static unsigned long (*kallsyms_lookup_name_funcp)(const char *);
 static void (*__zcopy_mmu_notifier_arch_invalidate_secondary_tlbs)(struct mm_struct *,
 					unsigned long, unsigned long);
+static void (*dump_pagetable)(unsigned long addr);
 
 static struct kretprobe __kretprobe;
 
@@ -756,6 +763,19 @@ static long zcopy_ioctl(struct file *file, unsigned int type, unsigned long ptr)
 					ctx.src_pid, ctx.size);
 		break;
 	}
+	case IO_DUMP:
+	{
+		struct zcopy_ioctl_dump param;
+
+		if (copy_from_user((void *)&param, (void *)ptr,
+								sizeof(struct zcopy_ioctl_dump))) {
+			pr_err("copy from user for dump failed\n");
+			ret = -EFAULT;
+			break;
+		}
+		dump_pagetable(param.addr);
+		break;
+	}
 	default:
 		break;
 	}
@@ -829,6 +849,9 @@ static int register_unexport_func(void)
 			"__mmu_notifier_arch_invalidate_secondary_tlbs");
 	if (ret)
 		goto out;
+
+	dump_pagetable = (void (*)(unsigned long))__kallsyms_lookup_name("show_pte");
+	ret = REGISTER_CHECK(dump_pagetable, "show_pte");
 
 out:
 	return ret;
