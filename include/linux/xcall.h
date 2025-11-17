d003f2617ead50484d51ee14f1298d64c1b86c44
Liao Chang <liaochang1@huawei.com>
2025-11-17 17:56:53 +0800

    xcall2.0: Add xcall module register interface
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/release-management/issues/ID5CMS
    
    --------------------------------
    
    Add xcall_prog_register()/unregister() interface for user module
    to register xcall syscall.
    
    Signed-off-by: Liao Chang <liaochang1@huawei.com>
    Signed-off-by: Zheng Xinyu <zhengxinyu6@huawei.com>
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- include/linux/xcall.h
+++ include/linux/xcall.h
@@ -23,4 +23,16 @@ struct xcall_prog {
 	struct xcall_prog_object objs[MAX_NR_SCNO];
 	unsigned int nr_scno;
 };
+
+#ifdef CONFIG_DYNAMIC_XCALL
+extern int xcall_prog_register(struct xcall_prog *prog);
+extern void xcall_prog_unregister(struct xcall_prog *prog);
+#else /* !CONFIG_DYNAMIC_XCALL */
+static inline int xcall_prog_register(struct xcall_prog *prog)
+{
+	return -EINVAL;
+}
+static inline void xcall_prog_unregister(struct xcall_prog *prog) {}
+#endif /* CONFIG_DYNAMIC_XCALL */
+
 #endif /* _LINUX_XCALL_H */
