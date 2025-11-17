50866178e479b84586aea5900d5b95532249d70a
Liao Chang <liaochang1@huawei.com>
2025-11-17 09:22:14 +0800

    xcall2.0: Add xcall module register interface
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/release-management/issues/ID5CMS
    
    --------------------------------
    
    Add xcall_prog_register()/unregister() interface for user module
    to register xcall syscall.
    
    Signed-off-by: Liao Chang <liaochang1@huawei.com>
    Signed-off-by: Zheng Xinyu <zhengxinyu6@huawei.com>
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- arch/arm64/kernel/xcall/core.c
+++ arch/arm64/kernel/xcall/core.c
@@ -23,17 +23,23 @@ static struct xcall_prog *get_xcall_prog(const char *module)
 {
 	struct xcall_prog *p;
 
-	spin_lock(&prog_list_lock);
 	list_for_each_entry(p, &progs_list, list) {
-		if (!strcmp(module, p->name)) {
-			spin_unlock(&prog_list_lock);
+		if (!strcmp(module, p->name))
 			return p;
-		}
 	}
-	spin_unlock(&prog_list_lock);
 	return NULL;
 }
 
+static struct xcall_prog *get_xcall_prog_locked(const char *module)
+{
+	struct xcall_prog *ret;
+
+	spin_lock(&prog_list_lock);
+	ret = get_xcall_prog(module);
+	spin_unlock(&prog_list_lock);
+
+	return ret;
+}
 
 static struct xcall *get_xcall(struct xcall *xcall)
 {
@@ -101,7 +107,7 @@ static void delete_xcall(struct xcall *xcall)
 /* Init xcall with a given inode */
 static int init_xcall(struct xcall *xcall, struct xcall_comm *comm)
 {
-	struct xcall_prog *program = get_xcall_prog(comm->module);
+	struct xcall_prog *program = get_xcall_prog_locked(comm->module);
 
 	if (!program || !try_module_get(program->owner))
 		return -EINVAL;
@@ -155,3 +161,70 @@ int xcall_detach(struct xcall_comm *comm)
 	delete_xcall(xcall);
 	return 0;
 }
+
+static int check_prog(struct xcall_prog *prog)
+{
+	int prog_len = strnlen(prog->name, PROG_NAME_LEN);
+	struct xcall_prog_object *obj = prog->objs;
+	DECLARE_BITMAP(hijack_map, NR_syscalls);
+
+	if (!prog->owner)
+		return -EINVAL;
+
+	if (prog_len <= 0 || prog_len >= PROG_NAME_LEN)
+		return -EINVAL;
+
+	prog->nr_scno = 0;
+	bitmap_zero(hijack_map, NR_syscalls);
+	while (prog->nr_scno < MAX_NR_SCNO && obj && obj->func) {
+		if (obj->scno >= __NR_syscalls)
+			return -EINVAL;
+
+		if (test_and_set_bit(obj->scno, hijack_map))
+			return -EINVAL;
+
+		prog->nr_scno++;
+		obj++;
+	}
+
+	if (!prog->nr_scno)
+		return -EINVAL;
+
+	return 0;
+}
+
+int xcall_prog_register(struct xcall_prog *prog)
+{
+	if (!static_key_enabled(&xcall_enable))
+		return -EACCES;
+
+	if (check_prog(prog))
+		return -EINVAL;
+
+	spin_lock(&prog_list_lock);
+	if (get_xcall_prog(prog->name)) {
+		spin_unlock(&prog_list_lock);
+		return -EBUSY;
+	}
+
+	INIT_LIST_HEAD(&prog->list);
+	list_add(&prog->list, &progs_list);
+	spin_unlock(&prog_list_lock);
+	return 0;
+}
+EXPORT_SYMBOL(xcall_prog_register);
+
+void xcall_prog_unregister(struct xcall_prog *prog)
+{
+	if (!static_key_enabled(&xcall_enable))
+		return;
+
+	if (check_prog(prog))
+		return;
+
+	spin_lock(&prog_list_lock);
+	if (get_xcall_prog(prog->name))
+		list_del(&prog->list);
+	spin_unlock(&prog_list_lock);
+}
+EXPORT_SYMBOL(xcall_prog_unregister);
