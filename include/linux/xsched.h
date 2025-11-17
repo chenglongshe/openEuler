a817cf0b8a9e78d3b942320efd7d5f0ccbfd89b7
Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
2025-11-17 03:39:46 +0000

    xsched: Introduce vstream management
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC5EHB
    
    -----------------------------------------
    
    Add vstream related data structures:
     - vstream_info.
    
    Add vstream related context and entity data structures:
     - xsched_entity
     - xsched_context
    
    Add xsched_init() implementation.
    Add vstream_alloc/free implementation.
    
    Signed-off-by: Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
    Signed-off-by: Artem Kuzin <artem.kuzin@huawei.com>
    Signed-off-by: Hui Tang <tanghui20@.huawei.com>
    Signed-off-by: Liu Kai <liukai284@huawei.com>
    Signed-off-by: Xia Fukun <xiafukun@huawei.com>
    Signed-off-by: Zicheng Qu <quzicheng@huawei.com>

--- include/linux/xsched.h
+++ include/linux/xsched.h
@@ -2,6 +2,10 @@
 #ifndef __LINUX_XSCHED_H__
 #define __LINUX_XSCHED_H__
 
+#include <linux/kref.h>
+#include <linux/vstream.h>
+#include <linux/xcu_group.h>
+
 #ifndef pr_fmt
 #define pr_fmt(fmt) fmt
 #endif
@@ -32,6 +36,8 @@
 #define XSCHED_EXIT_STUB()                                                     \
 	XSCHED_DEBUG(" -----* %s @ %s exited *-----\n", __func__, __FILE__)
 
+#define MAX_VSTREAM_NUM 512
+
 enum xsched_cu_status {
 	/* Worker not initialized. */
 	XSCHED_XCU_NONE,
@@ -53,11 +59,83 @@ struct xsched_cu {
 	uint32_t id;
 	uint32_t state;
 	struct task_struct *worker;
+	struct list_head ctx_list;
+	struct mutex ctx_list_lock;
+	vstream_info_t *vs_array[MAX_VSTREAM_NUM];
+	struct mutex vs_array_lock;
 	struct xcu_group *group;
 	struct mutex xcu_lock;
 	wait_queue_head_t wq_xcu_idle;
 };
 
+struct xsched_entity {
+	uint32_t task_type;
+
+	bool on_rq;
+
+	pid_t owner_pid;
+	pid_t tgid;
+
+	/* File descriptor coming from an associated context
+	 * used for identifying a given xsched entity in
+	 * info and error prints.
+	 */
+	uint32_t fd;
+
+	/* Xsched class for this xse. */
+	const struct xsched_class *class;
+
+	/* Pointer to context object. */
+	struct xsched_context *ctx;
+
+	/* Pointer to an XCU object that represents an XCU
+	 * on which this xse is to be processed or is being
+	 * processed currently.
+	 */
+	struct xsched_cu *xcu;
+
+	/* General purpose xse lock. */
+	spinlock_t xse_lock;
+};
+
+struct xsched_context {
+	uint32_t fd;
+	uint32_t dev_id;
+	pid_t tgid;
+
+	struct list_head vstream_list;
+	struct list_head ctx_node;
+
+	struct xsched_entity xse;
+
+	spinlock_t ctx_lock;
+	struct mutex ctx_mutex;
+	struct kref kref;
+};
+
+extern struct list_head xsched_ctx_list;
+extern struct mutex xsched_ctx_list_mutex;
+
+/* Returns a pointer to xsched_context object corresponding to a given
+ * tgid and xcu.
+ */
+static inline struct xsched_context *
+ctx_find_by_tgid_and_xcu(pid_t tgid, struct xsched_cu *xcu)
+{
+	struct xsched_context *ctx;
+	struct xsched_context *ret = NULL;
+
+	list_for_each_entry(ctx, &xcu->ctx_list, ctx_node) {
+		if (ctx->tgid == tgid) {
+			ret = ctx;
+			break;
+		}
+	}
+	return ret;
+}
+
 int xsched_xcu_init(struct xsched_cu *xcu, struct xcu_group *group, int xcu_id);
 int xsched_schedule(void *input_xcu);
+int xsched_init_entity(struct xsched_context *ctx, struct vstream_info *vs);
+int ctx_bind_to_xcu(vstream_info_t *vstream_info, struct xsched_context *ctx);
 #endif /* !__LINUX_XSCHED_H__ */
