OERV-RVCI/rvck-olk

commit ed205f43c85f9fc5bbe6a6719d42c39de1ce65f3

mm: convert swap_cluster_readahead and swap_vma_readahead to return a folio
mainline inclusion
from mainline-v6.8-rc1
commit a4575c4138db887bd27dc7f87cf7cfb0224c6f5e
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IC3A3N

Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=a4575c4138db887bd27dc7f87cf7cfb0224c6f5e

--------------------------------

shmem_swapin_cluster() immediately converts the page back to a folio, and
swapin_readahead() may as well call folio_file_page() once instead of
having each function call it.

[willy@infradead.org: avoid NULL pointer deref]
  Link: https://lkml.kernel.org/r/ZYI7OcVlM1voKfBl@casper.infradead.org
Link: https://lkml.kernel.org/r/20231213215842.671461-14-willy@infradead.org
Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
Conflicts:
	mm/shmem.c
	mm/swap.h
	mm/swap_state.c
[Context conflict: patchset "mempolicy: cleanups leading to NUMA mpol
without vma" is not merged.]
Signed-off-by: Tong Tiangen <tongtiangen@huawei.com>

mm/swap.h
@@ -51,7 +51,7 @@ struct folio *__read_swap_cache_async(swp_entry_t entry, gfp_t gfp_mask,
 				     struct vm_area_struct *vma,
 				     unsigned long addr,
 				     bool *new_page_allocated);
-struct page *swap_cluster_readahead(swp_entry_t entry, gfp_t flag,
+struct folio *swap_cluster_readahead(swp_entry_t entry, gfp_t flag,
 				    struct vm_fault *vmf);
 struct page *swapin_readahead(swp_entry_t entry, gfp_t flag,
 			      struct vm_fault *vmf);
@@ -79,7 +79,7 @@ static inline void show_swap_cache_info(void)
 {
 }
 
-static inline struct page *swap_cluster_readahead(swp_entry_t entry,
+static inline struct folio *swap_cluster_readahead(swp_entry_t entry,
 				gfp_t gfp_mask, struct vm_fault *vmf)
 {
 	return NULL;

mm/shmem.c
@@ -1719,18 +1719,16 @@ static struct folio *shmem_swapin(swp_entry_t swap, gfp_t gfp,
 			struct shmem_inode_info *info, pgoff_t index)
 {
 	struct vm_area_struct pvma;
-	struct page *page;
+	struct folio *folio;
 	struct vm_fault vmf = {
 		.vma = &pvma,
 	};
 
 	shmem_pseudo_vma_init(&pvma, info, index);
-	page = swap_cluster_readahead(swap, gfp, &vmf);
+	folio = swap_cluster_readahead(swap, gfp, &vmf);
 	shmem_pseudo_vma_destroy(&pvma);
 
-	if (!page)
-		return NULL;
-	return page_folio(page);
+	return folio;
 }
 
 /*

mm/swap_state.c
@@ -618,7 +618,7 @@ static unsigned long swapin_nr_pages(unsigned long offset)
  * @gfp_mask: memory allocation flags
  * @vmf: fault information
  *
- * Returns the struct page for entry and addr, after queueing swapin.
+ * Returns the struct folio for entry and addr, after queueing swapin.
  *
  * Primitive swap readahead code. We simply read an aligned block of
  * (1 << page_cluster) entries in the swap area. This method is chosen
@@ -630,7 +630,7 @@ static unsigned long swapin_nr_pages(unsigned long offset)
  *
  * Caller must hold read mmap_lock if vmf->vma is not NULL.
  */
-struct page *swap_cluster_readahead(swp_entry_t entry, gfp_t gfp_mask,
+struct folio *swap_cluster_readahead(swp_entry_t entry, gfp_t gfp_mask,
 				struct vm_fault *vmf)
 {
 	struct folio *folio;
@@ -680,9 +680,7 @@ struct page *swap_cluster_readahead(swp_entry_t entry, gfp_t gfp_mask,
 	lru_add_drain();	/* Push any new pages onto the LRU now */
 skip:
 	/* The page was likely read above, so no need for plugging here */
-	folio = read_swap_cache_async(entry, gfp_mask, vma, addr, NULL);
-
-	return folio_file_page(folio, swp_offset(entry));
+	return read_swap_cache_async(entry, gfp_mask, vma, addr, NULL);
 }
 
 int init_swap_address_space(unsigned int type, unsigned long nr_pages)
@@ -784,7 +782,7 @@ static void swap_ra_info(struct vm_fault *vmf,
  * @gfp_mask: memory allocation flags
  * @vmf: fault information
  *
- * Returns the struct page for entry and addr, after queueing swapin.
+ * Returns the struct folio for entry and addr, after queueing swapin.
  *
  * Primitive swap readahead code. We simply read in a few pages whose
  * virtual addresses are around the fault address in the same vma.
@@ -792,7 +790,7 @@ static void swap_ra_info(struct vm_fault *vmf,
  * Caller must hold read mmap_lock if vmf->vma is not NULL.
  *
  */
-static struct page *swap_vma_readahead(swp_entry_t fentry, gfp_t gfp_mask,
+static struct folio *swap_vma_readahead(swp_entry_t fentry, gfp_t gfp_mask,
 				       struct vm_fault *vmf)
 {
 	struct blk_plug plug;
@@ -849,10 +847,8 @@ static struct page *swap_vma_readahead(swp_entry_t fentry, gfp_t gfp_mask,
 	lru_add_drain();
 skip:
 	/* The page was likely read above, so no need for plugging here */
-	folio = read_swap_cache_async(fentry, gfp_mask, vma, vmf->address,
+	return read_swap_cache_async(fentry, gfp_mask, vma, vmf->address,
 				     NULL);
-
-	return folio_file_page(folio, swp_offset(entry));
 }
 
 /**
@@ -870,9 +866,15 @@ skip:
 struct page *swapin_readahead(swp_entry_t entry, gfp_t gfp_mask,
 				struct vm_fault *vmf)
 {
-	return swap_use_vma_readahead() ?
+	struct folio *folio;
+
+	folio = swap_use_vma_readahead() ?
 			swap_vma_readahead(entry, gfp_mask, vmf) :
 			swap_cluster_readahead(entry, gfp_mask, vmf);
+
+	if (!folio)
+		return NULL;
+	return folio_file_page(folio, swp_offset(entry));
 }
 
 #ifdef CONFIG_SYSFS

chenglongshe/openEuler

commit 5c8168d51412a11ce524041c48e28159997c685c

mm: fix shmem build errors with CONFIG_SWAP disable
Offering: HULK
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ID6RU3

--------------------------------

Fix kernel build errors when CONFIG_SWAP is not enabled:

1. include/linux/migrate.h:102 - syntax error in stub function.
2. mm/shmem.c:1699 - function signature mismatch in
swap_cluster_readahead().

Adjust the stub implementation of swap_cluster_readahead() to match the
actual function signature (4 parameters), ensuring consistent function
prototypes when CONFIG_SWAP is disabled.

Fixes: ed205f43c85f ("mm: convert swap_cluster_readahead and swap_vma_readahead to return a folio")
Signed-off-by: Ze Zuo <zuoze1@huawei.com>

mm/swap.h
@@ -107,8 +107,8 @@ static inline void show_swap_cache_info(void)
 {
 }
 
-static inline struct folio *swap_cluster_readahead(swp_entry_t entry,
-				gfp_t gfp_mask, struct vm_fault *vmf)
+struct folio *swap_cluster_readahead(swp_entry_t entry, gfp_t flag,
+				    struct mempolicy *mpol, pgoff_t ilx)
 {
 	return NULL;
 }