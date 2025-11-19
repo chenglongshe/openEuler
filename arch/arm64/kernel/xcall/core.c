OERV-RVCI/rvck-olk

commit b05676644e959871128638e362a060fac0683faf

xcall2.0: Add xcall_area
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/release-management/issues/ID5CMS

--------------------------------

In xcall 2.0 each process is associated with an unique
xcall area. In the mmap process, associate an xcall area with
all matching executable files and populate the system call table
to prepare for hijacking and replacing custom system calls.

Signed-off-by: Liao Chang <liaochang1@huawei.com>
Signed-off-by: Zheng Xinyu <zhengxinyu6@huawei.com>
Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

arch/arm64/kernel/xcall/core.c
@@ -6,6 +6,7 @@
 #define pr_fmt(fmt)	"xcall: " fmt
 
 #include <linux/slab.h>
+#include <linux/syscalls.h>
 #include <linux/xcall.h>
 
 #include <asm/xcall.h>
@@ -40,6 +41,8 @@ static struct xcall_prog *get_xcall_prog_locked(const char *module)
 
 	return ret;
 }
+
+#define inv_xcall_syscall ((unsigned long)__arm64_sys_ni_syscall)
 
 static struct xcall *get_xcall(struct xcall *xcall)
 {
@@ -108,6 +111,115 @@ static int init_xcall(struct xcall *xcall, struct xcall_comm *comm)
 	INIT_LIST_HEAD(&xcall->list);
 
 	return 0;
+}
+
+static int fill_xcall_syscall(struct xcall_area *area, struct xcall *xcall)
+{
+	unsigned int scno_offset, scno_count = 0;
+	struct xcall_prog_object *obj;
+
+	obj = xcall->program->objs;
+	while (scno_count < xcall->program->nr_scno && obj->func) {
+		scno_offset = NR_syscalls + obj->scno;
+		if (area->sys_call_table[scno_offset] != inv_xcall_syscall) {
+			pr_err("Process can not mount more than one xcall.\n");
+			return -EINVAL;
+		}
+
+		area->sys_call_table[scno_offset] = obj->func;
+		obj += 1;
+		scno_count++;
+	}
+
+	return 0;
+}
+
+static struct xcall_area *create_xcall_area(struct mm_struct *mm)
+{
+	struct xcall_area *area;
+	int i;
+
+	area = kzalloc(sizeof(*area), GFP_KERNEL);
+	if (!area)
+		return NULL;
+
+	refcount_set(&area->ref, 1);
+
+	for (i = 0; i < NR_syscalls; i++) {
+		area->sys_call_table[i] = inv_xcall_syscall;
+		area->sys_call_table[i + NR_syscalls] = inv_xcall_syscall;
+	}
+
+	/*
+	 * Pairs with the READ_ONCE(mm->xcall) in xcall_map() to avoid
+	 * the reord of the access on mm->xcall and filling mm->xcall.
+	 */
+	smp_store_release(&mm->xcall, area);
+	return area;
+}
+
+/*
+ * Initialize the xcall data of mm_struct data.
+ * And register xcall into one address space, which includes create
+ * the mm_struct associated xcall_area data
+ */
+int xcall_mmap(struct vm_area_struct *vma, struct mm_struct *mm)
+{
+	struct xcall_area *area;
+	struct xcall *xcall;
+	int ret = -EINVAL;
+
+	if (list_empty(&xcalls_list))
+		return 0;
+
+	spin_lock(&xcall_list_lock);
+	xcall = find_xcall(NULL, file_inode(vma->vm_file));
+	spin_unlock(&xcall_list_lock);
+	if (!xcall)
+		return ret;
+
+	if (!xcall->program)
+		goto put_xcall;
+
+	area = mm_xcall_area(mm);
+	if (!area && !create_xcall_area(mm)) {
+		ret = -ENOMEM;
+		goto put_xcall;
+	}
+
+	area = (struct xcall_area *)READ_ONCE(mm->xcall);
+	// Each process is allowed to be associated with only one xcall.
+	if (!cmpxchg(&area->xcall, NULL, xcall) && !fill_xcall_syscall(area, xcall))
+		return 0;
+
+put_xcall:
+	put_xcall(xcall);
+	return ret;
+}
+
+void mm_init_xcall_area(struct mm_struct *mm, struct task_struct *p)
+{
+	struct xcall_area *area = mm_xcall_area(mm);
+
+	if (area)
+		refcount_inc(&area->ref);
+}
+
+void clear_xcall_area(struct mm_struct *mm)
+{
+	struct xcall_area *area = mm_xcall_area(mm);
+
+	if (!area)
+		return;
+
+	if (!refcount_dec_and_test(&area->ref))
+		return;
+
+	if (area->xcall)
+		put_xcall(area->xcall);
+
+	kfree(area);
+	mm->xcall = NULL;
 }
 
 int xcall_attach(struct xcall_comm *comm)

include/linux/xcall.h
@@ -8,6 +8,10 @@
 
 #include <linux/module.h>
 
+struct vm_area_struct;
+struct mm_struct;
+struct inode;
+
 struct xcall_prog_object {
 	unsigned long scno;
 	unsigned long func;
@@ -27,12 +31,21 @@ struct xcall_prog {
 #ifdef CONFIG_DYNAMIC_XCALL
 extern int xcall_prog_register(struct xcall_prog *prog);
 extern void xcall_prog_unregister(struct xcall_prog *prog);
+extern void mm_init_xcall_area(struct mm_struct *mm, struct task_struct *p);
+extern void clear_xcall_area(struct mm_struct *mm);
+extern int xcall_mmap(struct vm_area_struct *vma, struct mm_struct *mm);
 #else /* !CONFIG_DYNAMIC_XCALL */
 static inline int xcall_prog_register(struct xcall_prog *prog)
 {
 	return -EINVAL;
 }
 static inline void xcall_prog_unregister(struct xcall_prog *prog) {}
+static inline void mm_init_xcall_area(struct mm_struct *mm, struct task_struct *p) {}
+static inline void clear_xcall_area(struct mm_struct *mm) {}
+static inline int xcall_mmap(struct vm_area_struct *vma, struct mm_struct *mm)
+{
+	return 0;
+}
 #endif /* CONFIG_DYNAMIC_XCALL */
 
 #endif /* _LINUX_XCALL_H */

kernel/fork.c
@@ -106,6 +106,7 @@
 #endif
 #include <linux/share_pool.h>
 #include <linux/tick.h>
+#include <linux/xcall.h>
 
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -974,6 +975,7 @@ void __mmdrop(struct mm_struct *mm)
 	mm_free_pgd(mm);
 	destroy_context(mm);
 	mmu_notifier_subscriptions_destroy(mm);
+	clear_xcall_area(mm);
 	check_mm(mm);
 	put_user_ns(mm->user_ns);
 	mm_pasid_drop(mm);
@@ -1373,6 +1375,7 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 #if defined(CONFIG_DAMON_MEM_SAMPLING)
 	mm->damon_fifo = NULL;
 #endif
+	mm_init_xcall_area(mm, p);
 	mm_init_uprobes_state(mm);
 	hugetlb_count_init(mm);
 

arch/arm64/include/asm/xcall.h
@@ -4,13 +4,16 @@
 
 #include <linux/atomic.h>
 #include <linux/jump_label.h>
+#include <linux/mm_types.h>
 #include <linux/percpu.h>
 #include <linux/sched.h>
 #include <linux/types.h>
 #include <linux/xcall.h>
+#include <linux/refcount.h>
 
 #include <asm/actlr.h>
 #include <asm/cpufeature.h>
+#include <asm/syscall.h>
 
 struct xcall_comm {
 	char *name;
@@ -30,9 +33,24 @@ struct xcall {
 	char			*name;
 };
 
+struct xcall_area {
+	/*
+	 * 0...NR_syscalls - 1: function pointers to hijack default syscall
+	 * NR_syscalls...NR_syscalls * 2 - 1: function pointers in kernel module
+	 */
+	unsigned long		sys_call_table[NR_syscalls * 2];
+	refcount_t		ref;
+	struct xcall		*xcall;
+	void			*sys_call_data[NR_syscalls];
+};
+
 #ifdef CONFIG_DYNAMIC_XCALL
 extern int xcall_attach(struct xcall_comm *info);
 extern int xcall_detach(struct xcall_comm *info);
+
+#define mm_xcall_area(mm)	((struct xcall_area *)((mm)->xcall))
+#else
+#define mm_xcall_area(mm)	(NULL)
 #endif /* CONFIG_DYNAMIC_XCALL */
 
 DECLARE_STATIC_KEY_FALSE(xcall_enable);

include/linux/mm_types.h
@@ -1025,7 +1025,11 @@ struct mm_struct {
 #else
 	KABI_RESERVE(2)
 #endif
+#ifdef CONFIG_DYNAMIC_XCALL
+	KABI_USE(3, void *xcall)
+#else
 	KABI_RESERVE(3)
+#endif
 	KABI_RESERVE(4)
 	KABI_RESERVE(5)
 	/*

mm/mmap.c
@@ -49,6 +49,7 @@
 #include <linux/ksm.h>
 #include <linux/share_pool.h>
 #include <linux/vm_object.h>
+#include <linux/xcall.h>
 
 #include <linux/uaccess.h>
 #include <asm/cacheflush.h>
@@ -592,9 +593,12 @@ static inline void vma_complete(struct vma_prepare *vp,
 
 		if (!vp->skip_vma_uprobe) {
 			uprobe_mmap(vp->vma);
+			xcall_mmap(vp->vma, mm);
 
-			if (vp->adj_next)
+			if (vp->adj_next) {
 				uprobe_mmap(vp->adj_next);
+				xcall_mmap(vp->adj_next, mm);
+			}
 		}
 	}
 
@@ -624,8 +628,10 @@ again:
 			goto again;
 		}
 	}
-	if (vp->insert && vp->file)
+	if (vp->insert && vp->file) {
 		uprobe_mmap(vp->insert);
+		xcall_mmap(vp->insert, mm);
+	}
 	validate_mm(mm);
 }
 
@@ -2998,8 +3004,10 @@ expanded:
 			mm->locked_vm += (len >> PAGE_SHIFT);
 	}
 
-	if (file)
+	if (file) {
 		uprobe_mmap(vma);
+		xcall_mmap(vma, mm);
+	}
 
 	/*
 	 * New (or expanded) vma always get soft dirty status.

chenglongshe/openEuler

commit 7789d0b77e9eaa6057527f256623426d3a650c62

xcall2.0: Fix module unload issue by fixing refcount
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/release-management/issues/ID5CMS

--------------------------------

After the process exits and xcall is unregistered, kernel module cannot
be unloaded properly.

The reason is that mmput() just need to reduce the refcount
of 'mm->mm_users' to 0 which indicates that no user-space processes
are using this address space, but mmdrop() need to reduce the refcount
of 'mm->mm_count' to zero, which indicates that all references to
the memory descriptor have reached zero and the mm_struct can now
be safely freed and this condition may not be satisfied after process
exit. So put_xcall() in mmdrop() can not guarantee the refcount of xcall
will be decremented immediately, so module_put() in put_xcall() may
not called.

Fix this problem by split clear_xcall_area() into free_xcall_area()
and clear_xcall_area(), call put_xcall() in mmput() and free
the mm->xcall in mmdrop().

Fixes: b05676644e95 ("xcall2.0: Add xcall_area")
Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

arch/arm64/kernel/xcall/core.c
@@ -279,6 +279,14 @@ void clear_xcall_area(struct mm_struct *mm)
 
 	if (area->xcall)
 		put_xcall(area->xcall);
+}
+
+void free_xcall_area(struct mm_struct *mm)
+{
+	struct xcall_area *area = mm_xcall_area(mm);
+
+	if (!area)
+		return;
 
 	kfree(area);
 	mm->xcall = NULL;

include/linux/xcall.h
@@ -33,6 +33,7 @@ extern int xcall_prog_register(struct xcall_prog *prog);
 extern void xcall_prog_unregister(struct xcall_prog *prog);
 extern void mm_init_xcall_area(struct mm_struct *mm, struct task_struct *p);
 extern void clear_xcall_area(struct mm_struct *mm);
+extern void free_xcall_area(struct mm_struct *mm);
 extern int xcall_mmap(struct vm_area_struct *vma, struct mm_struct *mm);
 #else /* !CONFIG_DYNAMIC_XCALL */
 static inline int xcall_prog_register(struct xcall_prog *prog)
@@ -42,6 +43,7 @@ static inline int xcall_prog_register(struct xcall_prog *prog)
 static inline void xcall_prog_unregister(struct xcall_prog *prog) {}
 static inline void mm_init_xcall_area(struct mm_struct *mm, struct task_struct *p) {}
 static inline void clear_xcall_area(struct mm_struct *mm) {}
+extern void free_xcall_area(struct mm_struct *mm) {}
 static inline int xcall_mmap(struct vm_area_struct *vma, struct mm_struct *mm)
 {
 	return 0;

kernel/fork.c
@@ -975,7 +975,7 @@ void __mmdrop(struct mm_struct *mm)
 	mm_free_pgd(mm);
 	destroy_context(mm);
 	mmu_notifier_subscriptions_destroy(mm);
-	clear_xcall_area(mm);
+	free_xcall_area(mm);
 	check_mm(mm);
 	put_user_ns(mm->user_ns);
 	mm_pasid_drop(mm);
@@ -1429,6 +1429,7 @@ static inline void __mmput(struct mm_struct *mm)
 {
 	VM_BUG_ON(atomic_read(&mm->mm_users));
 
+	clear_xcall_area(mm);
 	uprobe_clear_state(mm);
 	exit_aio(mm);
 	ksm_exit(mm);