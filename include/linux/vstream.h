3932552a4501a8deb10321336a1137c765d8e976
Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
2025-11-19 01:38:43 +0000

    xsched: Introduce vstream management
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC5EHB
    
    -----------------------------------------
    
    Add vstream related data structures:
     - vstream_info.
    
    Add vstream related context and entity data structures:
     - xsched_entity
     - xsched_context
    
    Add xsched_init() implementation.
    Add vstream_alloc/free implementation.
    
    Signed-off-by: Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
    Signed-off-by: Artem Kuzin <artem.kuzin@huawei.com>
    Signed-off-by: Hui Tang <tanghui20@.huawei.com>
    Signed-off-by: Liu Kai <liukai284@huawei.com>
    Signed-off-by: Xia Fukun <xiafukun@huawei.com>
    Signed-off-by: Zicheng Qu <quzicheng@huawei.com>

--- include/linux/vstream.h
+++ include/linux/vstream.h
@@ -6,6 +6,52 @@
 
 typedef int vstream_manage_t(struct vstream_args *arg);
 
+typedef struct vstream_info {
+	uint32_t user_stream_id;
+	uint32_t id;
+	uint32_t vcq_id;
+	uint32_t logic_vcq_id;
+	uint32_t dev_id;
+	uint32_t channel_id;
+	uint32_t fd;
+	uint32_t task_type;
+	int tgid;
+	int sqcq_type;
+
+	void *drv_ctx;
+
+	int inode_fd;
+
+	/* Pointer to corresponding context. */
+	struct xsched_context *ctx;
+
+	/* List node in context's vstream list. */
+	struct list_head ctx_node;
+
+	/* Pointer to an CU object on which this
+	 * vstream is currently being processed.
+	 * NULL if vstream is not being processed.
+	 */
+	struct xsched_cu *xcu;
+
+	/* List node in an CU list of vstreams that
+	 * are currently being processed by this specific CU.
+	 */
+	struct list_head xcu_node;
+
+	/* Private vstream data. */
+	void *data;
+
+	spinlock_t stream_lock;
+
+	uint32_t kicks_count;
+
+	/* List of metadata a.k.a. all recorded unprocesed
+	 * kicks for this exact vstream.
+	 */
+	struct list_head metadata_list;
+} vstream_info_t;
+
 int vstream_alloc(struct vstream_args *arg);
 int vstream_free(struct vstream_args *arg);
 int vstream_kick(struct vstream_args *arg);
