19fad2138d91dcba46347421d74bf25af5e18ed2
Jason Andryuk <jason.andryuk@amd.com>
2025-11-21 10:15:45 +0800

    xen/events: Return -EEXIST for bound VIRQs
    stable inclusion
    from stable-v6.6.113
    commit 612ef6056855c0aacb9b25d1d853c435754483f7
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/ID6B93
    CVE: CVE-2025-40160
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=612ef6056855c0aacb9b25d1d853c435754483f7
    
    --------------------------------
    
    commit 07ce121d93a5e5fb2440a24da3dbf408fcee978e upstream.
    
    Change find_virq() to return -EEXIST when a VIRQ is bound to a
    different CPU than the one passed in.  With that, remove the BUG_ON()
    from bind_virq_to_irq() to propogate the error upwards.
    
    Some VIRQs are per-cpu, but others are per-domain or global.  Those must
    be bound to CPU0 and can then migrate elsewhere.  The lookup for
    per-domain and global will probably fail when migrated off CPU 0,
    especially when the current CPU is tracked.  This now returns -EEXIST
    instead of BUG_ON().
    
    A second call to bind a per-domain or global VIRQ is not expected, but
    make it non-fatal to avoid trying to look up the irq, since we don't
    know which per_cpu(virq_to_irq) it will be in.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Jason Andryuk <jason.andryuk@amd.com>
    Reviewed-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Message-ID: <20250828003604.8949-3-jason.andryuk@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	drivers/xen/events/events_base.c
    [Wupeng Ma: conflict due to commit 3fcdaf3d7634, no function change.]
    Signed-off-by: Wupeng Ma <mawupeng1@huawei.com>

--- drivers/xen/events/events_base.c
+++ drivers/xen/events/events_base.c
@@ -1259,10 +1259,12 @@ int bind_interdomain_evtchn_to_irq_lateeoi(unsigned int remote_domain,
 }
 EXPORT_SYMBOL_GPL(bind_interdomain_evtchn_to_irq_lateeoi);
 
-static int find_virq(unsigned int virq, unsigned int cpu, evtchn_port_t *evtchn)
+static int find_virq(unsigned int virq, unsigned int cpu, evtchn_port_t *evtchn,
+		     bool percpu)
 {
 	struct evtchn_status status;
 	evtchn_port_t port;
+	bool exists = false;
 
 	memset(&status, 0, sizeof(status));
 	for (port = 0; port < xen_evtchn_max_channels(); port++) {
@@ -1275,12 +1277,16 @@ static int find_virq(unsigned int virq, unsigned int cpu, evtchn_port_t *evtchn)
 			continue;
 		if (status.status != EVTCHNSTAT_virq)
 			continue;
-		if (status.u.virq == virq && status.vcpu == xen_vcpu_nr(cpu)) {
+		if (status.u.virq != virq)
+			continue;
+		if (status.vcpu == xen_vcpu_nr(cpu)) {
 			*evtchn = port;
 			return 0;
+		} else if (!percpu) {
+			exists = true;
 		}
 	}
-	return -ENOENT;
+	return exists ? -EEXIST : -ENOENT;
 }
 
 /**
@@ -1326,8 +1332,11 @@ int bind_virq_to_irq(unsigned int virq, unsigned int cpu, bool percpu)
 			evtchn = bind_virq.port;
 		else {
 			if (ret == -EEXIST)
-				ret = find_virq(virq, cpu, &evtchn);
-			BUG_ON(ret < 0);
+				ret = find_virq(virq, cpu, &evtchn, percpu);
+			if (ret) {
+				__unbind_from_irq(irq);
+				goto out;
+			}
 		}
 
 		ret = xen_irq_info_virq_setup(cpu, irq, evtchn, virq);
