OERV-RVCI/rvck-olk

commit 01f8a9007306f71ce2f9d1642d0de754b976c353

nfs/enfs: recreate shard info when re-adding enfs module
kylin inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ICXJSA

--------------------------------

Steps:

  1. mount -t nfs -o vers=3 localaddrs=xxx,remoteaddrs=xxx \
       ${server_ip}:/svr/export /mnt/
  2. modprobe -r enfs
  3. modprobe enfs

Shard information should be recreated when the enfs module is re-added.

Signed-off-by: ChenXiaoSong <chenxiaosong@kylinos.cn>

fs/nfs/enfs/shard_route.c
@@ -40,9 +40,15 @@ struct shard_view_ctrl {
 	struct list_head clnt_info_list;
 };
 
+struct enfs_shard_info {
+	struct work_struct work;
+	struct nfs_server *server;
+};
+
 static struct shard_view_ctrl *shard_ctrl;
 static struct task_struct *shard_thread;
 static struct workqueue_struct *shard_workq;
+static struct workqueue_struct *shard_check_wq;
 
 struct clnt_debug_cmd {
 	const char *name;
@@ -691,6 +697,17 @@ void enfs_print_uuid(struct enfs_file_uuid *file_uuid)
 
 	sprint_uuid(buf, 80, file_uuid);
 	enfs_log_debug("UUID:%s\n", buf);
+}
+
+static void enfs_debug_fh(const struct nfs_fh *fh)
+{
+	int i;
+	char str[NFS_MAXFHSIZE * 2];
+
+	for (i = 0; i < fh->size; i++)
+		sprintf(str + 2 * i, "%02X", fh->data[i]);
+	str[2 * i] = '\0';
+	enfs_log_debug("fh: %s\n", str);
 }
 
 static int get_uuid_from_task(struct rpc_clnt *clnt, struct rpc_task *task,
@@ -1700,12 +1717,101 @@ static struct shard_view_ctrl *enfs_shard_ctrl_init(void)
 	return ctrl;
 }
 
+static void enfs_shard_workfn(struct work_struct *work)
+{
+	struct enfs_shard_info *info =
+		container_of(work, struct enfs_shard_info, work);
+	struct nfs_server *server = info->server;
+	struct super_block *super = server->super;
+	struct inode *inode;
+	struct nfs_fh *fh;
+	struct dentry *dentry;
+	struct enfs_file_uuid root_uuid;
+
+	dentry = super->s_root;
+	if (!dentry)
+		goto out;
+
+	inode = d_inode(dentry);
+	if (!inode)
+		goto out;
+
+	fh = NFS_FH(inode);
+	if (is_enfs_debug())
+		enfs_debug_fh(fh);
+	memset(&root_uuid, 0, sizeof(struct enfs_file_uuid));
+	fh_file_uuid(fh, &root_uuid);
+
+	insert_and_update_shard(server->client, &root_uuid);
+out:
+	nfs_sb_deactive(super);
+	kfree(info);
+}
+
+static int enfs_check_shard(void)
+{
+	int ret = 0;
+	struct nfs_net *nn;
+	struct net *net;
+	struct nfs_server *server;
+	struct super_block *super;
+	struct enfs_shard_info *info;
+
+	shard_check_wq = create_workqueue("shard_check_wq");
+	if (!shard_check_wq) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	rcu_read_lock();
+	for_each_net_rcu(net) {
+		nn = net_generic(net, nfs_net_id);
+		if (nn == NULL)
+			continue;
+
+		spin_lock(&nn->nfs_client_lock);
+		list_for_each_entry(server, &nn->nfs_volume_list, master_link) {
+			super = server->super;
+			if (!super)
+				continue;
+			if (atomic_read(&super->s_active) == 0)
+				continue;
+			if (!nfs_sb_active(super))
+				continue;
+			info = kmalloc(sizeof(*info), GFP_ATOMIC);
+			if (!info) {
+				nfs_sb_deactive(super);
+				ret = -ENOMEM;
+				goto out;
+			}
+			INIT_WORK(&info->work, enfs_shard_workfn);
+			info->server = server;
+			queue_work(shard_check_wq, &info->work);
+		}
+		spin_unlock(&nn->nfs_client_lock);
+		break;
+	}
+	rcu_read_unlock();
+
+out:
+	// free shard_check_wq in enfs_shard_exit() if an error occurs
+	return ret;
+}
+
 int enfs_shard_init(void)
 {
+	int rc;
+
 	shard_ctrl = enfs_shard_ctrl_init();
 	if (!shard_ctrl) {
 		enfs_log_error("create shard view ctrl failed.\n");
 		return -ENOMEM;
+	}
+
+	rc = enfs_check_shard();
+	if (rc) {
+		enfs_log_error("check shard failed!");
+		return rc;
 	}
 
 	shard_workq = create_workqueue("enfs_shard_workqueue");
@@ -1735,4 +1841,7 @@ void enfs_shard_exit(void)
 
 	if (shard_ctrl)
 		enfs_shard_ctrl_clear();
+
+	if (shard_check_wq)
+		destroy_workqueue(shard_check_wq);
 }

chenglongshe/openEuler

commit e6d0224cec54f019833824ee808d627edd8c65d4

nfs/enfs: fix missing unlock in enfs_check_shard
kylin inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ID52S4

--------------------------------

Deadlock and hungtask will occur when kmalloc() fails. Fix this by adding
the missing unlock.

Fixes: 01f8a9007306 ("nfs/enfs: recreate shard info when re-adding enfs module")
Co-developed-by: ChenXiaoSong <chenxiaosong@kylinos.cn>
Signed-off-by: ChenXiaoSong <chenxiaosong@kylinos.cn>
Signed-off-by: ZhangGuoDong <zhangguodong@kylinos.cn>

fs/nfs/enfs/shard_route.c
@@ -1771,7 +1771,7 @@ static int enfs_check_shard(void)
 	shard_check_wq = create_workqueue("shard_check_wq");
 	if (!shard_check_wq) {
 		ret = -ENOMEM;
-		goto out;
+		goto out2;
 	}
 
 	rcu_read_lock();
@@ -1799,12 +1799,13 @@ static int enfs_check_shard(void)
 			info->server = server;
 			queue_work(shard_check_wq, &info->work);
 		}
+out:
 		spin_unlock(&nn->nfs_client_lock);
 		break;
 	}
 	rcu_read_unlock();
 
-out:
+out2:
 	// free shard_check_wq in enfs_shard_exit() if an error occurs
 	return ret;
 }