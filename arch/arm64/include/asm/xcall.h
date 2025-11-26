OERV-RVCI/rvck-olk

commit 7d904de7feb25aa9b7a47545a5c56116dfd971e7

xcall2.0: Add userspace proc interface
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/release-management/issues/ID5CMS

--------------------------------

Add "xcall", "xcall_comm" key struct and provide the procfs interface
to userspace.

Add '/proc/xcall/command' interface, using this file for attaching xcall
programs onto one executable.

Argument syntax:
  +:COMM BINARY KERNEL_MODULE : Attach a xcall
  -:COMM		      : Detach a xcall

  COMM:			Unique string for attached xcall.
  BINARY:		Path to an executable.
  KERNEL_MODULE:	Module name listed in /proc/modules provide xcall program.

Signed-off-by: Liao Chang <liaochang1@huawei.com>
Signed-off-by: Zheng Xinyu <zhengxinyu6@huawei.com>
Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

arch/arm64/include/asm/xcall.h
@@ -7,9 +7,33 @@
 #include <linux/percpu.h>
 #include <linux/sched.h>
 #include <linux/types.h>
+#include <linux/xcall.h>
 
 #include <asm/actlr.h>
 #include <asm/cpufeature.h>
+
+struct xcall_comm {
+	char *name;
+	char *binary;
+	struct path binary_path;
+	char *module;
+	struct list_head list;
+};
+
+struct xcall {
+	/* used for xcall_attach */
+	struct list_head	list;
+	refcount_t		ref;
+	/* file attached xcall */
+	struct inode		*binary;
+	struct xcall_prog	*program;
+	char			*name;
+};
+
+#ifdef CONFIG_DYNAMIC_XCALL
+extern int xcall_attach(struct xcall_comm *info);
+extern int xcall_detach(struct xcall_comm *info);
+#endif /* CONFIG_DYNAMIC_XCALL */
 
 DECLARE_STATIC_KEY_FALSE(xcall_enable);
 
@@ -93,4 +117,4 @@ static inline void cpu_switch_xcall_entry(struct task_struct *tsk)
 }
 #endif /* CONFIG_ACTLR_XCALL_XINT */
 
-#endif /*__ASM_XCALL_H*/
+#endif /* __ASM_XCALL_H */

arch/arm64/kernel/xcall/core.c
@@ -1,0 +1,152 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2025 Huawei Limited.
+ */
+
+#define pr_fmt(fmt)	"xcall: " fmt
+
+#include <linux/slab.h>
+#include <linux/xcall.h>
+
+#include <asm/xcall.h>
+
+static DEFINE_SPINLOCK(xcall_list_lock);
+static LIST_HEAD(xcalls_list);
+static DEFINE_SPINLOCK(prog_list_lock);
+static LIST_HEAD(progs_list);
+
+/*
+ * Travel the list of all registered xcall_prog during module installation
+ * to find the xcall_prog.
+ */
+static struct xcall_prog *get_xcall_prog(const char *module)
+{
+	struct xcall_prog *p;
+
+	spin_lock(&prog_list_lock);
+	list_for_each_entry(p, &progs_list, list) {
+		if (!strcmp(module, p->name)) {
+			spin_unlock(&prog_list_lock);
+			return p;
+		}
+	}
+	spin_unlock(&prog_list_lock);
+	return NULL;
+}
+
+
+static struct xcall *get_xcall(struct xcall *xcall)
+{
+	refcount_inc(&xcall->ref);
+	return xcall;
+}
+
+static void put_xcall(struct xcall *xcall)
+{
+	if (!refcount_dec_and_test(&xcall->ref))
+		return;
+
+	kfree(xcall->name);
+	if (xcall->program)
+		module_put(xcall->program->owner);
+
+	kfree(xcall);
+}
+
+static struct xcall *find_xcall(const char *name, struct inode *binary)
+{
+	struct xcall *xcall;
+
+	list_for_each_entry(xcall, &xcalls_list, list) {
+		if ((name && !strcmp(name, xcall->name)) ||
+		    (binary && xcall->binary == binary))
+			return get_xcall(xcall);
+	}
+	return NULL;
+}
+
+static struct xcall *insert_xcall_locked(struct xcall *xcall)
+{
+	struct xcall *ret = NULL;
+
+	spin_lock(&xcall_list_lock);
+	ret = find_xcall(xcall->name, xcall->binary);
+	if (!ret)
+		list_add(&xcall->list, &xcalls_list);
+	else
+		put_xcall(ret);
+	spin_unlock(&xcall_list_lock);
+	return ret;
+}
+
+static void delete_xcall(struct xcall *xcall)
+{
+	spin_lock(&xcall_list_lock);
+	list_del(&xcall->list);
+	spin_unlock(&xcall_list_lock);
+
+	put_xcall(xcall);
+}
+
+/* Init xcall with a given inode */
+static int init_xcall(struct xcall *xcall, struct xcall_comm *comm)
+{
+	struct xcall_prog *program = get_xcall_prog(comm->module);
+
+	if (!program || !try_module_get(program->owner))
+		return -EINVAL;
+
+	xcall->binary = d_real_inode(comm->binary_path.dentry);
+	xcall->program = program;
+	refcount_set(&xcall->ref, 1);
+	INIT_LIST_HEAD(&xcall->list);
+
+	return 0;
+}
+
+int xcall_attach(struct xcall_comm *comm)
+{
+	struct xcall *xcall;
+	int ret;
+
+	xcall = kzalloc(sizeof(struct xcall), GFP_KERNEL);
+	if (!xcall)
+		return -ENOMEM;
+
+	ret = init_xcall(xcall, comm);
+	if (ret) {
+		kfree(xcall);
+		return ret;
+	}
+
+	xcall->name = kstrdup(comm->name, GFP_KERNEL);
+	if (!xcall->name) {
+		delete_xcall(xcall);
+		return -ENOMEM;
+	}
+
+	if (insert_xcall_locked(xcall)) {
+		delete_xcall(xcall);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+int xcall_detach(struct xcall_comm *comm)
+{
+	struct xcall *xcall;
+
+	spin_lock(&xcall_list_lock);
+	xcall = find_xcall(comm->name, NULL);
+	if (!xcall) {
+		spin_unlock(&xcall_list_lock);
+		return -EINVAL;
+	}
+
+	put_xcall(xcall);
+	list_del(&xcall->list);
+	put_xcall(xcall);
+	spin_unlock(&xcall_list_lock);
+	return 0;
+}

arch/arm64/kernel/xcall/proc.c
@@ -1,0 +1,238 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2025 Huawei Limited.
+ */
+
+#include <linux/namei.h>
+#include <linux/path.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/slab.h>
+#include <linux/string.h>
+#include <linux/xcall.h>
+
+#include <asm/xcall.h>
+
+static LIST_HEAD(comm_list);
+static DECLARE_RWSEM(comm_rwsem);
+
+static void free_xcall_comm(struct xcall_comm *info)
+{
+	if (!info)
+		return;
+	kfree(info->name);
+	kfree(info->binary);
+	kfree(info->module);
+	path_put(&info->binary_path);
+	kfree(info);
+}
+
+static struct xcall_comm *find_xcall_comm(struct xcall_comm *comm)
+{
+	struct xcall_comm *temp;
+
+	list_for_each_entry(temp, &comm_list, list) {
+		if (!strcmp(comm->name, temp->name))
+			return temp;
+	}
+
+	return NULL;
+}
+
+static void delete_xcall_comm_locked(struct xcall_comm *info)
+{
+	struct xcall_comm *ret;
+
+	down_write(&comm_rwsem);
+	ret = find_xcall_comm(info);
+	if (ret)
+		list_del(&ret->list);
+	up_write(&comm_rwsem);
+	free_xcall_comm(ret);
+}
+
+static void insert_xcall_comm_locked(struct xcall_comm *info)
+{
+	down_write(&comm_rwsem);
+	if (!find_xcall_comm(info))
+		list_add(&info->list, &comm_list);
+	up_write(&comm_rwsem);
+}
+
+static int is_absolute_path(const char *path)
+{
+	return path[0] == '/';
+}
+
+static int parse_xcall_command(int argc, char **argv,
+			       struct xcall_comm *info)
+{
+	struct dentry *dentry;
+
+	if (strlen(argv[0]) < 3)
+		return -ECANCELED;
+
+	if (argv[0][0] != '+' && argv[0][0] != '-')
+		return -ECANCELED;
+
+	if (argv[0][1] != ':')
+		return -ECANCELED;
+
+	if (argv[0][0] == '+' && argc != 3)
+		return -ECANCELED;
+
+	if (argv[0][0] == '-' && argc != 1)
+		return -ECANCELED;
+
+	info->name = kstrdup(&argv[0][2], GFP_KERNEL);
+	if (!info->name)
+		return -ENOMEM;
+
+	if (argv[0][0] == '-')
+		return '-';
+
+	info->binary = kstrdup(argv[1], GFP_KERNEL);
+	if (!info->binary)
+		goto free_name;
+
+	if (!is_absolute_path(info->binary))
+		goto free_binary;
+
+	if (kern_path(info->binary, LOOKUP_FOLLOW, &info->binary_path))
+		goto free_binary;
+
+	dentry = info->binary_path.dentry;
+	if (!dentry || !S_ISREG(d_inode(dentry)->i_mode) ||
+	    !(d_inode(dentry)->i_mode & 0111))
+		goto put_path;
+
+	info->module = kstrdup(argv[2], GFP_KERNEL);
+	if (!info->module)
+		goto put_path;
+
+	return argv[0][0];
+
+put_path:
+	path_put(&info->binary_path);
+free_binary:
+	kfree(info->binary);
+free_name:
+	kfree(info->name);
+	return -EINVAL;
+}
+
+/*
+ * /proc/xcall/comm
+ * Argument syntax:
+ *   +:COMM ELF_FILE [KERNEL_MODULE] : Attach a xcall
+ *   -:COMM							 : Detach a xcall
+ *
+ *   COMM:		: Unique string for attached xcall.
+ *   ELF_FILE		: Path to an executable or library.
+ *   KERNEL_MODULE	: Module name listed in /proc/modules provide xcall program.
+ */
+int proc_xcall_command(int argc, char **argv)
+{
+	struct xcall_comm *info;
+	int ret, op;
+
+	info = kzalloc(sizeof(*info), GFP_KERNEL);
+	if (!info)
+		return -ENOMEM;
+	INIT_LIST_HEAD(&info->list);
+
+	op = parse_xcall_command(argc, argv, info);
+	switch (op) {
+	case '+':
+		ret = xcall_attach(info);
+		if (!ret)
+			insert_xcall_comm_locked(info);
+		else
+			free_xcall_comm(info);
+		break;
+	case '-':
+		ret = xcall_detach(info);
+		if (!ret)
+			delete_xcall_comm_locked(info);
+		free_xcall_comm(info);
+		break;
+	default:
+		kfree(info);
+		return -EINVAL;
+	}
+
+	return ret;
+}
+
+static int xcall_comm_show(struct seq_file *m, void *v)
+{
+	struct xcall_comm *info;
+
+	down_read(&comm_rwsem);
+	list_for_each_entry(info, &comm_list, list) {
+		seq_printf(m, "+:%s %s %s\n",
+			   info->name, info->binary,
+			   info->module);
+	}
+	up_read(&comm_rwsem);
+	return 0;
+}
+
+static int xcall_comm_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, xcall_comm_show, NULL);
+}
+
+static ssize_t xcall_comm_write(struct file *file,
+				const char __user *user_buf,
+				size_t nbytes, loff_t *ppos)
+{
+	int argc = 0, ret = 0;
+	char *raw_comm;
+	char **argv;
+
+	if (nbytes <= 1)
+		return -EINVAL;
+
+	raw_comm = memdup_user_nul(user_buf, nbytes - 1);
+	if (IS_ERR(raw_comm))
+		return PTR_ERR(raw_comm);
+
+	argv = argv_split(GFP_KERNEL, raw_comm, &argc);
+	if (!argv) {
+		kfree(raw_comm);
+		return -ENOMEM;
+	}
+
+	ret = proc_xcall_command(argc, argv);
+
+	argv_free(argv);
+
+	kfree(raw_comm);
+
+	return ret ? ret : nbytes;
+}
+
+static const struct proc_ops xcall_comm_ops = {
+	.proc_open	= xcall_comm_open,
+	.proc_read	= seq_read,
+	.proc_lseek	= seq_lseek,
+	.proc_write	= xcall_comm_write,
+	.proc_release	= single_release,
+};
+
+static int __init xcall_proc_init(void)
+{
+	struct proc_dir_entry *root_xcall_dir;
+
+	if (!static_key_enabled(&xcall_enable))
+		return 0;
+
+	root_xcall_dir = proc_mkdir("xcall", NULL);
+	if (!root_xcall_dir)
+		return 0;
+
+	proc_create("comm", 0640, root_xcall_dir, &xcall_comm_ops);
+	return 0;
+}
+module_init(xcall_proc_init);

include/linux/xcall.h
@@ -1,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2025 Huawei.
+ */
+
+#ifndef _LINUX_XCALL_H
+#define _LINUX_XCALL_H
+
+#include <linux/module.h>
+
+struct xcall_prog_object {
+	unsigned long scno;
+	unsigned long func;
+};
+
+#define PROG_NAME_LEN	64
+#define MAX_NR_SCNO	32
+
+struct xcall_prog {
+	char name[PROG_NAME_LEN];
+	struct module *owner;
+	struct list_head list;
+	struct xcall_prog_object objs[MAX_NR_SCNO];
+	unsigned int nr_scno;
+};
+#endif /* _LINUX_XCALL_H */

arch/arm64/Kconfig.turbo
@@ -71,4 +71,16 @@ config ACTLR_XCALL_XINT
 	  Use the 0x680 as the offset to the exception vector base address for
 	  the Armv8.8 NMI taken from EL0.
 
+config DYNAMIC_XCALL
+	bool "Support dynamically replace and load system call"
+	depends on FAST_SYSCALL
+	default n
+	help
+	  Xcall 2.0 add "/proc/xcall/comm" interface to
+	  attach xcall programs onto one executable,
+	  and support different custom syscall implementation
+	  by dynamic instruction replaced with 'svc ffff'
+	  and a kernel module which provides customized
+	  implementation.
+
 endmenu # "Turbo features selection"

arch/arm64/kernel/xcall/Makefile
@@ -1,2 +1,3 @@
 # SPDX-License-Identifier: GPL-2.0
-obj-y	+= xcall.o
+obj-y += xcall.o
+obj-$(CONFIG_DYNAMIC_XCALL) += core.o proc.o

chenglongshe/openEuler

commit 9d87a2b3eb6ac98525412fe157ab56043f32180f

xcall2.0: Fix mem leak in proc_xcall_command
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/release-management/issues/ID5CMS

--------------------------------

There is a test which is registering and unregistering xcall by
/proc/xcall/comm in multithread. Then kmemleak reported a leak

unreferenced object 0xffff00104bd86d00 (size 64):
  comm "bash", pid 4246, jiffies 4295025941 (age 1701.080s)
  hex dump (first 32 bytes):
    60 fd ff 4b 10 00 ff ff 80 6f d8 4b 10 00 ff ff  `..K.....o.K....
    a0 df 28 04 90 00 ff ff 18 41 aa 04 88 00 ff ff  ..(......A......
  backtrace:
    kmemleak_alloc+0xb4/0xd0
    __kmem_cache_alloc_node+0x330/0x420
    kmalloc_trace+0x48/0xf8
    proc_xcall_command+0x44/0x378
    xcall_comm_write+0xc8/0x160
    proc_reg_write+0x110/0x180
    vfs_write+0x150/0x3a8
    ksys_write+0xd0/0x180
    __arm64_sys_write+0x4c/0x68
    invoke_syscall+0x68/0x1a0
    el0_svc_common.constprop.0+0x11c/0x150
    do_el0_svc+0x68/0xb0
    el0_slow_syscall+0x44/0x1e8
    .slow_syscall+0x18c/0x190

In proccesing below, we cannot promise xcall_comm_list and xcall_comm
is consistent. This is why the memleak happened. And it has another bug
which is xcall has been unregistered, but we can still find it in /proc/
xcall/comm.
-------------------------------------------------------
    A thread                    B thread
-------------------------------------------------------
xcall_attach
insert_xcall_locked
                               xcall_detach
                               delete_xcall_comm_locked
insert_xcall_comm_locked
-------------------------------------------------------

To solve this problem, merge struct xcall_comm into struct xcall. Make
sure this two struct is protected by the same lock. So they will be
consistent.

Fixes: 7d904de7feb2 ("xcall2.0: Add userspace proc interface")
Signed-off-by: Xinyu Zheng <zhengxinyu6@huawei.com>

arch/arm64/include/asm/xcall.h
@@ -36,7 +36,7 @@ struct xcall {
 	/* file attached xcall */
 	struct inode		*binary;
 	struct xcall_prog	*program;
-	char			*name;
+	struct xcall_comm	*info;
 };
 
 struct xcall_area {
@@ -52,6 +52,7 @@ struct xcall_area {
 
 extern const syscall_fn_t *default_sys_call_table(void);
 #ifdef CONFIG_DYNAMIC_XCALL
+extern void xcall_info_show(struct seq_file *m);
 extern int xcall_attach(struct xcall_comm *info);
 extern int xcall_detach(struct xcall_comm *info);
 extern int xcall_pre_sstep_check(struct pt_regs *regs);

arch/arm64/kernel/xcall/core.c
@@ -111,12 +111,25 @@ static struct xcall *get_xcall(struct xcall *xcall)
 	return xcall;
 }
 
+static void free_xcall_comm(struct xcall_comm *info)
+{
+	if (!info)
+		return;
+
+	kfree(info->name);
+	kfree(info->binary);
+	kfree(info->module);
+	path_put(&info->binary_path);
+	kfree(info);
+}
+
 static void put_xcall(struct xcall *xcall)
 {
 	if (!refcount_dec_and_test(&xcall->ref))
 		return;
 
-	kfree(xcall->name);
+	free_xcall_comm(xcall->info);
+
 	if (xcall->program)
 		module_put(xcall->program->owner);
 
@@ -128,7 +141,7 @@ static struct xcall *find_xcall(const char *name, struct inode *binary)
 	struct xcall *xcall;
 
 	list_for_each_entry(xcall, &xcalls_list, list) {
-		if ((name && !strcmp(name, xcall->name)) ||
+		if ((name && !strcmp(name, xcall->info->name)) ||
 		    (binary && xcall->binary == binary))
 			return get_xcall(xcall);
 	}
@@ -140,7 +153,7 @@ static struct xcall *insert_xcall_locked(struct xcall *xcall)
 	struct xcall *ret = NULL;
 
 	spin_lock(&xcall_list_lock);
-	ret = find_xcall(xcall->name, xcall->binary);
+	ret = find_xcall(xcall->info->name, xcall->binary);
 	if (!ret)
 		list_add(&xcall->list, &xcalls_list);
 	else
@@ -166,6 +179,7 @@ static int init_xcall(struct xcall *xcall, struct xcall_comm *comm)
 	if (!program || !try_module_get(program->owner))
 		return -EINVAL;
 
+	xcall->info = comm;
 	xcall->binary = d_real_inode(comm->binary_path.dentry);
 	xcall->program = program;
 	refcount_set(&xcall->ref, 1);
@@ -284,25 +298,35 @@ void clear_xcall_area(struct mm_struct *mm)
 	mm->xcall = NULL;
 }
 
+void xcall_info_show(struct seq_file *m)
+{
+	struct xcall *xcall;
+
+	spin_lock(&xcall_list_lock);
+	list_for_each_entry(xcall, &xcalls_list, list) {
+		seq_printf(m, "+:%s %s %s\n",
+			   xcall->info->name, xcall->info->binary,
+			   xcall->info->module);
+	}
+	spin_unlock(&xcall_list_lock);
+}
+
 int xcall_attach(struct xcall_comm *comm)
 {
 	struct xcall *xcall;
 	int ret;
 
 	xcall = kzalloc(sizeof(struct xcall), GFP_KERNEL);
-	if (!xcall)
+	if (!xcall) {
+		free_xcall_comm(comm);
 		return -ENOMEM;
+	}
 
 	ret = init_xcall(xcall, comm);
 	if (ret) {
+		free_xcall_comm(comm);
 		kfree(xcall);
 		return ret;
-	}
-
-	xcall->name = kstrdup(comm->name, GFP_KERNEL);
-	if (!xcall->name) {
-		delete_xcall(xcall);
-		return -ENOMEM;
 	}
 
 	if (insert_xcall_locked(xcall)) {

arch/arm64/kernel/xcall/proc.c
@@ -13,53 +13,7 @@
 
 #include <asm/xcall.h>
 
-static LIST_HEAD(comm_list);
-static DECLARE_RWSEM(comm_rwsem);
-
 struct proc_dir_entry *root_xcall_dir;
-
-static void free_xcall_comm(struct xcall_comm *info)
-{
-	if (!info)
-		return;
-	kfree(info->name);
-	kfree(info->binary);
-	kfree(info->module);
-	path_put(&info->binary_path);
-	kfree(info);
-}
-
-static struct xcall_comm *find_xcall_comm(struct xcall_comm *comm)
-{
-	struct xcall_comm *temp;
-
-	list_for_each_entry(temp, &comm_list, list) {
-		if (!strcmp(comm->name, temp->name))
-			return temp;
-	}
-
-	return NULL;
-}
-
-static void delete_xcall_comm_locked(struct xcall_comm *info)
-{
-	struct xcall_comm *ret;
-
-	down_write(&comm_rwsem);
-	ret = find_xcall_comm(info);
-	if (ret)
-		list_del(&ret->list);
-	up_write(&comm_rwsem);
-	free_xcall_comm(ret);
-}
-
-static void insert_xcall_comm_locked(struct xcall_comm *info)
-{
-	down_write(&comm_rwsem);
-	if (!find_xcall_comm(info))
-		list_add(&info->list, &comm_list);
-	up_write(&comm_rwsem);
-}
 
 static int is_absolute_path(const char *path)
 {
@@ -147,16 +101,11 @@ int proc_xcall_command(int argc, char **argv)
 	switch (op) {
 	case '+':
 		ret = xcall_attach(info);
-		if (!ret)
-			insert_xcall_comm_locked(info);
-		else
-			free_xcall_comm(info);
 		break;
 	case '-':
 		ret = xcall_detach(info);
-		if (!ret)
-			delete_xcall_comm_locked(info);
-		free_xcall_comm(info);
+		kfree(info->name);
+		kfree(info);
 		break;
 	default:
 		kfree(info);
@@ -168,15 +117,8 @@ int proc_xcall_command(int argc, char **argv)
 
 static int xcall_comm_show(struct seq_file *m, void *v)
 {
-	struct xcall_comm *info;
+	xcall_info_show(m);
 
-	down_read(&comm_rwsem);
-	list_for_each_entry(info, &comm_list, list) {
-		seq_printf(m, "+:%s %s %s\n",
-			   info->name, info->binary,
-			   info->module);
-	}
-	up_read(&comm_rwsem);
 	return 0;
 }
 