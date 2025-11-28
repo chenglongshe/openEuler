77b2e551d291eab410deb06a8fd63868704c4981
Toke Høiland-Jørgensen <toke@redhat.com>
2025-11-28 15:33:42 +0800

    page_pool: Move pp_magic check into helper functions
    mainline inclusion
    from mainline-v6.16-rc1
    commit cd3c93167da0e760b5819246eae7a4ea30fd014b
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICPKD5
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=cd3c93167da0e760b5819246eae7a4ea30fd014b
    
    --------------------------------
    
    Since we are about to stash some more information into the pp_magic
    field, let's move the magic signature checks into a pair of helper
    functions so it can be changed in one place.
    
    Reviewed-by: Mina Almasry <almasrymina@google.com>
    Tested-by: Yonglong Liu <liuyonglong@huawei.com>
    Acked-by: Jesper Dangaard Brouer <hawk@kernel.org>
    Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Link: https://patch.msgid.link/20250409-page-pool-track-dma-v9-1-6a9ef2e0cba8@redhat.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Conflicts:
    	include/linux/mm.h
    	net/core/netmem_priv.h
    	net/core/skbuff.c
    	net/core/xdp.c
    [commit 5796d3967c09 and 239e9a90c887 are not backport lead to
    conflicts in mm.h commit 8ab79ed50cf1 and f7dc3248dcfb are not
    backport lead to conflicts in other files]
    Signed-off-by: Dong Chenchen <dongchenchen2@huawei.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -4355,4 +4355,24 @@ static inline bool vma_is_peer_shared(struct vm_area_struct *vma)
 }
 #endif
 
+/* Mask used for checking in page_pool_page_is_pp() below. page->pp_magic is
+ * OR'ed with PP_SIGNATURE after the allocation in order to preserve bit 0 for
+ * the head page of compound page and bit 1 for pfmemalloc page.
+ * page_is_pfmemalloc() is checked in __page_pool_put_page() to avoid recycling
+ * the pfmemalloc page.
+ */
+#define PP_MAGIC_MASK ~0x3UL
+
+#ifdef CONFIG_PAGE_POOL
+static inline bool page_pool_page_is_pp(struct page *page)
+{
+	return (page->pp_magic & PP_MAGIC_MASK) == PP_SIGNATURE;
+}
+#else
+static inline bool page_pool_page_is_pp(struct page *page)
+{
+	return false;
+}
+#endif
+
 #endif /* _LINUX_MM_H */
