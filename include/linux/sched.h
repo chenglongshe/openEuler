OERV-RVCI/rvck-olk

commit 832ec54e11a079d968d8f23780ab455b1537b214

xsched: Add xsched RT class
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IC5EHB

-----------------------------------------

Add rt class callbacks implementation:
- dequeue_ctx
- enqueue_ctx
- pick_next_ctx
- put_prev_ctx
- submit_prepare_ctx
- select_work
- check_preempt

Add xsched_rt.c in /kernel/xsched Makefile.
Add RT class callbacks support in core.c.

Signed-off-by: Konstantin Meskhidze <konstantin.meskhidze@huawei.com>
Signed-off-by: Hui Tang <tanghui20@.huawei.com>
Signed-off-by: Liu Kai <liukai284@huawei.com>
Signed-off-by: Xia Fukun <xiafukun@huawei.com>
Signed-off-by: Zicheng Qu <quzicheng@huawei.com>

include/linux/xsched.h
@@ -38,9 +38,27 @@
 
 #define MAX_VSTREAM_NUM 512
 
+/*
+ * A default kick slice for RT class XSEs.
+ */
+#define XSCHED_RT_KICK_SLICE 2
+
+extern struct xsched_cu *xsched_cu_mgr[XSCHED_NR_CUS];
+
 enum xcu_sched_type {
-	XSCHED_TYPE_NUM
+	XSCHED_TYPE_RT = 0,
+	XSCHED_TYPE_NUM,
+	XSCHED_TYPE_DFLT = XSCHED_TYPE_RT
 };
+
+enum xse_prio {
+	XSE_PRIO_HIGH = 0,
+	XSE_PRIO_LOW = 4,
+	NR_XSE_PRIO,
+	XSE_PRIO_DFLT = XSE_PRIO_LOW
+};
+
+extern struct xsched_class rt_xsched_class;
 
 #define xsched_first_class \
 	list_first_entry(&(xsched_class_list), struct xsched_class, node)
@@ -48,8 +66,22 @@ enum xcu_sched_type {
 #define for_each_xsched_class(class)                                           \
 	list_for_each_entry((class), &(xsched_class_list), node)
 
+#define for_each_xse_prio(prio)                                                \
+	for (prio = XSE_PRIO_HIGH; prio < NR_XSE_PRIO; prio++)
 #define for_each_vstream_in_ctx(vs, ctx)                                       \
 	list_for_each_entry((vs), &((ctx)->vstream_list), ctx_node)
+
+
+/* Manages xsched RT-like class linked list based runqueue.
+ *
+ * Now RT-like class runqueue structs is identical
+ * but will most likely grow different in the
+ * future as the Xsched evolves.
+ */
+struct xsched_rq_rt {
+	struct list_head rq[NR_XSE_PRIO];
+	unsigned int nr_running;
+};
 
 /* Base XSched runqueue object structure that contains both mutual and
  * individual parameters for different scheduling classes.
@@ -60,6 +92,8 @@ struct xsched_rq {
 
 	int state;
 	int nr_running;
+	/* RT class run queue.*/
+	struct xsched_rq_rt rt;
 };
 
 enum xsched_cu_status {
@@ -102,6 +136,18 @@ struct xsched_cu {
 	wait_queue_head_t wq_xcu_idle;
 };
 
+extern int num_active_xcu;
+#define for_each_active_xcu(xcu, id)                                           \
+	for ((id) = 0, xcu = xsched_cu_mgr[(id)];                                  \
+	     (id) < num_active_xcu && (xcu = xsched_cu_mgr[(id)]); (id)++)
+
+struct xsched_entity_rt {
+	struct list_head list_node;
+	enum xse_prio prio;
+
+	ktime_t timeslice;
+};
+
 struct xsched_entity {
 	uint32_t task_type;
 
@@ -127,6 +173,9 @@ struct xsched_entity {
 
 	/* Xsched class for this xse. */
 	const struct xsched_class *class;
+
+	/* RT class entity. */
+	struct xsched_entity_rt rt;
 
 	/* Pointer to context object. */
 	struct xsched_context *ctx;
@@ -279,6 +328,7 @@ int xsched_init_entity(struct xsched_context *ctx, struct vstream_info *vs);
 int ctx_bind_to_xcu(vstream_info_t *vstream_info, struct xsched_context *ctx);
 int xsched_vsm_add_tail(struct vstream_info *vs, vstream_args_t *arg);
 struct vstream_metadata *xsched_vsm_fetch_first(struct vstream_info *vs);
+int xsched_rt_prio_set(pid_t tgid, unsigned int prio);
 void enqueue_ctx(struct xsched_entity *xse, struct xsched_cu *xcu);
 void dequeue_ctx(struct xsched_entity *xse, struct xsched_cu *xcu);
 int delete_ctx(struct xsched_context *ctx);

kernel/xsched/core.c
@@ -188,9 +188,16 @@ int delete_ctx(struct xsched_context *ctx)
 
 int xsched_xse_set_class(struct xsched_entity *xse)
 {
-	struct xsched_class *sched = xsched_first_class;
+	switch (xse->task_type) {
+	case XSCHED_TYPE_RT:
+		xse->class = &rt_xsched_class;
+		XSCHED_DEBUG("Context is in RT class %s\n", __func__);
+		break;
+	default:
+		XSCHED_ERR("Xse has incorrect class @ %s\n", __func__);
+		return -EINVAL;
+	}
 
-	xse->class = sched;
 	return 0;
 }
 
@@ -354,7 +361,8 @@ int xsched_schedule(void *input_xcu)
 
 	while (!kthread_should_stop()) {
 		mutex_unlock(&xcu->xcu_lock);
-		wait_event_interruptible(xcu->wq_xcu_idle, 1);
+		wait_event_interruptible(xcu->wq_xcu_idle,
+			xcu->xrq.rt.nr_running);
 
 		mutex_lock(&xcu->xcu_lock);
 		if (kthread_should_stop()) {
@@ -480,4 +488,20 @@ int xsched_init_entity(struct xsched_context *ctx, struct vstream_info *vs)
 	spin_lock_init(&xse->xse_lock);
 	return err;
 }
+
+static void xsched_register_sched_class(struct xsched_class *sched)
+{
+	list_add_tail(&sched->node, &xsched_class_list);
+}
+
+__init int xsched_sched_init(void)
+{
+	INIT_LIST_HEAD(&xsched_class_list);
+#ifdef CONFIG_XCU_SCHED_RT
+	xsched_register_sched_class(&rt_xsched_class);
+#endif
+
+	return 0;
+}
+late_initcall(xsched_sched_init);
 

kernel/xsched/rt.c
@@ -1,0 +1,281 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Real-Time Scheduling Class for XPU device
+ *
+ * Copyright (C) 2025-2026 Huawei Technologies Co., Ltd
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+#include <uapi/linux/sched/types.h>
+#include <linux/hash.h>
+#include <linux/hashtable.h>
+#include <linux/kthread.h>
+#include <linux/slab.h>
+#include <linux/xsched.h>
+#include <linux/vstream.h>
+
+#define XSCHED_RT_TIMESLICE	(10 * NSEC_PER_MSEC)
+
+#define TGID_HASH_BITS 8
+
+/* Mapping between tgid and context */
+struct tgid_prio {
+	pid_t tgid;
+	int32_t prio;
+	struct hlist_node hnode;
+};
+
+static DEFINE_HASHTABLE(tgid_prio_map, TGID_HASH_BITS);
+static DEFINE_SPINLOCK(tgid_prio_lock);
+
+static int tgid_prio_insert(pid_t tgid, int32_t prio)
+{
+	struct tgid_prio *new_map;
+	unsigned int hash_key;
+
+	if (prio >= NR_XSE_PRIO)
+		return -EINVAL;
+
+	new_map = kzalloc(sizeof(struct tgid_prio), GFP_KERNEL);
+	if (!new_map) {
+		XSCHED_ERR("Fail to alloc mapping (tgid=%d) @ %s\n",
+			tgid, __func__);
+		return -ENOMEM;
+	}
+
+	new_map->tgid = tgid;
+	new_map->prio = prio;
+
+	hash_key = hash_32(tgid, TGID_HASH_BITS);
+
+	spin_lock(&tgid_prio_lock);
+	hash_add_rcu(tgid_prio_map, &new_map->hnode, hash_key);
+	spin_unlock(&tgid_prio_lock);
+
+	return 0;
+}
+
+static struct tgid_prio *tgid_prio_find(pid_t tgid)
+{
+	struct tgid_prio *map = NULL;
+	unsigned int hash_key = hash_32(tgid, TGID_HASH_BITS);
+
+	rcu_read_lock();
+	hash_for_each_possible_rcu(tgid_prio_map, map, hnode, hash_key) {
+		if (map->tgid == tgid)
+			break;
+	}
+	rcu_read_unlock();
+	return map;
+}
+
+static void tgid_prio_delete(pid_t tgid)
+{
+	struct tgid_prio *map;
+	unsigned int hash_key = hash_32(tgid, TGID_HASH_BITS);
+
+	spin_lock(&tgid_prio_lock);
+	hash_for_each_possible(tgid_prio_map, map, hnode, hash_key) {
+		if (map->tgid == tgid) {
+			hash_del_rcu(&map->hnode);
+			spin_unlock(&tgid_prio_lock);
+			kfree(map);
+			return;
+		}
+	}
+	spin_unlock(&tgid_prio_lock);
+}
+
+static inline void
+xse_rt_add(struct xsched_entity *xse, struct xsched_cu *xcu)
+{
+	list_add_tail(&xse->rt.list_node, &xcu->xrq.rt.rq[xse->rt.prio]);
+}
+
+static inline void xse_rt_del(struct xsched_entity *xse)
+{
+	list_del_init(&xse->rt.list_node);
+}
+
+static inline void xse_rt_move_tail(struct xsched_entity *xse)
+{
+	struct xsched_cu *xcu = xse->xcu;
+
+	list_move_tail(&xse->rt.list_node, &xcu->xrq.rt.rq[xse->rt.prio]);
+}
+
+/* Increase RT runqueue total and per prio nr_running stat. */
+static inline void xrq_inc_nr_running(struct xsched_entity *xse,
+				      struct xsched_cu *xcu)
+{
+	xcu->xrq.rt.nr_running++;
+}
+
+/* Decrease RT runqueue total and per prio nr_running stat
+ * and raise a bug if nr_running decrease beyond zero.
+ */
+static inline void xrq_dec_nr_running(struct xsched_entity *xse)
+{
+	struct xsched_cu *xcu = xse->xcu;
+
+	xcu->xrq.rt.nr_running--;
+}
+
+static void dequeue_ctx_rt(struct xsched_entity *xse)
+{
+	xse_rt_del(xse);
+	xrq_dec_nr_running(xse);
+}
+
+static void enqueue_ctx_rt(struct xsched_entity *xse, struct xsched_cu *xcu)
+{
+	xse_rt_add(xse, xcu);
+	xrq_inc_nr_running(xse, xcu);
+}
+
+static inline struct xsched_entity *xrq_next_xse(struct xsched_cu *xcu,
+						 int prio)
+{
+	return list_first_entry(&xcu->xrq.rt.rq[prio], struct xsched_entity,
+				rt.list_node);
+}
+
+/* Return the next priority for pick_next_ctx taking into
+ * account if there are pending kicks on certain priority.
+ */
+static inline uint32_t get_next_prio_rt(struct xsched_rq *xrq)
+{
+	unsigned int curr_prio;
+
+	for_each_xse_prio(curr_prio) {
+		if (!list_empty(&xrq->rt.rq[curr_prio]))
+			return curr_prio;
+	}
+	return NR_XSE_PRIO;
+}
+
+static struct xsched_entity *pick_next_ctx_rt(struct xsched_cu *xcu)
+{
+	struct xsched_entity *result;
+	int next_prio;
+
+	next_prio = get_next_prio_rt(&xcu->xrq);
+	if (next_prio >= NR_XSE_PRIO) {
+		XSCHED_DEBUG("No pending kicks in RT class @ %s\n", __func__);
+		return NULL;
+	}
+
+	result = xrq_next_xse(xcu, next_prio);
+	if (!result)
+		XSCHED_ERR("Next XSE not found @ %s\n", __func__);
+	else
+		XSCHED_DEBUG("Next XSE %u at prio %u @ %s\n", result->tgid, next_prio, __func__);
+
+	return result;
+}
+
+static void put_prev_ctx_rt(struct xsched_entity *xse)
+{
+	xse->rt.timeslice -= xse->last_exec_runtime;
+	XSCHED_DEBUG(
+		"Update XSE=%d timeslice=%lld, XSE submitted=%lld in RT class @ %s\n",
+		xse->tgid, xse->rt.timeslice,
+		xse->last_exec_runtime, __func__);
+
+	if (xse->rt.timeslice <= 0) {
+		xse->rt.timeslice = XSCHED_RT_TIMESLICE;
+		XSCHED_DEBUG("Refill XSE=%d kick_slice=%lld in RT class @ %s\n",
+			    xse->tgid, xse->rt.timeslice, __func__);
+		xse_rt_move_tail(xse);
+	}
+}
+
+static bool check_preempt_ctx_rt(struct xsched_entity *xse)
+{
+	return true;
+}
+
+void rq_init_rt(struct xsched_cu *xcu)
+{
+	int prio = 0;
+
+	xcu->xrq.rt.nr_running = 0;
+
+	for_each_xse_prio(prio) {
+		INIT_LIST_HEAD(&xcu->xrq.rt.rq[prio]);
+	}
+}
+
+void xse_init_rt(struct xsched_entity *xse)
+{
+	struct tgid_prio *map = tgid_prio_find(xse->tgid);
+
+	xse->rt.prio = (map) ? map->prio : XSE_PRIO_DFLT;
+	XSCHED_DEBUG("Xse init: set priority=%d.\n", xse->rt.prio);
+	xse->rt.timeslice = XSCHED_RT_TIMESLICE;
+	INIT_LIST_HEAD(&xse->rt.list_node);
+}
+
+void xse_deinit_rt(struct xsched_entity *xse)
+{
+	struct tgid_prio *map = tgid_prio_find(xse->tgid);
+
+	if (map) {
+		tgid_prio_delete(xse->tgid);
+		XSCHED_DEBUG("Map deleted: tgid=%d\n", xse->tgid);
+	}
+}
+
+struct xsched_class rt_xsched_class = {
+	.class_id = XSCHED_TYPE_RT,
+	.kick_slice = XSCHED_RT_KICK_SLICE,
+	.rq_init = rq_init_rt,
+	.xse_init = xse_init_rt,
+	.xse_deinit = xse_deinit_rt,
+	.dequeue_ctx = dequeue_ctx_rt,
+	.enqueue_ctx = enqueue_ctx_rt,
+	.pick_next_ctx = pick_next_ctx_rt,
+	.put_prev_ctx = put_prev_ctx_rt,
+	.check_preempt = check_preempt_ctx_rt
+};
+
+int xsched_rt_prio_set(pid_t tgid, unsigned int prio)
+{
+	unsigned int id;
+	struct xsched_cu *xcu;
+	struct xsched_context *ctx;
+	struct xsched_entity *xse;
+
+	tgid_prio_delete(tgid);
+	tgid_prio_insert(tgid, prio);
+
+	for_each_active_xcu(xcu, id) {
+		mutex_lock(&xcu->ctx_list_lock);
+		mutex_lock(&xcu->xcu_lock);
+
+		ctx = ctx_find_by_tgid_and_xcu(tgid, xcu);
+		if (ctx) {
+			xse = &ctx->xse;
+			xse->rt.prio = clamp_t(unsigned int, prio, XSE_PRIO_HIGH, XSE_PRIO_LOW);
+			if (xse->on_rq) {
+				xse_rt_del(xse);
+				xse_rt_add(xse, xcu);
+			}
+		}
+
+		mutex_unlock(&xcu->xcu_lock);
+		mutex_unlock(&xcu->ctx_list_lock);
+	}
+
+	return 0;
+}
+

drivers/xcu/xcu_group.c
@@ -20,7 +20,7 @@
 #include <linux/xcu_group.h>
 #include <linux/xsched.h>
 
-static int num_active_xcu;
+int num_active_xcu;
 static DEFINE_SPINLOCK(xcu_mgr_lock);
 struct xsched_cu *xsched_cu_mgr[XSCHED_NR_CUS];
 static DECLARE_RWSEM(xcu_group_rwsem);

kernel/xsched/Kconfig
@@ -4,6 +4,7 @@ config XCU_SCHEDULER
     bool "Enable XSched functionality"
     default n
     select XCU_VSTREAM
+    select XCU_SCHED_RT
     help
       This option enables the XSched scheduler, a custom scheduling mechanism
       designed for heterogeneous compute units (e.g., XPUs). It provides:
@@ -34,3 +35,17 @@ config XSCHED_NR_CUS
       This option defines the maximum number of Compute Units (CUs) that can be
       managed by the XSched scheduler, consider changing this value proportionally
       to the number of available XCU cores.
+
+config XCU_SCHED_RT
+    bool "XCU RT scheduling class"
+    default y
+    depends on XCU_SCHEDULER
+    help
+      Enable support for the RT scheduling class in the XCU scheduler.
+
+      This option allows XCU to schedule tasks using real-time priorities
+      (XSCHED_TYPE_RT). When enabled, tasks in RT cgroups can be assigned
+      deterministic priorities and will be scheduled ahead of CFS tasks.
+
+      Unless you are using RT workloads that rely on strict priority-based
+      scheduling within XCU, it is recommended to keep the default setting.

kernel/xsched/Makefile
@@ -3,4 +3,5 @@ obj-y += vstream.o
 
 ifdef CONFIG_XCU_SCHEDULER
 obj-y += core.o
+obj-$(CONFIG_XCU_SCHED_RT) += rt.o
 endif

chenglongshe/openEuler

commit d55a06e87fdfa8446370db762ebc780517d6510b

xsched: add xsched_{set,get}attr syscall
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ID9IFI

-----------------------------------------

If CONFIG_XCU_CGROUP is not enabled, the xsched kernel defaults to only
the RT scheduling class, and there is no interface available to
configure the RT priority. Therefore, two new syscalls are added to set
and get the configuration of the RT scheduling class.

The default priority for RT is 4 (the lowest priority) in the kernel.
In contrast, user-configured values work the opposity way: the smaller
the value, the lower the priority. The child process inherits the
priority configuration of the parent process during fork. If pid=0, it
indicates that the configuration is applied to the current process.
Additionally, an interface is reserved for configuring the xsched
scheduling class, which will be used to support CFS in the future.

Currently, these system calls are only available for ARM64 and x86_64
architectures.

Fixes: 832ec54e11a0 ("xsched: Add xsched RT class")
Signed-off-by: Liu Kai <liukai284@huawei.com>

arch/arm/tools/syscall.tbl
@@ -480,5 +480,5 @@
 464	common	kabi_reserved464		sys_ni_syscall
 465	common	kabi_reserved465		sys_ni_syscall
 466	common	kabi_reserved466		sys_ni_syscall
-467	common	kabi_reserved467		sys_ni_syscall
-468	common	kabi_reserved468		sys_ni_syscall
+467	common	xsched_setattr		sys_ni_syscall
+468	common	xsched_getattr		sys_ni_syscall

arch/powerpc/kernel/syscalls/syscall.tbl
@@ -557,5 +557,5 @@
 464	common	kabi_reserved464		sys_ni_syscall
 465	common	kabi_reserved465		sys_ni_syscall
 466	common	kabi_reserved466		sys_ni_syscall
-467	common	kabi_reserved467		sys_ni_syscall
-468	common	kabi_reserved468		sys_ni_syscall
+467	common	xsched_setattr		sys_ni_syscall
+468	common	xsched_getattr		sys_ni_syscall

arch/x86/entry/syscalls/syscall_32.tbl
@@ -471,5 +471,5 @@
 464	i386	kabi_reserved464	sys_ni_syscall
 465	i386	kabi_reserved465	sys_ni_syscall
 466	i386	kabi_reserved466	sys_ni_syscall
-467	i386	kabi_reserved467	sys_ni_syscall
-468	i386	kabi_reserved468	sys_ni_syscall
+467	i386	xsched_setattr	sys_ni_syscall
+468	i386	xsched_getattr	sys_ni_syscall

arch/x86/entry/syscalls/syscall_64.tbl
@@ -388,8 +388,8 @@
 464	common	kabi_reserved464	sys_ni_syscall
 465	common	kabi_reserved465	sys_ni_syscall
 466	common	kabi_reserved466	sys_ni_syscall
-467	common	kabi_reserved467	sys_ni_syscall
-468	common	kabi_reserved468	sys_ni_syscall
+467	common	xsched_setattr	    sys_xsched_setattr
+468	common	xsched_getattr	    sys_xsched_getattr
 
 #
 # Due to a historical design error, certain syscalls are numbered differently

include/linux/sched.h
@@ -41,6 +41,10 @@
 #include <linux/thread_bits.h>
 #include <linux/kabi.h>
 
+#ifdef CONFIG_XCU_SCHEDULER
+#include <linux/xsched_types.h>
+#endif
+
 /* task_struct member predeclarations (sorted alphabetically): */
 struct audit_context;
 struct bio_list;
@@ -776,6 +780,10 @@ struct kmap_ctrl {
 struct task_struct_resvd {
 	/* pointer back to the main task_struct */
 	struct task_struct	*task;
+
+#ifdef CONFIG_XCU_SCHEDULER
+	struct xsched_attr xse_attr;
+#endif
 };
 
 struct task_struct {

include/linux/syscalls.h
@@ -950,6 +950,9 @@ asmlinkage long sys_cachestat(unsigned int fd,
 asmlinkage long sys_map_shadow_stack(unsigned long addr, unsigned long size, unsigned int flags);
 
 asmlinkage long sys_vstream_manage(struct vstream_args __user *arg, int cmd);
+
+asmlinkage long sys_xsched_setattr(pid_t pid, struct xsched_attr __user *arg);
+asmlinkage long sys_xsched_getattr(pid_t pid, struct xsched_attr __user *arg);
 /*
  * Architecture-specific system calls
  */

include/linux/xsched.h
@@ -6,6 +6,7 @@
 #include <linux/kref.h>
 #include <linux/vstream.h>
 #include <linux/xcu_group.h>
+#include <linux/xsched_types.h>
 
 #ifndef pr_fmt
 #define pr_fmt(fmt) fmt
@@ -52,20 +53,6 @@
 #define XSCHED_CFS_KICK_SLICE 10
 
 extern struct xsched_cu *xsched_cu_mgr[XSCHED_NR_CUS];
-
-enum xcu_sched_type {
-	XSCHED_TYPE_RT = 0,
-	XSCHED_TYPE_CFS = 1,
-	XSCHED_TYPE_NUM,
-	XSCHED_TYPE_DFLT = XSCHED_TYPE_RT
-};
-
-enum xse_prio {
-	XSE_PRIO_HIGH = 0,
-	XSE_PRIO_LOW = 4,
-	NR_XSE_PRIO,
-	XSE_PRIO_DFLT = XSE_PRIO_LOW
-};
 
 extern struct xsched_class rt_xsched_class;
 extern struct xsched_class fair_xsched_class;
@@ -406,7 +393,7 @@ static inline u64 gcd(u64 a, u64 b)
 }
 
 struct xsched_class {
-	enum xcu_sched_type class_id;
+	enum xcu_sched_class class_id;
 	size_t kick_slice;
 	struct list_head node;
 
@@ -456,7 +443,7 @@ int xsched_init_entity(struct xsched_context *ctx, struct vstream_info *vs);
 int ctx_bind_to_xcu(vstream_info_t *vstream_info, struct xsched_context *ctx);
 int xsched_vsm_add_tail(struct vstream_info *vs, vstream_args_t *arg);
 struct vstream_metadata *xsched_vsm_fetch_first(struct vstream_info *vs);
-int xsched_rt_prio_set(pid_t tgid, unsigned int prio);
+void xsched_rt_prio_set(pid_t tgid, unsigned int prio);
 void enqueue_ctx(struct xsched_entity *xse, struct xsched_cu *xcu);
 void dequeue_ctx(struct xsched_entity *xse, struct xsched_cu *xcu);
 int delete_ctx(struct xsched_context *ctx);

include/linux/xsched_types.h
@@ -1,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _XSCHED_TYPE_H
+#define _XSCHED_TYPE_H
+
+struct xsched_attr {
+	/* Scheduling class type, from enum xcu_sched_class */
+	unsigned int xsched_class;
+
+	/* RT scheduling priority, from enum xse_prio */
+	unsigned int xsched_priority;
+};
+
+enum xcu_sched_class {
+	XSCHED_TYPE_RT = 0,
+	XSCHED_TYPE_CFS = 1,
+	XSCHED_TYPE_NUM,
+	XSCHED_TYPE_DFLT = XSCHED_TYPE_RT
+};
+
+enum xse_prio {
+	XSE_PRIO_HIGH = 0,
+	XSE_PRIO_LOW = 4,
+	NR_XSE_PRIO,
+	XSE_PRIO_DFLT = XSE_PRIO_LOW
+};
+
+#endif /* ! _XSCHED_TYPE_H */

include/uapi/asm-generic/unistd.h
@@ -853,10 +853,10 @@ __SYSCALL(__NR_kabi_reserved464, sys_ni_syscall)
 __SYSCALL(__NR_kabi_reserved465, sys_ni_syscall)
 #define __NR_kabi_reserved466 466
 __SYSCALL(__NR_kabi_reserved466, sys_ni_syscall)
-#define __NR_kabi_reserved467 467
-__SYSCALL(__NR_kabi_reserved467, sys_ni_syscall)
-#define __NR_kabi_reserved468 468
-__SYSCALL(__NR_kabi_reserved468, sys_ni_syscall)
+#define __NR_xsched_setattr 467
+__SYSCALL(__NR_xsched_setattr, sys_xsched_setattr)
+#define __NR_xsched_getattr 468
+__SYSCALL(__NR_xsched_getattr, sys_xsched_getattr)
 
 #undef __NR_syscalls
 #define __NR_syscalls 469

init/init_task.c
@@ -14,6 +14,9 @@
 #include <linux/scs.h>
 
 #include <linux/uaccess.h>
+#ifdef CONFIG_XCU_SCHEDULER
+#include <linux/xsched_types.h>
+#endif
 
 static struct signal_struct init_signals = {
 	.nr_threads	= 1,
@@ -59,6 +62,9 @@ unsigned long init_shadow_call_stack[SCS_SIZE / sizeof(long)]
 
 static struct task_struct_resvd init_task_struct_resvd = {
 	.task = &init_task,
+#ifdef CONFIG_XCU_SCHEDULER
+	.xse_attr = { .xsched_priority = XSE_PRIO_DFLT },
+#endif
 };
 
 /*

kernel/fork.c
@@ -1165,6 +1165,9 @@ static bool dup_resvd_task_struct(struct task_struct *dst,
 		return false;
 
 	dst->_resvd->task = dst;
+#ifdef CONFIG_XCU_SCHEDULER
+	dst->_resvd->xse_attr = orig->_resvd->xse_attr;
+#endif
 	return true;
 }
 

kernel/xsched/core.c
@@ -20,6 +20,7 @@
 #include <linux/kthread.h>
 #include <linux/slab.h>
 #include <linux/spinlock_types.h>
+#include <linux/syscalls.h>
 #include <linux/types.h>
 #include <linux/xsched.h>
 
@@ -533,3 +534,76 @@ __init int xsched_sched_init(void)
 }
 late_initcall(xsched_sched_init);
 
+static int xsched_setattr(struct task_struct *p, const struct xsched_attr *attr)
+{
+	struct xsched_attr *old_attr = &p->_resvd->xse_attr;
+
+	if (old_attr->xsched_class == attr->xsched_class &&
+		old_attr->xsched_priority == attr->xsched_priority)
+		return 0;
+
+	old_attr->xsched_class = attr->xsched_class;
+	old_attr->xsched_priority = attr->xsched_priority;
+	xsched_rt_prio_set(p->pid, old_attr->xsched_priority);
+
+	return 0;
+}
+
+SYSCALL_DEFINE2(xsched_setattr, pid_t, pid, struct xsched_attr __user *, arg)
+{
+	struct xsched_attr kattr;
+	struct task_struct *p;
+	int rt_prio, retval = 0;
+
+	if (pid < 0 || !arg)
+		return -EINVAL;
+
+	if (copy_from_user(&kattr, arg, sizeof(*arg))) {
+		XSCHED_ERR("Fail to copy_from_user @ %s\n", __func__);
+		return -EFAULT;
+	}
+
+	rt_prio = NR_XSE_PRIO - kattr.xsched_priority;
+	/* Only support RT */
+	if (rt_prio < XSE_PRIO_HIGH || rt_prio > XSE_PRIO_LOW ||
+		kattr.xsched_class != XSCHED_TYPE_RT)
+		return -EINVAL;
+
+	kattr.xsched_priority = rt_prio;
+
+	rcu_read_lock();
+	p = pid ? find_task_by_vpid(pid) : current;
+	if (likely(p))
+		get_task_struct(p);
+	rcu_read_unlock();
+
+	if (likely(p)) {
+		retval = xsched_setattr(p, &kattr);
+		put_task_struct(p);
+	}
+
+	return retval;
+}
+
+SYSCALL_DEFINE2(xsched_getattr, pid_t, pid, struct xsched_attr __user *, arg)
+{
+	struct xsched_attr kattr = { };
+	struct task_struct *p;
+
+	if (pid < 0 || !arg)
+		return -EINVAL;
+
+	rcu_read_lock();
+	p = pid ? find_task_by_vpid(pid) : current;
+	kattr = p->_resvd->xse_attr;
+	rcu_read_unlock();
+
+	kattr.xsched_priority = NR_XSE_PRIO - kattr.xsched_priority;
+
+	if (copy_to_user(arg, &kattr, sizeof(kattr))) {
+		XSCHED_ERR("Fail to copy_to_user @ %s\n", __func__);
+		return -EFAULT;
+	}
+
+	return 0;
+}

kernel/xsched/rt.c
@@ -25,76 +25,6 @@
 
 #define XSCHED_RT_TIMESLICE	(10 * NSEC_PER_MSEC)
 
-#define TGID_HASH_BITS 8
-
-/* Mapping between tgid and context */
-struct tgid_prio {
-	pid_t tgid;
-	int32_t prio;
-	struct hlist_node hnode;
-};
-
-static DEFINE_HASHTABLE(tgid_prio_map, TGID_HASH_BITS);
-static DEFINE_SPINLOCK(tgid_prio_lock);
-
-static int tgid_prio_insert(pid_t tgid, int32_t prio)
-{
-	struct tgid_prio *new_map;
-	unsigned int hash_key;
-
-	if (prio >= NR_XSE_PRIO)
-		return -EINVAL;
-
-	new_map = kzalloc(sizeof(struct tgid_prio), GFP_KERNEL);
-	if (!new_map) {
-		XSCHED_ERR("Fail to alloc mapping (tgid=%d) @ %s\n",
-			tgid, __func__);
-		return -ENOMEM;
-	}
-
-	new_map->tgid = tgid;
-	new_map->prio = prio;
-
-	hash_key = hash_32(tgid, TGID_HASH_BITS);
-
-	spin_lock(&tgid_prio_lock);
-	hash_add_rcu(tgid_prio_map, &new_map->hnode, hash_key);
-	spin_unlock(&tgid_prio_lock);
-
-	return 0;
-}
-
-static struct tgid_prio *tgid_prio_find(pid_t tgid)
-{
-	struct tgid_prio *map = NULL;
-	unsigned int hash_key = hash_32(tgid, TGID_HASH_BITS);
-
-	rcu_read_lock();
-	hash_for_each_possible_rcu(tgid_prio_map, map, hnode, hash_key) {
-		if (map->tgid == tgid)
-			break;
-	}
-	rcu_read_unlock();
-	return map;
-}
-
-static void tgid_prio_delete(pid_t tgid)
-{
-	struct tgid_prio *map;
-	unsigned int hash_key = hash_32(tgid, TGID_HASH_BITS);
-
-	spin_lock(&tgid_prio_lock);
-	hash_for_each_possible(tgid_prio_map, map, hnode, hash_key) {
-		if (map->tgid == tgid) {
-			hash_del_rcu(&map->hnode);
-			spin_unlock(&tgid_prio_lock);
-			kfree(map);
-			return;
-		}
-	}
-	spin_unlock(&tgid_prio_lock);
-}
-
 static inline void
 xse_rt_add(struct xsched_entity *xse, struct xsched_cu *xcu)
 {
@@ -115,7 +45,7 @@ static inline void xse_rt_move_tail(struct xsched_entity *xse)
 
 /* Increase RT runqueue total and per prio nr_running stat. */
 static inline void xrq_inc_nr_running(struct xsched_entity *xse,
-				      struct xsched_cu *xcu)
+					struct xsched_cu *xcu)
 {
 	xcu->xrq.rt.nr_running++;
 }
@@ -143,7 +73,7 @@ static void enqueue_ctx_rt(struct xsched_entity *xse, struct xsched_cu *xcu)
 }
 
 static inline struct xsched_entity *xrq_next_xse(struct xsched_cu *xcu,
-						 int prio)
+						int prio)
 {
 	return list_first_entry(&xcu->xrq.rt.rq[prio], struct xsched_entity,
 				rt.list_node);
@@ -217,23 +147,16 @@ void rq_init_rt(struct xsched_cu *xcu)
 
 void xse_init_rt(struct xsched_entity *xse)
 {
-	struct tgid_prio *map = tgid_prio_find(xse->tgid);
+	struct task_struct *p;
 
-	xse->rt.prio = (map) ? map->prio : XSE_PRIO_DFLT;
+	p = find_task_by_vpid(xse->tgid);
+	xse->rt.prio = p->_resvd->xse_attr.xsched_priority;
 	XSCHED_DEBUG("Xse init: set priority=%d.\n", xse->rt.prio);
 	xse->rt.timeslice = XSCHED_RT_TIMESLICE;
 	INIT_LIST_HEAD(&xse->rt.list_node);
 }
 
-void xse_deinit_rt(struct xsched_entity *xse)
-{
-	struct tgid_prio *map = tgid_prio_find(xse->tgid);
-
-	if (map) {
-		tgid_prio_delete(xse->tgid);
-		XSCHED_DEBUG("Map deleted: tgid=%d\n", xse->tgid);
-	}
-}
+void xse_deinit_rt(struct xsched_entity *xse) { }
 
 struct xsched_class rt_xsched_class = {
 	.class_id = XSCHED_TYPE_RT,
@@ -248,15 +171,12 @@ struct xsched_class rt_xsched_class = {
 	.check_preempt = check_preempt_ctx_rt
 };
 
-int xsched_rt_prio_set(pid_t tgid, unsigned int prio)
+void xsched_rt_prio_set(pid_t tgid, unsigned int prio)
 {
 	unsigned int id;
 	struct xsched_cu *xcu;
 	struct xsched_context *ctx;
 	struct xsched_entity *xse;
-
-	tgid_prio_delete(tgid);
-	tgid_prio_insert(tgid, prio);
 
 	for_each_active_xcu(xcu, id) {
 		mutex_lock(&xcu->ctx_list_lock);
@@ -275,7 +195,5 @@ int xsched_rt_prio_set(pid_t tgid, unsigned int prio)
 		mutex_unlock(&xcu->xcu_lock);
 		mutex_unlock(&xcu->ctx_list_lock);
 	}
-
-	return 0;
 }
 

tools/perf/arch/powerpc/entry/syscalls/syscall.tbl
@@ -553,5 +553,5 @@
 464	common	kabi_reserved464		sys_ni_syscall
 465	common	kabi_reserved465		sys_ni_syscall
 466	common	kabi_reserved466		sys_ni_syscall
-467	common	kabi_reserved467		sys_ni_syscall
-468	common	kabi_reserved468		sys_ni_syscall
+467	common	xsched_setattr		sys_ni_syscall
+468	common	xsched_getattr		sys_ni_syscall

tools/perf/arch/x86/entry/syscalls/syscall_64.tbl
@@ -388,8 +388,8 @@
 464	common	kabi_reserved464	sys_ni_syscall
 465	common	kabi_reserved465	sys_ni_syscall
 466	common	kabi_reserved466	sys_ni_syscall
-467	common	kabi_reserved467	sys_ni_syscall
-468	common	kabi_reserved468	sys_ni_syscall
+467	common	xsched_setattr	sys_ni_syscall
+468	common	xsched_getattr	sys_ni_syscall
 
 #
 # Due to a historical design error, certain syscalls are numbered differently