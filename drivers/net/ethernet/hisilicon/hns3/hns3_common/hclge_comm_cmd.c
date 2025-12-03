OERV-RVCI/rvck-olk

commit bc47b7dc2b0180a0d6e7ad218e6c8f5e4fca07a9

net: hns3: add support for vf multiple tcs
driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID44N0

----------------------------------------------------------------------

Add ieee_setets and ieee_getets supported for vf. Configures and queries
vf multi-TC parameters, including the number of vf TCs, scheduling mode,
and bandwidth weight. The number of TCs, scheduling mode, and bandwidth
weight configured for the vf must be the same as those configured
for the pf. When multiple TCs are configured for a vf, the pf does
not allow the modification of TC parameters until all vfs are configured
to work in single-TC mode.

Signed-off-by: Jian Shen <shenjian15@huawei.com>
Signed-off-by: Hao Lan <lanhao@huawei.com>
Signed-off-by: Peiyang Wang <wangpeiyang1@huawei.com>
Signed-off-by: Donghua Huang <huangdonghua3@h-partners.com>

drivers/net/ethernet/hisilicon/hns3/hns3_common/hclge_comm_cmd.c
@@ -92,6 +92,7 @@ int hclge_comm_firmware_compat_config(struct hnae3_ae_dev *ae_dev,
 		hnae3_set_bit(compat, HCLGE_COMM_MAC_STATS_EXT_EN_B, 1);
 		hnae3_set_bit(compat, HCLGE_COMM_SYNC_RX_RING_HEAD_EN_B, 1);
 		hnae3_set_bit(compat, HCLGE_COMM_LLRS_FEC_EN_B, 1);
+		hnae3_set_bit(compat, HCLGE_COMM_VF_MULTI_TC_EN_B, 1);
 
 		req->compat = cpu_to_le32(compat);
 	}
@@ -160,6 +161,7 @@ static const struct hclge_comm_caps_bit_map hclge_pf_cmd_caps[] = {
 	{HCLGE_COMM_CAP_VF_FAULT_B, HNAE3_DEV_SUPPORT_VF_FAULT_B},
 	{HCLGE_COMM_CAP_NOTIFY_PKT_B, HNAE3_DEV_SUPPORT_NOTIFY_PKT_B},
 	{HCLGE_COMM_CAP_ERR_MOD_GEN_REG_B, HNAE3_DEV_SUPPORT_ERR_MOD_GEN_REG_B},
+	{HCLGE_COMM_CAP_VF_MULTI_TCS, HNAE3_DEV_SUPPORT_VF_MULTI_TCS_B},
 };
 
 static const struct hclge_comm_caps_bit_map hclge_vf_cmd_caps[] = {
@@ -173,6 +175,7 @@ static const struct hclge_comm_caps_bit_map hclge_vf_cmd_caps[] = {
 	{HCLGE_COMM_CAP_RXD_ADV_LAYOUT_B, HNAE3_DEV_SUPPORT_RXD_ADV_LAYOUT_B},
 	{HCLGE_COMM_CAP_CQ_B, HNAE3_DEV_SUPPORT_CQ_B},
 	{HCLGE_COMM_CAP_GRO_B, HNAE3_DEV_SUPPORT_GRO_B},
+	{HCLGE_COMM_CAP_VF_MULTI_TCS, HNAE3_DEV_SUPPORT_VF_MULTI_TCS_B},
 };
 
 static void

drivers/net/ethernet/hisilicon/hns3/hns3_common/hclge_comm_cmd.h
@@ -21,6 +21,7 @@
 #define HCLGE_COMM_MAC_STATS_EXT_EN_B		3
 #define HCLGE_COMM_SYNC_RX_RING_HEAD_EN_B	4
 #define HCLGE_COMM_LLRS_FEC_EN_B		5
+#define HCLGE_COMM_VF_MULTI_TC_EN_B		7
 
 #define hclge_comm_dev_phy_imp_supported(ae_dev) \
 	test_bit(HNAE3_DEV_SUPPORT_PHY_IMP_B, (ae_dev)->caps)
@@ -377,6 +378,7 @@ enum HCLGE_COMM_CAP_BITS {
 	HCLGE_COMM_CAP_NOTIFY_PKT_B = 29,
 	HCLGE_COMM_CAP_TM_FLUSH_B = 31,
 	HCLGE_COMM_CAP_ERR_MOD_GEN_REG_B = 32,
+	HCLGE_COMM_CAP_VF_MULTI_TCS = 34,
 };
 
 enum HCLGE_COMM_API_CAP_BITS {

drivers/net/ethernet/hisilicon/hns3/Makefile
@@ -21,6 +21,8 @@ obj-$(CONFIG_HNS3_HCLGEVF) += hclgevf.o
 hclgevf-objs = hns3vf/hclgevf_main.o hns3vf/hclgevf_mbx.o  hns3vf/hclgevf_devlink.o hns3vf/hclgevf_regs.o \
 		hns3_common/hclge_comm_cmd.o hns3_common/hclge_comm_rss.o hns3_common/hclge_comm_tqp_stats.o
 
+hclgevf-$(CONFIG_HNS3_DCB) += hns3vf/hclgevf_dcb.o
+
 obj-$(CONFIG_HNS3_HCLGE) += hclge.o
 hclge-objs = hns3pf/hclge_main.o hns3pf/hclge_mdio.o hns3pf/hclge_tm.o hns3pf/hclge_regs.o \
 		hns3pf/hclge_mbx.o hns3pf/hclge_err.o  hns3pf/hclge_debugfs.o hns3pf/hclge_ptp.o hns3pf/hclge_devlink.o \

drivers/net/ethernet/hisilicon/hns3/hclge_mbx.h
@@ -6,6 +6,7 @@
 #include <linux/init.h>
 #include <linux/mutex.h>
 #include <linux/types.h>
+#include "hnae3.h"
 
 enum HCLGE_MBX_OPCODE {
 	HCLGE_MBX_RESET = 0x01,		/* (VF -> PF) assert reset */
@@ -49,6 +50,7 @@ enum HCLGE_MBX_OPCODE {
 	HCLGE_MBX_GET_RING_VECTOR_MAP,	/* (VF -> PF) get ring-to-vector map */
 	HCLGE_MBX_SET_QB = 0x28,        /* (VF -> PF) set queue bonding */
 	HCLGE_MBX_PUSH_QB_STATE,        /* (PF -> VF) push qb state */
+	HCLGE_MBX_SET_TC = 0x30,        /* (VF -> PF) set tc */
 
 	HCLGE_MBX_GET_VF_FLR_STATUS = 200, /* (M7 -> PF) get vf flr status */
 	HCLGE_MBX_PUSH_LINK_STATUS,	/* (M7 -> PF) get port link status */
@@ -100,7 +102,7 @@ struct hclge_ring_chain_param {
 
 struct hclge_basic_info {
 	u8 hw_tc_map;
-	u8 rsv;
+	u8 tc_max;
 	__le16 mbx_api_version;
 	__le32 pf_caps;
 };
@@ -226,6 +228,13 @@ struct hclge_mbx_vlan_filter {
 
 struct hclge_mbx_mtu_info {
 	__le32 mtu;
+};
+
+struct hclge_mbx_tc_info {
+	__le32 prio_tc_map;
+	u8 tc_dwrr[HNAE3_MAX_TC];
+	u8 num_tc;
+	u8 tc_sch_mode; /* 0: sp; 1: dwrr */
 };
 
 #pragma pack()

drivers/net/ethernet/hisilicon/hns3/hnae3.h
@@ -119,6 +119,7 @@ enum HNAE3_DEV_CAP_BITS {
 	HNAE3_DEV_SUPPORT_VF_FAULT_B,
 	HNAE3_DEV_SUPPORT_NOTIFY_PKT_B,
 	HNAE3_DEV_SUPPORT_ERR_MOD_GEN_REG_B,
+	HNAE3_DEV_SUPPORT_VF_MULTI_TCS_B,
 };
 
 #define hnae3_ae_dev_fd_supported(ae_dev) \
@@ -202,8 +203,12 @@ enum HNAE3_DEV_CAP_BITS {
 #define hnae3_ae_dev_gen_reg_dfx_supported(hdev) \
 	test_bit(HNAE3_DEV_SUPPORT_ERR_MOD_GEN_REG_B, (hdev)->ae_dev->caps)
 
+#define hnae3_ae_dev_vf_multi_tcs_supported(hdev) \
+	test_bit(HNAE3_DEV_SUPPORT_VF_MULTI_TCS_B, (hdev)->ae_dev->caps)
+
 enum HNAE3_PF_CAP_BITS {
 	HNAE3_PF_SUPPORT_VLAN_FLTR_MDF_B = 0,
+	HNAE3_PF_SUPPORT_VF_MULTI_TCS_B = 1,
 };
 #define ring_ptr_move_fw(ring, p) \
 	((ring)->p = ((ring)->p + 1) % (ring)->desc_num)

drivers/net/ethernet/hisilicon/hns3/hns3_dcbnl.c
@@ -147,7 +147,7 @@ void hns3_dcbnl_setup(struct hnae3_handle *handle)
 {
 	struct net_device *dev = handle->kinfo.netdev;
 
-	if ((!handle->kinfo.dcb_ops) || (handle->flags & HNAE3_SUPPORT_VF))
+	if (!handle->kinfo.dcb_ops)
 		return;
 
 	dev->dcbnl_ops = &hns3_dcbnl_ops;

drivers/net/ethernet/hisilicon/hns3/hns3_debugfs.c
@@ -375,6 +375,9 @@ static struct hns3_dbg_cap_info hns3_dbg_cap[] = {
 	}, {
 		.name = "support vf fault detect",
 		.cap_bit = HNAE3_DEV_SUPPORT_VF_FAULT_B,
+	}, {
+		.name = "support vf multi tcs",
+		.cap_bit = HNAE3_DEV_SUPPORT_VF_MULTI_TCS_B,
 	}
 };
 

drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_dcb.c
@@ -81,6 +81,14 @@ static int hclge_dcb_common_validate(struct hclge_dev *hdev, u8 num_tc,
 {
 	int i;
 
+	/* Based on hardware limitation, VFs share the configuration of PF. */
+	if (hnae3_ae_dev_vf_multi_tcs_supported(hdev) &&
+	    !bitmap_empty(hdev->vf_multi_tcs_en, HCLGE_VPORT_NUM)) {
+		dev_err(&hdev->pdev->dev,
+			"the tc resource is still being used by VF\n");
+		return -EOPNOTSUPP;
+	}
+
 	if (num_tc > hdev->tc_max) {
 		dev_err(&hdev->pdev->dev,
 			"tc num checking failed, %u > tc_max(%u)\n",
@@ -198,6 +206,24 @@ static int hclge_ets_validate(struct hclge_dev *hdev, struct ieee_ets *ets,
 	return 0;
 }
 
+static bool hclge_ets_not_need_config(struct hclge_dev *hdev,
+				      struct ieee_ets *ets)
+{
+	struct ieee_ets current_ets, request_ets;
+
+	/* following parameters are not supported, so ignore to compare */
+	request_ets = *ets;
+	request_ets.cbs = 0;
+	memset(&request_ets.tc_rx_bw, 0, IEEE_8021QAZ_MAX_TCS);
+	memset(&request_ets.tc_reco_bw, 0, IEEE_8021QAZ_MAX_TCS);
+	memset(&request_ets.tc_reco_tsa, 0, IEEE_8021QAZ_MAX_TCS);
+	memset(&request_ets.reco_prio_tc, 0, IEEE_8021QAZ_MAX_TCS);
+
+	hclge_tm_info_to_ieee_ets(hdev, &current_ets);
+
+	return memcmp(&current_ets, &request_ets, sizeof(struct ieee_ets)) == 0;
+}
+
 static int hclge_map_update(struct hclge_dev *hdev)
 {
 	int ret;
@@ -262,6 +288,9 @@ static int hclge_ieee_setets(struct hnae3_handle *h, struct ieee_ets *ets)
 	    h->kinfo.tc_info.mqprio_active)
 		return -EINVAL;
 
+	if (hclge_ets_not_need_config(hdev, ets))
+		return 0;
+
 	ret = hclge_ets_validate(hdev, ets, &num_tc, &map_changed);
 	if (ret)
 		return ret;
@@ -274,6 +303,7 @@ static int hclge_ieee_setets(struct hnae3_handle *h, struct ieee_ets *ets)
 			return ret;
 	}
 
+	mutex_lock(&hdev->vport_lock);
 	hclge_tm_schd_info_update(hdev, num_tc);
 	h->kinfo.tc_info.dcb_ets_active = num_tc > 1;
 
@@ -286,12 +316,15 @@ static int hclge_ieee_setets(struct hnae3_handle *h, struct ieee_ets *ets)
 		if (ret)
 			goto err_out;
 
+		mutex_unlock(&hdev->vport_lock);
 		return hclge_notify_init_up(hdev);
 	}
 
+	mutex_unlock(&hdev->vport_lock);
 	return hclge_tm_dwrr_cfg(hdev);
 
 err_out:
+	mutex_unlock(&hdev->vport_lock);
 	if (!map_changed)
 		return ret;
 

chenglongshe/openEuler

commit d469737146ee726bc9813c3002153f66afb2b228

net: hns3: remove VF multi-TC compat bit
driver inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IDAEHA

----------------------------------------------------------------------

This patch drops the hnae3_set_bit() operation for HCLGE_COMM_VF_MULTI_TC_EN_B
and the corresponding macro definition.

Fixes: bc47b7dc2b01 ("net: hns3: add support for vf multiple tcs")
Signed-off-by: Donghua Huang <huangdonghua3@h-partners.com>

drivers/net/ethernet/hisilicon/hns3/hns3_common/hclge_comm_cmd.c
@@ -92,7 +92,6 @@ int hclge_comm_firmware_compat_config(struct hnae3_ae_dev *ae_dev,
 		hnae3_set_bit(compat, HCLGE_COMM_MAC_STATS_EXT_EN_B, 1);
 		hnae3_set_bit(compat, HCLGE_COMM_SYNC_RX_RING_HEAD_EN_B, 1);
 		hnae3_set_bit(compat, HCLGE_COMM_LLRS_FEC_EN_B, 1);
-		hnae3_set_bit(compat, HCLGE_COMM_VF_MULTI_TC_EN_B, 1);
 
 		req->compat = cpu_to_le32(compat);
 	}

drivers/net/ethernet/hisilicon/hns3/hns3_common/hclge_comm_cmd.h
@@ -21,7 +21,6 @@
 #define HCLGE_COMM_MAC_STATS_EXT_EN_B		3
 #define HCLGE_COMM_SYNC_RX_RING_HEAD_EN_B	4
 #define HCLGE_COMM_LLRS_FEC_EN_B		5
-#define HCLGE_COMM_VF_MULTI_TC_EN_B		7
 
 #define hclge_comm_dev_phy_imp_supported(ae_dev) \
 	test_bit(HNAE3_DEV_SUPPORT_PHY_IMP_B, (ae_dev)->caps)