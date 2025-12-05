OERV-RVCI/rvck-olk

commit 59874d0ded9c139a2f3dad1ad90df3a70f4c09d6

arm64/mpam: Fix MBWU monitor overflow handling
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ID29QH

--------------------------------

Bandwidth counters need to run continuously to correctly reflect the
bandwidth. When reading the previously configured MSMON_CFG_MBWU_CTL,
software must recognize that the MSMON_CFG_x_CTL_OFLOW_STATUS bit may
have been set by hardware because of the counter overflow.

The existing logic incorrectly treats this bit as an indication that the
monitor configuration has been changed and consequently zeros the MBWU
statistics by mistake.

Also fix the handling of overflow amount calculation. There's no need to
subtract mbwu_state->prev_val when calculating overflow_val.

Fixes: 21ac9fc8f275 ("arm_mpam: Track bandwidth counter state for overflow and power management")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_devices.c
@@ -977,6 +977,21 @@ static u64 mpam_csu_hisi_need_halved(struct mpam_msc_ris *ris, u64 now)
 	return now >> 1;
 }
 
+static bool read_msmon_mbwu_is_overflow(struct mpam_msc *msc)
+{
+	u32 ctl;
+	bool overflow;
+
+	ctl = mpam_read_monsel_reg(msc, CFG_MBWU_CTL);
+	overflow = ctl & MSMON_CFG_x_CTL_OFLOW_STATUS ? true : false;
+
+	if (overflow)
+		mpam_write_monsel_reg(msc, CFG_MBWU_CTL, ctl &
+				     ~MSMON_CFG_x_CTL_OFLOW_STATUS);
+
+	return overflow;
+}
+
 static void __ris_msmon_read(void *arg)
 {
 	bool nrdy = false;
@@ -984,6 +999,7 @@ static void __ris_msmon_read(void *arg)
 	bool config_mismatch;
 	struct mon_read *m = arg;
 	u64 now, overflow_val = 0;
+	bool mbwu_overflow = false;
 	struct mon_cfg *ctx = m->ctx;
 	bool reset_on_next_read = false;
 	struct mpam_msc_ris *ris = m->ris;
@@ -1007,6 +1023,8 @@ static void __ris_msmon_read(void *arg)
 			reset_on_next_read = mbwu_state->reset_on_next_read;
 			mbwu_state->reset_on_next_read = false;
 		}
+
+		mbwu_overflow = read_msmon_mbwu_is_overflow(msc);
 	}
 
 	/*
@@ -1074,8 +1092,8 @@ static void __ris_msmon_read(void *arg)
 		}
 
 		/* Add any pre-overflow value to the mbwu_state->val */
-		if (mbwu_state->prev_val > now)
-			overflow_val = mpam_msmon_overflow_val(ris) - mbwu_state->prev_val;
+		if (mbwu_overflow)
+			overflow_val = mpam_msmon_overflow_val(ris);
 
 		mbwu_state->prev_val = now;
 		mbwu_state->correction += overflow_val;

chenglongshe/openEuler

commit b370471e32409bffeb17637e7a71b222bc81a9b3

arm64/mpam: Remove the redundant prev_val member variable
hulk inclusion
category: cleanup
bugzilla: https://gitee.com/openeuler/kernel/issues/ID29QH

--------------------------------

After merging the commit 59874d0ded9c, the prev_val variable is no longer
referenced, so it can be removed.

Fixes: 59874d0ded9c ("arm64/mpam: Fix MBWU monitor overflow handling")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_devices.c
@@ -937,7 +937,6 @@ static void write_msmon_ctl_flt_vals(struct mon_read *m, u32 ctl_val,
 				     u32 flt_val)
 {
 	struct mpam_msc *msc = m->ris->msc;
-	struct msmon_mbwu_state *mbwu_state;
 
 	/*
 	 * Write the ctl_val with the enable bit cleared, reset the counter,
@@ -960,11 +959,6 @@ static void write_msmon_ctl_flt_vals(struct mon_read *m, u32 ctl_val,
 			mpam_write_monsel_reg(msc, MBWU, 0);
 
 		mpam_write_monsel_reg(msc, CFG_MBWU_CTL, ctl_val|MSMON_CFG_x_CTL_EN);
-
-		mbwu_state = &m->ris->mbwu_state[m->ctx->mon];
-		if (mbwu_state)
-			mbwu_state->prev_val = 0;
-
 		break;
 	default:
 		return;
@@ -1070,7 +1064,6 @@ static void __ris_msmon_read(void *arg)
 	    mpam_csu_hisi_need_retrigger(ris, m->err == -EBUSY)) {
 		write_msmon_ctl_flt_vals(m, ctl_val, flt_val);
 		if (mbwu_state) {
-			mbwu_state->prev_val = 0;
 			mbwu_state->correction = 0;
 			mbwu_overflow = false;
 		}
@@ -1132,7 +1125,6 @@ static void __ris_msmon_read(void *arg)
 		if (mbwu_overflow)
 			overflow_val = mpam_msmon_overflow_val(ris);
 
-		mbwu_state->prev_val = now;
 		mbwu_state->correction += overflow_val;
 
 		/* Include bandwidth consumed before the last hardware reset */

drivers/platform/mpam/mpam_internal.h
@@ -224,9 +224,6 @@ struct msmon_mbwu_state {
 	bool		reset_on_next_read;
 	struct mon_cfg	cfg;
 
-	/* The value last read from the hardware. Used to detect overflow. */
-	u64		prev_val;
-
 	/*
 	 * The value to add to the new reading to account for power management,
 	 * and shifts to trigger the overflow interrupt.