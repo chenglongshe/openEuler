chenglongshe/openEuler

commit 34ff52f03e1f5920bc3cec9b5100f512007a0118

EDAC/amd64: Correct the address translation for hygon family 18h model 4h
hygon inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IDBHVM
CVE: N/A

Reference: https://gitee.com/anolis/cloud-kernel/commit/d15e452ea7ce67772d078158c676ddd5b9cd903d

--------------------------------------------------------------------------------

ANBZ: #21447

For Hygon family 18h model 4h processor, the normal address does
not contain sub-channel bit, so it need to reserve the bit field
for sub-channel and fill it in the address translating process.

In the 3 channels interleaving scenario, the calculation of cs id
is different from other scenarios.

Hygon-SIG: commit d15e452ea7ce anolis EDAC/amd64: Correct the address translation for hygon family 18h model 4h
Backport to fix EDAC address translation issue for Hygon processor.

Fixes: 845ff7314833 ("EDAC/amd64: Adjust address translation for Hygon family 18h model 4h")
Signed-off-by: Wenhui Fan <fanwh@hygon.cn>
Reviewed-by: Ruidong Tian <tianruidong@linux.alibaba.com>
Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/5346
[ Aichun Shi: amend commit log ]
Signed-off-by: Aichun Shi <shiaichun@hygon.cn>

drivers/edac/amd64_edac.c
@@ -1127,14 +1127,16 @@ struct addr_ctx {
 	u8 inst_id;
 };
 
-static int hygon_umc_normaddr_to_sysaddr(u64 norm_addr, u16 nid, u8 umc, u64 *sys_addr)
+static int hygon_umc_normaddr_to_sysaddr(u64 norm_addr, u16 nid, u8 umc,
+					 u8 sub_channel, u64 *sys_addr)
 {
 	u64 dram_base_addr, dram_limit_addr, dram_hole_base;
 
-	u16 die_id_mask, socket_id_mask, cs_id = 0;
+	u16 die_id_mask, socket_id_mask, dst_fabric_id, cs_id = 0;
 	u8 die_id_shift, socket_id_shift;
 	u8 intlv_num_dies, intlv_num_chan, intlv_num_sockets;
 	u8 intlv_addr_sel, intlv_addr_bit;
+	u8 chan_addr_sel, chan_hash_enable, ddr5_enable, start_bit;
 	u8 num_intlv_bits, hashed_bit;
 	u8 lgcy_mmio_hole_en, base = 0;
 	u8 cs_mask;
@@ -1194,6 +1196,8 @@ static int hygon_umc_normaddr_to_sysaddr(u64 norm_addr, u16 nid, u8 umc, u64 *sy
 
 	intlv_num_dies	  = (ctx.tmp >> 10) & 0x3;
 	dram_limit_addr	  = ((ctx.tmp & GENMASK_ULL(31, 12)) << 16) | GENMASK_ULL(27, 0);
+	if (boot_cpu_data.x86_model == 0x4)
+		dst_fabric_id = ctx.tmp & GENMASK_ULL(9, 0);
 
 	intlv_addr_bit = intlv_addr_sel + 8;
 
@@ -1201,6 +1205,31 @@ static int hygon_umc_normaddr_to_sysaddr(u64 norm_addr, u16 nid, u8 umc, u64 *sy
 		if (df_indirect_read_instance(nid, 0, 0x60, umc, &ctx.tmp))
 			goto out_err;
 		intlv_num_dies = ctx.tmp & 0x3;
+	}
+
+	if (boot_cpu_data.x86_model == 0x4) {
+		if (df_indirect_read_broadcast(nid, 2, 0x48, &ctx.tmp))
+			goto out_err;
+		chan_addr_sel = (ctx.tmp >> 24) & 0x1;
+		chan_hash_enable = (ctx.tmp >> 23) & 0x1;
+		ddr5_enable = (ctx.tmp >> 19) & 0x1;
+		if (ddr5_enable) {
+			u64 low_addr, high_addr;
+
+			if (chan_addr_sel)
+				start_bit = 8;
+			else
+				start_bit = 7;
+
+			low_addr = ctx.ret_addr & GENMASK_ULL(start_bit - 1, 0);
+			/*
+			 * Reserve the sub-channel bit filed(ret_addr[start_bit]),
+			 * and fill the sub-channel bit in the later channel hashing
+			 * process.
+			 */
+			high_addr = (ctx.ret_addr & GENMASK_ULL(63, start_bit)) << 1;
+			ctx.ret_addr = high_addr | low_addr;
+		}
 	}
 
 	/* Re-use intlv_num_chan by setting it equal to log2(#channels) */
@@ -1244,7 +1273,7 @@ static int hygon_umc_normaddr_to_sysaddr(u64 norm_addr, u16 nid, u8 umc, u64 *sy
 	}
 
 	if (num_intlv_bits > 0) {
-		u64 temp_addr_x, temp_addr_i, temp_addr_y;
+		u64 temp_addr_x, temp_addr_i, temp_addr_y, addr_mul3;
 		u8 die_id_bit, sock_id_bit;
 		u16 cs_fabric_id;
 
@@ -1263,8 +1292,30 @@ static int hygon_umc_normaddr_to_sysaddr(u64 norm_addr, u16 nid, u8 umc, u64 *sy
 		/* If interleaved over more than 1 channel: */
 		if (intlv_num_chan) {
 			die_id_bit = intlv_num_chan;
-			cs_mask	   = (1 << die_id_bit) - 1;
-			cs_id	   = cs_fabric_id & cs_mask;
+
+			/*
+			 * In the 3 channels interleaving scenario, the calculation
+			 * of cs id is different from other scenarios.
+			 */
+			if (boot_cpu_data.x86_model == 0x4 && intlv_num_chan == 2) {
+				u8 cs_offset;
+
+				cs_offset = (cs_fabric_id & 0x3) - (dst_fabric_id & 0x3);
+				if (cs_offset > 3) {
+					pr_err("%s: Invalid cs offset: 0x%x cs_fabric_id: 0x%x dst_fabric_id: 0x%x.\n",
+						__func__, cs_offset, cs_fabric_id, dst_fabric_id);
+					goto out_err;
+				}
+				temp_addr_x = (ctx.ret_addr & GENMASK_ULL(63, intlv_addr_bit)) >>
+						intlv_addr_bit;
+				temp_addr_y = ctx.ret_addr & GENMASK_ULL(intlv_addr_bit - 1, 0);
+
+				addr_mul3 = temp_addr_x * 3 + cs_offset;
+				cs_id = addr_mul3 & GENMASK_ULL(intlv_num_chan - 1, 0);
+			} else {
+				cs_mask	   = (1 << die_id_bit) - 1;
+				cs_id	   = cs_fabric_id & cs_mask;
+			}
 		}
 
 		sock_id_bit = die_id_bit;
@@ -1299,10 +1350,16 @@ static int hygon_umc_normaddr_to_sysaddr(u64 norm_addr, u16 nid, u8 umc, u64 *sy
 		 * bits there are. "intlv_addr_bit" tells us how many "Y" bits
 		 * there are (where "I" starts).
 		 */
-		temp_addr_y = ctx.ret_addr & GENMASK_ULL(intlv_addr_bit - 1, 0);
-		temp_addr_i = (cs_id << intlv_addr_bit);
-		temp_addr_x = (ctx.ret_addr & GENMASK_ULL(63, intlv_addr_bit)) << num_intlv_bits;
-		ctx.ret_addr    = temp_addr_x | temp_addr_i | temp_addr_y;
+		if (boot_cpu_data.x86_model == 0x4 && intlv_num_chan == 2) {
+			temp_addr_i = ((addr_mul3 >> intlv_num_chan) << intlv_num_chan) | cs_id;
+			ctx.ret_addr = temp_addr_y | (temp_addr_i << intlv_addr_bit);
+		} else {
+			temp_addr_y = ctx.ret_addr & GENMASK_ULL(intlv_addr_bit - 1, 0);
+			temp_addr_i = (cs_id << intlv_addr_bit);
+			temp_addr_x = (ctx.ret_addr & GENMASK_ULL(63, intlv_addr_bit)) <<
+					num_intlv_bits;
+			ctx.ret_addr    = temp_addr_x | temp_addr_i | temp_addr_y;
+		}
 	}
 
 	/* Add dram base address */
@@ -1330,6 +1387,21 @@ static int hygon_umc_normaddr_to_sysaddr(u64 norm_addr, u16 nid, u8 umc, u64 *sy
 
 		if (hashed_bit != ((ctx.ret_addr >> intlv_addr_bit) & BIT(0)))
 			ctx.ret_addr ^= BIT(intlv_addr_bit);
+	}
+
+	/* The channel hashing process. */
+	if (boot_cpu_data.x86_model == 0x4 && ddr5_enable) {
+		if (chan_hash_enable) {
+			hashed_bit =	(ctx.ret_addr >> 12) ^
+					(ctx.ret_addr >> 21) ^
+					(ctx.ret_addr >> 30) ^
+					sub_channel;
+			hashed_bit &= BIT(0);
+			ctx.ret_addr |= hashed_bit << start_bit;
+
+		} else {
+			ctx.ret_addr |= sub_channel << start_bit;
+		}
 	}
 
 	/* Is calculated system address is above DRAM limit address? */
@@ -3130,7 +3202,7 @@ static void decode_umc_error(int node_id, struct mce *m)
 	struct atl_err a_err;
 	struct err_info err;
 	u64 sys_addr;
-	u8 umc;
+	u8 umc, sub_channel = 0;
 
 	node_id = fixup_node_id(node_id, m);
 
@@ -3162,12 +3234,14 @@ static void decode_umc_error(int node_id, struct mce *m)
 	pvt->ops->get_err_info(m, &err);
 
 	if (hygon_f18h_m4h() || hygon_f18h_m10h()) {
+		sub_channel = (m->ipid & BIT(13)) >> 13;
 		if (boot_cpu_data.x86_model >= 0x6)
-			umc = (err.channel << 1) + ((m->ipid & BIT(13)) >> 13);
+			umc = (err.channel << 1) + sub_channel;
 		else
 			umc = err.channel;
 
-		if (hygon_umc_normaddr_to_sysaddr(m->addr, pvt->mc_node_id, umc, &sys_addr)) {
+		if (hygon_umc_normaddr_to_sysaddr(m->addr, pvt->mc_node_id, umc,
+						  sub_channel, &sys_addr)) {
 			err.err_code = ERR_NORM_ADDR;
 			goto log_error;
 		}

commit afa054cd52c9434b902d54466ce89d5c9b6466c4

EDAC/amd64: Use unicast instead of broadcast to access the cs_config register
hygon inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IDBHVM
CVE: N/A

Reference: https://gitee.com/anolis/cloud-kernel/commit/18341c6c5a0f59b0b6173b07bd070a1fece13cad

--------------------------------------------------------------------------------

ANBZ: #21447

In the scenario that DDR memory numbers are different in per node,
broadcast access can not get the correct value of the cs_config,
so use unicast access instead.

Fixes: 34ff52f03e1f ("EDAC/amd64: Correct the address translation for hygon family 18h model 4h")

Hygon-SIG: commit 18341c6c5a0f anolis EDAC/amd64: Use unicast instead of broadcast to access the cs_config register
Backport to fix EDAC address translation issue for Hygon processor.

Signed-off-by: Wenhui Fan <fanwh@hygon.cn>
Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/5439
[ Aichun Shi: amend commit log ]
Signed-off-by: Aichun Shi <shiaichun@hygon.cn>

drivers/edac/amd64_edac.c
@@ -1208,7 +1208,7 @@ static int hygon_umc_normaddr_to_sysaddr(u64 norm_addr, u16 nid, u8 umc,
 	}
 
 	if (boot_cpu_data.x86_model == 0x4) {
-		if (df_indirect_read_broadcast(nid, 2, 0x48, &ctx.tmp))
+		if (df_indirect_read_instance(nid, 2, 0x48, umc, &ctx.tmp))
 			goto out_err;
 		chan_addr_sel = (ctx.tmp >> 24) & 0x1;
 		chan_hash_enable = (ctx.tmp >> 23) & 0x1;