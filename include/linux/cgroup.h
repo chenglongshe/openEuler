OERV-RVCI/rvck-olk

commit 9c735dcd118dfb5e867c56d125f5ff1ea249f0e8

interference: Use hardware timer counter
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ICAOAT

--------------------------------

Use hardware timer counter

Signed-off-by: Pu Lehui <pulehui@huawei.com>

include/linux/cgroup.h
@@ -932,7 +932,19 @@ static inline void cgroup_ifs_account_delta(struct cgroup_ifs_cpu *ifsc,
 
 static inline u64 cgroup_ifs_time_counter(void)
 {
+#if defined(__aarch64__)
+	u64 counter;
+
+	asm volatile("mrs %0, cntvct_el0" : "=r" (counter) :: "memory");
+	return counter;
+#elif defined(__x86_64__)
+	unsigned int lo, hi;
+
+	asm volatile("rdtsc" : "=a"(lo), "=d"(hi) :: "memory");
+	return ((u64)hi << 32) | lo;
+#else
 	return sched_clock();
+#endif
 }
 
 static inline void cgroup_ifs_enter_lock(u64 *clock)

kernel/cgroup/ifs.c
@@ -21,6 +21,7 @@ struct smt_info {
 	bool is_noidle;
 };
 
+static DEFINE_PER_CPU(u64, ifs_tsc_freq);
 static DEFINE_PER_CPU(struct smt_itf, smt_itf);
 static DEFINE_PER_CPU(struct smt_info, smt_info);
 static DEFINE_PER_CPU_READ_MOSTLY(int, smt_sibling) = -1;
@@ -236,6 +237,37 @@ static const char *ifs_type_name(int type)
 	return name;
 }
 
+static u64 tsc_cycles_to_nsec(u64 tsc_cycles)
+{
+#if defined(__aarch64__) || defined(__x86_64__)
+	return (((u64)NSEC_PER_SEC << 5) / this_cpu_read(ifs_tsc_freq) * tsc_cycles) >> 5;
+#else
+	return tsc_cycles;
+#endif
+}
+
+static int cgroup_ifs_tsc_init(void)
+{
+	u64 freq = 0;
+	int cpu;
+
+#if defined(__aarch64__)
+	asm volatile ("MRS %0, CNTFRQ_EL0" : "=r" (freq));
+#elif defined(__x86_64__)
+	if (likely(boot_cpu_has(X86_FEATURE_CONSTANT_TSC)))
+		freq = tsc_khz * 1000;
+#endif
+	if (!freq) {
+		pr_warn("Disable CGROUP IFS: no constant tsc\n");
+		return -1;
+	}
+
+	for_each_possible_cpu(cpu)
+		per_cpu(ifs_tsc_freq, cpu) = freq;
+
+	return 0;
+}
+
 static bool should_print(int type)
 {
 #ifdef CONFIG_SCHEDSTATS
@@ -268,6 +300,8 @@ static int print_sum_time(struct cgroup_ifs *ifs, struct seq_file *seq)
 	for (i = 0; i < NR_IFS_TYPES; i++) {
 		if (!should_print(i))
 			continue;
+		if (i == IFS_SPINLOCK || i == IFS_MUTEX)
+			time[i] = tsc_cycles_to_nsec(time[i]);
 		seq_printf(seq, "%-18s%llu\n", ifs_type_name(i), time[i]);
 	}
 
@@ -339,6 +373,9 @@ struct cftype cgroup_v1_ifs_files[] = {
 void cgroup_ifs_init(void)
 {
 	if (!ifs_enable)
+		return;
+
+	if (cgroup_ifs_tsc_init() < 0)
 		return;
 
 	BUG_ON(cgroup_init_cftypes(NULL, cgroup_ifs_files));

chenglongshe/openEuler

commit 4db8d24081c16913487ad5982e8a414a13d5ed42

interference: Add fallback to sched_clock when TSC is unavailable
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICAOAT

-----------------------------------------

Currently, if TSC is not available, IFS will be directly disabled,
which prevents enabling IFS even in environments like qemu where
higher overhead is tolerable.

To address this, add a fallback logic to cgroup_ifs_time_counter.
Even if TSC is unavailable on x86/aarch64, it can now fall back to
sched_clock.

Note: This change also incidentally fixes a null pointer issue in
cgroup_addrm_files when TSC is unavailable.

Fixes: 9c735dcd118d ("interference: Use hardware timer counter")
Signed-off-by: Tengda Wu <wutengda2@huawei.com>

include/linux/cgroup.h
@@ -920,6 +920,12 @@ static inline bool cgroup_ifs_enabled(void)
 	return static_branch_unlikely(&cgrp_ifs_enabled);
 }
 
+DECLARE_STATIC_KEY_TRUE(cgrp_ifs_tsc_available);
+static inline bool cgroup_ifs_tsc_available(void)
+{
+	return static_branch_likely(&cgrp_ifs_tsc_available);
+}
+
 static inline struct cgroup_ifs *cgroup_ifs(struct cgroup *cgrp)
 {
 	return cgroup_ino(cgrp) == 1 ? &cgroup_root_ifs : cgrp->ifs;
@@ -955,19 +961,22 @@ static inline void cgroup_ifs_account_delta(struct cgroup_ifs_cpu *ifsc,
 
 static inline u64 cgroup_ifs_time_counter(void)
 {
+	if (cgroup_ifs_tsc_available()) {
 #if defined(__aarch64__)
-	u64 counter;
+		u64 counter;
 
-	asm volatile("mrs %0, cntvct_el0" : "=r" (counter) :: "memory");
-	return counter;
+		asm volatile("mrs %0, cntvct_el0" : "=r" (counter) :: "memory");
+		return counter;
 #elif defined(__x86_64__)
-	unsigned int lo, hi;
+		unsigned int lo, hi;
 
-	asm volatile("rdtsc" : "=a"(lo), "=d"(hi) :: "memory");
-	return ((u64)hi << 32) | lo;
-#else
-	return sched_clock();
+		asm volatile("rdtsc" : "=a"(lo), "=d"(hi) :: "memory");
+		return ((u64)hi << 32) | lo;
 #endif
+	}
+
+	/* fallback */
+	return sched_clock();
 }
 
 static inline void cgroup_ifs_enter_lock(u64 *clock)

kernel/cgroup/ifs.c
@@ -49,6 +49,7 @@ struct cgroup_ifs cgroup_root_ifs = {
 };
 
 DEFINE_STATIC_KEY_FALSE(cgrp_ifs_enabled);
+DEFINE_STATIC_KEY_TRUE(cgrp_ifs_tsc_available);
 
 #ifdef CONFIG_CGROUP_IFS_DEFAULT_ENABLED
 static bool ifs_enable = true;
@@ -315,7 +316,8 @@ static void tdesc_init(struct ifs_tdesc *desc, u64 freq)
 static void cgroup_ifs_tdesc_init(void)
 {
 	tdesc_init(&ifs_tdesc[IFS_TIMER_CLK], NSEC_PER_SEC);
-	tdesc_init(&ifs_tdesc[IFS_TIMER_TSC], this_cpu_read(ifs_tsc_freq));
+	if (cgroup_ifs_tsc_available())
+		tdesc_init(&ifs_tdesc[IFS_TIMER_TSC], this_cpu_read(ifs_tsc_freq));
 }
 
 static u64 tsc_cycles_to_nsec(u64 tsc_cycles)
@@ -327,7 +329,7 @@ static u64 tsc_cycles_to_nsec(u64 tsc_cycles)
 #endif
 }
 
-static int cgroup_ifs_tsc_init(void)
+static void cgroup_ifs_tsc_init(void)
 {
 	u64 freq = 0;
 	int cpu;
@@ -339,14 +341,13 @@ static int cgroup_ifs_tsc_init(void)
 		freq = tsc_khz * 1000;
 #endif
 	if (!freq) {
-		pr_warn("Disable CGROUP IFS: no constant tsc\n");
-		return -1;
+		pr_warn("IFS: no constant tsc, use default clocksource as time source\n");
+		static_branch_disable(&cgrp_ifs_tsc_available);
+		return;
 	}
 
 	for_each_possible_cpu(cpu)
 		per_cpu(ifs_tsc_freq, cpu) = freq;
-
-	return 0;
 }
 
 static bool should_print(int type)
@@ -360,6 +361,11 @@ static bool should_print(int type)
 		return ifs_irq_enable;
 #endif
 	return true;
+}
+
+static bool use_tsc(enum ifs_types t)
+{
+	return cgroup_ifs_tsc_available() && (t == IFS_SPINLOCK || t == IFS_MUTEX);
 }
 
 static int print_sum_time(struct cgroup_ifs *ifs, struct seq_file *seq)
@@ -381,7 +387,7 @@ static int print_sum_time(struct cgroup_ifs *ifs, struct seq_file *seq)
 	for (i = 0; i < NR_IFS_TYPES; i++) {
 		if (!should_print(i))
 			continue;
-		if (i == IFS_SPINLOCK || i == IFS_MUTEX)
+		if (use_tsc(i))
 			time[i] = tsc_cycles_to_nsec(time[i]);
 		seq_printf(seq, "%-18s%llu\n", ifs_type_name(i), time[i]);
 	}
@@ -425,7 +431,7 @@ static int print_hist_count(struct cgroup_ifs *ifs, struct seq_file *seq)
 		if (!should_print(i))
 			continue;
 
-		if (i == IFS_SPINLOCK || i == IFS_MUTEX)
+		if (use_tsc(i))
 			desc = &ifs_tdesc[IFS_TIMER_TSC];
 		else
 			desc = &ifs_tdesc[IFS_TIMER_CLK];
@@ -532,9 +538,7 @@ void cgroup_ifs_init(void)
 	if (!ifs_enable)
 		return;
 
-	if (cgroup_ifs_tsc_init() < 0)
-		return;
-
+	cgroup_ifs_tsc_init();
 	BUG_ON(cgroup_init_cftypes(NULL, cgroup_ifs_files));
 	cgroup_ifs_tdesc_init();
 }
@@ -578,9 +582,6 @@ void cgroup_ifs_enable_irq_account(bool enable)
 static __init int cgroup_ifs_enable(void)
 {
 	if (!ifs_enable)
-		return 0;
-
-	if (!this_cpu_read(ifs_tsc_freq))
 		return 0;
 
 	static_branch_enable(&cgrp_ifs_enabled);