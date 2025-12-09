OERV-RVCI/rvck-olk

commit 28d4e4995d76ad77d894bed25f17f352b3676f73

interference: Add cgroup v1 support for CGROUP_IFS
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ICAOAT

--------------------------------

Add cgroup v1 support for CGROUP_IFS.

Signed-off-by: Pu Lehui <pulehui@huawei.com>

kernel/cgroup/ifs.c
@@ -282,6 +282,16 @@ static struct cftype cgroup_ifs_files[] = {
 	{ }     /* terminate */
 };
 
+struct cftype cgroup_v1_ifs_files[] = {
+	{
+		.name = "interference.stat",
+		.flags = CFTYPE_NO_PREFIX,
+		.seq_show = cgroup_ifs_show,
+		.write = cgroup_ifs_write,
+	},
+	{ }     /* terminate */
+};
+
 void cgroup_ifs_init(void)
 {
 	if (!ifs_enable)

kernel/sched/cpuacct.c
@@ -386,3 +386,18 @@ static int __init cgroup_v1_psi_init(void)
 
 late_initcall_sync(cgroup_v1_psi_init);
 #endif
+
+#ifdef CONFIG_CGROUP_IFS
+extern struct cftype cgroup_v1_ifs_files[];
+
+static int __init cgroup_v1_ifs_init(void)
+{
+	if (!cgroup_ifs_enabled())
+		return 0;
+
+	cgroup_add_legacy_cftypes(&cpuacct_cgrp_subsys, cgroup_v1_ifs_files);
+
+	return 0;
+}
+late_initcall_sync(cgroup_v1_ifs_init);
+#endif

chenglongshe/openEuler

commit 21959a17278b3aff07ed26f111dd90af3dfa15bb

interference: add version compatibility check between subsystem and cgroup
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICAOAT

-----------------------------------------

Add a sanity check to ensure the cgroup subsystem and the target cgroup
are using the same cgroup version (either both v1 or both v2). This
prevents operations that mix incompatible cgroup hierarchies, which
could lead to incorrect accounting for interference.

Fixes: 28d4e4995d76 ("interference: Add cgroup v1 support for CGROUP_IFS")
Signed-off-by: Tengda Wu <wutengda2@huawei.com>

kernel/cgroup/ifs.c
@@ -477,6 +477,16 @@ static int cgroup_ifs_show(struct seq_file *seq, void *v)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_CGROUP_CPUACCT
+	if ((!cgroup_subsys_on_dfl(cpuacct_cgrp_subsys) && cgroup_on_dfl(cgrp)) ||
+	    (cgroup_subsys_on_dfl(cpuacct_cgrp_subsys) && !cgroup_on_dfl(cgrp))) {
+		pr_info("cgroup version mismatch: subsystem %s, cgroup %s\n",
+			cgroup_subsys_on_dfl(cpuacct_cgrp_subsys) ? "v2" : "v1",
+			cgroup_on_dfl(cgrp) ? "v2" : "v1");
+		return -EOPNOTSUPP;
+	}
+#endif
+
 	ret = print_sum_time(ifs, seq);
 	if (ret)
 		return ret;